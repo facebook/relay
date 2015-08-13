be2a466e09f920516dd583396c816d8e896e3ecb
transedward <transedward@gmail.com>
2015-08-13 16:26:16 +0800

    change to Bill to me for consistency with todo and and markMessageAsRead

--- examples/chat/data/database.js
+++ examples/chat/data/database.js
@@ -2,7 +2,7 @@ export class Message extends Object {}
 export class User extends Object {}
 
 // Mock authenticated ID
-const VIEWER_ID = 'Bill';
+const VIEWER_ID = 'me';
 
 // Mock user data
 var viewer = new User();
@@ -12,35 +12,27 @@ var usersById = {
 };
 
 // Mock message data
-var messagesById = messages.reduce((preMessages, message) => {
-  preMessages[message.id] = message;
-  return preMessages;
-}, {});
-var messageIdsByUser = {
-  [VIEWER_ID]: []
-};
-
 var messages = [
   {
     id: 'm_1',
     threadID: 't_1',
-    threadName: 'Jing and Bill',
-    authorName: 'Bill',
+    threadName: 'Jing and Me',
+    authorName: 'me',
     text: 'Hey Jing, want to give a Flux talk at ForwardJS?',
     timestamp: Date.now() - 99999
   },
   {
     id: 'm_2',
     threadID: 't_1',
-    threadName: 'Jing and Bill',
-    authorName: 'Bill',
+    threadName: 'Jing and me',
+    authorName: 'me',
     text: 'Seems like a pretty cool conference.',
     timestamp: Date.now() - 89999
   },
   {
     id: 'm_3',
     threadID: 't_1',
-    threadName: 'Jing and Bill',
+    threadName: 'Jing and me',
     authorName: 'Jing',
     text: 'Sounds good.  Will they be serving dessert?',
     timestamp: Date.now() - 79999
@@ -48,15 +40,15 @@ var messages = [
   {
     id: 'm_4',
     threadID: 't_2',
-    threadName: 'Dave and Bill',
-    authorName: 'Bill',
+    threadName: 'Dave and me',
+    authorName: 'me',
     text: 'Hey Dave, want to get a beer after the conference?',
     timestamp: Date.now() - 69999
   },
   {
     id: 'm_5',
     threadID: 't_2',
-    threadName: 'Dave and Bill',
+    threadName: 'Dave and me',
     authorName: 'Dave',
     text: 'Totally!  Meet you at the hotel bar.',
     timestamp: Date.now() - 59999
@@ -65,28 +57,34 @@ var messages = [
     id: 'm_6',
     threadID: 't_3',
     threadName: 'Functional Heads',
-    authorName: 'Bill',
+    authorName: 'me',
     text: 'Hey Brian, are you going to be talking about functional stuff?',
     timestamp: Date.now() - 49999
   },
   {
     id: 'm_7',
     threadID: 't_3',
-    threadName: 'Bill and Brian',
+    threadName: 'me and Brian',
     authorName: 'Brian',
     text: 'At ForwardJS?  Yeah, of course.  See you there!',
     timestamp: Date.now() - 39999
   }
 ];
 
-
+var messagesById = messages.reduce((preMessages, message) => {
+  preMessages[message.id] = message;
+  return preMessages;
+}, {});
+var messageIdsByUser = {
+  [VIEWER_ID]: []
+};
 
 export function addMessage(text, currentThreadID) {
   var timestamp = Date.now();
   var message = new Message();
   message.id = 'm_' + timestamp;
   message.threadID = currentThreadID || 't_' + timestamp;
-  message.authorName = 'Bill'; // hard coded for the exampl
+  message.authorName = 'me'; // hard coded for the exampl
   message.text = text;
   message.timestamp = timestamp;
   message.isRead = true;
@@ -102,6 +100,11 @@ export function getMessage(id) {
 
 export function getMessages() {
   return messageIdsByUser[VIEWER_ID].map((id) => getMessage[id]);
+}
+
+export function markMessageAsRead(id, isRead) {
+  var message = getMessage(id);
+  message.isRead = isRead;
 }
 
 export function getUser(id) {

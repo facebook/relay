50d01637d5c94fc8fdf24b001f69f14ea5f71f6f
Albert Still <albertjamesstill@gmail.com>
2015-08-24 17:49:58 +0100

    Add type system shorthand printing to the examples
    See https://github.com/relayjs/relay-starter-kit/pull/10

--- examples/relay-treasurehunt/scripts/updateSchema.js
+++ examples/relay-treasurehunt/scripts/updateSchema.js
@@ -4,12 +4,16 @@ import fs from 'fs';
 import path from 'path';
 import { Schema } from '../data/schema';
 import { graphql }  from 'graphql';
-import { introspectionQuery } from 'graphql/utilities';
+import { introspectionQuery, printSchema } from 'graphql/utilities';
 
+// Save JSON of full schema introspection for Babel Relay Plugin to use
 async () => {
   var result = await (graphql(Schema, introspectionQuery));
   if (result.errors) {
-    console.error('ERROR: ', JSON.stringify(result.errors, null, 2));
+    console.error(
+      'ERROR introspecting schema: ',
+      JSON.stringify(result.errors, null, 2)
+    );
   } else {
     fs.writeFileSync(
       path.join(__dirname, '../data/schema.json'),
@@ -17,3 +21,9 @@ async () => {
     );
   }
 }();
+
+// Save user readable type system shorthand of schema
+fs.writeFileSync(
+  path.join(__dirname, '../data/schema.graphql'),
+  printSchema(Schema)
+);

12a70c96d3bdff3dd4253790cd547aa0e9de1d27
dmitri <dmitri@vistarmedia.com>
2015-08-31 22:02:40 -0400

    Fix HomeRoute queries for all examples

--- examples/relay-treasurehunt/js/routes/AppHomeRoute.js
+++ examples/relay-treasurehunt/js/routes/AppHomeRoute.js
@@ -1,6 +1,12 @@
 export default class extends Relay.Route {
   static queries = {
-    game: () => Relay.QL`query { game }`,
+    game: Component => Relay.QL`
+      query {
+        game {
+          ${Component.getFragment('game')},
+        }
+      }
+    `,
   };
   static routeName = 'AppHomeRoute';
 }
--- examples/star-wars/js/routes/StarWarsAppHomeRoute.js
+++ examples/star-wars/js/routes/StarWarsAppHomeRoute.js
@@ -1,6 +1,12 @@
 export default class extends Relay.Route {
   static queries = {
-    factions: () => Relay.QL`query { factions(names: $factionNames) }`,
+    factions: Component => Relay.QL`
+      query {
+        factions(names: $factionNames) {
+          ${Component.getFragment('factions')},
+        }
+      }
+    `,
   };
   static routeName = 'StarWarsAppHomeRoute';
 }

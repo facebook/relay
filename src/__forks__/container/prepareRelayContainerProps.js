c2b354527346acd49756fe3159bf5858644c10f3
Greg Hurrell <glh@fb.com>
2015-09-04 15:39:58 -0700

    Fix use of `requireActual` outside of test suite
    This is a Jest-provided extension, so not suitable for use outside the
    test suite.
    
    Test plan: Boot up Relay Starter Kit. See it load, and no errors about
    inexistent `requireActual`.

--- src/__forks__/container/prepareRelayContainerProps.js
+++ src/__forks__/container/prepareRelayContainerProps.js
@@ -11,4 +11,4 @@
 
 'use strict';
 
-module.exports = require.requireActual('prepareRelayOSSContainerProps');
+module.exports = require('prepareRelayOSSContainerProps');
--- src/__forks__/interface/RelayConnectionInterface.js
+++ src/__forks__/interface/RelayConnectionInterface.js
@@ -11,4 +11,4 @@
 
 'use strict';
 
-module.exports = require.requireActual('RelayOSSConnectionInterface');
+module.exports = require('RelayOSSConnectionInterface');

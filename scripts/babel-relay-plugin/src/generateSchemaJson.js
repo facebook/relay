d44029c4ee8ece40893fca38fb5fbeeda8ec8255
vedmalex <vedmalex@gmail.com>
2015-09-10 12:06:08 +0300

    fix: use GraphQL parser
    it didn't work because of Expand GraphQL language https://github.com/graphql/graphql-js/commit/73b4d4d301a232ebdd790bb3cf1042e3a67a8fca

--- scripts/babel-relay-plugin/src/generateSchemaJson.js
+++ scripts/babel-relay-plugin/src/generateSchemaJson.js
@@ -14,7 +14,7 @@
 
 var fs = require('fs');
 var path = require('path');
-var schema = require('graphql/language/schema');
+var parser = require('graphql/language').parser;
 var utilities = require('graphql/utilities');
 var graphql = require('graphql');
 
@@ -23,7 +23,7 @@ try {
   var outFile = path.resolve(__dirname, '__tests__', 'testschema.rfc.json');
 
   var body = fs.readFileSync(inFile, 'utf8');
-  var ast = schema.parseSchemaIntoAST(body);
+  var ast = parser.parseSchemaIntoAST(body);
   var astSchema = utilities.buildASTSchema(ast, 'Root', 'Mutation');
   graphql.graphql(astSchema, utilities.introspectionQuery).then(
     function(result) {

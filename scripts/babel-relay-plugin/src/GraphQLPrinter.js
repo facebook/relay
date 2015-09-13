0673c032fa3dc6b240a20c6e83b97cb670e3133e
Tim Yung <yungsters@fb.com>
2015-09-13 13:23:38 -0700

    Relay: Fix Copyright Headers for `babel-relay-plugin`
    Summary: Fixing this across the whole plugin directory so that our internal linter doesn't complain every time we touch one of these files. Also, we're not using jslint anymore.
    
    Reviewed By: @​#relay, @josephsavona
    
    Differential Revision: D2438119

--- scripts/babel-relay-plugin/src/GraphQLPrinter.js
+++ scripts/babel-relay-plugin/src/GraphQLPrinter.js
@@ -1,5 +1,13 @@
-/*jslint node:true*/
-"use strict";
+/**
+ * Copyright 2013-2015, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+'use strict';
 
 var kinds = require('graphql/language/kinds');
 var printer = require('graphql/language/printer');

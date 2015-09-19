943bb7aa20dd25643e88d8f34868e2537204f022
Steven Luscher <steveluscher@fb.com>
2015-09-18 19:12:20 -0700

    Open the Relay Playground to the public
    Summary: Wrote a new ‘Hello World’ app and schema to use as the default code in the Relay Playground, and added a link to the playground in the nav bar.
    
    Complete text of the Hello World app: https://github.com/facebook/relay/blob/a7fd50678f06314f1c3eb351792cc8f7fb5c4724/website-prototyping-tools/HelloApp.js
    Complete text of the Hello World schema: https://github.com/facebook/relay/blob/a7fd50678f06314f1c3eb351792cc8f7fb5c4724/website-prototyping-tools/HelloSchema.js
    Closes https://github.com/facebook/relay/pull/343
    
    Reviewed By: @yungsters
    
    Differential Revision: D2457909

--- website-prototyping-tools/HelloSchema.js
+++ website-prototyping-tools/HelloSchema.js
@@ -8,6 +8,13 @@ var GREETINGS = {
   hello: 'Hello world',
 };
 
+/**
+ * Objects.
+ * Build up a portrait of your data universe
+ * using the object type. Here, we define a
+ * type of object that has a 'hello' field
+ * that is of the string type.
+ */
 var GreetingsType = new GraphQLObjectType({
   name: 'Greetings',
   fields: () => ({
@@ -15,12 +22,26 @@ var GreetingsType = new GraphQLObjectType({
   }),
 });
 
+/**
+ * The schema.
+ * Here we export a schema that offers one root
+ * field named 'greetings', and a method to
+ * resolve its data.
+ *
+ * To learn more about writing GraphQL schemas for Relay, visit:
+ *   https://github.com/graphql/graphql-relay-js
+ */
 export default new GraphQLSchema({
   query: new GraphQLObjectType({
     name: 'Query',
     fields: () => ({
       greetings: {
         type: GreetingsType,
+        // Here we define a resolver that returns
+        // the data defined above. Were this schema
+        // executing on the server side, you could
+        // write a resolve method that fetches
+        // live data from a database.
         resolve: () => GREETINGS,
       },
     }),

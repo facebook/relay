41c45a96fd09123378bcecb431eb4aa6cdb5e1e9
Greg Hurrell <glh@fb.com>
2015-09-29 15:48:09 -0700

    Harmonize Star Wars example with relay-starter-kit
    This and the other examples are intended to be minimal demos over a
    minimal substrate of infrastructure. "relay-starter-kit" is basically
    the template for that substrate.
    
    In order to make it easier to bring across changes from the starter kit
    to here, this commit makes the example app more like the starter kit, so
    that if we want to update `server.js` in the future, for example, we can
    just copy it over directly with no modifications.
    
    Changes:
    
    - Made schema filenames generic (`starWarsSchema` -> `schema` and
      `starWarsDatabase` -> `database`).
    - Got rid of eslint, which the starter kit does not use and isn't really
      needed in such a tiny example app.
    
    Test plan: `npm run update-schema`, `npm run start`, and hit site with
    web browser.

--- examples/star-wars/server.js
+++ examples/star-wars/server.js
@@ -12,14 +12,14 @@ import graphQLHTTP from 'express-graphql';
 import path from 'path';
 import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
-import {StarWarsSchema} from './data/starWarsSchema';
+import {schema} from './data/schema';
 
 const APP_PORT = 3000;
 const GRAPHQL_PORT = 8080;
 
 // Expose a GraphQL endpoint
 var graphQLServer = express();
-graphQLServer.use('/', graphQLHTTP({schema: StarWarsSchema, pretty: true}));
+graphQLServer.use('/', graphQLHTTP({schema, pretty: true}));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
 ));
@@ -35,15 +35,8 @@ var compiler = webpack({
       {
         test: /\.js$/,
         loader: 'babel',
-        query: {
-          stage: 0,
-          plugins: ['./build/babelRelayPlugin']
-        }
+        query: {stage: 0, plugins: ['./build/babelRelayPlugin']}
       },
-      {
-        test: /\.js$/,
-        loader: 'eslint'
-      }
     ]
   },
   output: {filename: 'app.js', path: '/'}
@@ -57,5 +50,5 @@ var app = new WebpackDevServer(compiler, {
 // Serve static resources
 app.use('/', express.static(path.resolve(__dirname, 'public')));
 app.listen(APP_PORT, () => {
-  console.log(`Relay Star Wars is now running on http://localhost:${APP_PORT}`);
+  console.log(`App is now running on http://localhost:${APP_PORT}`);
 });

1e1131371f756f46bcbcf739f1b98af1002e6c8a
akashnimare <svnitakash@gmail.com>
2015-10-12 08:46:23 -0700

    Licence bug fixed
    Summary: 1. Fixed the licence of examples. Now it is same as define [here](https://github.com/facebook/relay/tree/master/examples/todo#license).
    2. Fixed some lint errors and warnings.
    
    Ps - All npm test are passed and lint shows no major errors. Please see the diff.
    
    Npm Test
    <img src="https://box.everhelper.me/attachment/301722/NuD6t5ei9i0VAGxX7avOK5Ifa4bkQt12/312416-0JwJzhXkXu4ZATUS/screen.png"/>
    
    Lint test
    <img src="https://box.everhelper.me/attachment/301723/pGqakY01HpgeAsKPfGOtus6xDsK7NCIc/312416-AJM6V5LS06uq0P5c/screen.png"/>
    Closes https://github.com/facebook/relay/pull/433
    
    Reviewed By: @wincent
    
    Differential Revision: D2514888
    
    fb-gh-sync-id: c9defaf7b7694ebba4a7017be07ff6ae56dbe9dd

--- examples/star-wars/build/babelRelayPlugin.js
+++ examples/star-wars/build/babelRelayPlugin.js
@@ -1,10 +1,13 @@
 /**
- * Copyright 2013-2015, Facebook, Inc.
- * All rights reserved.
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
  *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 var getBabelRelayPlugin = require('babel-relay-plugin');

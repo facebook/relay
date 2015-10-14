dce56790152350934a83fa0db0caf641d9fe7b00
Jan Kassens <jkassens@fb.com>
2015-10-14 09:58:05 -0700

    Fix lint issues and use single .eslintrc
    Summary: This fixes a few lint issues and also uses a single `.eslintrc` to prevent duplication and divergence of lint rules.
    
    Follow up for concerns raised in #453.
    Closes https://github.com/facebook/relay/pull/460
    
    Reviewed By: @yungsters
    
    Differential Revision: D2537193
    
    fb-gh-sync-id: 116a852c0f75bb0ea2d657f862db6c211622632d

--- examples/relay-treasurehunt/scripts/updateSchema.js
+++ examples/relay-treasurehunt/scripts/updateSchema.js
@@ -14,7 +14,7 @@
 import fs from 'fs';
 import path from 'path';
 import { schema } from '../data/schema';
-import { graphql }  from 'graphql';
+import { graphql } from 'graphql';
 import { introspectionQuery, printSchema } from 'graphql/utilities';
 
 // Save JSON of full schema introspection for Babel Relay Plugin to use

b2f325f84443fabff7c0863e053c666f67a8618f
Steven Luscher <steveluscher@fb.com>
2015-12-09 21:42:38 -0800

    Analytics for the Relay website
    Reviewed By: yungsters
    
    Differential Revision: D2732820
    
    fb-gh-sync-id: 0856271793724793827653120e24fe275bc93430

--- website/core/Site.js
+++ website/core/Site.js
@@ -50,6 +50,9 @@ var Site = React.createClass({
 
           <script dangerouslySetInnerHTML={{__html: `
             !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+              ga('create', 'UA-71174216-1', 'auto');
+              ga('send', 'pageview');
           `}} />
         </body>
       </html>

e1624706c9605a572906396f46db59bc752d44ee
Ryan Cole <ryan@rycole.com>
2016-02-07 17:12:47 -0600

    use generateRQLFieldAlias instead of getSerializationKey

--- src/traversal/__tests__/printRelayOSSQuery-test.js
+++ src/traversal/__tests__/printRelayOSSQuery-test.js
@@ -195,21 +195,20 @@ describe('printRelayOSSQuery', () => {
       const query = getNode(Relay.QL`
         query FooQuery {
           defaultSettings {
-            web: notifications(environment: WEB),
+            web: notifications(environment: WEB)
             foo: notifications(environment: $env)
           }
         }
       `, {
         env: enumValue,
       });
-      const alias1 = query.getChildren()[0].getSerializationKey();
-      const alias2 = query.getChildren()[1].getSerializationKey();
+      const alias = generateRQLFieldAlias('notifications.environment(WEB)');
       const {text, variables} = printRelayOSSQuery(query);
       expect(text).toEqualPrintedQuery(`
         query FooQuery($environment_0:Environment) {
           defaultSettings {
-            ${alias1}: notifications(environment:$environment_0),
-            ${alias2}: notifications(environment:$environment_0)
+            ${alias}: notifications(environment:$environment_0),
+            ${alias}: notifications(environment:$environment_0)
           }
         }
       `);

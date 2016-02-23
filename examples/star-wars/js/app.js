8e498e7a5529c8a9199a4c6a722302f5c9a00cd4
C.T. Lin <chentsulin@gmail.com>
2016-02-23 14:12:50 -0800

    Use 'babel-polyfill' instead of 'babel/polyfill' in examples
    Summary:use `babel-polyfill` which is listed in `package.json`.
    Closes https://github.com/facebook/relay/pull/867
    
    Differential Revision: D2966850
    
    fb-gh-sync-id: 0c3964dd411b0777697cec38258bfa9d7ae1f898
    shipit-source-id: 0c3964dd411b0777697cec38258bfa9d7ae1f898

--- examples/star-wars/js/app.js
+++ examples/star-wars/js/app.js
@@ -10,7 +10,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import 'babel/polyfill';
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Relay from 'react-relay';

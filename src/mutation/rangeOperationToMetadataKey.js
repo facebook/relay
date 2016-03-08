16c7b9a6e97a65e93d10f130cc8160cff03f8fd3
Greg Hurrell <glh@fb.com>
2016-03-08 07:18:45 -0800

    Add missing @internal annotation to rangeOperationToMetadataKey
    Reviewed By: kassens
    
    Differential Revision: D3023781
    
    fb-gh-sync-id: a73945ed1d5e15be3c084ac68492e0b0532cd4ba
    shipit-source-id: a73945ed1d5e15be3c084ac68492e0b0532cd4ba

--- src/mutation/rangeOperationToMetadataKey.js
+++ src/mutation/rangeOperationToMetadataKey.js
@@ -20,6 +20,8 @@ const RANGE_OPERATION_METADATA_PREFIX = '__rangeOperation';
 const RANGE_OPERATION_METADATA_SUFFIX = '__';
 
 /**
+ * @internal
+ *
  * A map from developer-friendly operation names ("append", "prepend", "remove")
  * to internal book-keeping keys used to store metadata on records
  * ("__rangeOperationAppend__" etc).

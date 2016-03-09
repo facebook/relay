14ffc5bc89c6d264b7996141b3e6e0101317233f
Steven Luscher <steveluscher@fb.com>
2016-03-09 07:31:11 -0800

    First pass at a React Native / Relay TodoMVC example app
    Summary:![rntodomvc](https://cloud.githubusercontent.com/assets/13243/13621667/09af6dbc-e591-11e5-9c2b-a5cefc05d814.gif)
    
    Closes https://github.com/facebook/relay/pull/929
    
    Reviewed By: josephsavona
    
    Differential Revision: D3028048
    
    Pulled By: steveluscher
    
    fb-gh-sync-id: fbbbf15657f44f4469bc0bd4c40d22206a97a613
    shipit-source-id: fbbbf15657f44f4469bc0bd4c40d22206a97a613

---
+++ examples/TodoMVC/server.js
@@ -0,0 +1,24 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import express from 'express';
+import graphQLHTTP from 'express-graphql';
+import {schema} from './data/schema';
+
+const GRAPHQL_PORT = 8080;
+
+// Expose a GraphQL endpoint
+var graphQLServer = express();
+graphQLServer.use('/', graphQLHTTP({schema, pretty: true}));
+graphQLServer.listen(GRAPHQL_PORT, () => console.log(
+  `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
+));

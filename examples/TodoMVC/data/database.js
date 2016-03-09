90bcaac4718ce4e7f41b2f1236b1dcb939079adb
Steven Luscher <steveluscher@fb.com>
2016-03-09 00:50:08 +0000

    First pass at a React Native / Relay TodoMVC example app

---
+++ examples/TodoMVC/data/database.js
@@ -0,0 +1,98 @@
+/**
+ * This file provided by Facebook is for non-commercial testing and evaluation
+ * purposes only.  Facebook reserves all rights not expressly granted.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+export class Todo {}
+export class User {}
+
+// Mock authenticated ID
+const VIEWER_ID = 'me';
+
+// Mock user data
+var viewer = new User();
+viewer.id = VIEWER_ID;
+var usersById = {
+  [VIEWER_ID]: viewer,
+};
+
+// Mock todo data
+var todosById = {};
+var todoIdsByUser = {
+  [VIEWER_ID]: [],
+};
+var nextTodoId = 0;
+addTodo('Taste JavaScript', true);
+addTodo('Buy a unicorn', false);
+
+export function addTodo(text, complete) {
+  var todo = new Todo();
+  todo.complete = !!complete;
+  todo.id = `${nextTodoId++}`;
+  todo.text = text;
+  todosById[todo.id] = todo;
+  todoIdsByUser[VIEWER_ID].push(todo.id);
+  return todo.id;
+}
+
+export function changeTodoStatus(id, complete) {
+  var todo = getTodo(id);
+  todo.complete = complete;
+}
+
+export function getTodo(id) {
+  return todosById[id];
+}
+
+export function getTodos(status = 'any') {
+  var todos = todoIdsByUser[VIEWER_ID].map(id => todosById[id]);
+  if (status === 'any') {
+    return todos;
+  }
+  return todos.filter(todo => todo.complete === (status === 'completed'));
+}
+
+export function getUser(id) {
+  return usersById[id];
+}
+
+export function getViewer() {
+  return getUser(VIEWER_ID);
+}
+
+export function markAllTodos(complete) {
+  var changedTodos = [];
+  getTodos().forEach(todo => {
+    if (todo.complete !== complete) {
+      todo.complete = complete;
+      changedTodos.push(todo);
+    }
+  });
+  return changedTodos.map(todo => todo.id);
+}
+
+export function removeTodo(id) {
+  var todoIndex = todoIdsByUser[VIEWER_ID].indexOf(id);
+  if (todoIndex !== -1) {
+    todoIdsByUser[VIEWER_ID].splice(todoIndex, 1);
+  }
+  delete todosById[id];
+}
+
+export function removeCompletedTodos() {
+  var todosToRemove = getTodos().filter(todo => todo.complete);
+  todosToRemove.forEach(todo => removeTodo(todo.id));
+  return todosToRemove.map(todo => todo.id);
+}
+
+export function renameTodo(id, text) {
+  var todo = getTodo(id);
+  todo.text = text;
+}

ac95909c90cefc219144011df28c7918497ef669
Timothy Yung <yungsters@fb.com>
2016-03-14 17:12:48 -0700

    Relay: Add `compactLines` to Plugin Fixture Transformer
    Summary:Fixes printing of the fixtures so that "pretty printing" is preserved.
    
    This will tie us over until #944 is ready to be merged.
    Closes https://github.com/facebook/relay/pull/950
    
    Reviewed By: josephsavona
    
    Differential Revision: D3049390
    
    Pulled By: yungsters
    
    fb-gh-sync-id: a3e2a626a301734846c0b5f726e9aab11757ca10
    shipit-source-id: a3e2a626a301734846c0b5f726e9aab11757ca10

--- scripts/babel-relay-plugin/lib/tools/transformGraphQL.js
+++ scripts/babel-relay-plugin/lib/tools/transformGraphQL.js
@@ -44,8 +44,8 @@ function transformGraphQL(schemaPath, source, filename) {
     moduleOpts: { prefix: '' }
   });
   babelOptions.plugins.unshift(plugin);
+  babelOptions.compact = false;
   babelOptions.filename = filename;
-  babelOptions.retainLines = true;
   return babel.transform(source, babelOptions).code;
 }
 

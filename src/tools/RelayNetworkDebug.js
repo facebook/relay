62e0bd9b91f2eca35b8ef32603e2dea07589fd17
Tim Yung <yungsters@fb.com>
2016-04-06 23:31:54 -0700

    Relay: Injected Requests in RelayNetworkDebug
    Summary:Refactors `RelayNetworkDebug` in order to support injecting requests that may be sent without the use of `fetch` or `RelayNetworkLayer`.
    
    This also makes a few other improvements:
    
    - Improve formatting of the printed output.
    - Unifies the code paths for logging debug information for Relay requests and `fetch`.
    - If initialized multiple times, `fetch` won't be wrapped over and over again.
    
    Reviewed By: yuzhi
    
    Differential Revision: D3146973
    
    fb-gh-sync-id: 8328f57c85b65cc76d633823920f1b88410cfb22
    fbshipit-source-id: 8328f57c85b65cc76d633823920f1b88410cfb22

--- src/tools/RelayNetworkDebug.js
+++ src/tools/RelayNetworkDebug.js
@@ -20,50 +20,31 @@ import type {NetworkLayer} from 'RelayTypes';
 
 const performanceNow = require('performanceNow');
 
-const RelayNetworkDebug = {
-  init(networkLayer: NetworkLayer): void {
-    const initTime = performanceNow();
+export type RelayNetworkDebuggable = {
+  name: string;
+  type: string;
+  promise: Promise;
+  logResult: (error: ?Object, response: ?Object) => void;
+};
 
-    const queryCallback = (id: number, pendingQuery, error, results) => {
-      const time = performanceNow() - initTime;
-      const name = pendingQuery.getDebugName();
-      console.timeStamp(`← END: Relay query ${id} ${name}`);
-      console.groupCollapsed(
-        `%cRelay query ${id} ${time / 1000} ${name}`,
-        `color:${error ? 'red' : 'black'};`
-      );
-      console.timeEnd(id);
-      const query = pendingQuery.getQueryString();
-      console.debug(
-        '%c%s\n',
-        'font-size:10px; color:#333; font-family:mplus-2m-regular,menlo,' +
-        'monospaced;',
-        query
-      );
-      error && console.error(error);
-      results && console.log(results);
-      console.groupEnd();
-    };
+class RelayNetworkDebugger {
+  _fetch: Function;
+  _initTime: number;
+  _queryID: number;
 
-    const handlePending = pendingQuery => {
-      const id = queryID++;
-      const name = pendingQuery.getDebugName();
-      console.timeStamp(`START: Relay query ${id} ${name} →`);
-      console.time(id);
-      pendingQuery.then(
-        response => queryCallback(id, pendingQuery, null, response),
-        error => queryCallback(id, pendingQuery, error)
-      );
-    };
+  constructor(networkLayer: NetworkLayer) {
+    this._initTime = performanceNow();
+    this._queryID = 0;
 
-    let queryID = 0;
     Relay.injectNetworkLayer({
-      sendQueries(requests: Array<RelayQueryRequest>): ?Promise {
-        requests.forEach(handlePending);
+      sendQueries: requests => {
+        requests.forEach(request => {
+          this.logRequest(createDebuggableFromRequest('Relay Query', request));
+        });
         return networkLayer.sendQueries(requests);
       },
-      sendMutation(request: RelayMutationRequest): ?Promise {
-        handlePending(request);
+      sendMutation: request => {
+        this.logRequest(createDebuggableFromRequest('Relay Mutation', request));
         return networkLayer.sendMutation(request);
       },
       supports(...options: Array<string>): boolean {
@@ -71,40 +52,98 @@ const RelayNetworkDebug = {
       },
     });
 
-    const _fetch = global.fetch;
-    global.fetch = function(url, options, ...args) {
-      const id = queryID++;
+    this._fetch = global.fetch;
+    global.fetch = (url, options, ...args) => {
       const name = url.split('/')[2];
-      console.timeStamp(`START: fetch ${id} ${name} →`);
-      console.time(id);
-
-      const fetchCallback = (error, results, args1) => {
-        console.timeStamp(`← END: fetch ${id} ${name}`);
-        console.groupCollapsed(
-          `%cfetch ${id} ${name}`,
-          `color:${error ? 'red' : 'black'};`
-        );
-        console.timeEnd(id);
-        console.debug({url, options, args, args1});
-        error && console.error(error);
-        results && console.warn(results);
-        try {
-          results && console.debug(JSON.parse(results._bodyText));
-        } catch (e) {}
-        console.groupEnd();
-      };
-
-      return _fetch(url, options, ...args).then(
-        (results, ...args1) => {
-          fetchCallback(null, results, args1);
-          return results;
-        },
-        (error, ...args1) => {
-          fetchCallback(error, undefined, args1);
-          return error;
-        }
-      );
+      this.logRequest(createDebuggableFromFetch(
+        name,
+        {url, options, args},
+        this._fetch(url, options, ...args)
+      ));
     };
+  }
+
+  uninstall(): void {
+    global.fetch = this._fetch;
+  }
+
+  logRequest({name, type, promise, logResult}: RelayNetworkDebuggable): void {
+    const id = this._queryID++;
+    const timerName = `[${id}] Request Duration`;
+
+    console.timeStamp(`START: [${id}] ${type}: ${name} →`);
+    console.time(timerName);
+
+    const onSettled = (error, response) => {
+      const time = (performanceNow() - this._initTime) / 1000;
+      console.timeStamp(`← END: [${id}] ${type}: ${name}`);
+      console.groupCollapsed(
+        `%c[${id}] ${type}: ${name} @ ${time}s`,
+        `color:${error ? 'red' : 'black'};`
+      );
+      console.timeEnd(timerName);
+      logResult(error, response);
+      console.groupEnd();
+    };
+
+    promise.then(
+      response => onSettled(null, response),
+      error => onSettled(error, null),
+    );
+  }
+}
+
+function createDebuggableFromRequest(
+  type: string,
+  request: RelayQueryRequest | RelayMutationRequest
+): RelayNetworkDebuggable {
+  return {
+    name: request.getDebugName(),
+    type: 'Relay Query',
+    promise: request.getPromise(),
+    logResult(error, response) {
+      console.debug(
+        '%c%s\n',
+        'font-size:10px; color:#333; font-family:mplus-2m-regular,menlo,' +
+        'monospaced;',
+        request.getQueryString()
+      );
+      error && console.error(error);
+      response && console.log(response);
+    },
+  };
+}
+
+function createDebuggableFromFetch(
+  name: string,
+  config: Object,
+  promise: Promise
+): RelayNetworkDebuggable {
+  return {
+    name,
+    type: 'Relay Mutation',
+    promise,
+    logResult(error, response) {
+      console.debug(config);
+      error && console.error(error);
+      response && console.warn(response);
+      try {
+        response && console.debug(JSON.parse(response._bodyText));
+      } catch (_) {
+      }
+    },
+  };
+}
+
+let networkDebugger: ?RelayNetworkDebugger;
+
+const RelayNetworkDebug = {
+  init(networkLayer: NetworkLayer): void {
+    networkDebugger && networkDebugger.uninstall();
+    networkDebugger = new RelayNetworkDebugger(networkLayer);
+  },
+  logRequest(request: RelayNetworkDebuggable): void {
+    networkDebugger && networkDebugger.logRequest(request);
   },
 };
 

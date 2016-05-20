5636b4b845bd137de8993d5ab5a92a7646c6aa1b
Kevin Ross <kevin.ross@alienfast.com>
2016-05-20 15:31:31 -0500

    update usage of react-router-relay

--- examples/todo/js/app.js
+++ examples/todo/js/app.js
@@ -12,30 +12,25 @@
 
 import 'babel-polyfill';
 import 'todomvc-common';
-import {createHashHistory} from 'history';
-import {IndexRoute, Route} from 'react-router';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {RelayRouter} from 'react-router-relay';
 import TodoApp from './components/TodoApp';
 import TodoList from './components/TodoList';
 import ViewerQueries from './queries/ViewerQueries';
+import {createHashHistory} from 'history';
+import {IndexRoute, Route, applyRouterMiddleware, Router, useRouterHistory} from 'react-router';
+import useRelay from 'react-router-relay';
+import Relay from 'react-relay';
+
+const history = useRouterHistory(createHashHistory)({ queryKey: false });
+const mountNode = document.getElementById('root')
 
 ReactDOM.render(
-  <RelayRouter history={createHashHistory({queryKey: false})}>
-    <Route
-      path="/" component={TodoApp}
-      queries={ViewerQueries}>
-      <IndexRoute
-        component={TodoList}
-        queries={ViewerQueries}
-        prepareParams={() => ({status: 'any'})}
-      />
-      <Route
-        path=":status" component={TodoList}
-        queries={ViewerQueries}
-      />
+  <Router history={history} render={applyRouterMiddleware(useRelay)} environment={Relay.Store}>
+    <Route path="/" component={TodoApp} queries={ViewerQueries}>
+      <IndexRoute component={TodoList} queries={ViewerQueries} prepareParams={() => ({status: 'any'})} />
+      <Route path=":status" component={TodoList} queries={ViewerQueries} />
     </Route>
-  </RelayRouter>,
-  document.getElementById('root')
+  </Router>,
+  mountNode
 );

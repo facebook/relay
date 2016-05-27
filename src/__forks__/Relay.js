facebook/relay

commit 173ca148f07f5df069fe098d88fa642ec530f874

Make network layer injection clearer and more robust
Reviewed By: yuzhi

Differential Revision: D3188197

fb-gh-sync-id: 563b6721f52a506985f6c1be2d4b90911f169692
fbshipit-source-id: 563b6721f52a506985f6c1be2d4b90911f169692

src/network/RelayNetworkLayer.js
@@ -21,6 +21,7 @@ import type {NetworkLayer} from 'RelayTypes';
 
 const invariant = require('invariant');
 const resolveImmediate = require('resolveImmediate');
+const warning = require('warning');
 
 /**
  * @internal
@@ -28,46 +29,66 @@ const resolveImmediate = require('resolveImmediate');
  * `RelayNetworkLayer` provides a method to inject custom network behavior.
  */
 class RelayNetworkLayer {
-  _injectedNetworkLayer: ?NetworkLayer;
+  _defaultImplementation: ?NetworkLayer;
+  _implementation: ?NetworkLayer;
   _queue: ?Array<RelayQueryRequest>;
 
   constructor() {
-    this._injectedNetworkLayer = null;
+    this._implementation = null;
     this._queue = null;
   }
 
-  injectNetworkLayer(networkLayer: ?NetworkLayer): void {
-    this._injectedNetworkLayer = networkLayer;
+  injectDefaultImplementation(implementation: ?NetworkLayer): void {
+    if (this._defaultImplementation) {
+      warning(
+        false,
+        'RelayNetworkLayer: Call received to injectDefaultImplementation(), ' +
+        'but a default layer was already injected.'
+      );
+    }
+    this._defaultImplementation = implementation;
+  }
+
+  injectImplementation(implementation: ?NetworkLayer): void {
+    if (this._implementation) {
+      warning(
+        false,
+        'RelayNetworkLayer: Call received to injectImplementation(), but ' +
+        'a layer was already injected.'
+      );
+    }
+    this._implementation = implementation;
   }
 
   sendMutation(mutationRequest: RelayMutationRequest): void {
-    const networkLayer = this._getCurrentNetworkLayer();
-    const promise = networkLayer.sendMutation(mutationRequest);
+    const implementation = this._getImplementation();
+    const promise = implementation.sendMutation(mutationRequest);
     if (promise) {
       Promise.resolve(promise).done();
     }
   }
 
   sendQueries(queryRequests: Array<RelayQueryRequest>): void {
-    const networkLayer = this._getCurrentNetworkLayer();
-    const promise = networkLayer.sendQueries(queryRequests);
+    const implementation = this._getImplementation();
+    const promise = implementation.sendQueries(queryRequests);
     if (promise) {
       Promise.resolve(promise).done();
     }
   }
 
   supports(...options: Array<string>): boolean {
-    const networkLayer = this._getCurrentNetworkLayer();
-    return networkLayer.supports(...options);
+    const implementation = this._getImplementation();
+    return implementation.supports(...options);
   }
 
-  _getCurrentNetworkLayer(): NetworkLayer {
+  _getImplementation(): NetworkLayer {
+    const implementation = this._implementation || this._defaultImplementation;
     invariant(
-      this._injectedNetworkLayer,
-      'RelayNetworkLayer: Use `injectNetworkLayer` to configure a network ' +
-      'layer.'
+      implementation,
+      'RelayNetworkLayer: Use `RelayEnvironment.injectNetworkLayer` to ' +
+      'configure a network layer.'
     );
-    return this._injectedNetworkLayer;
+    return implementation;
   }
 
   /**
@@ -108,7 +129,7 @@ function profileQueue(currentQueue: Array<RelayQueryRequest>): void {
         firstResultProfiler = null;
       }
     };
-    query.getPromise().done(onSettle, onSettle);
+    query.done(onSettle, onSettle);
   });
 }
 

src/RelayPublic.js
@@ -51,6 +51,8 @@ const RelayPublic = {
   createContainer: RelayContainer.create,
   createQuery: createRelayQuery,
   getQueries: getRelayQueries,
+  injectDefaultNetworkLayer:
+    RelayStore.injectDefaultNetworkLayer.bind(RelayStore),
   injectNetworkLayer: RelayStore.injectNetworkLayer.bind(RelayStore),
   injectTaskScheduler: RelayStore.injectTaskScheduler.bind(RelayStore),
   isContainer: isRelayContainer,

src/legacy/store/__tests__/GraphQLQueryRunner-test.js
@@ -67,7 +67,7 @@ describe('GraphQLQueryRunner', () => {
     queryRunner = storeData.getQueryRunner();
     pendingQueryTracker = storeData.getPendingQueryTracker();
 
-    networkLayer.injectNetworkLayer({
+    networkLayer.injectImplementation({
       supports: () => true,
     });
 
@@ -109,7 +109,7 @@ describe('GraphQLQueryRunner', () => {
   it('warns and uses fallback when defer is unsupported', () => {
     diffRelayQuery.mockImplementation(query => [query]);
     checkRelayQueryData.mockImplementation(() => false);
-    networkLayer.injectNetworkLayer({
+    networkLayer.injectImplementation({
       supports: () => false,
     });
 

src/mutation/__tests__/RelayGraphQLMutation-test.js
@@ -78,7 +78,7 @@ describe('RelayGraphQLMutation', function() {
       requests.push(request);
       return request.getPromise();
     });
-    storeData.getNetworkLayer().injectNetworkLayer({sendMutation});
+    storeData.getNetworkLayer().injectImplementation({sendMutation});
 
     feedbackLikeQuery =
       Relay.QL`mutation FeedbackLikeMutation {

src/network/__tests__/RelayNetworkLayer-test.js
@@ -28,6 +28,7 @@ describe('RelayNetworkLayer', () => {
 
     const RelayQuery = jest.genMockFromModule('RelayQuery');
     jest.setMock('RelayQuery', RelayQuery);
+    jest.mock('warning');
 
     injectedNetworkLayer = {
       sendMutation: jest.fn(),
@@ -35,19 +36,77 @@ describe('RelayNetworkLayer', () => {
       supports: jest.fn(() => true),
     };
     networkLayer = new RelayNetworkLayer();
-    networkLayer.injectNetworkLayer(injectedNetworkLayer);
+    networkLayer.injectImplementation(injectedNetworkLayer);
 
     jasmine.addMatchers(RelayTestUtils.matchers);
   });
 
+  describe('layer injection', () => {
+    beforeEach(() => {
+      networkLayer = new RelayNetworkLayer();
+    });
+
+    it('complains if no implementation is injected', () => {
+      expect(() => networkLayer.supports([])).toFailInvariant(
+        'RelayNetworkLayer: Use `RelayEnvironment.injectNetworkLayer` to ' +
+        'configure a network layer.'
+      );
+    });
+
+    it('accepts a default implementation', () => {
+      const supports = jest.fn();
+      networkLayer.injectDefaultImplementation({supports});
+      expect(() => networkLayer.supports([])).not.toThrow();
+      expect(supports.mock.calls.length).toBe(1);
+    });
+
+    it('allows the default implementation to be overridden', () => {
+      const defaultSupports = jest.fn();
+      const supports = jest.fn();
+      networkLayer.injectDefaultImplementation({supports: defaultSupports});
+      networkLayer.injectImplementation({supports});
+      expect(() => networkLayer.supports([])).not.toThrow();
+      expect(defaultSupports.mock.calls.length).toBe(0);
+      expect(supports.mock.calls.length).toBe(1);
+    });
+
+    it('complains if the default is set more than once', () => {
+      const first = jest.fn();
+      const second = jest.fn();
+      networkLayer.injectDefaultImplementation({supports: first});
+      networkLayer.injectDefaultImplementation({supports: second});
+      expect(() => networkLayer.supports([])).not.toThrow();
+      expect(first.mock.calls.length).toBe(0);
+      expect(second.mock.calls.length).toBe(1);
+      expect([
+        'RelayNetworkLayer: Call received to injectDefaultImplementation(), ' +
+        'but a default layer was already injected.',
+      ]).toBeWarnedNTimes(1);
+    });
+
+    it('complains if the (non-default) implementation is overridden', () => {
+      const first = jest.fn();
+      const second = jest.fn();
+      networkLayer.injectImplementation({supports: first});
+      networkLayer.injectImplementation({supports: second});
+      expect(() => networkLayer.supports([])).not.toThrow();
+      expect(first.mock.calls.length).toBe(0);
+      expect(second.mock.calls.length).toBe(1);
+      expect([
+        'RelayNetworkLayer: Call received to injectImplementation(), but ' +
+        'a layer was already injected.',
+      ]).toBeWarnedNTimes(1);
+    });
+  });
+
   describe('supports', () => {
     it('throws when no network layer is injected', () => {
-      networkLayer.injectNetworkLayer(null);
+      networkLayer = new RelayNetworkLayer();
       expect(() => {
         networkLayer.sendQueries([]);
       }).toFailInvariant(
-        'RelayNetworkLayer: Use `injectNetworkLayer` to configure a network ' +
-        'layer.'
+        'RelayNetworkLayer: Use `RelayEnvironment.injectNetworkLayer` to ' +
+        'configure a network layer.'
       );
     });
 
@@ -60,12 +119,12 @@ describe('RelayNetworkLayer', () => {
 
   describe('sendQueries', () => {
     it('throws when no network layer is injected', () => {
-      networkLayer.injectNetworkLayer(null);
+      networkLayer = new RelayNetworkLayer();
       expect(() => {
         networkLayer.sendQueries([]);
       }).toFailInvariant(
-        'RelayNetworkLayer: Use `injectNetworkLayer` to configure a network ' +
-        'layer.'
+        'RelayNetworkLayer: Use `RelayEnvironment.injectNetworkLayer` to ' +
+        'configure a network layer.'
       );
     });
 
@@ -90,16 +149,16 @@ describe('RelayNetworkLayer', () => {
       resolvedCallback = jest.fn();
       rejectedCallback = jest.fn();
       deferred = new Deferred();
-      deferred.getPromise().done(resolvedCallback, rejectedCallback);
+      deferred.done(resolvedCallback, rejectedCallback);
     });
 
     it('throws when no network layer is injected', () => {
-      networkLayer.injectNetworkLayer(null);
+      networkLayer = new RelayNetworkLayer();
       expect(() => {
         networkLayer.sendMutation({mutation, variables, deferred});
       }).toFailInvariant(
-        'RelayNetworkLayer: Use `injectNetworkLayer` to configure a network ' +
-        'layer.'
+        'RelayNetworkLayer: Use `RelayEnvironment.injectNetworkLayer` to ' +
+        'configure a network layer.'
       );
     });
 

src/store/RelayEnvironment.js
@@ -102,8 +102,12 @@ class RelayEnvironment {
     return this._storeData;
   }
 
+  injectDefaultNetworkLayer(networkLayer: ?NetworkLayer) {
+    this._storeData.getNetworkLayer().injectDefaultImplementation(networkLayer);
+  }
+
   injectNetworkLayer(networkLayer: ?NetworkLayer) {
-    this._storeData.getNetworkLayer().injectNetworkLayer(networkLayer);
+    this._storeData.getNetworkLayer().injectImplementation(networkLayer);
   }
 
   injectTaskScheduler(scheduler: ?TaskScheduler): void {

commit fd076592c74c6126d57af3c2dd63c657faa5b50d

Fix erroneous warning about already injected network layer
Issue likey caused by https://github.com/facebook/relay/commit/173ca148f07f5df069fe098d88fa642ec530f874.

src/__forks__/Relay.js
@@ -27,7 +27,7 @@ if (__DEV__) {
 
 // By default, assume that GraphQL is served at `/graphql` on the same domain.
 // To override, use `Relay.injectNetworkLayer`.
-RelayStore.injectDefaultNetworkLayer(new RelayDefaultNetworkLayer('/graphql'));
+RelayStore.injectDefaultNetworkLayer();
 
 module.exports = {
   ...RelayPublic,

src/network/RelayNetworkLayer.js
@@ -14,6 +14,7 @@
 
 import type {ChangeSubscription} from 'RelayInternalTypes';
 import type RelayMutationRequest from 'RelayMutationRequest';
+const RelayDefaultNetworkLayer = require('RelayDefaultNetworkLayer');
 const RelayProfiler = require('RelayProfiler');
 import type RelayQuery from 'RelayQuery';
 const RelayQueryRequest = require('RelayQueryRequest');
@@ -63,6 +64,12 @@ class RelayNetworkLayer {
   }
 
   injectImplementation(implementation: ?NetworkLayer): void {
+    // Handle the case where this is called with the default implementation
+    // instead of using `injectDefaultImplementation`.
+    if (implementation instanceof RelayDefaultNetworkLayer) {
+      this.injectDefaultImplementation(implementation);
+      return;
+    }
     if (this._implementation) {
       warning(
         false,
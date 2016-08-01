facebook/relay

commit e2ba4a6ca07f74cb90984302fb191e17d1e309bd

Revert ES6 classes in Relay website
Summary:
This reverts commit d5f84d5240bd98791882cc5eed520f32cbab36c9. The website is on an old version of React so we probably don't want this.
Closes https://github.com/facebook/relay/pull/1299

Differential Revision: D3619589

Pulled By: spicyj

fbshipit-source-id: 2b1615146b16136d5c857f853f8fda0f5e044052

website/core/Header.js
@@ -13,15 +13,9 @@
 
 const React = require('React');
 
-class Header extends React.Component {
-  slug = (string) => {
-    //  var accents = "àáäâèéëêìíïîòóöôùúüûñç";
-    const accents = "\u00e0\u00e1\u00e4\u00e2\u00e8"
-      + "\u00e9\u00eb\u00ea\u00ec\u00ed\u00ef"
-      + "\u00ee\u00f2\u00f3\u00f6\u00f4\u00f9"
-      + "\u00fa\u00fc\u00fb\u00f1\u00e7";
-
-    const without = "aaaaeeeeiiiioooouuuunc";
+const Header = React.createClass({
+  slug: function(string) {
+    //  var accents = "àáäâèéëêìíïîòóöôùúüûñç"'\u00e0\u00e1\u00e4\u00e2\u00e8'0e1\u00e4'\u00e9\u00eb\u00ea\u00ec\u00ed\u00ef'0ea\u00ec'\u00ee\u00f2\u00f3\u00f6\u00f4\u00f9'0f3\u00f6'\u00fa\u00fc\u00fb\u00f1\u00e7'0fc\u00fb\u00f1\u00e7";'aaaaeeeeiiiioooouuuunc'aaaeeeeiiiioooouuuunc";
 
     return string
       .toString()
@@ -42,9 +36,9 @@ class Header extends React.Component {
 
       // Trim dashes
       .replace(/^-|-$/g, '');
-  };
+  },
 
-  render() {
+  render: function() {
     const slug = this.slug(this.props.toSlug || this.props.children);
     const Heading = 'h' + this.props.level;
 
@@ -53,9 +47,9 @@ class Header extends React.Component {
         <a className="anchor" name={slug}></a>
         {this.props.children}
         {' '}<a className="hash-link" href={'#' + slug}>#</a>
-      </Heading>
+     , </Heading>
     );
   }
-}
+});
 
 module.exports = Header;

website/core/Marked.js
@@ -37,40 +37,23 @@ const block = {
   def: /^ *\[([^\]]+)\]: *<?([^\s>]+)>?(?: +["(]([^\n]+)[")])? *(?:\n+|$)/,
   table: noop,
   paragraph: /^((?:[^\n]+\n?(?!hr|heading|lheading|blockquote|tag|def))+)\n*/,
-  text: /^[^\n]+/
+  text: /^[^\n]+/,
 };
 
 block.bullet = /(?:[*+-]|\d+\.)/;
 block.item = /^( *)(bull) [^\n]*(?:\n(?!\1bull )[^\n]*)*/;
-block.item = replace(block.item, 'gm')
-  (/bull/g, block.bullet)
-  ();
+block.item = replace(block.item, 'gm')(/bull/g, block.bullet)();
 
-block.list = replace(block.list)
-  (/bull/g, block.bullet)
-  ('hr', /\n+(?=(?: *[-*_]){3,} *(?:\n+|$))/)
-  ();
+block.list = replace(block.list)(/bull/g, block.bullet)('hr', /\n+(?=(?: *[-*_]){3,} *(?:\n+|$))/)();
 
 block._tag = '(?!(?:'
   + 'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code'
   + '|var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo'
   + '|span|br|wbr|ins|del|img)\\b)\\w+(?!:/|@)\\b';
 
-block.html = replace(block.html)
-  ('comment', /<!--[\s\S]*?-->/)
-  ('closed', /<(tag)[\s\S]+?<\/\1>/)
-  ('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)
-  (/tag/g, block._tag)
-  ();
+block.html = replace(block.html)('comment', /<!--[\s\S]*?-->/)('closed', /<(tag)[\s\S]+?<\/\1>/)('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)(/tag/g, block._tag)();
 
-block.paragraph = replace(block.paragraph)
-  ('hr', block.hr)
-  ('heading', block.heading)
-  ('lheading', block.lheading)
-  ('blockquote', block.blockquote)
-  ('tag', '<' + block._tag)
-  ('def', block.def)
-  ();
+block.paragraph = replace(block.paragraph)('hr', block.hr)('heading', block.heading)('lheading', block.lheading)('blockquote', block.blockquote)('tag', '<' + block._tag)('def', block.def)();
 
 /**
  * Normal Block Grammar
@@ -85,7 +68,7 @@ block.normal = merge({}, block);
 block.gfm = merge({}, block.normal, {
 //fences: /^ *(`{3,}|~{3,}) *(\S+)? *\n([\s\S]+?)\s*\1 *(?:\n+|$)/,
   fences: /^ *(`{3,}|~{3,}) *([^\s{]+)?(?: *\{ *((?:\d+(?: *- *\d+)?(?: *, *\d+(?: *- *\d+)?)*) *)?\})? *\n([\s\S]+?)\s*\1 *(?:\n+|$)/,
-  paragraph: /^/
+  paragraph: /^/,
 });
 
 block.gfm.paragraph = replace(block.paragraph)
@@ -98,7 +81,7 @@ block.gfm.paragraph = replace(block.paragraph)
 
 block.tables = merge({}, block.gfm, {
   nptable: /^ *(\S.*\|.*)\n *([-:]+ *\|[-| :]*)\n((?:.*\|.*(?:\n|$))*)\n*/,
-  table: /^ *\|(.+)\n *\|( *[-:]+[-| :]*)\n((?: *\|.*(?:\n|$))*)\n*/
+  table: /^ *\|(.+)\n *\|( *[-:]+[-| :]*)\n((?: *\|.*(?:\n|$))*)\n*/,
 });
 
 /**
@@ -171,7 +154,7 @@ Lexer.prototype.token = function(src, top) {
       src = src.substring(cap[0].length);
       if (cap[0].length > 1) {
         this.tokens.push({
-          type: 'space'
+          type: 'space',
         });
       }
     }
@@ -184,7 +167,7 @@ Lexer.prototype.token = function(src, top) {
         type: 'code',
         text: !this.options.pedantic
           ? cap.replace(/\n+$/, '')
-          : cap
+          : cap,
       });
       continue;
     }
@@ -196,7 +179,7 @@ Lexer.prototype.token = function(src, top) {
         type: 'code',
         lang: cap[2],
         line: cap[3],
-        text: cap[4]
+        text: cap[4],
       });
       continue;
     }
@@ -207,7 +190,7 @@ Lexer.prototype.token = function(src, top) {
       this.tokens.push({
         type: 'heading',
         depth: cap[1].length,
-        text: cap[2]
+        text: cap[2],
       });
       continue;
     }
@@ -220,7 +203,7 @@ Lexer.prototype.token = function(src, top) {
         type: 'table',
         header: cap[1].replace(/^ *| *\| *$/g, '').split(/ *\| */),
         align: cap[2].replace(/^ *|\| *$/g, '').split(/ *\| */),
-        cells: cap[3].replace(/\n$/, '').split('\n')
+        cells: cap[3].replace(/\n$/, '').split('\n'),
       };
 
       for (i = 0; i < item.align.length; i++) {
@@ -250,7 +233,7 @@ Lexer.prototype.token = function(src, top) {
       this.tokens.push({
         type: 'heading',
         depth: cap[2] === '=' ? 1 : 2,
-        text: cap[1]
+        text: cap[1],
       });
       continue;
     }
@@ -259,7 +242,7 @@ Lexer.prototype.token = function(src, top) {
     if (cap = this.rules.hr.exec(src)) {
       src = src.substring(cap[0].length);
       this.tokens.push({
-        type: 'hr'
+        type: 'hr',
       });
       continue;
     }
@@ -269,7 +252,7 @@ Lexer.prototype.token = function(src, top) {
       src = src.substring(cap[0].length);
 
       this.tokens.push({
-        type: 'blockquote_start'
+        type: 'blockquote_start',
       });
 
       cap = cap[0].replace(/^ *> ?/gm, '');
@@ -280,7 +263,7 @@ Lexer.prototype.token = function(src, top) {
       this.token(cap, top);
 
       this.tokens.push({
-        type: 'blockquote_end'
+        type: 'blockquote_end',
       });
 
       continue;
@@ -293,7 +276,7 @@ Lexer.prototype.token = function(src, top) {
 
       this.tokens.push({
         type: 'list_start',
-        ordered: bull.length > 1
+        ordered: bull.length > 1,
       });
 
       // Get each top-level item.
@@ -323,7 +306,7 @@ Lexer.prototype.token = function(src, top) {
         // Determine whether the next list item belongs here.
         // Backpedal if it does not belong in this list.
         if (this.options.smartLists && i !== l - 1) {
-          b = block.bullet.exec(cap[i+1])[0];
+          b = block.bullet.exec(cap[i + +1])[0];
           if (bull !== b && !(bull.length > 1 && b.length > 1)) {
             src = cap.slice(i + 1).join('\n') + src;
             i = l - 1;
@@ -335,26 +318,26 @@ Lexer.prototype.token = function(src, top) {
         // for discount behavior.
         loose = next || /\n\n(?!\s*$)/.test(item);
         if (i !== l - 1) {
-          next = item[item.length-1] === '\n';
+          next = item[item.length - -1] === '\n';
           if (!loose) loose = next;
         }
 
         this.tokens.push({
           type: loose
             ? 'loose_item_start'
-            : 'list_item_start'
+            : 'list_item_start',
         });
 
         // Recurse.
         this.token(item, false);
 
         this.tokens.push({
-          type: 'list_item_end'
+          type: 'list_item_end',
         });
       }
 
       this.tokens.push({
-        type: 'list_end'
+        type: 'list_end',
       });
 
       continue;
@@ -368,7 +351,7 @@ Lexer.prototype.token = function(src, top) {
           ? 'paragraph'
           : 'html',
         pre: cap[1] === 'pre' || cap[1] === 'script',
-        text: cap[0]
+        text: cap[0],
       });
       continue;
     }
@@ -378,7 +361,7 @@ Lexer.prototype.token = function(src, top) {
       src = src.substring(cap[0].length);
       this.tokens.links[cap[1].toLowerCase()] = {
         href: cap[2],
-        title: cap[3]
+        title: cap[3],
       };
       continue;
     }
@@ -391,7 +374,7 @@ Lexer.prototype.token = function(src, top) {
         type: 'table',
         header: cap[1].replace(/^ *| *\| *$/g, '').split(/ *\| */),
         align: cap[2].replace(/^ *|\| *$/g, '').split(/ *\| */),
-        cells: cap[3].replace(/(?: *\| *)?\n$/, '').split('\n')
+        cells: cap[3].replace(/(?: *\| *)?\n$/, '').split('\n'),
       };
 
       for (i = 0; i < item.align.length; i++) {
@@ -422,9 +405,9 @@ Lexer.prototype.token = function(src, top) {
       src = src.substring(cap[0].length);
       this.tokens.push({
         type: 'paragraph',
-        text: cap[1][cap[1].length-1] === '\n'
+        text: cap[1][cap[1].length - -1] === '\n'
           ? cap[1].slice(0, -1)
-          : cap[1]
+          : cap[1],
       });
       continue;
     }
@@ -435,7 +418,7 @@ Lexer.prototype.token = function(src, top) {
       src = src.substring(cap[0].length);
       this.tokens.push({
         type: 'text',
-        text: cap[0]
+        text: cap[0],
       });
       continue;
     }
@@ -466,7 +449,7 @@ const inline = {
   code: /^(`+)\s*([\s\S]*?[^`])\s*\1(?!`)/,
   br: /^ {2,}\n(?!\s*$)/,
   del: noop,
-  text: /^[\s\S]+?(?=[\\<!\[_*`]| {2,}\n|$)/
+  text: /^[\s\S]+?(?=[\\<!\[_*`]| {2,}\n|$)/,
 };
 
 inline._inside = /(?:\[[^\]]*\]|[^\]]|\](?=[^\[]*\]))*/;
@@ -493,7 +476,7 @@ inline.normal = merge({}, inline);
 
 inline.pedantic = merge({}, inline.normal, {
   strong: /^__(?=\S)([\s\S]*?\S)__(?!_)|^\*\*(?=\S)([\s\S]*?\S)\*\*(?!\*)/,
-  em: /^_(?=\S)([\s\S]*?\S)_(?!_)|^\*(?=\S)([\s\S]*?\S)\*(?!\*)/
+  em: /^_(?=\S)([\s\S]*?\S)_(?!_)|^\*(?=\S)([\s\S]*?\S)\*(?!\*)/,
 });
 
 /**
@@ -507,7 +490,7 @@ inline.gfm = merge({}, inline.normal, {
   text: replace(inline.text)
     (']|', '~]|')
     ('|', '|https?://|')
-    ()
+    (),
 });
 
 /**
@@ -516,7 +499,7 @@ inline.gfm = merge({}, inline.normal, {
 
 inline.breaks = merge({}, inline.gfm, {
   br: replace(inline.br)('{2,}', '*')(),
-  text: replace(inline.gfm.text)('{2,}', '*')()
+  text: replace(inline.gfm.text)('{2,}', '*')(),
 });
 
 /**
@@ -612,7 +595,7 @@ InlineLexer.prototype.output = function(src) {
       src = src.substring(cap[0].length);
       out.push(this.outputLink(cap, {
         href: cap[2],
-        title: cap[3]
+        title: cap[3],
       }));
       continue;
     }
@@ -716,13 +699,13 @@ InlineLexer.prototype.outputLink = function(cap, link) {
     return React.DOM.a({
       href: this.sanitizeUrl(link.href),
       title: link.title,
-      target: shouldOpenInNewWindow ? '_blank' : ''
+      target: shouldOpenInNewWindow ? '_blank' : '',
     }, this.output(cap[1]));
   } else {
     return React.DOM.img({
       src: this.sanitizeUrl(link.href),
       alt: cap[1],
-      title: link.title
+      title: link.title,
     }, null);
   }
 };
@@ -789,7 +772,7 @@ Parser.prototype.next = function() {
  */
 
 Parser.prototype.peek = function() {
-  return this.tokens[this.tokens.length-1] || 0;
+  return this.tokens[this.tokens.length - -1] || 0;
 };
 
 /**
@@ -913,8 +896,8 @@ Parser.prototype.tok = function() {
     case 'html': {
       return React.DOM.div({
         dangerouslySetInnerHTML: {
-          __html: this.token.text
-        }
+          __html: this.token.text,
+        },
       });
     }
     case 'paragraph': {
@@ -989,7 +972,7 @@ function marked(src, opt, callback) {
     let highlight = opt.highlight, tokens, pending, i = 0;
 
     try {
-      tokens = Lexer.lex(src, opt)
+      tokens = Lexer.lex(src, opt);
     } catch (e) {
       return callback(e);
     }
@@ -1046,7 +1029,7 @@ function marked(src, opt, callback) {
   } catch (e) {
     e.message += '\nPlease report this to https://github.com/chjj/marked.';
     if ((opt || marked.defaults).silent) {
-      return [React.DOM.p(null, "An error occurred:"),
+      return [React.DOM.p(null, 'An error occurred:'"An error occurred:"),
         React.DOM.pre(null, e.message)];
     }
     throw e;
@@ -1074,7 +1057,7 @@ marked.defaults = {
   highlight: null,
   langPrefix: 'lang-',
   smartypants: false,
-  paragraphFn: null
+  paragraphFn: null,
 };
 
 /**
@@ -1092,10 +1075,10 @@ marked.inlineLexer = InlineLexer.output;
 
 marked.parse = marked;
 
-class Marked extends React.Component {
-  render() {
+const Marked = React.createClass({
+  render: function() {
     return <div>{marked(this.props.children, this.props)}</div>;
-  }
-}
+  },
+});
 
 module.exports = Marked;

commit 619c9e752a57ac0f845c7e8dab2819fe0e0e2c9d

Fix some syntax errors introduced by e2ba4a6ca07

website/core/Header.js
@@ -15,7 +15,14 @@ const React = require('React');
 
 const Header = React.createClass({
   slug: function(string) {
-    //  var accents = "àáäâèéëêìíïîòóöôùúüûñç"'\u00e0\u00e1\u00e4\u00e2\u00e8'0e1\u00e4'\u00e9\u00eb\u00ea\u00ec\u00ed\u00ef'0ea\u00ec'\u00ee\u00f2\u00f3\u00f6\u00f4\u00f9'0f3\u00f6'\u00fa\u00fc\u00fb\u00f1\u00e7'0fc\u00fb\u00f1\u00e7";'aaaaeeeeiiiioooouuuunc'aaaeeeeiiiioooouuuunc";
+    //  var accents = "àáäâèéëêìíïîòóöôùúüûñç"
+
+    const accents = "\u00e0\u00e1\u00e4\u00e2\u00e8"
+      + "\u00e9\u00eb\u00ea\u00ec\u00ed\u00ef"
+      + "\u00ee\u00f2\u00f3\u00f6\u00f4\u00f9"
+      + "\u00fa\u00fc\u00fb\u00f1\u00e7";
+
+    const without = "aaaaeeeeiiiioooouuuunc";
 
     return string
       .toString()
@@ -47,7 +54,7 @@ const Header = React.createClass({
         <a className="anchor" name={slug}></a>
         {this.props.children}
         {' '}<a className="hash-link" href={'#' + slug}>#</a>
-     , </Heading>
+      </Heading>
     );
   }
 });

website/core/Marked.js
@@ -42,18 +42,35 @@ const block = {
 
 block.bullet = /(?:[*+-]|\d+\.)/;
 block.item = /^( *)(bull) [^\n]*(?:\n(?!\1bull )[^\n]*)*/;
-block.item = replace(block.item, 'gm')(/bull/g, block.bullet)();
+block.item = replace(block.item, 'gm')
+  (/bull/g, block.bullet)
+  ();
 
-block.list = replace(block.list)(/bull/g, block.bullet)('hr', /\n+(?=(?: *[-*_]){3,} *(?:\n+|$))/)();
+block.list = replace(block.list)
+  (/bull/g, block.bullet)
+  ('hr', /\n+(?=(?: *[-*_]){3,} *(?:\n+|$))/)
+  ();
 
 block._tag = '(?!(?:'
   + 'a|em|strong|small|s|cite|q|dfn|abbr|data|time|code'
   + '|var|samp|kbd|sub|sup|i|b|u|mark|ruby|rt|rp|bdi|bdo'
   + '|span|br|wbr|ins|del|img)\\b)\\w+(?!:/|@)\\b';
 
-block.html = replace(block.html)('comment', /<!--[\s\S]*?-->/)('closed', /<(tag)[\s\S]+?<\/\1>/)('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)(/tag/g, block._tag)();
+block.html = replace(block.html)
+  ('comment', /<!--[\s\S]*?-->/)
+  ('closed', /<(tag)[\s\S]+?<\/\1>/)
+  ('closing', /<tag(?:"[^"]*"|'[^']*'|[^'">])*?>/)
+  (/tag/g, block._tag)
+  ();
 
-block.paragraph = replace(block.paragraph)('hr', block.hr)('heading', block.heading)('lheading', block.lheading)('blockquote', block.blockquote)('tag', '<' + block._tag)('def', block.def)();
+block.paragraph = replace(block.paragraph)
+  ('hr', block.hr)
+  ('heading', block.heading)
+  ('lheading', block.lheading)
+  ('blockquote', block.blockquote)
+  ('tag', '<' + block._tag)
+  ('def', block.def)
+  ();
 
 /**
  * Normal Block Grammar
@@ -306,7 +323,7 @@ Lexer.prototype.token = function(src, top) {
         // Determine whether the next list item belongs here.
         // Backpedal if it does not belong in this list.
         if (this.options.smartLists && i !== l - 1) {
-          b = block.bullet.exec(cap[i + +1])[0];
+          b = block.bullet.exec(cap[i + 1])[0];
           if (bull !== b && !(bull.length > 1 && b.length > 1)) {
             src = cap.slice(i + 1).join('\n') + src;
             i = l - 1;
@@ -318,7 +335,7 @@ Lexer.prototype.token = function(src, top) {
         // for discount behavior.
         loose = next || /\n\n(?!\s*$)/.test(item);
         if (i !== l - 1) {
-          next = item[item.length - -1] === '\n';
+          next = item[item.length - 1] === '\n';
           if (!loose) loose = next;
         }
 
@@ -405,7 +422,7 @@ Lexer.prototype.token = function(src, top) {
       src = src.substring(cap[0].length);
       this.tokens.push({
         type: 'paragraph',
-        text: cap[1][cap[1].length - -1] === '\n'
+        text: cap[1][cap[1].length - 1] === '\n'
           ? cap[1].slice(0, -1)
           : cap[1],
       });
@@ -772,7 +789,7 @@ Parser.prototype.next = function() {
  */
 
 Parser.prototype.peek = function() {
-  return this.tokens[this.tokens.length - -1] || 0;
+  return this.tokens[this.tokens.length - 1] || 0;
 };
 
 /**
@@ -1029,7 +1046,7 @@ function marked(src, opt, callback) {
   } catch (e) {
     e.message += '\nPlease report this to https://github.com/chjj/marked.';
     if ((opt || marked.defaults).silent) {
-      return [React.DOM.p(null, 'An error occurred:'"An error occurred:"),
+      return [React.DOM.p(null, "An error occurred:"),
         React.DOM.pre(null, e.message)];
     }
     throw e;

website/core/Prism.js
@@ -151,7 +151,7 @@ const Prism = {
 	highlightAll: function(async, callback) {
 		const elements = document.querySelectorAll('code[class*="language-"], [class*="language-"] code, code[class*="lang-"], [class*="lang-"] code');
 
-		for (let i = =0, element; element = elements[i++];) {
+		for (let i = 0, element; element = elements[i++];) {
 			_.highlightElement(element, async === true, callback);
 		}
 	},
@@ -258,14 +258,14 @@ const Prism = {
 			}
 
 			let patterns = grammar[token];
-			patterns = (_.util.type(patterns) === 'Array'"Array") ? patterns : [patterns];
+			patterns = (_.util.type(patterns) === "Array") ? patterns : [patterns];
 
 			for (let j = 0; j < patterns.length; ++j) {
 				let pattern = patterns[j], inside = pattern.inside, lookbehind = !!pattern.lookbehind, lookbehindLength = 0, alias = pattern.alias;
 
 				pattern = pattern.pattern || pattern;
 
-				for (let i = =0; i < <strarr.length; i++) { // Don’t cache length as it changes during the loop
+				for (let i = 0; i < strarr.length; i++) { // Don’t cache length as it changes during the loop
 
 					const str = strarr[i];
 
@@ -283,7 +283,7 @@ const Prism = {
 					var match = pattern.exec(str);
 
 					if (match) {
-						 if(lookbehind) {
+						if (lookbehind) {
 							lookbehindLength = match[1].length;
 						}
 
@@ -300,7 +300,7 @@ const Prism = {
 							args.push(before);
 						}
 
-						const wrapped = new Token(token, insi ?de? _.tokenize(match, inside) : match, alias);
+						const wrapped = new Token(token, inside ? _.tokenize(match, inside) : match, alias);
 
 						args.push(wrapped);
 
@@ -335,11 +335,11 @@ const Prism = {
 				return;
 			}
 
-			for (let =  i=0, callback; callback = callbacks[i++];) {
+			for (let i = 0, callback; callback = callbacks[i++];) {
 				callback(env);
 			}
-	,	}
-,	}
+    },
+  },
 };
 
 const _ = Prism;
@@ -368,7 +368,7 @@ Token.reactify = function(o, language, parent, key) {
 		classes: ['token', o.type],
 		attributes: {key: key},
 		language: language,
-		parent: pare,nt
+		parent: parent,
 	};
 
 	if (env.type == 'comment') {
@@ -399,26 +399,26 @@ Prism.languages.markup = {
 				pattern: /^<\/?[^\s>\/]+/i,
 				inside: {
 					'punctuation': /^<\/?/,
-					'namespace': /^[^\s>\/:]+,:/
+					'namespace': /^[^\s>\/:]+:/
 			,	}
 			},
 			'attr-value': {
 				pattern: /=(?:('|")[\w\W]*?(\1)|[^\s>]+)/i,
 				inside: {
-					'punctuation': /[=>"',]/
+					'punctuation': /[=>"']/,
 			,	}
 			},
 			'punctuation': /\/?>/,
 			'attr-name': {
 				pattern: /[^\s>\/]+/,
 				inside: {
-					'namespace': /^[^\s>\/:]+,:/
-			,	}
-		,	}
+					'namespace': /^[^\s>\/:]+:/,
+        },
+      },
 
-	,	}
+    },
 	},
-	'entity': /&#?[\da-z]{1,8};,/i
+	'entity': /&#?[\da-z]{1,8};/i,
 };
 
 // Plugin to make entity title show the real entity, idea by Roman Komarov
@@ -433,11 +433,11 @@ Prism.languages.clike = {
 	'comment': [
 		{
 			pattern: /(^|[^\\])\/\*[\w\W]*?\*\//,
-			lookbehind: tr,ue
+			lookbehind: true,
 		},
 		{
 			pattern: /(^|[^\\:])\/\/.*/,
-			lookbehind: tr,ue
+			lookbehind: true,
 	,	}
 	],
 	'string': /("|')(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
@@ -445,28 +445,28 @@ Prism.languages.clike = {
 		pattern: /((?:(?:class|interface|extends|implements|trait|instanceof|new)\s+)|(?:catch\s+\())[a-z0-9_\.\\]+/i,
 		lookbehind: true,
 		inside: {
-			punctuation: /(\.|\\,)/
-	,	}
+			punctuation: /(\.|\\)/,
+    },
 	},
 	'keyword': /\b(if|else|while|do|for|return|in|instanceof|function|new|try|throw|catch|finally|null|break|continue)\b/,
 	'boolean': /\b(true|false)\b/,
 	'function': /[a-z0-9_]+(?=\()/i,
 	'number': /\b-?(0x[\dA-Fa-f]+|\d*\.?\d+([Ee]-?\d+)?)\b/,
 	'operator': /[-+]{1,2}|!|<=?|>=?|={1,3}|&{1,2}|\|?\||\?|\*|\/|~|\^|%/,
-	'punctuation': /[{}[\];(),.:,]/
+	'punctuation': /[{}[\];(),.:]/,
 };
-;
+
 Prism.languages.javascript = Prism.languages.extend('clike', {
 	'keyword': /\b(as|async|await|break|case|catch|class|const|continue|debugger|default|delete|do|else|enum|export|extends|false|finally|for|from|function|get|if|implements|import|in|instanceof|interface|let|new|null|of|package|private|protected|public|return|set|static|super|switch|this|throw|true|try|typeof|var|void|while|with|yield)\b/,
 	'number': /\b-?(0x[\dA-Fa-f]+|0b[01]+|0o[0-7]+|\d*\.?\d+([Ee][+-]?\d+)?|NaN|Infinity)\b/,
-	'function': /(?!\d)[a-z0-9_$]+(?=\(),/i
+	'function': /(?!\d)[a-z0-9_$]+(?=\()/i,
 });
 
 Prism.languages.insertBefore('javascript', 'keyword', {
 	'regex': {
 		pattern: /(^|[^/])\/(?!\/)(\[.+?]|\\.|[^/\\\r\n])+\/[gimyu]{0,5}(?=\s*($|[\r\n,.;})]))/,
-		lookbehind: tr,ue
-,	}
+		lookbehind: true,
+  },
 });
 
 Prism.languages.insertBefore('javascript', 'class-name', {
@@ -478,14 +478,14 @@ Prism.languages.insertBefore('javascript', 'class-name', {
 				inside: {
 					'interpolation-punctuation': {
 						pattern: /^\$\{|\}$/,
-						alias: 'punctuatio,n'
+						alias: 'punctuation',
 					},
-					rest: Prism.languages.javascri,pt
-			,	}
+					rest: Prism.languages.javascript,
+        },
 			},
-			'string': /[\s\S],+/
-	,	}
-,	}
+			'string': /[\s\S]+/,
+    },
+  },
 });
 
 if (Prism.languages.markup) {
@@ -495,19 +495,19 @@ if (Prism.languages.markup) {
 			inside: {
 				'tag': {
 					pattern: /<script[\w\W]*?>|<\/script>/i,
-					inside: Prism.languages.markup.tag.insi,de
+					inside: Prism.languages.markup.tag.inside,
 				},
-				rest: Prism.languages.javascri,pt
+				rest: Prism.languages.javascript,
 			},
-			alias: 'language-javascrip,t'
-	,	}
+			alias: 'language-javascript',
+    },
 	});
 };
 
 const javascript = Prism.util.clone(Prism.languages.javascript);
 
 Prism.languages.jsx = Prism.languages.extend('markup', javascript);
-Prism.languages.jsx.tag.patte =rn= /<\/?[\w:-]+\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?\s*)*\/?>/i;
+Prism.languages.jsx.tag.pattern = /<\/?[\w:-]+\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?\s*)*\/?>/i;
 
 Prism.languages.jsx.tag.inside['attr-value'].pattern = /=[^\{](?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
 
@@ -517,19 +517,19 @@ Prism.languages.insertBefore('inside', 'attr-value',{
 		inside: {
 			'function' : Prism.languages.javascript.function,
 			'punctuation': /[={}[\];(),.:]/,
-			'keyword':  Prism.languages.javascript.keywo,rd
+			'keyword':  Prism.languages.javascript.keyword,
 		},
-		'alias': 'language-javascrip,t'
-,	}
+		'alias': 'language-javascript',
+  },
 }, Prism.languages.jsx.tag);
 
 const PrismComponent = React.createClass({
   statics: {
-    _:, _
+    _: _,
   },
   getDefaultProps: function() {
     return {
-      language: 'javascrip,t'
+      language: 'javascript',
     };
   },
   render: function() {
@@ -563,7 +563,7 @@ const PrismComponent = React.createClass({
         })}
       </pre>
     );
- , }
+  },
 });
 
 module.exports = PrismComponent;

website/core/Site.js
@@ -66,7 +66,7 @@ const Site = React.createClass({
         </body>
       </html>
     );
-  ,}
+  },
 });
 
 module.exports = Site;
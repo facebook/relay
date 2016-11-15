9f63bc28f738c131bff2969f4a43f0e84ea4ce84
Gabe Levi <gabe@fb.com>
2016-11-14 20:39:21 -0800

    Deploy v0.35.0
    Reviewed By: davidaurelio
    
    Differential Revision: D4177391
    
    fbshipit-source-id: c02cf98ed275b7032103b11eab6d0e189f8d8f24

--- src/mutation/RelayGraphQLMutation.js
+++ src/mutation/RelayGraphQLMutation.js
@@ -385,6 +385,10 @@ class PendingGraphQLTransaction {
     }
     return {
       ...this._variables,
+      /* $FlowFixMe(>=0.35.0 site=www,mobile,react_native_fb) - This comment
+       * suppresses an error that was found when Flow v0.35.0 was deployed. To
+       * see the error, remove this comment and run flow ~/www or
+       * flow ~/www/html/js/mobile or flow ~/fbobjc/Libraries/FBReactKit/js */
       input: {
         ...input,
         [CLIENT_MUTATION_ID]: this.id,

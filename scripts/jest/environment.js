ff7959f84a03239f73cfe72b89514e9874173852
Jan Kassens <jkassens@fb.com>
2017-04-05 00:51:45 -0700

    silence warning about __configs__ in Relay tests
    Reviewed By: kassens
    
    Differential Revision: D4833618
    
    fbshipit-source-id: 4bd7cc85babf75c5827336668b0c3cb1bee64018

--- scripts/jest/environment.js
+++ scripts/jest/environment.js
@@ -1,1 +1,13 @@
 require('regenerator-runtime/runtime');
+
+/* eslint-disable no-console */
+
+// TODO(t16118961) remove below once graphql-js updates to suppress this error
+const origError = console.error;
+console.error = (format, ...args) => {
+  if (typeof format === 'string' && format.indexOf('__configs__') >= 0) {
+    // ignore warning about __configs__ being a reserved word
+    return;
+  }
+  origError.call(console, format, ...args);
+};

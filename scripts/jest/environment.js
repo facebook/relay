1860dcc25bf87d8ddae2ee4a29dd9016dc73bffc
Jan Kassens <jan@kassens.net>
2017-04-11 10:42:09 -0700

    Rename __configs__ to configs
    `__configs__` is a legacy field and shouldn't be used anymore. Trying if we can
    rename it in the test schema without breaking too much internally.

--- scripts/jest/environment.js
+++ scripts/jest/environment.js
@@ -10,15 +10,3 @@
 'use strict';
 
 require('babel-runtime/regenerator');
-
-/* eslint-disable no-console */
-
-// TODO(t16118961) remove below once graphql-js updates to suppress this error
-const origError = console.error;
-console.error = (format, ...args) => {
-  if (typeof format === 'string' && format.indexOf('__configs__') >= 0) {
-    // ignore warning about __configs__ being a reserved word
-    return;
-  }
-  origError.call(console, format, ...args);
-};

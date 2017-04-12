3df7261b29fdca2922b3ffafbb09cc31ac053d42
Jan Kassens <jan@kassens.net>
2017-04-12 12:52:55 -0700

    Update license headers to BSD style
    Summary:
    In the process of open sourcing, we forgot to update the license header in some
    places. This fixes the headers.
    Closes https://github.com/facebook/relay/pull/1604
    
    Reviewed By: leebyron
    
    Differential Revision: D4874699
    
    Pulled By: kassens
    
    fbshipit-source-id: c5f34927dab58ebb6544d84e53cecff876bcef25

--- packages/relay-compiler/codegen/writeRelayQLFile.js
+++ packages/relay-compiler/codegen/writeRelayQLFile.js
@@ -1,8 +1,13 @@
 /**
- * Copyright 2004-present Facebook. All Rights Reserved.
+ * Copyright (c) 2013-present, Facebook, Inc.
+ * All rights reserved.
  *
- * @flow
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ *
  * @providesModule writeRelayQLFile
+ * @flow
  */
 
 'use strict';

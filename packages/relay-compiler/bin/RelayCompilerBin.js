d98bae873f43b64838fe5d3b6123ba0657f1554b
Yasser Kaddour <yasser.kaddour@gmail.com>
2017-04-23 10:34:51 +0100

    Add support for Json schema to relay-compiler
    Fix #1639

--- packages/relay-compiler/bin/RelayCompilerBin.js
+++ packages/relay-compiler/bin/RelayCompilerBin.js
@@ -21,7 +21,13 @@ const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs');
 
-const {buildASTSchema, parse} = require('graphql');
+const {
+  buildASTSchema,
+  buildClientSchema,
+  parse,
+  printSchema,
+} = require('graphql');
+
 const {
   codegenTransforms,
   fragmentTransforms,
@@ -122,6 +128,9 @@ function getRelayFileWriter(baseDir: string) {
 function getSchema(schemaPath: string): GraphQLSchema {
   try {
     let source = fs.readFileSync(schemaPath, 'utf8');
+    if (path.extname(schemaPath) === '.json') {
+      source = printSchema(buildClientSchema(JSON.parse(source).data));
+    }
     source = `
   directive @include(if: Boolean) on FRAGMENT | FIELD
   directive @skip(if: Boolean) on FRAGMENT | FIELD
@@ -132,8 +141,8 @@ function getSchema(schemaPath: string): GraphQLSchema {
     return buildASTSchema(parse(source));
   } catch (error) {
     throw new Error(`
-Error loading schema. Expected the schema to be a .graphql file using the
-GraphQL schema definition language. Error detail:
+Error loading schema. Expected the schema to be a .graphql or a .json
+file, describing your GraphQL server's API. Error detail:
 
 ${error.stack}
     `.trim());
@@ -162,7 +171,7 @@ const argv = yargs
     '$0 --schema <path> --src <path> [--watch]')
   .options({
     'schema': {
-      describe: 'Path to schema.graphql',
+      describe: 'Path to schema.graphql or schema.json',
       demandOption: true,
       type: 'string',
     },

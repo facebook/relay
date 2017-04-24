d8f977fa1a1b330dfbdb7b1bacfc374eabe38d35
Jan Kassens <jkassens@fb.com>
2017-04-24 12:45:31 -0700

    change RelayRelayDirectiveTransform to IRTransform
    Reviewed By: leebyron
    
    Differential Revision: D4931002
    
    fbshipit-source-id: 54b8b3688d7e6663bab472b52f88989e063f60a2

--- packages/relay-compiler/transforms/RelayRelayDirectiveTransform.js
+++ packages/relay-compiler/transforms/RelayRelayDirectiveTransform.js
@@ -14,11 +14,10 @@
 
 const GraphQL = require('graphql');
 const RelayCompilerContext = require('RelayCompilerContext');
+const RelayIRTransformer = require('RelayIRTransformer');
 
 const getRelayLiteralArgumentValues = require('getRelayLiteralArgumentValues');
 const invariant = require('invariant');
-
-const {visit} = require('RelayIRVisitor');
 
 import type {Fragment} from 'RelayIR';
 import type {GraphQLSchema} from 'graphql';
@@ -30,9 +29,10 @@ function transformSchema(schema: GraphQLSchema): GraphQLSchema {
   if (schema.getDirectives().find(directive => directive.name === RELAY)) {
     return schema;
   }
-  return GraphQL.extendSchema(schema, GraphQL.parse(
-    'directive @relay(plural: Boolean) on FRAGMENT'
-  ));
+  return GraphQL.extendSchema(
+    schema,
+    GraphQL.parse('directive @relay(plural: Boolean) on FRAGMENT'),
+  );
 }
 
 /**
@@ -40,15 +40,13 @@ function transformSchema(schema: GraphQLSchema): GraphQLSchema {
  * them to metadata that can be accessed at runtime.
  */
 function transform(context: RelayCompilerContext): RelayCompilerContext {
-  /* $FlowFixMe(>=0.44.0 site=react_native_fb) Flow error found while deploying
-   * v0.44.0. Remove this comment to see the error */
-  return context.documents().reduce((ctx, node) => {
-    /* $FlowFixMe(>=0.44.0 site=react_native_fb) Flow error found while
-     * deploying v0.44.0. Remove this comment to see the error */
-    return ctx.add(visit(node, {
+  return RelayIRTransformer.transform(
+    context,
+    {
       Fragment: visitFragment,
-    }));
-  }, new RelayCompilerContext(context.schema));
+    },
+    () => ({}), // empty state
+  );
 }
 
 function visitFragment(fragment: Fragment): Fragment {
@@ -60,13 +58,15 @@ function visitFragment(fragment: Fragment): Fragment {
   invariant(
     plural === undefined || typeof plural === 'boolean',
     'RelayRelayDirectiveTransform: Expected the %s argument to @%s to be ' +
-    'a boolean literal or not specified.',
+      'a boolean literal or not specified.',
     PLURAL,
     RELAY,
   );
   return {
     ...fragment,
-    directives: fragment.directives.filter(directive => directive !== relayDirective),
+    directives: fragment.directives.filter(
+      directive => directive !== relayDirective,
+    ),
     metadata: {
       ...(fragment.metadata || {}),
       plural,

1dc0a120950c68ff6966cdb676002469c40c717b
Jimmy Jia <tesrin@gmail.com>
2017-06-03 01:42:15 -0400

    Make graphiQLPrinter optional

--- packages/relay-runtime/network/RelayNetworkLogger.js
+++ packages/relay-runtime/network/RelayNetworkLogger.js
@@ -39,7 +39,7 @@ let queryID = 1;
 const RelayNetworkLogger = {
   wrapFetch(
     fetch: FetchFunction,
-    graphiQLPrinter: GraphiQLPrinter,
+    graphiQLPrinter?: GraphiQLPrinter,
   ): FetchFunction {
     return (
       operation: ConcreteBatch,
@@ -55,9 +55,13 @@ const RelayNetworkLogger = {
       console.time && console.time(idName);
 
       const onSettled = (error: ?Error, response: ?QueryPayload): void => {
-        console.groupCollapsed(`%c${idName}`, error ? 'color:red' : '');
+        if (console.groupCollapsed) {
+          console.groupCollapsed(`%c${idName}`, error ? 'color:red' : '');
+        }
         console.timeEnd && console.timeEnd(idName);
-        console.log('GraphiQL:', graphiQLPrinter(operation, variables));
+        if (graphiQLPrinter) {
+          console.log('GraphiQL:', graphiQLPrinter(operation, variables));
+        }
         console.log('Cache Config:', cacheConfig);
         console.log('Variables:', prettyStringify(variables));
         if (error) {
@@ -93,7 +97,7 @@ const RelayNetworkLogger = {
 
   wrapSubscribe(
     subscribe: SubscribeFunction,
-    graphiQLPrinter: GraphiQLPrinter,
+    graphiQLPrinter?: GraphiQLPrinter,
   ): SubscribeFunction {
     return (
       operation: ConcreteBatch,
@@ -111,8 +115,12 @@ const RelayNetworkLogger = {
         response: ?QueryPayload,
         status: ?string,
       ): void => {
-        console.groupCollapsed(`%c${idName}`, error ? 'color:red' : '');
-        console.log('GraphiQL:', graphiQLPrinter(operation, variables));
+        if (console.groupCollapsed) {
+          console.groupCollapsed(`%c${idName}`, error ? 'color:red' : '');
+        }
+        if (graphiQLPrinter) {
+          console.log('GraphiQL:', graphiQLPrinter(operation, variables));
+        }
         console.log('Cache Config:', cacheConfig);
         console.log('Variables:', prettyStringify(variables));
         if (status) {

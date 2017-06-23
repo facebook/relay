cb7680fff2116e27ffa813a1b486afe713f08c89
Jan Kassens <jkassens@fb.com>
2017-06-22 21:09:30 -0700

    Better error reporting
    Summary:
    Introducing a `RelayReporter` interface to abstract logging. For now, we only
    use it for error reporting, but this could potentially be expanded to any
    logging we do.
    This could make it easier to log to a backend system or have a JSON reporter
    that logs in a machine consumable format.
    
    Error have a `location` to allow us to see where the error was thrown more
    easily than going through a stack trace after babelification.
    
    Reviewed By: leebyron
    
    Differential Revision: D5308276
    
    fbshipit-source-id: 693fb79e7c145f2ee42c03a3374a253294fa2353

--- packages/relay-compiler/codegen/RelayCodegenRunner.js
+++ packages/relay-compiler/codegen/RelayCodegenRunner.js
@@ -22,6 +22,7 @@ const {Map: ImmutableMap} = require('immutable');
 import type CodegenDirectory from 'CodegenDirectory';
 import type FileParser from 'FileParser';
 import type {FileFilter, WatchmanExpression} from 'RelayCodegenWatcher';
+import type {RelayReporter} from 'RelayReporter';
 import type {DocumentNode, GraphQLSchema} from 'graphql';
 
 /* eslint-disable no-console-disallow */
@@ -68,17 +69,20 @@ class RelayCodegenRunner {
   parsers: Parsers = {};
   // parser => writers that are affected by it
   parserWriters: {[parser: string]: Set<string>};
+  _reporter: RelayReporter;
 
   constructor(options: {
     parserConfigs: ParserConfigs,
     writerConfigs: WriterConfigs,
     onlyValidate: boolean,
+    reporter: RelayReporter,
     skipPersist: boolean,
   }) {
     this.parserConfigs = options.parserConfigs;
     this.writerConfigs = options.writerConfigs;
     this.onlyValidate = options.onlyValidate;
     this.skipPersist = options.skipPersist;
+    this._reporter = options.reporter;
 
     this.parserWriters = {};
     for (const parser in options.parserConfigs) {
@@ -101,7 +105,13 @@ class RelayCodegenRunner {
     // reset the parsers
     this.parsers = {};
     for (const parserName in this.parserConfigs) {
-      await this.parseEverything(parserName);
+      try {
+        await this.parseEverything(parserName);
+      } catch (e) {
+        this._reporter.reportError('RelayCodegenRunner.compileAll', e);
+        // Return true so validation reports a non-clean status
+        return true;
+      }
     }
 
     for (const writerName in this.writerConfigs) {
@@ -156,76 +166,69 @@ class RelayCodegenRunner {
   // We cannot do incremental writes right now.
   // When we can, this could be writeChanges(writerName, parserName, parsedDefinitions)
   async write(writerName: string): Promise<boolean> {
-    console.log('\nWriting %s', writerName);
-    const tStart = Date.now();
-    const {getWriter, parser, baseParsers} = this.writerConfigs[writerName];
-
-    let baseDocuments = ImmutableMap();
-    if (baseParsers) {
-      baseParsers.forEach(baseParserName => {
-        baseDocuments = baseDocuments.merge(
-          this.parsers[baseParserName].documents(),
-        );
-      });
-    }
-
-    // always create a new writer: we have to write everything anyways
-    const documents = this.parsers[parser].documents();
-    const schema = this.parserConfigs[parser].getSchema();
-    const writer = getWriter(
-      this.onlyValidate,
-      schema,
-      documents,
-      baseDocuments,
-    );
-
-    let outputDirectories = null;
-
     try {
-      outputDirectories = await writer.writeAll();
-    } catch (e) {
-      if (e.isRelayUserError) {
-        // TODO could use chalk here for red output
-        console.log('Error: ' + e.message);
-      } else {
-        throw e;
+      console.log('\nWriting %s', writerName);
+      const tStart = Date.now();
+      const {getWriter, parser, baseParsers} = this.writerConfigs[writerName];
+
+      let baseDocuments = ImmutableMap();
+      if (baseParsers) {
+        baseParsers.forEach(baseParserName => {
+          baseDocuments = baseDocuments.merge(
+            this.parsers[baseParserName].documents(),
+          );
+        });
       }
+
+      // always create a new writer: we have to write everything anyways
+      const documents = this.parsers[parser].documents();
+      const schema = this.parserConfigs[parser].getSchema();
+      const writer = getWriter(
+        this.onlyValidate,
+        schema,
+        documents,
+        baseDocuments,
+      );
+
+      const outputDirectories = await writer.writeAll();
+
+      const tWritten = Date.now();
+
+      function combineChanges(accessor) {
+        const combined = [];
+        invariant(
+          outputDirectories,
+          'RelayCodegenRunner: Expected outputDirectories to be set',
+        );
+        for (const dir of outputDirectories.values()) {
+          combined.push(...accessor(dir.changes));
+        }
+        return combined;
+      }
+      const created = combineChanges(_ => _.created);
+      const updated = combineChanges(_ => _.updated);
+      const deleted = combineChanges(_ => _.deleted);
+      const unchanged = combineChanges(_ => _.unchanged);
+
+      if (this.onlyValidate) {
+        printFiles('Missing', created);
+        printFiles('Out of date', updated);
+        printFiles('Extra', deleted);
+      } else {
+        printFiles('Created', created);
+        printFiles('Updated', updated);
+        printFiles('Deleted', deleted);
+        console.log('Unchanged: %s files', unchanged.length);
+      }
+
+      console.log('Written %s in %s', writerName, toSeconds(tStart, tWritten));
+
+      const hasChanges = created.length + updated.length + deleted.length > 0;
+      return hasChanges;
+    } catch (e) {
+      this._reporter.reportError('RelayCodegenRunner.write', e);
       return true;
     }
-
-    const tWritten = Date.now();
-
-    function combineChanges(accessor) {
-      const combined = [];
-      invariant(
-        outputDirectories,
-        'RelayCodegenRunner: Expected outputDirectories to be set',
-      );
-      for (const dir of outputDirectories.values()) {
-        combined.push(...accessor(dir.changes));
-      }
-      return combined;
-    }
-    const created = combineChanges(_ => _.created);
-    const updated = combineChanges(_ => _.updated);
-    const deleted = combineChanges(_ => _.deleted);
-    const unchanged = combineChanges(_ => _.unchanged);
-
-    if (this.onlyValidate) {
-      printFiles('Missing', created);
-      printFiles('Out of date', updated);
-      printFiles('Extra', deleted);
-    } else {
-      printFiles('Created', created);
-      printFiles('Updated', updated);
-      printFiles('Deleted', deleted);
-      console.log('Unchanged: %s files', unchanged.length);
-    }
-
-    console.log('Written %s in %s', writerName, toSeconds(tStart, tWritten));
-
-    const hasChanges = created.length + updated.length + deleted.length > 0;
-    return hasChanges;
   }
 
   async watchAll(): Promise<void> {
@@ -275,7 +278,7 @@ class RelayCodegenRunner {
           }
           await Promise.all(dependentWriters.map(writer => this.write(writer)));
         } catch (error) {
-          console.log('Error: ' + error);
+          this._reporter.reportError('RelayCodegenRunner.watch', error);
         }
         console.log('Watching for changes to %s...', parserName);
       },

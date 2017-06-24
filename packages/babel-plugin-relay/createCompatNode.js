1e1ab800beaaef761878abc1f918cbece5d80224
Lee Byron <lee@leebyron.com>
2017-06-23 18:18:07 -0700

    Add flow types to babel-plugin-relay
    Summary: Closes https://github.com/facebook/relay/pull/1914
    
    Reviewed By: kassens
    
    Differential Revision: D5315168
    
    Pulled By: leebyron
    
    fbshipit-source-id: 24b66c4a9efbe41694f60d15c14e19246b9825ef

--- packages/babel-plugin-relay/createCompatNode.js
+++ packages/babel-plugin-relay/createCompatNode.js
@@ -7,16 +7,23 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  *
  * @providesModule createCompatNode
+ * @flow
  * @format
  */
 
 'use strict';
 
+import typeof BabelTypes from 'babel-types';
+
 /**
  * Relay Compat transforms graphql definitions into objects with `modern` and
  * `classic` keys, each containing the resulting transforms.
  */
-function createCompatNode(t, modernNode, classicNode) {
+function createCompatNode(
+  t: BabelTypes,
+  modernNode: Object,
+  classicNode: Object,
+): Object {
   return t.objectExpression([
     t.objectProperty(t.identifier('modern'), modernNode),
     t.objectProperty(t.identifier('classic'), classicNode),

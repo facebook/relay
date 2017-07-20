68241f7c9126b6c44646ffb078df63c2e3810d43
Nat Mote <nmote@fb.com>
2017-07-20 14:37:04 -0700

    Upgrade to Flow v0.50.0
    Reviewed By: gabelevi
    
    Differential Revision: D5438335
    
    fbshipit-source-id: 7a96f68e7147e984c6f0cb84f957e639d36ca6b3

--- packages/relay-compiler/generic/core/RelaySchemaUtils.js
+++ packages/relay-compiler/generic/core/RelaySchemaUtils.js
@@ -53,6 +53,9 @@ type GraphQLSingularType =
   | GraphQLUnionType
   | GraphQLEnumType
   | GraphQLInputObjectType
+  /* $FlowFixMe(>=0.50.0 site=react_native_fb) This comment suppresses an error
+   * found when Flow v0.50 was deployed. To see the error delete this comment
+   * and run Flow. */
   | GraphQLNullableType<*>;
 
 /**

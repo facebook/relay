facebook/relay

commit 68241f7c9126b6c44646ffb078df63c2e3810d43

Upgrade to Flow v0.50.0
Reviewed By: gabelevi

Differential Revision: D5438335

fbshipit-source-id: 7a96f68e7147e984c6f0cb84f957e639d36ca6b3

packages/relay-compiler/generic/core/RelaySchemaUtils.js
@@ -53,6 +53,9 @@ type GraphQLSingularType =
   | GraphQLUnionType
   | GraphQLEnumType
   | GraphQLInputObjectType
+  /* $FlowFixMe(>=0.50.0 site=react_native_fb) This comment suppresses an error
+   * found when Flow v0.50 was deployed. To see the error delete this comment
+   * and run Flow. */
   | GraphQLNullableType<*>;
 
 /**

.flowconfig
@@ -31,4 +31,4 @@ suppress_comment=\\(.\\|\n\\)*\\$FlowFixedInNextDeploy
 suppress_comment=\\(.\\|\n\\)*\\$FlowExpectedError
 
 [version]
-^0.49.1
+^0.50.0

packages/react-relay/classic/mutation/RelayMutationQueue.js
@@ -312,7 +312,13 @@ class RelayMutationQueue {
     this._storeData.getNetworkLayer().sendMutation(request);
 
     request.done(
+      /* $FlowFixMe(>=0.50.0 site=react_native_fb) This comment suppresses an
+       * error found when Flow v0.50 was deployed. To see the error delete this
+       * comment and run Flow. */
       result => this._handleCommitSuccess(transaction, result.response),
+      /* $FlowFixMe(>=0.50.0 site=react_native_fb) This comment suppresses an
+       * error found when Flow v0.50 was deployed. To see the error delete this
+       * comment and run Flow. */
       error => this._handleCommitFailure(transaction, error),
     );
   }

packages/react-relay/classic/store/RelayEnvironment.js
@@ -350,9 +350,9 @@ class RelayEnvironment implements Environment, RelayEnvironmentInterface {
     );
     const request = new RelayQueryRequest(query);
     request.then(
-      /* $FlowFixMe(>=0.50.0 site=www,mobile) This comment suppresses an
-       * error found when Flow v0.50 was deployed. To see the error delete
-       * this comment and run Flow. */
+      /* $FlowFixMe(>=0.50.0 site=www,mobile,react_native_fb) This comment
+       * suppresses an error found when Flow v0.50 was deployed. To see the
+       * error delete this comment and run Flow. */
       payload => {
         if (isDisposed) {
           return;
@@ -368,9 +368,9 @@ class RelayEnvironment implements Environment, RelayEnvironmentInterface {
         onNext && onNext(operation.root);
         onCompleted && onCompleted();
       },
-      /* $FlowFixMe(>=0.50.0 site=www,mobile) This comment suppresses an
-       * error found when Flow v0.50 was deployed. To see the error delete
-       * this comment and run Flow. */
+      /* $FlowFixMe(>=0.50.0 site=www,mobile,react_native_fb) This comment
+       * suppresses an error found when Flow v0.50 was deployed. To see the
+       * error delete this comment and run Flow. */
       error => {
         if (isDisposed) {
           return;

commit d8693336c41803733ddf8bb53be564973fa25ab2

Upgrade fbjs and flow-bin and fix type errors
Summary:
We've been failing travis builds since 68241f7c9126b6c44646ffb078df63c2e3810d43 due to a bunch of flow errors. I'm making this PR so we can fix those errors :)
Closes https://github.com/facebook/relay/pull/1993

Reviewed By: leebyron

Differential Revision: D5497054

Pulled By: pranaygp

fbshipit-source-id: fc9421ef05d032beb440fbefc619d065d967cd46

packages/react-relay/classic/network/RelayNetworkLayer.js
@@ -173,10 +173,6 @@ function profileQueue(currentQueue: Array<RelayQueryRequest>): void {
         firstResultProfiler = null;
       }
     };
-    /* $FlowFixMe(site=oss) - Flow now prevents you from calling a
-     * function with more arguments than it expects. This comment suppresses an
-     * error that was noticed when we made this change. Delete this comment to
-     * see the error. */
     query.done(onSettle, onSettle);
   });
 }

packages/relay-compiler/generic/core/RelaySchemaUtils.js
@@ -37,7 +37,7 @@ import type {
   GraphQLEnumType,
   GraphQLInputObjectType,
   GraphQLNamedType,
-  GraphQLNullableType,
+  GraphQLNonNull,
   GraphQLScalarType,
   GraphQLType,
   TypeNode,
@@ -53,10 +53,7 @@ type GraphQLSingularType =
   | GraphQLUnionType
   | GraphQLEnumType
   | GraphQLInputObjectType
-  /* $FlowFixMe(>=0.50.0 site=react_native_fb) This comment suppresses an error
-   * found when Flow v0.50 was deployed. To see the error delete this comment
-   * and run Flow. */
-  | GraphQLNullableType<*>;
+  | GraphQLNonNull<*>;
 
 /**
  * Determine if the given type may implement the named type:

packages/relay-runtime/network/RelayNetwork.js
@@ -122,7 +122,7 @@ function create(fetch: FetchFunction, subscribe?: SubscribeFunction): Network {
 
     const requestResponse = fetch(operation, variables, cacheConfig);
     if (isPromise(requestResponse)) {
-      requestResponse.then(onRequestSuccess, onRequestError).done();
+      requestResponse.then(onRequestSuccess, onRequestError).catch(rethrow);
     } else if (requestResponse instanceof Error) {
       onRequestError(requestResponse);
     } else {

packages/react-relay/classic/container/__tests__/RelayContainer-test.js
@@ -16,6 +16,7 @@ jest.enableAutomock();
 
 require('configureForRelayOSS');
 
+jest.unmock('create-react-class');
 jest.unmock('RelayContainerComparators').mock('warning');
 
 const GraphQLStoreQueryResolver = require('GraphQLStoreQueryResolver');
@@ -1173,7 +1174,6 @@ describe('RelayContainer', function() {
     };
     render = jest.genMockFunction().mockImplementation(() => <div />);
     const shouldComponentUpdate = jest.fn(() => true);
-
     const MockAlwaysUpdateComponent = Relay.createContainer(
       createReactClass({
         displayName: 'MockAlwaysUpdateComponent',
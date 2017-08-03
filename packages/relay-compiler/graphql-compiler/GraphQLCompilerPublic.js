e6985bb63257be777f4d34bee3765fab31ea873e
Yan Jiang <yanj1213@fb.com>
2017-08-02 17:32:05 -0700

    Add remaining modules to graphql-compiler package
    Reviewed By: wincent
    
    Differential Revision: D5549412
    
    fbshipit-source-id: a7d1215751b9cf637a3fe2790d82f11ead95ee02

--- packages/relay-compiler/graphql-compiler/GraphQLCompilerPublic.js
+++ packages/relay-compiler/graphql-compiler/GraphQLCompilerPublic.js
@@ -13,23 +13,94 @@
 
 'use strict';
 
+const ASTConvert = require('ASTConvert');
+const AutoAliasTransform = require('AutoAliasTransform');
 const CodegenDirectory = require('CodegenDirectory');
 const CodegenRunner = require('RelayCodegenRunner');
 const FileParser = require('FileParser');
+const FilterDirectivesTransform = require('FilterDirectivesTransform');
 const GraphQLFileParser = require('GraphQLFileParser');
+const GraphQLIRTransforms = require('GraphQLIRTransforms');
 const GraphQLTextParser = require('GraphQLTextParser');
+const GraphQLValidator = require('GraphQLValidator');
+const RelayCompiler = require('RelayCompiler');
+const RelayCompilerContext = require('RelayCompilerContext');
 const RelayConsoleReporter = require('RelayConsoleReporter');
+const RelayFlattenTransform = require('RelayFlattenTransform');
+const RelayIRTransformer = require('RelayIRTransformer');
+const RelayIRVisitor = require('RelayIRVisitor');
 const RelayMultiReporter = require('RelayMultiReporter');
+const RelayParser = require('RelayParser');
+const RelayPrinter = require('RelayPrinter');
+const SkipClientFieldTransform = require('SkipClientFieldTransform');
+const SkipRedundantNodesTransform = require('SkipRedundantNodesTransform');
+const SkipUnreachableNodeTransform = require('SkipUnreachableNodeTransform');
+const StripUnusedVariablesTransform = require('StripUnusedVariablesTransform');
 
+const filterContextForNode = require('filterContextForNode');
+const getIdentifierForRelayArgumentValue = require('getIdentifierForRelayArgumentValue');
+
+export type {
+  CompiledNode,
+  CompiledDocumentMap,
+  CompilerTransforms,
+} from 'RelayCompiler';
 export type {File, FileWriterInterface, CompileResult} from 'RelayCodegenTypes';
 export type {FileFilter, WatchmanExpression} from 'RelayCodegenWatcher';
+export type {IRTransform} from 'GraphQLIRTransforms';
+export type {FlattenOptions} from 'RelayFlattenTransform';
+export type {
+  Argument,
+  ArgumentDefinition,
+  ArgumentValue,
+  Condition,
+  Directive,
+  Field,
+  Fragment,
+  FragmentSpread,
+  IR,
+  RootArgumentDefinition,
+  InlineFragment,
+  Handle,
+  LinkedField,
+  ListValue,
+  Literal,
+  LocalArgumentDefinition,
+  Node,
+  ObjectFieldValue,
+  ObjectValue,
+  Root,
+  ScalarFieldType,
+  ScalarField,
+  Selection,
+  Variable,
+} from 'RelayIR';
 
 module.exports = {
+  ASTConvert: ASTConvert,
   CodegenDirectory: CodegenDirectory,
   CodegenRunner: CodegenRunner,
+  Compiler: RelayCompiler,
+  CompilerContext: RelayCompilerContext,
   ConsoleReporter: RelayConsoleReporter,
   FileParser: FileParser,
+  filterContextForNode: filterContextForNode,
   GraphQLFileParser: GraphQLFileParser,
+  GraphQLIRTransforms: GraphQLIRTransforms,
+  getIdentifierForRelayArgumentValue: getIdentifierForRelayArgumentValue,
   GraphQLTextParser: GraphQLTextParser,
+  GraphQLValidator: GraphQLValidator,
+  IRTransformer: RelayIRTransformer,
+  IRVisitor: RelayIRVisitor,
   MultiReporter: RelayMultiReporter,
+  RelayParser: RelayParser,
+  Printer: RelayPrinter,
+
+  AutoAliasTransform: AutoAliasTransform,
+  FilterDirectivesTransform: FilterDirectivesTransform,
+  FlattenTransform: RelayFlattenTransform,
+  SkipClientFieldTransform: SkipClientFieldTransform,
+  SkipRedundantNodesTransform: SkipRedundantNodesTransform,
+  SkipUnreachableNodeTransform: SkipUnreachableNodeTransform,
+  StripUnusedVariablesTransform: StripUnusedVariablesTransform,
 };

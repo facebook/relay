43d3cf44e35d0e5938c6899fa662e359835a95cc
Daniel Woelfel <dww@fb.com>
2017-08-28 15:41:35 -0700

    update logger to work with multiple payloads better
    Reviewed By: leebyron
    
    Differential Revision: D5708303
    
    fbshipit-source-id: 21ebad071bb54769e5558d8649513460c57b2524

--- packages/relay-runtime/network/createRelayNetworkLogger.js
+++ packages/relay-runtime/network/createRelayNetworkLogger.js
@@ -73,66 +73,74 @@ function wrapObserve(
   graphiQLPrinter: ?GraphiQLPrinter,
 ): ObserveFunction {
   return (operation, variables, cacheConfig, uploadables) => {
-    function createTransaction() {
-      return new LoggerTransaction({
-        operation,
-        variables,
-        cacheConfig,
-        uploadables,
-      });
-    }
+    const transaction = new LoggerTransaction({
+      operation,
+      variables,
+      cacheConfig,
+      uploadables,
+    });
 
-    function log(loggerTransaction, error, response, status) {
+    function addLogs(error, response, status) {
       if (graphiQLPrinter) {
-        loggerTransaction.addLog(
-          'GraphiQL',
-          graphiQLPrinter(operation, variables),
-        );
+        transaction.addLog('GraphiQL', graphiQLPrinter(operation, variables));
       }
-      loggerTransaction.addLog('Cache Config', cacheConfig);
-      loggerTransaction.addLog('Variables', prettyStringify(variables));
+      transaction.addLog('Cache Config', cacheConfig);
+      transaction.addLog('Variables', prettyStringify(variables));
       if (status) {
-        loggerTransaction.addLog('Status', status);
+        transaction.addLog('Status', status);
       }
       if (error) {
-        loggerTransaction.addLog('Error', error);
+        transaction.addLog('Error', error);
       }
       if (response) {
-        loggerTransaction.addLog('Response', response);
+        transaction.addLog('Response', response);
       }
-      loggerTransaction.commitLogs(error, response, status);
+    }
+
+    function flushLogs(error, response, status) {
+      addLogs(error, response, status);
+      transaction.flushLogs(error, response, status);
+    }
+
+    function commitLogs(error, response, status) {
+      addLogs(error, response, status);
+      transaction.commitLogs(error, response, status);
     }
 
     const observable = observe(operation, variables, cacheConfig, uploadables);
 
-    // Create a new transaction for every event for subscriptions.
-    if (operation.query.operation === 'subscription') {
-      return observable.do({
-        start: () =>
-          log(createTransaction(), null, null, 'subscription is sent'),
-        next: payload =>
-          log(
-            createTransaction(),
-            null,
-            payload,
-            'subscription receives update',
-          ),
-        error: error => log(createTransaction(), error, null),
-        complete: () =>
-          log(createTransaction(), null, null, 'subscription was closed.'),
-        unsubscribe: () =>
-          log(createTransaction(), null, null, 'subscription is unsubscribed.'),
-      });
-    } else {
-      const transaction = createTransaction();
-      return observable.do({
-        start: () => {
-          console.time && console.time(transaction.getIdentifier());
-        },
-        next: payload => log(transaction, null, payload),
-        error: error => log(transaction, error, null),
-      });
-    }
+    const isSubscription = operation.query.operation === 'subscription';
+
+    return observable.do({
+      start: () => {
+        console.time && console.time(transaction.getIdentifier());
+        if (isSubscription) {
+          flushLogs(null, null, 'subscription is sent.');
+        }
+      },
+      next: payload => {
+        flushLogs(null, payload);
+        console.time && console.time(transaction.getIdentifier());
+      },
+      error: error => commitLogs(error, null, null),
+      complete: () => {
+        if (isSubscription) {
+          commitLogs(null, null, 'subscription was closed.');
+        } else {
+          // the last `next` already flushed the logs, just mark as committed
+          // without spamming the logs
+          transaction.markCommitted();
+        }
+      },
+      unsubscribe: () =>
+        commitLogs(
+          null,
+          null,
+          isSubscription
+            ? 'subscription is unsubscribed.'
+            : 'observe is unsubscribed.',
+        ),
+    });
   };
 }
 

e77923465ab223ce43682bcae172042c4e2c35eb
Greg Hurrell <glh@fb.com>
2017-08-29 10:18:59 -0700

    Drop `isRelayUserError`
    Reviewed By: leebyron
    
    Differential Revision: D5722597
    
    fbshipit-source-id: cb51ae61f242bf2471abe5e82e66d9e47a2bac18

--- packages/relay-compiler/graphql-compiler/core/GraphQLCompilerUserError.js
+++ packages/relay-compiler/graphql-compiler/core/GraphQLCompilerUserError.js
@@ -16,9 +16,7 @@
 const createUserError = (format: string, ...args: any): Error => {
   let index = 0;
   const formatted = format.replace(/%s/g, match => args[index++]);
-  const err = new Error(formatted);
-  (err: any).isRelayUserError = true;
-  return err;
+  return new Error(formatted);
 };
 
 module.exports = {createUserError};

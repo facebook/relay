a2b5c2ebd441f0aef03ddadf2e8907ed9a470cd4
Lee Byron <leebyron@fb.com>
2017-11-16 13:06:44 -0800

    Rewrite FlattenTransform
    Reviewed By: kassens
    
    Differential Revision: D6296372
    
    fbshipit-source-id: 71598dba1b5ead9198ec41d8da5458b0d0c5ead2

--- packages/relay-compiler/graphql-compiler/transforms/FlattenTransform.js
+++ packages/relay-compiler/graphql-compiler/transforms/FlattenTransform.js
@@ -6,6 +6,7 @@
  *
  * @providesModule FlattenTransform
  * @format
+ * @flow
  */
 
 'use strict';
@@ -18,13 +19,13 @@ const areEqual = require('../util/areEqualOSS');
 const getIdentifierForSelection = require('../core/getIdentifierForSelection');
 const invariant = require('invariant');
 const isEquivalentType = require('../core/isEquivalentType');
-const stableJSONStringify = require('../util/stableJSONStringifyOSS');
 
 const {createUserError} = require('../core/GraphQLCompilerUserError');
 const {printField} = require('../core/GraphQLIRPrinter');
 const {GraphQLNonNull, GraphQLList} = require('graphql');
 
 import type {
+  Condition,
   Field,
   Fragment,
   Handle,
@@ -32,6 +33,7 @@ import type {
   Node,
   Root,
   ScalarField,
+  LinkedField,
   Selection,
 } from '../core/GraphQLIR';
 import type {GraphQLType} from 'graphql';
@@ -42,12 +44,14 @@ export type FlattenOptions = {
   flattenAbstractTypes?: boolean,
   flattenInlineFragments?: boolean,
 };
-type FlattenState = {
-  kind: 'FlattenState',
-  node: Node,
-  selections: {[key: string]: FlattenState | ScalarField},
-  type: Type,
+
+type State = {
+  flattenAbstractTypes: boolean,
+  flattenInlineFragments: boolean,
+  parentType: ?GraphQLType,
 };
+
+type HasSelections = Root | Fragment | Condition | InlineFragment | LinkedField;
 
 /**
  * Transform that flattens inline fragments, fragment spreads, and conditionals.
@@ -62,155 +66,169 @@ function flattenTransformImpl(
   context: GraphQLCompilerContext,
   options?: FlattenOptions,
 ): GraphQLCompilerContext {
-  const flattenOptions = {
+  const state = {
     flattenAbstractTypes: !!(options && options.flattenAbstractTypes),
     flattenInlineFragments: !!(options && options.flattenInlineFragments),
+    parentType: null,
   };
 
-  return GraphQLIRTransformer.transform(context, {
-    Root: root => flattenNode(root, context, flattenOptions),
-    Fragment: fragment => flattenNode(fragment, context, flattenOptions),
-  });
-}
-
-function flattenNode(
-  node: Root | Fragment,
-  context: GraphQLCompilerContext,
-  options: FlattenOptions,
-): Root | Fragment {
-  const state = {
-    kind: 'FlattenState',
-    node,
-    selections: {},
-    type: node.type,
-  };
-  visitNode(context, options, state, node);
-  const flattenedNode = buildNode(state);
-  invariant(
-    flattenedNode.kind === 'Root' || flattenedNode.kind === 'Fragment',
-    'FlattenTransform: Expected Root `%s` to flatten back to a Root ' +
-      ' or Fragment.',
-    node.name,
+  return GraphQLIRTransformer.transform(
+    context,
+    {
+      Root: flattenSelections,
+      Fragment: flattenSelections,
+      Condition: flattenSelections,
+      InlineFragment: flattenSelections,
+      LinkedField: flattenSelections,
+    },
+    () => state,
   );
-  return flattenedNode;
-}
-
-function buildNode(state: FlattenState): Root | Selection {
-  return {
-    ...state.node,
-    selections: Object.keys(state.selections).map(key => {
-      const selectionState = state.selections[key];
-      if (
-        selectionState.kind === 'FragmentSpread' ||
-        selectionState.kind === 'ScalarField'
-      ) {
-        return selectionState;
-      } else if (selectionState.kind === 'FlattenState') {
-        const node = buildNode(selectionState);
-        invariant(
-          node.kind !== 'Root' && node.kind !== 'Fragment',
-          'FlattenTransform: got a `%s`, expected a selection.',
-          node.kind,
-        );
-        return node;
-      } else {
-        invariant(
-          false,
-          'FlattenTransform: Unexpected kind `%s`.',
-          selectionState.kind,
-        );
-      }
-    }),
-  };
 }
 
 /**
  * @internal
  */
-function visitNode(
-  context: GraphQLCompilerContext,
-  options: FlattenOptions,
-  state: FlattenState,
-  node: Node,
-): void {
-  node.selections.forEach(selection => {
-    if (
-      selection.kind === 'InlineFragment' &&
-      shouldFlattenInlineFragment(selection, options, state)
-    ) {
-      visitNode(context, options, state, selection);
-      return;
-    }
-    const nodeIdentifier = getIdentifierForSelection(selection);
-    if (selection.kind === 'Condition' || selection.kind === 'InlineFragment') {
-      let selectionState = state.selections[nodeIdentifier];
-      if (!selectionState) {
-        selectionState = state.selections[nodeIdentifier] = {
-          kind: 'FlattenState',
-          node: selection,
-          selections: {},
-          type:
-            selection.kind === 'InlineFragment'
-              ? selection.typeCondition
-              : state.type,
-        };
-      }
-      visitNode(context, options, selectionState, selection);
-    } else if (selection.kind === 'FragmentSpread') {
-      state.selections[nodeIdentifier] = selection;
-    } else if (selection.kind === 'LinkedField') {
-      let selectionState = state.selections[nodeIdentifier];
-      if (!selectionState) {
-        selectionState = state.selections[nodeIdentifier] = {
-          kind: 'FlattenState',
-          node: selection,
-          selections: {},
-          type: selection.type,
-        };
-      } else {
-        invariant(
-          selectionState.node.kind === 'LinkedField' ||
-            selectionState.node.kind === 'ScalarField',
-          'FlattenTransform: Expected a Field, got %s.',
-          selectionState.node.kind,
-        );
-        const prevField: Field = selectionState.node;
-        assertUniqueArgsForAlias(selection, prevField);
-        // merge fields
-        const handles = dedupe(prevField.handles, selection.handles);
-        selectionState.node = {
-          ...selection,
-          handles,
-        };
-      }
-      visitNode(context, options, selectionState, selection);
-    } else if (selection.kind === 'ScalarField') {
-      let field: ScalarField = selection;
-      const prevSelection = state.selections[nodeIdentifier];
-      if (
-        prevSelection &&
-        (prevSelection.kind === 'ScalarField' ||
-          prevSelection.kind === 'LinkedField')
-      ) {
-        const prevField: Field = prevSelection;
-        assertUniqueArgsForAlias(field, prevField);
-        if (field.handles || prevField.handles) {
-          const handles = dedupe(field.handles, prevField.handles);
-          field = {
-            ...selection,
-            handles,
-          };
-        }
-      }
-      state.selections[nodeIdentifier] = field;
-    } else {
-      invariant(false, 'FlattenTransform: Unknown kind `%s`.', selection.kind);
-    }
-  });
+function flattenSelections<T: HasSelections>(node: T, state: State): T {
+  // Determine the current type.
+  const parentType = state.parentType;
+  const type =
+    node.kind === 'Condition'
+      ? parentType
+      : node.kind === 'InlineFragment' ? node.typeCondition : node.type;
+  invariant(type, 'FlattenTransform: Expected parent type.');
+
+  // Flatten the selections in this node, creating a new node with flattened
+  // selections if possible, then deeply traverse the flattened node, while
+  // keeping track of the parent type.
+  const nextSelections = new Map();
+  const hasFlattened = flattenSelectionsInto(nextSelections, node, state, type);
+  const flattenedNode = hasFlattened
+    ? {...node, selections: Array.from(nextSelections.values())}
+    : node;
+  state.parentType = type;
+  const deeplyFlattenedNode = this.traverse(flattenedNode, state);
+  state.parentType = parentType;
+  return deeplyFlattenedNode;
 }
 
 /**
  * @internal
+ */
+function flattenSelectionsInto(
+  flattenedSelections: Map<string, Selection>,
+  node: HasSelections,
+  state: State,
+  type: GraphQLType,
+): boolean {
+  let hasFlattened = false;
+  node.selections.forEach(selection => {
+    if (
+      selection.kind === 'InlineFragment' &&
+      shouldFlattenInlineFragment(selection, state, type)
+    ) {
+      hasFlattened = true;
+      flattenSelectionsInto(flattenedSelections, selection, state, type);
+      return;
+    }
+    const nodeIdentifier = getIdentifierForSelection(selection);
+    const flattenedSelection = flattenedSelections.get(nodeIdentifier);
+    // If this selection hasn't been seen before, keep track of it.
+    if (!flattenedSelection) {
+      flattenedSelections.set(nodeIdentifier, selection);
+      return;
+    }
+    // Otherwise a similar selection exists which should be merged.
+    hasFlattened = true;
+    if (flattenedSelection.kind === 'InlineFragment') {
+      invariant(
+        selection.kind === 'InlineFragment',
+        'FlattenTransform: Expected a ScalarField, got a %s',
+        selection.kind,
+      );
+      flattenedSelections.set(nodeIdentifier, {
+        ...flattenedSelection,
+        selections: mergeSelections(
+          flattenedSelection,
+          selection,
+          state,
+          selection.typeCondition,
+        ),
+      });
+    } else if (flattenedSelection.kind === 'Condition') {
+      invariant(
+        selection.kind === 'Condition',
+        'FlattenTransform: Expected a Condition, got a %s',
+        selection.kind,
+      );
+      flattenedSelections.set(nodeIdentifier, {
+        ...flattenedSelection,
+        selections: mergeSelections(flattenedSelection, selection, state, type),
+      });
+    } else if (flattenedSelection.kind === 'FragmentSpread') {
+      // Ignore duplicate fragment spreads.
+    } else if (flattenedSelection.kind === 'LinkedField') {
+      invariant(
+        selection.kind === 'LinkedField',
+        'FlattenTransform: Expected a LinkedField, got a %s',
+        selection.kind,
+      );
+      // Note: arguments are intentionally reversed to avoid rebuilds
+      assertUniqueArgsForAlias(selection, flattenedSelection);
+      flattenedSelections.set(nodeIdentifier, {
+        kind: 'LinkedField',
+        ...flattenedSelection,
+        handles: mergeHandles(flattenedSelection, selection),
+        selections: mergeSelections(
+          flattenedSelection,
+          selection,
+          state,
+          selection.type,
+        ),
+      });
+    } else if (flattenedSelection.kind === 'ScalarField') {
+      invariant(
+        selection.kind === 'ScalarField',
+        'FlattenTransform: Expected a ScalarField, got a %s',
+        selection.kind,
+      );
+      // Note: arguments are intentionally reversed to avoid rebuilds
+      assertUniqueArgsForAlias(selection, flattenedSelection);
+      flattenedSelections.set(nodeIdentifier, {
+        kind: 'ScalarField',
+        ...flattenedSelection,
+        // Note: arguments are intentionally reversed to avoid rebuilds
+        handles: mergeHandles(selection, flattenedSelection),
+      });
+    } else {
+      invariant(
+        false,
+        'FlattenTransform: Unknown kind `%s`.',
+        (flattenedSelection.kind: empty),
+      );
+    }
+  });
+  return hasFlattened;
+}
+
+/**
+ * @internal
+ */
+function mergeSelections(
+  nodeA: HasSelections,
+  nodeB: HasSelections,
+  state: State,
+  type: GraphQLType,
+): Array<Selection> {
+  const flattenedSelections = new Map();
+  flattenSelectionsInto(flattenedSelections, nodeA, state, type);
+  flattenSelectionsInto(flattenedSelections, nodeB, state, type);
+  return Array.from(flattenedSelections.values());
+}
+
+/**
+ * @internal
+ * TODO(T19327202) This is redundant with OverlappingFieldsCanBeMergedRule once
+ * it can be enabled.
  */
 function assertUniqueArgsForAlias(field: Field, otherField: Field): void {
   if (!areEqualFields(field, otherField)) {
@@ -229,13 +247,13 @@ function assertUniqueArgsForAlias(field: Field, otherField: Field): void {
  */
 function shouldFlattenInlineFragment(
   fragment: InlineFragment,
-  options: FlattenOptions,
-  state: FlattenState,
+  state: State,
+  type: GraphQLType,
 ): boolean {
   return !!(
-    isEquivalentType(fragment.typeCondition, state.type) ||
-    options.flattenInlineFragments ||
-    (options.flattenAbstractTypes &&
+    state.flattenInlineFragments ||
+    isEquivalentType(fragment.typeCondition, type) ||
+    (state.flattenAbstractTypes &&
       isAbstractType(getRawType(fragment.typeCondition)))
   );
 }
@@ -258,14 +276,20 @@ function areEqualFields(thisField: Field, thatField: Field): boolean {
 /**
  * @internal
  */
-function dedupe(...arrays: Array<?Array<Handle>>): Array<Handle> {
+function mergeHandles<T: LinkedField | ScalarField>(
+  nodeA: T,
+  nodeB: T,
+): ?Array<Handle> {
+  if (!nodeA.handles) {
+    return nodeB.handles;
+  }
+  if (!nodeB.handles) {
+    return nodeA.handles;
+  }
   const uniqueItems = new Map();
-  arrays.forEach(items => {
-    items &&
-      items.forEach(item => {
-        uniqueItems.set(stableJSONStringify(item), item);
-      });
-  });
+  nodeA.handles
+    .concat(nodeB.handles)
+    .forEach(item => uniqueItems.set(item.name + item.key, item));
   return Array.from(uniqueItems.values());
 }
 

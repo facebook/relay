0704cf149705abc7ae724b9b319db0ac0e3c3874
Jan Kassens <jkassens@fb.com>
2017-12-19 20:15:23 -0800

    reduce indentation in setRelayModernMutationConfigs
    Summary:
    Reduces indentation by switching deep ifs into early returns.
    
    One small functional fix is to apply RANGE_DELETE on all found connections by just removing a `break;`.
    
    Reviewed By: devknoll
    
    Differential Revision: D6605813
    
    fbshipit-source-id: 53a679da4857d4441846f0428fd500224932bef8

--- packages/relay-runtime/mutations/setRelayModernMutationConfigs.js
+++ packages/relay-runtime/mutations/setRelayModernMutationConfigs.js
@@ -80,37 +80,35 @@ function nodeDelete(
   config: RelayMutationConfig,
   request: RequestNode,
 ): ?SelectorStoreUpdater {
-  let updater;
   if (config.type !== 'NODE_DELETE') {
-    return;
+    return null;
   }
   const {deletedIDFieldName} = config;
   const rootField = getRootField(request);
-  if (rootField) {
-    updater = (store: RecordSourceSelectorProxy, data: ?SelectorData) => {
-      const payload = store.getRootField(rootField);
-      if (!payload) {
-        return;
-      }
-      const deleteID = payload.getValue(deletedIDFieldName);
-      const deleteIDs = Array.isArray(deleteID) ? deleteID : [deleteID];
-      deleteIDs.forEach(id => {
-        if (id && typeof id === 'string') {
-          store.delete(id);
-        }
-      });
-    };
+  if (!rootField) {
+    return null;
   }
-  return updater;
+  return (store: RecordSourceSelectorProxy, data: ?SelectorData) => {
+    const payload = store.getRootField(rootField);
+    if (!payload) {
+      return;
+    }
+    const deleteID = payload.getValue(deletedIDFieldName);
+    const deleteIDs = Array.isArray(deleteID) ? deleteID : [deleteID];
+    deleteIDs.forEach(id => {
+      if (id && typeof id === 'string') {
+        store.delete(id);
+      }
+    });
+  };
 }
 
 function rangeAdd(
   config: RelayMutationConfig,
   request: RequestNode,
 ): ?SelectorStoreUpdater {
-  let updater;
   if (config.type !== 'RANGE_ADD') {
-    return;
+    return null;
   }
   const {parentID, connectionInfo, edgeName} = config;
   if (!parentID) {
@@ -119,71 +117,72 @@ function rangeAdd(
       'setRelayModernMutationConfigs: For mutation config RANGE_ADD ' +
         'to work you must include a parentID',
     );
-    return;
+    return null;
   }
   const rootField = getRootField(request);
-  if (connectionInfo && rootField) {
-    updater = (store: RecordSourceSelectorProxy, data: ?SelectorData) => {
-      const parent = store.get(parentID);
-      if (parent) {
-        const payload = store.getRootField(rootField);
-        if (!payload) {
-          return;
-        }
-        const serverEdge = payload.getLinkedRecord(edgeName);
-        for (const info of connectionInfo) {
-          if (serverEdge) {
-            const connection = RelayConnectionHandler.getConnection(
-              parent,
-              info.key,
-              info.filters,
-            );
-            if (!connection) {
-              continue;
-            }
-            const clientEdge = RelayConnectionHandler.buildConnectionEdge(
-              store,
-              connection,
-              serverEdge,
-            );
-            if (!clientEdge) {
-              continue;
-            }
-            switch (info.rangeBehavior) {
-              case 'append':
-                RelayConnectionHandler.insertEdgeAfter(connection, clientEdge);
-                break;
-              case 'ignore':
-                // Do nothing
-                break;
-              case 'prepend':
-                RelayConnectionHandler.insertEdgeBefore(connection, clientEdge);
-                break;
-              default:
-                warning(
-                  false,
-                  'setRelayModernMutationConfigs: RANGE_ADD range behavior ' +
-                    `'${
-                      info.rangeBehavior
-                    }' will not work as expected in RelayModern, ` +
-                    "supported range behaviors are 'append', 'prepend', and " +
-                    "'ignore'",
-                );
-                break;
-            }
-          }
-        }
-      }
-    };
+  if (!connectionInfo || !rootField) {
+    return null;
   }
-  return updater;
+  return (store: RecordSourceSelectorProxy, data: ?SelectorData) => {
+    const parent = store.get(parentID);
+    if (!parent) {
+      return;
+    }
+    const payload = store.getRootField(rootField);
+    if (!payload) {
+      return;
+    }
+    const serverEdge = payload.getLinkedRecord(edgeName);
+    for (const info of connectionInfo) {
+      if (!serverEdge) {
+        continue;
+      }
+      const connection = RelayConnectionHandler.getConnection(
+        parent,
+        info.key,
+        info.filters,
+      );
+      if (!connection) {
+        continue;
+      }
+      const clientEdge = RelayConnectionHandler.buildConnectionEdge(
+        store,
+        connection,
+        serverEdge,
+      );
+      if (!clientEdge) {
+        continue;
+      }
+      switch (info.rangeBehavior) {
+        case 'append':
+          RelayConnectionHandler.insertEdgeAfter(connection, clientEdge);
+          break;
+        case 'ignore':
+          // Do nothing
+          break;
+        case 'prepend':
+          RelayConnectionHandler.insertEdgeBefore(connection, clientEdge);
+          break;
+        default:
+          warning(
+            false,
+            'setRelayModernMutationConfigs: RANGE_ADD range behavior ' +
+              `'${
+                info.rangeBehavior
+              }' will not work as expected in RelayModern, ` +
+              "supported range behaviors are 'append', 'prepend', and " +
+              "'ignore'",
+          );
+          break;
+      }
+    }
+  };
 }
 
 function rangeDelete(
   config: RelayMutationConfig,
   request: RequestNode,
 ): ?SelectorStoreUpdater {
-  let updater;
   if (config.type !== 'RANGE_DELETE') {
     return;
   }
@@ -199,65 +198,60 @@ function rangeDelete(
       'setRelayModernMutationConfigs: For mutation config RANGE_DELETE ' +
         'to work you must include a parentID',
     );
-    return;
+    return null;
   }
   const rootField = getRootField(request);
-  if (rootField) {
-    updater = (store: RecordSourceSelectorProxy, data: ?SelectorData) => {
-      if (data) {
-        const deleteIDs = [];
-        let deletedIDField = data[rootField];
-        if (deletedIDField && Array.isArray(deletedIDFieldName)) {
-          for (const eachField of deletedIDFieldName) {
-            if (deletedIDField && typeof deletedIDField === 'object') {
-              deletedIDField = deletedIDField[eachField];
-            }
-          }
-          if (Array.isArray(deletedIDField)) {
-            deletedIDField.forEach(idObject => {
-              if (
-                idObject &&
-                idObject.id &&
-                typeof idObject === 'object' &&
-                typeof idObject.id === 'string'
-              ) {
-                deleteIDs.push(idObject.id);
-              }
-            });
-          } else if (
-            deletedIDField &&
-            deletedIDField.id &&
-            typeof deletedIDField.id === 'string'
-          ) {
-            deleteIDs.push(deletedIDField.id);
-          }
-        } else if (
-          deletedIDField &&
-          typeof deletedIDFieldName === 'string' &&
-          typeof deletedIDField === 'object'
-        ) {
-          deletedIDField = deletedIDField[deletedIDFieldName];
-          if (typeof deletedIDField === 'string') {
-            deleteIDs.push(deletedIDField);
-          } else if (Array.isArray(deletedIDField)) {
-            deletedIDField.forEach(id => {
-              if (typeof id === 'string') {
-                deleteIDs.push(id);
-              }
-            });
-          }
-        }
-        deleteNode(
-          parentID,
-          connectionKeys,
-          pathToConnection,
-          store,
-          deleteIDs,
-        );
-      }
-    };
+  if (!rootField) {
+    return null;
   }
-  return updater;
+  return (store: RecordSourceSelectorProxy, data: ?SelectorData) => {
+    if (!data) {
+      return;
+    }
+    const deleteIDs = [];
+    let deletedIDField = data[rootField];
+    if (deletedIDField && Array.isArray(deletedIDFieldName)) {
+      for (const eachField of deletedIDFieldName) {
+        if (deletedIDField && typeof deletedIDField === 'object') {
+          deletedIDField = deletedIDField[eachField];
+        }
+      }
+      if (Array.isArray(deletedIDField)) {
+        deletedIDField.forEach(idObject => {
+          if (
+            idObject &&
+            idObject.id &&
+            typeof idObject === 'object' &&
+            typeof idObject.id === 'string'
+          ) {
+            deleteIDs.push(idObject.id);
+          }
+        });
+      } else if (
+        deletedIDField &&
+        deletedIDField.id &&
+        typeof deletedIDField.id === 'string'
+      ) {
+        deleteIDs.push(deletedIDField.id);
+      }
+    } else if (
+      deletedIDField &&
+      typeof deletedIDFieldName === 'string' &&
+      typeof deletedIDField === 'object'
+    ) {
+      deletedIDField = deletedIDField[deletedIDFieldName];
+      if (typeof deletedIDField === 'string') {
+        deleteIDs.push(deletedIDField);
+      } else if (Array.isArray(deletedIDField)) {
+        deletedIDField.forEach(id => {
+          if (typeof id === 'string') {
+            deleteIDs.push(id);
+          }
+        });
+      }
+    }
+    deleteNode(parentID, connectionKeys, pathToConnection, store, deleteIDs);
+  };
 }
 
 function deleteNode(
@@ -279,57 +273,55 @@ function deleteNode(
   if (!parent) {
     return;
   }
-  if (pathToConnection.length >= 2) {
-    let recordProxy = parent;
-    for (let i = 1; i < pathToConnection.length - 1; i++) {
-      if (recordProxy) {
-        recordProxy = recordProxy.getLinkedRecord(pathToConnection[i]);
-      }
-    }
-    // Should never enter loop except edge cases
-    if (connectionKeys && recordProxy) {
-      for (const key of connectionKeys) {
-        const connection = RelayConnectionHandler.getConnection(
-          recordProxy,
-          key.key,
-          key.filters,
-        );
-        if (connection) {
-          deleteIDs.forEach(deleteID => {
-            RelayConnectionHandler.deleteNode(connection, deleteID);
-          });
-          break;
-        }
-      }
-    } else {
-      warning(
-        false,
-        'setRelayModernMutationConfigs: RANGE_DELETE ' +
-          'pathToConnection is incorrect. Unable to find connection with ' +
-          'parentID: %s and path: %s',
-        parentID,
-        pathToConnection.toString(),
-      );
-    }
-  } else {
+  if (pathToConnection.length < 2) {
     warning(
       false,
       'setRelayModernMutationConfigs: RANGE_DELETE ' +
         'pathToConnection must include at least parent and connection',
     );
+    return;
+  }
+  let recordProxy = parent;
+  for (let i = 1; i < pathToConnection.length - 1; i++) {
+    if (recordProxy) {
+      recordProxy = recordProxy.getLinkedRecord(pathToConnection[i]);
+    }
+  }
+  // Should never enter loop except edge cases
+  if (!connectionKeys || !recordProxy) {
+    warning(
+      false,
+      'setRelayModernMutationConfigs: RANGE_DELETE ' +
+        'pathToConnection is incorrect. Unable to find connection with ' +
+        'parentID: %s and path: %s',
+      parentID,
+      pathToConnection.toString(),
+    );
+    return;
+  }
+  for (const key of connectionKeys) {
+    const connection = RelayConnectionHandler.getConnection(
+      recordProxy,
+      key.key,
+      key.filters,
+    );
+    if (connection) {
+      deleteIDs.forEach(deleteID => {
+        RelayConnectionHandler.deleteNode(connection, deleteID);
+      });
+    }
   }
 }
 
 function getRootField(request: RequestNode): ?string {
-  let rootField;
   if (
     request.fragment.selections &&
     request.fragment.selections.length > 0 &&
     request.fragment.selections[0].kind === 'LinkedField'
   ) {
-    rootField = request.fragment.selections[0].name;
+    return request.fragment.selections[0].name;
   }
-  return rootField;
+  return null;
 }
 
 module.exports = setRelayModernMutationConfigs;

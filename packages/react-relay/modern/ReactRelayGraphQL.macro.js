76e4a386e9c6c3a2c3472b40a4d7c54fa263ca52
Andrew Palm <andrewtpalm@gmail.com>
2017-12-26 10:08:32 -0500

    Add comment

--- packages/react-relay/modern/ReactRelayGraphQL.macro.js
+++ packages/react-relay/modern/ReactRelayGraphQL.macro.js
@@ -1,1 +1,5 @@
+// This module is named the way it is because babel-plugin-macros requires 
+// the source string to match the regex /[./]macro(\.js)?$/, and 
+// this module is imported in the test suite.
+
 module.exports = require('../../babel-plugin-relay/BabelPluginRelayMacro');

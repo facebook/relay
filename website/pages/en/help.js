81f22b5174b51318546f20a2da93ae745944a953
Alex McKelvey <alexmck@fb.com>
2018-03-04 10:38:59 -0800

    Apply fb-deprecate-figcolor to RKJSModules/Libraries
    Reviewed By: TheSavior
    
    Differential Revision: D7115997
    
    fbshipit-source-id: 7a39e9d7dcffe60a6511181c681905190c5026bf

--- website/pages/en/help.js
+++ website/pages/en/help.js
@@ -7,9 +7,8 @@
  * @format
  */
 
-const React = require('react');
-
 const CompLibrary = require('../../core/CompLibrary.js');
+const React = require('react');
 const Container = CompLibrary.Container;
 const GridBlock = CompLibrary.GridBlock;
 

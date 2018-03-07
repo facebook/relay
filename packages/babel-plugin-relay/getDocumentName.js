5c91835c76c5e4059d62cd2a1c8c662a7e7348c3
Peter van der Zee <kuvos@fb.com>
2018-03-07 01:23:47 -0800

    Get the base name from the full filename
    Reviewed By: kassens, mjesun
    
    Differential Revision: D7167088
    
    fbshipit-source-id: 502a3963f7ce07a448a4eeca8218e765f02d88e8

--- packages/babel-plugin-relay/getDocumentName.js
+++ packages/babel-plugin-relay/getDocumentName.js
@@ -11,6 +11,8 @@
 
 'use strict';
 
+const {basename: getBaseName} = require('path');
+
 const PROVIDES_MODULE = 'providesModule';
 
 import type {BabelState} from './BabelPluginRelay';
@@ -40,7 +42,7 @@ function getDocumentName(path: any, state: BabelState): string {
         }
       }
     }
-    const basename = state.file && state.file.opts.basename;
+    const basename = state.file && getBaseName(state.file.opts.filename);
     if (basename && !documentName) {
       const captures = basename.match(/^[_A-Za-z][_0-9A-Za-z]*/);
       if (captures) {

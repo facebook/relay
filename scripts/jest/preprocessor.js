facebook/relay

commit 0d24a725c7ad5e2414e755c5ab47850fb85b824c

babylon7 -> metro-babylon7 and drop the alias
Reviewed By: arcanis

Differential Revision: D7194524

fbshipit-source-id: 85b11b7319c6e0985b166ba9ce990a2581ff1dd2

scripts/jest/preprocessor.js
@@ -26,7 +26,7 @@ const babelOptions = getBabelOptions({
   autoImport: true,
   moduleMap: {
     // TODO(T25740028) once we're fully on babylon 7, we can remove this hack.
-    babylon7: 'babylon',
+    'metro-babylon7': 'babylon',
     immutable: 'immutable',
     React: 'react',
     reactComponentExpect: 'react-dom/lib/reactComponentExpect',
@@ -63,7 +63,7 @@ module.exports = {
     testSchemaPath,
     path.join(
       path.dirname(require.resolve('babel-preset-fbjs')),
-      'package.json'
+      'package.json',
     ),
     path.join(__dirname, '..', 'getBabelOptions.js'),
   ]),

.flowconfig
@@ -19,7 +19,7 @@ node_modules/fbjs/flow/lib
 module.system=haste
 module.name_mapper='^ReactDOM$' -> 'react-dom'
 ; TODO(T25740028) once we're fully on babylon 7, we can remove this hack.
-module.name_mapper='^babylon7$' -> 'babylon'
+module.name_mapper='^metro-babylon7$' -> 'babylon'
 module.system.haste.paths.blacklist=<PROJECT_ROOT>/packages/react-relay/.*
 
 esproposal.class_static_fields=enable

gulpfile.js
@@ -33,7 +33,7 @@ const babelOptions = require('./scripts/getBabelOptions')({
     'babel-types': 'babel-types',
     // TODO(T25740028) once we're fully on babylon 7, we can revert this to just
     // babylon
-    babylon7: 'babylon',
+    'metro-babylon7': 'babylon',
     chalk: 'chalk',
     child_process: 'child_process',
     crypto: 'crypto',

packages/relay-compiler/codegen/FindGraphQLTags.js
@@ -13,7 +13,7 @@
 
 const RelayCompilerCache = require('../util/RelayCompilerCache');
 
-const babylon = require('babylon7');
+const babylon = require('metro-babylon7');
 const getModuleName = require('../util/getModuleName');
 const graphql = require('graphql');
 const path = require('path');

commit 17cc27bfeffaccb4f0ab594f5d8085b421e1ac74

Fix CI for node < 8
Summary:
Thanks alloy for noticing that https://github.com/facebook/relay/commit/0d24a725c7ad5e2414e755c5ab47850fb85b824c broke CI.  The reason is that node < 8 does not support trailing commas for function calls which prettier adds in our configuration. This should fix it.
Closes https://github.com/facebook/relay/pull/2373

Differential Revision: D7311796

Pulled By: kassens

fbshipit-source-id: fcc7a172917d398e8022d3bb6f5f5f1a90f20e0d

scripts/jest/preprocessor.js
@@ -61,9 +61,11 @@ module.exports = {
   getCacheKey: createCacheKeyFunction([
     __filename,
     testSchemaPath,
+    // We cannot have trailing commas in this file for node < 8
+    // prettier-ignore
     path.join(
       path.dirname(require.resolve('babel-preset-fbjs')),
-      'package.json',
+      'package.json'
     ),
     path.join(__dirname, '..', 'getBabelOptions.js'),
   ]),
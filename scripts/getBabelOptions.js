c6ee599cbd0d79663ab854e558e7df2025ce5a16
Jan Kassens <jan@kassens.net>
2018-04-24 13:06:13 -0700

    Fix CI for node 6

--- scripts/getBabelOptions.js
+++ scripts/getBabelOptions.js
@@ -4,8 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- *
- * @format
+ * @noformat
  */
 
 'use strict';
@@ -17,7 +16,7 @@ module.exports = function(options) {
       moduleMap: {},
       plugins: [],
     },
-    options,
+    options
   );
 
   const fbjsPreset = require('babel-preset-fbjs/configure')({

955bb80987676bde7a08d27866d663c264c94c05
Jan Kassens <jkassens@fb.com>
2018-08-02 16:45:45 -0700

    lazy create cache directory
    Reviewed By: adamjernst
    
    Differential Revision: D9140458
    
    fbshipit-source-id: 15344edcb5a7a69ab0beef4d8b2601cf96251561

--- packages/relay-compiler/util/RelayCompilerCache.js
+++ packages/relay-compiler/util/RelayCompilerCache.js
@@ -22,7 +22,9 @@ const {Profiler} = require('graphql-compiler');
  * serializable values should be used.
  */
 class RelayCompilerCache<T> {
-  _dir: string;
+  _name: string;
+  _cacheBreaker: string;
+  _dir: ?string = null;
 
   /**
    * @param name         Human readable identifier for the cache
@@ -30,23 +32,32 @@ class RelayCompilerCache<T> {
    *                     caches.
    */
   constructor(name: string, cacheBreaker: string) {
-    // Include username in the cache dir to avoid issues with directories being
-    // owned by a different user.
-    const username = os.userInfo().username;
-    const cacheID = crypto
-      .createHash('md5')
-      .update(cacheBreaker)
-      .update(username)
-      .digest('hex');
-    this._dir = path.join(os.tmpdir(), `${name}-${cacheID}`);
-    if (!fs.existsSync(this._dir)) {
-      fs.mkdirSync(this._dir);
+    this._name = name;
+    this._cacheBreaker = cacheBreaker;
+  }
+
+  _getFile(key: string): string {
+    if (this._dir == null) {
+      // Include username in the cache dir to avoid issues with directories being
+      // owned by a different user.
+      const username = os.userInfo().username;
+      const cacheID = crypto
+        .createHash('md5')
+        .update(this._cacheBreaker)
+        .update(username)
+        .digest('hex');
+      const dir = path.join(os.tmpdir(), `${this._name}-${cacheID}`);
+      if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+      }
+      this._dir = dir;
     }
+    return path.join(this._dir, key);
   }
 
   getOrCompute(key: string, compute: () => T): T {
     return Profiler.run('RelayCompilerCache.getOrCompute', () => {
-      const cacheFile = path.join(this._dir, key);
+      const cacheFile = this._getFile(key);
       if (fs.existsSync(cacheFile)) {
         try {
           return JSON.parse(fs.readFileSync(cacheFile, 'utf8'));

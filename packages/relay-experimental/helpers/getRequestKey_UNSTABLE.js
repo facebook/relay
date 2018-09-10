32251dd98c8d3fa1bcbc0ea67aba76d0e375ec5c
Juan Tejada <jstejada@fb.com>
2018-09-10 15:47:26 -0700

    'Re-sync with internal repository'

---
+++ packages/relay-experimental/helpers/getRequestKey_UNSTABLE.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+import type {RequestNode, Variables} from 'relay-runtime';
+
+function getRequestKey_UNSTABLE(
+  requestNode: RequestNode,
+  variables: Variables,
+) {
+  let queryKey = '';
+  if (requestNode.kind === 'BatchRequest') {
+    queryKey = JSON.stringify({
+      id: requestNode.requests.map(req =>
+        String(req.id != null ? req.id : req.text),
+      ),
+    });
+  } else {
+    const requestID =
+      requestNode.id != null ? requestNode.id : requestNode.text;
+    queryKey = String(requestID);
+  }
+  return queryKey + JSON.stringify(variables);
+}
+
+module.exports = getRequestKey_UNSTABLE;
---
+++ packages/relay-experimental/helpers/readQuery_UNSTABLE.js
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2013-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ */
+
+'use strict';
+
+const invariant = require('invariant');
+
+import type {
+  GraphQLTaggedNode,
+  IEnvironment,
+  Snapshot,
+  Variables,
+} from 'relay-runtime';
+
+/**
+ * Reads a query from the local Relay Store.
+ * Returns the Snapshot which contains the data result for the query.
+ */
+function readQuery_UNSTABLE(
+  environment: IEnvironment,
+  gqlNode: GraphQLTaggedNode,
+  variables: Variables,
+): Snapshot {
+  const {
+    getRequest,
+    isRequest,
+    createOperationSelector,
+  } = environment.unstable_internal;
+  invariant(
+    isRequest(gqlNode),
+    'readQuery_UNSTABLE: Expected graphql node to be a query',
+  );
+  const queryNode = getRequest(gqlNode);
+  const operation = createOperationSelector(queryNode, variables);
+  return environment.lookup(operation.fragment);
+}
+
+module.exports = readQuery_UNSTABLE;

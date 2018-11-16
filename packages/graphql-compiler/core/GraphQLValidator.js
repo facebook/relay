c4c1e7714fc49511cb18edca86db0e3b9fa8ba20
Joe Savona <joesavona@fb.com>
2018-11-16 10:17:50 -0800

    Improve GraphQL validation messages
    Summary: We're currently asking graphql-js for nicely formatted validation error messages and then throwing them away and just getting a short description. This changes to use the default error message formatting which includes the relevant source locations - vastly more debuggable.
    
    Reviewed By: kassens, avijohnson
    
    Differential Revision: D13085901
    
    fbshipit-source-id: 1d942af96e2de67f216ffca47a58cb5498786c2a

--- packages/graphql-compiler/core/GraphQLValidator.js
+++ packages/graphql-compiler/core/GraphQLValidator.js
@@ -43,9 +43,7 @@ function validateOrThrow(
   const validationErrors = validate(schema, document, rules);
   if (validationErrors && validationErrors.length > 0) {
     const formattedErrors = validationErrors.map(formatError);
-    const errorMessages = validationErrors.map(
-      e => (e.source ? `${e.source.name}: ${e.message}` : e.message),
-    );
+    const errorMessages = validationErrors.map(e => e.toString());
 
     const error = new Error(
       util.format(

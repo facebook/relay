cd9315a03420a6d36e7d6d6bd6d6b8ba59c948f4
Joe Savona <joesavona@fb.com>
2019-02-20 15:06:42 -0800

    Disable broken graphql-js validation rule
    Summary: Disables a buggy graphql-js validation rule. Note that relay-compiler already has validation that variables are used in the correct positions which understands `arguments` and `argumentDefinitions`.
    
    Reviewed By: kassens
    
    Differential Revision: D14152264
    
    fbshipit-source-id: 780dcd326b8c299531c91f19d5b87cbf340d3b85

--- packages/relay-compiler/core/RelayValidator.js
+++ packages/relay-compiler/core/RelayValidator.js
@@ -4,15 +4,61 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
+ * @flow
  * @format
  */
 
 'use strict';
 
-const {GLOBAL_RULES, LOCAL_RULES, validate} = require('./GraphQLValidator');
+const Profiler = require('./GraphQLCompilerProfiler');
 
-import type {FieldNode, ValidationContext} from 'graphql';
+const util = require('util');
+
+const {
+  formatError,
+  FragmentsOnCompositeTypesRule,
+  KnownArgumentNamesRule,
+  KnownTypeNamesRule,
+  LoneAnonymousOperationRule,
+  NoUnusedVariablesRule,
+  PossibleFragmentSpreadsRule,
+  UniqueArgumentNamesRule,
+  UniqueFragmentNamesRule,
+  UniqueInputFieldNamesRule,
+  UniqueOperationNamesRule,
+  UniqueVariableNamesRule,
+  validate,
+  ValuesOfCorrectTypeRule,
+  VariablesAreInputTypesRule,
+} = require('graphql');
+
+import type {
+  DocumentNode,
+  FieldNode,
+  GraphQLSchema,
+  ValidationContext,
+} from 'graphql';
+
+function validateOrThrow(
+  document: DocumentNode,
+  schema: GraphQLSchema,
+  rules: $ReadOnlyArray<Function>,
+): void {
+  const validationErrors = validate(schema, document, rules);
+  if (validationErrors && validationErrors.length > 0) {
+    const formattedErrors = validationErrors.map(formatError);
+    const errorMessages = validationErrors.map(e => e.toString());
+
+    const error = new Error(
+      util.format(
+        'You supplied a GraphQL document with validation errors:\n%s',
+        errorMessages.join('\n'),
+      ),
+    );
+    (error: any).validationErrors = formattedErrors;
+    throw error;
+  }
+}
 
 function DisallowIdAsAliasValidationRule(context: ValidationContext) {
   return {
@@ -32,12 +78,53 @@ function DisallowIdAsAliasValidationRule(context: ValidationContext) {
   };
 }
 
-const relayGlobalRules = GLOBAL_RULES;
-
-const relayLocalRules = [...LOCAL_RULES, DisallowIdAsAliasValidationRule];
-
 module.exports = {
-  GLOBAL_RULES: relayGlobalRules,
-  LOCAL_RULES: relayLocalRules,
-  validate,
+  GLOBAL_RULES: [
+    KnownArgumentNamesRule,
+    /* Some rules are not enabled (potentially non-exhaustive)
+     *
+     * - KnownFragmentNamesRule: RelayClassic generates fragments at runtime,
+     *   so RelayCompat queries might reference fragments unknown in build time.
+     * - NoFragmentCyclesRule: Because of @argumentDefinitions, this validation
+     *   incorrectly flags a subset of fragments using @include/@skip as
+     *   recursive.
+     * - NoUndefinedVariablesRule: Because of @argumentDefinitions, this
+     *   validation incorrectly marks some fragment variables as undefined.
+     * - NoUnusedFragmentsRule: Queries generated dynamically with RelayCompat
+     *   might use unused fragments.
+     * - OverlappingFieldsCanBeMergedRule: RelayClassic auto-resolves
+     *   overlapping fields by generating aliases.
+     */
+    NoUnusedVariablesRule,
+    UniqueArgumentNamesRule,
+    UniqueFragmentNamesRule,
+    UniqueInputFieldNamesRule,
+    UniqueOperationNamesRule,
+    UniqueVariableNamesRule,
+  ],
+  LOCAL_RULES: [
+    /*
+     * GraphQL built-in rules: a subset of these rules are enabled, some of the
+     * default rules conflict with Relays-specific features:
+     * - FieldsOnCorrectTypeRule: is not aware of @fixme_fat_interface.
+     * - KnownDirectivesRule: doesn't pass with @arguments and other Relay
+     *   directives.
+     * - ScalarLeafsRule: is violated by the @match directive since these rules
+     *   run before any transform steps.
+     * - VariablesInAllowedPositionRule: violated by the @arguments directive,
+     *   since @arguments is not defined in the schema. relay-compiler does its
+     *   own type-checking for variable/argument usage that is aware of fragment
+     *   variables.
+     */
+    FragmentsOnCompositeTypesRule,
+    KnownTypeNamesRule,
+    LoneAnonymousOperationRule,
+    PossibleFragmentSpreadsRule,
+    ValuesOfCorrectTypeRule,
+    VariablesAreInputTypesRule,
+
+    // Relay-specific validation
+    DisallowIdAsAliasValidationRule,
+  ],
+  validate: Profiler.instrument(validateOrThrow, 'GraphQLValidator.validate'),
 };

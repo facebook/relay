1731d389ecf1b4eec58d989820cb39b861351e3a
Juan Tejada <jstejada@fb.com>
2019-04-17 12:35:16 -0700

    Client Extensions: Remove SkipClientFieldsTransform
    Summary:
    This commit completely removes the `SkipClientFieldsTransform`, which is no longer used as of D14947255.
    
    Specifically:
    - Deletes transform, tests and fixtures
    - Updates the base GraphQLIRTransforms to include ClientExtensionsTransform + SkipClientExtensionsTransform instead of SkipClientFieldsTransform
    
    Reviewed By: alunyov
    
    Differential Revision: D14969327
    
    fbshipit-source-id: b87719b82fadb47cf41fa783f8ffe41a32ef5c52

--- packages/relay-compiler/core/GraphQLIRTransforms.js
+++ packages/relay-compiler/core/GraphQLIRTransforms.js
@@ -10,10 +10,11 @@
 
 'use strict';
 
+const ClientExtensionsTransform = require('../transforms/ClientExtensionsTransform');
 const FilterDirectivesTransform = require('../transforms/FilterDirectivesTransform');
 const FlattenTransform = require('../transforms/FlattenTransform');
 const InlineFragmentsTransform = require('../transforms/InlineFragmentsTransform');
-const SkipClientFieldTransform = require('../transforms/SkipClientFieldTransform');
+const SkipClientExtensionsTransform = require('../transforms/SkipClientExtensionsTransform');
 const SkipRedundantNodesTransform = require('../transforms/SkipRedundantNodesTransform');
 const SkipUnreachableNodeTransform = require('../transforms/SkipUnreachableNodeTransform');
 
@@ -23,6 +24,7 @@ export type IRTransform = CompilerContext => CompilerContext;
 
 // Transforms applied to fragments used for reading data from a store
 const FRAGMENT_TRANSFORMS: Array<IRTransform> = [
+  ClientExtensionsTransform.transform,
   FlattenTransform.transformWithOptions({flattenAbstractTypes: true}),
   SkipRedundantNodesTransform.transform,
 ];
@@ -30,12 +32,14 @@ const FRAGMENT_TRANSFORMS: Array<IRTransform> = [
 // Transforms applied to queries/mutations/subscriptions that are used for
 // fetching data from the server and parsing those responses.
 const QUERY_TRANSFORMS: Array<IRTransform> = [
-  SkipClientFieldTransform.transform,
+  ClientExtensionsTransform.transform,
+  SkipClientExtensionsTransform.transform,
   SkipUnreachableNodeTransform.transform,
 ];
 
 // Transforms applied to the code used to process a query response.
 const CODEGEN_TRANSFORMS: Array<IRTransform> = [
+  ClientExtensionsTransform.transform,
   InlineFragmentsTransform.transform,
   FlattenTransform.transformWithOptions({
     flattenAbstractTypes: true,

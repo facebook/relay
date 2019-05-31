69a2626dbe6482f99e00f71554c0f841d8cc2aab
Tianyu Yao <skyyao@fb.com>
2019-05-30 22:44:54 -0700

    Disable RelayDirectiveClientExtensionValidationTransform
    Reviewed By: josephsavona
    
    Differential Revision: D15572228
    
    fbshipit-source-id: 21a6bd82d35f40e3c010c96d7e5be98a17f9bd34

--- packages/relay-compiler/core/RelayIRTransforms.js
+++ packages/relay-compiler/core/RelayIRTransforms.js
@@ -18,7 +18,6 @@ const RefineOperationVariablesTransform = require('../transforms/RefineOperation
 const RelayApplyFragmentArgumentTransform = require('../transforms/RelayApplyFragmentArgumentTransform');
 const RelayConnectionTransform = require('../handlers/connection//RelayConnectionTransform');
 const RelayDeferStreamTransform = require('../transforms/RelayDeferStreamTransform');
-const RelayDirectiveClientExtensionValidationTransform = require('../transforms/RelayDirectiveClientExtensionValidationTransform');
 const RelayFieldHandleTransform = require('../transforms/RelayFieldHandleTransform');
 const RelayGenerateIDFieldTransform = require('../transforms/RelayGenerateIDFieldTransform');
 const RelayGenerateTypeNameTransform = require('../transforms/RelayGenerateTypeNameTransform');
@@ -59,7 +58,6 @@ const relayCommonTransforms: Array<IRTransform> = [
 // Transforms applied to fragments used for reading data from a store
 const relayFragmentTransforms: Array<IRTransform> = [
   ClientExtensionsTransform.transform,
-  RelayDirectiveClientExtensionValidationTransform.transform,
   RelayFieldHandleTransform.transform,
   FlattenTransform.transformWithOptions({flattenAbstractTypes: true}),
   SkipRedundantNodesTransform.transform,

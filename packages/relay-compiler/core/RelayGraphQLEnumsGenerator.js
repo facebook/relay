20ed1e560439e373dd561509afe1ccf83bfe9a49
Jan Kassens <jkassens@fb.com>
2019-08-05 11:47:00 -0700

    split GraphQLEnum files (22/n)
    Summary: Write individual flow files for GraphQL enums in order to improve incremental flow type checking.
    
    Reviewed By: josephsavona
    
    Differential Revision: D16648218
    
    fbshipit-source-id: a79ad1a7085f0185e18c40dc2b7e829098af163c

--- packages/relay-compiler/core/RelayGraphQLEnumsGenerator.js
+++ packages/relay-compiler/core/RelayGraphQLEnumsGenerator.js
@@ -21,38 +21,49 @@ function writeForSchema(
   schema: GraphQLSchema,
   licenseHeader: $ReadOnlyArray<string>,
   codegenDir: CodegenDirectory,
-  moduleName: string,
+  getModuleName: (enumName: string) => string,
+  aggregateModuleNames: $ReadOnlyArray<string>,
 ): void {
-  const typeMap = schema.getTypeMap();
-  const stableTypeNames = Object.keys(typeMap).sort();
-  const types = [];
-  for (const name of stableTypeNames) {
-    const type = typeMap[name];
-    if (type instanceof GraphQLEnumType) {
-      const values = type.getValues().map(({value}) => value);
-      values.sort();
-      types.push(
-        `export type ${name} =\n  | '` +
-          values.join("'\n  | '") +
-          "'\n  | '%future added value';",
-      );
-    }
-  }
-
-  const content =
+  const header =
     '/**\n' +
     licenseHeader.map(line => ` * ${line}\n`).join('') +
     ' *\n' +
     ` * ${SignedSource.getSigningToken()}\n` +
     ' * @flow\n' +
     ' */\n' +
-    '\n' +
-    // use Flow comment to avoid long Babel compile times
-    '/*::\n' +
-    types.join('\n\n') +
-    '\n*/\n';
+    '\n';
 
-  codegenDir.writeFile(moduleName + '.js', SignedSource.signFile(content));
+  const typeMap = schema.getTypeMap();
+  const stableTypeNames = Object.keys(typeMap).sort();
+  const types = [];
+  for (const name of stableTypeNames) {
+    const type = typeMap[name];
+    if (type instanceof GraphQLEnumType) {
+      types.push(`export type {${name}} from '${getModuleName(name)}';`);
+      const values = type
+        .getValues()
+        .map(({value}) => value)
+        .sort();
+      const enumFileContent =
+        header +
+        `export type ${name} =\n  | '` +
+        values.join("'\n  | '") +
+        "'\n  | '%future added value';\n";
+      codegenDir.writeFile(
+        `${getModuleName(name)}.js`,
+        SignedSource.signFile(enumFileContent),
+      );
+    }
+  }
+
+  const content = header + types.join('\n') + '\n';
+
+  aggregateModuleNames.forEach(aggregateModuleName => {
+    codegenDir.writeFile(
+      aggregateModuleName + '.js',
+      SignedSource.signFile(content),
+    );
+  });
 }
 
 module.exports = {

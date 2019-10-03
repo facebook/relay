27ce4c33ead80ce0d149673d4b7e46aaa35bbfb0
Jan Kassens <jan@kassens.net>
2019-10-02 23:07:41 -0400

    relay-compiler no longer exports nullthrows

--- packages/relay-compiler/util/nullthrowsOSS.js
+++ packages/relay-compiler/util/nullthrowsOSS.js
@@ -7,18 +7,17 @@
  * @flow strict
  * @format
  */
+
 'use strict';
 
-var nullthrows: <T>(x: ?T, message?: string) => T = function<T>(
+function nullthrows<T>(
   x: ?T,
   message?: string = 'Got unexpected null or undefined',
 ): T {
-  if (x != null) {
-    return x;
+  if (x == null) {
+    throw new Error(message);
   }
-  var error = new Error(message);
-  (error: $FlowFixMe).framesToPop = 1; // Skip nullthrows own stack frame.
-  throw error;
-};
+  return x;
+}
 
 module.exports = nullthrows;

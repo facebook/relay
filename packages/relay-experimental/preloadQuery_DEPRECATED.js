62cb6dfa78a0acbfb5a85d280eac6561b7c2491b
Juan Tejada <jstejada@fb.com>
2020-02-12 15:35:25 -0800

    Fix push safety of PreloadableConcreteRequest artifacts
    Reviewed By: josephsavona
    
    Differential Revision: D19865954
    
    fbshipit-source-id: 3f9633877d8634e1c484374c419d90636592c3c9

--- packages/relay-experimental/preloadQuery_DEPRECATED.js
+++ packages/relay-experimental/preloadQuery_DEPRECATED.js
@@ -132,6 +132,15 @@ function preloadQueryDeduped<TQuery: OperationType>(
     const preloadableConcreteRequest: PreloadableConcreteRequest<TQuery> = (preloadableRequest: $FlowFixMe);
     params = preloadableConcreteRequest.params;
     query = params.id != null ? PreloadableQueryRegistry.get(params.id) : null;
+  } else if (
+    // TODO(T62200461): Need to still check for previouss version of
+    // generated artifacts for push safety. Remove when fully
+    // rolled out.
+    (preloadableRequest: $FlowFixMe).queryResource != null
+  ) {
+    const preloadableConcreteRequest = (preloadableRequest: $FlowFixMe);
+    params = preloadableConcreteRequest.params;
+    query = preloadableConcreteRequest.queryResource.getModuleIfRequired();
   } else {
     query = getRequest((preloadableRequest: $FlowFixMe));
     params = query.params;

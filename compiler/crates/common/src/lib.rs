050422668ffa38ab05f38bdf58d42856153e45bf
Joe Savona <joesavona@fb.com>
2020-02-12 14:56:58 -0500

    [rust-compiler] add common crate

---
+++ compiler/crates/common/src/lib.rs
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#![deny(warnings)]
+#![deny(rust_2018_idioms)]
+#![deny(clippy::all)]
+
+mod location;
+mod murmurhash;
+mod span;
+mod timer;
+
+pub use location::{FileKey, Location};
+pub use murmurhash::murmurhash;
+pub use span::{Span, Spanned};
+pub use timer::Timer;

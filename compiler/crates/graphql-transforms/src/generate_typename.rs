468ecdae00575d0b25cdebb70d47d70ecf05c990
Juan Tejada <jstejada@fb.com>
2020-03-03 07:46:45 -0800

    Reuse pointer address util in generate_typename transform
    Reviewed By: josephsavona
    
    Differential Revision: D20222003
    
    fbshipit-source-id: f941a85ce10fc2d5d915f8d1c213727e87499db9

--- compiler/crates/graphql-transforms/src/generate_typename.rs
+++ compiler/crates/graphql-transforms/src/generate_typename.rs
@@ -5,6 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use crate::util::PointerAddress;
 use common::WithLocation;
 use graphql_ir::{
     FragmentSpread, InlineFragment, LinkedField, Program, ScalarField, Selection, Transformed,
@@ -130,20 +131,4 @@ fn has_typename_field(schema: &Schema, selections: &[Selection]) -> bool {
         }
         _ => false,
     })
-}
-
-// A wrapper type that allows comparing pointer equality of references. Two
-// `PointerAddress` values are equal if they point to the same memory location.
-//
-// This type is _sound_, but misuse can easily lead to logical bugs if the memory
-// of one PointerAddress could not have been freed and reused for a subsequent
-// PointerAddress.
-#[derive(Hash, Eq, PartialEq, Clone, Copy)]
-struct PointerAddress(usize);
-
-impl PointerAddress {
-    pub fn new<T>(ptr: &T) -> Self {
-        let ptr_address: usize = unsafe { std::mem::transmute(ptr) };
-        Self(ptr_address)
-    }
 }

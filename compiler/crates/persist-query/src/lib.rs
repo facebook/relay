9c6fbb9fda3e69535e50eee0fbd548db785583d7
Jan Kassens <jkassens@fb.com>
2020-03-04 14:02:41 -0800

    persist-query uses thiserror for errors
    Reviewed By: alunyov
    
    Differential Revision: D20255119
    
    fbshipit-source-id: 91b639e52475837dec25ca56d4248fcb61f0bea2

--- compiler/crates/persist-query/src/lib.rs
+++ compiler/crates/persist-query/src/lib.rs
@@ -9,21 +9,23 @@
 #![deny(rust_2018_idioms)]
 #![deny(clippy::all)]
 
+mod errors;
+
+pub use errors::PersistError;
 use hyper::{Body, Client, Method, Request};
 use hyper_tls::HttpsConnector;
-use serde::{Deserialize, Serialize};
-use std::error::Error;
+use serde::Deserialize;
 use std::fmt;
 use url::form_urlencoded;
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 #[serde(untagged)]
 enum Response {
     Success { id: String },
     Error { error: ResponseError },
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Deserialize)]
 struct ResponseError {
     message: String,
 }
@@ -34,19 +36,11 @@ impl fmt::Display for ResponseError {
     }
 }
 
-impl Error for ResponseError {}
-
-impl Response {
-    fn from_bytes(bytes: &[u8]) -> Self {
-        serde_json::from_slice(bytes).expect("Failed to deserialize response from the API: {:?}")
-    }
-}
-
 pub async fn persist(
     document: &str,
     uri: &str,
     params: impl IntoIterator<Item = (&String, &String)>,
-) -> Result<String, Box<dyn Error>> {
+) -> Result<String, PersistError> {
     let mut request_body = form_urlencoded::Serializer::new(String::new());
     for param in params {
         request_body.append_pair(&param.0, &param.1);
@@ -57,15 +51,20 @@ pub async fn persist(
         .method(Method::POST)
         .uri(uri)
         .header("content-type", "application/x-www-form-urlencoded")
-        .body(Body::from(request_body.finish()))?;
+        .body(Body::from(request_body.finish()))
+        .map_err(|err| PersistError::NetworkCreateError {
+            error: Box::new(err),
+        })?;
     let https = HttpsConnector::new();
-    let client = Client::builder().build::<_, Body>(https);
+    let client = Client::builder().build(https);
     let res = client.request(req).await?;
     let bytes = hyper::body::to_bytes(res.into_body()).await?;
-    let result = Response::from_bytes(bytes.as_ref());
+    let result: Response = serde_json::from_slice(&bytes)?;
 
     match result {
         Response::Success { id } => Ok(id),
-        Response::Error { error } => Err(Box::new(error)),
+        Response::Error { error } => Err(PersistError::ErrorResponse {
+            message: error.message,
+        }),
     }
 }

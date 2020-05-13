b592ac7e739b45f119f08bad03eac38bcd8002f7
Jan Kassens <jkassens@fb.com>
2020-05-13 11:48:56 -0700

    ignore persist errors for now
    Reviewed By: alunyov
    
    Differential Revision: D21552374
    
    fbshipit-source-id: 0c532ff770e73b1aed0408e6b75e2c5a1a277f55

--- compiler/crates/relay-compiler/src/build_project/persist_operations.rs
+++ compiler/crates/relay-compiler/src/build_project/persist_operations.rs
@@ -50,15 +50,18 @@ pub async fn persist_operations(
                 let text = text.clone();
                 let url = persist_config.url.clone();
                 let params = persist_config.params.clone();
-                let errors = Arc::clone(&persist_errors);
+                // let errors = Arc::clone(&persist_errors);
                 handles.push(async move {
                     let request = persist(&text, &url, &params);
                     match request.await {
                         Ok(id) => {
                             *id_and_text_hash = Some((id, text_hash));
                         }
-                        Err(err) => {
-                            errors.lock().unwrap().push(err);
+                        Err(_err) => {
+                            // TODO(T66947325) re-enable errors instead of setting
+                            // "ERROR" as id.
+                            *id_and_text_hash = Some(("ERROR".into(), text_hash));
+                            // errors.lock().unwrap().push(err);
                         }
                     };
                 });

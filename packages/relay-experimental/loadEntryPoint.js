f512c9f6beb155ac851ff2e1dad3d1a24e746721
Robert Balicki <robertbalicki@fb.com>
2020-05-27 15:06:19 -0700

    loadQuery and loadEntryPoint only
    Reviewed By: josephsavona
    
    Differential Revision: D21510677
    
    fbshipit-source-id: c658dd5417fba73ec65d8e5890ddef65ebffa8d2

---
+++ packages/relay-experimental/loadEntryPoint.js
@@ -0,0 +1,137 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @emails oncall+relay
+ */
+
+'use strict';
+
+const {loadQuery} = require('./loadQuery');
+
+import type {ElementConfig} from 'React';
+import type {
+  EntryPoint,
+  EntryPointComponent,
+  EnvironmentProviderOptions,
+  IEnvironmentProvider,
+} from 'relay-experimental/EntryPointTypes.flow';
+
+// Return type of the `prepareEntryPoint(...)` and `loadEntryPoint(...)` functions
+type PreloadedEntryPointInner<TEntryPointComponent> = $ReadOnly<{|
+  dispose?: () => void,
+  kind: 'PreloadedEntryPoint',
+  entryPoints: $PropertyType<
+    ElementConfig<TEntryPointComponent>,
+    'entryPoints',
+  >,
+  extraProps: $PropertyType<ElementConfig<TEntryPointComponent>, 'extraProps'>,
+  getComponent: () => TEntryPointComponent,
+  queries: $PropertyType<ElementConfig<TEntryPointComponent>, 'queries'>,
+|}>;
+
+function loadEntryPoint<
+  TEntryPointParams: {...},
+  TPreloadedQueries: {...},
+  TPreloadedEntryPoints: {...},
+  TRuntimeProps: {...},
+  TExtraProps,
+  TEntryPointComponent: EntryPointComponent<
+    TPreloadedQueries,
+    TPreloadedEntryPoints,
+    TRuntimeProps,
+    TExtraProps,
+  >,
+  TEntryPoint: EntryPoint<TEntryPointParams, TEntryPointComponent>,
+>(
+  environmentProvider: IEnvironmentProvider<EnvironmentProviderOptions>,
+  entryPoint: TEntryPoint,
+  entryPointParams: TEntryPointParams,
+): PreloadedEntryPointInner<TEntryPointComponent> {
+  // Start loading the code for the entrypoint
+  let loadingPromise = null;
+  if (entryPoint.root.getModuleIfRequired() == null) {
+    loadingPromise = entryPoint.root.load();
+  }
+  const preloadProps = entryPoint.getPreloadProps(entryPointParams);
+  const {queries, entryPoints, extraProps} = preloadProps;
+  const preloadedQueries: $Shape<TPreloadedQueries> = {};
+  const preloadedEntryPoints: $Shape<TPreloadedEntryPoints> = {};
+  if (queries != null) {
+    const queriesPropNames = Object.keys(queries);
+    queriesPropNames.forEach(queryPropName => {
+      const {
+        environmentProviderOptions,
+        options,
+        parameters,
+        variables,
+      } = queries[queryPropName];
+
+      const environment = environmentProvider.getEnvironment(
+        environmentProviderOptions,
+      );
+
+      preloadedQueries[queryPropName] = loadQuery(
+        environment,
+        parameters,
+        variables,
+        {
+          fetchPolicy: options?.fetchPolicy,
+          networkCacheConfig: options?.networkCacheConfig,
+        },
+      );
+    });
+  }
+
+  if (entryPoints != null) {
+    const entryPointPropNames = Object.keys(entryPoints);
+    entryPointPropNames.forEach(entryPointPropName => {
+      const entryPointDescription = entryPoints[entryPointPropName];
+      if (entryPointDescription == null) {
+        return;
+      }
+      const {
+        entryPoint: nestedEntryPoint,
+        entryPointParams: nestedParams,
+      } = entryPointDescription;
+      preloadedEntryPoints[entryPointPropName] = loadEntryPoint(
+        environmentProvider,
+        nestedEntryPoint,
+        nestedParams,
+      );
+    });
+  }
+
+  const dispose = () => {
+    if (preloadedQueries != null) {
+      Object.values(preloadedQueries).forEach(
+        ({dispose: innerDispose}: $FlowFixMe) => {
+          innerDispose && innerDispose();
+        },
+      );
+    }
+  };
+
+  return {
+    kind: 'PreloadedEntryPoint',
+    dispose,
+    entryPoints: (preloadedEntryPoints: TPreloadedEntryPoints),
+    extraProps: extraProps ?? null,
+    getComponent: () => {
+      const component = entryPoint.root.getModuleIfRequired();
+      if (component == null) {
+        loadingPromise = loadingPromise ?? entryPoint.root.load();
+        throw loadingPromise;
+      }
+      // $FlowFixMe - trust me Flow, its entryPoint component
+      return (component: TEntryPointComponent);
+    },
+    queries: (preloadedQueries: TPreloadedQueries),
+  };
+}
+
+module.exports = loadEntryPoint;
---
+++ packages/relay-experimental/loadQuery.js
@@ -0,0 +1,226 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow strict-local
+ * @format
+ * @emails oncall+relay
+ */
+
+'use strict';
+
+const PreloadableQueryRegistry = require('./PreloadableQueryRegistry');
+const React = require('react');
+
+const invariant = require('invariant');
+
+const {
+  ReplaySubject,
+  createOperationDescriptor,
+  getRequest,
+  Observable,
+} = require('relay-runtime');
+
+import type {
+  PreloadableConcreteRequest,
+  PreloadFetchPolicy,
+} from './EntryPointTypes.flow';
+import type {
+  IEnvironment,
+  OperationType,
+  GraphQLTaggedNode,
+  GraphQLResponse,
+  CacheConfig,
+} from 'relay-runtime';
+
+export type LoadQueryOptions = {|
+  +fetchPolicy?: ?PreloadFetchPolicy,
+  +networkCacheConfig?: ?CacheConfig,
+  +onQueryAstLoadTimeout?: ?(Error) => void,
+|};
+
+type PreloadedQueryInner<TQuery: OperationType> = {|
+  kind: 'PreloadedQuery',
+  dispose: () => void,
+  environment: IEnvironment,
+  fetchPolicy: PreloadFetchPolicy,
+  name: string,
+  source: ?Observable<GraphQLResponse>,
+  variables: $ElementType<TQuery, 'variables'>,
+|};
+
+const LOAD_QUERY_AST_MAX_TIMEOUT = 30 * 1000;
+
+let RenderDispatcher = null;
+
+function useTrackLoadQueryInRender() {
+  if (RenderDispatcher === null) {
+    // Flow does not know of React internals (rightly so), but we need to
+    // ensure here that this function isn't called inside render.
+    RenderDispatcher =
+      // $FlowFixMe
+      React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+        ?.ReactCurrentDispatcher?.current;
+  }
+}
+
+function loadQuery<TQuery: OperationType>(
+  environment: IEnvironment,
+  preloadableRequest: GraphQLTaggedNode | PreloadableConcreteRequest<TQuery>,
+  variables: $ElementType<TQuery, 'variables'>,
+  options?: LoadQueryOptions,
+): PreloadedQueryInner<TQuery> {
+  // Flow does not know of React internals (rightly so), but we need to
+  // ensure here that this function isn't called inside render.
+  const CurrentDispatcher =
+    // $FlowFixMe
+    React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+      ?.ReactCurrentDispatcher?.current;
+  invariant(
+    RenderDispatcher == null || CurrentDispatcher !== RenderDispatcher,
+    'Relay: `preloadQuery` (or `prepareEntryPoint`) should not be called inside a React render function.',
+  );
+
+  const fetchPolicy = options?.fetchPolicy ?? 'store-or-network';
+
+  let madeNetworkRequest = false;
+  const makeNetworkRequest = (params): Observable<GraphQLResponse> => {
+    // N.B. this function is called synchronously or not at all
+    madeNetworkRequest = true;
+    const network = environment.getNetwork();
+    const sourceObservable = network.execute(params, variables, {
+      force: true,
+      ...options?.networkCacheConfig,
+    });
+
+    const subject = new ReplaySubject();
+    sourceObservable.subscribe({
+      error(err) {
+        subject.error(err);
+      },
+      next(data) {
+        subject.next(data);
+      },
+      complete() {
+        subject.complete();
+      },
+    });
+
+    return Observable.create(sink => subject.subscribe(sink));
+  };
+
+  const normalizationSubject = new ReplaySubject();
+  const returnedObservable = Observable.create(sink =>
+    normalizationSubject.subscribe(sink),
+  );
+
+  let unsubscribeFromExecute;
+  let retainReference;
+  const executeWithSource = (operation, sourceObservable) => {
+    retainReference = environment.retain(operation);
+    ({unsubscribe: unsubscribeFromExecute} = environment
+      .executeWithSource({
+        operation,
+        source: sourceObservable,
+      })
+      .subscribe({
+        error(err) {
+          normalizationSubject.error(err);
+        },
+        next(data) {
+          normalizationSubject.next(data);
+        },
+        complete() {
+          normalizationSubject.complete();
+        },
+      }));
+  };
+
+  const checkAvailabilityAndExecute = concreteRequest => {
+    const operation = createOperationDescriptor(concreteRequest, variables);
+    const shouldFetch =
+      fetchPolicy !== 'store-or-network' ||
+      environment.check(operation).status !== 'available';
+
+    if (shouldFetch) {
+      const source = makeNetworkRequest(concreteRequest.params);
+      executeWithSource(operation, source);
+    }
+    // if the fetch policy allows fulfillment from the store and the environment
+    // has the appropriate data, we do nothing. In particular, we don't call
+    // makeNetworkRequest, which has side effects that show up in the return value.
+  };
+
+  let params;
+  let loadQueryAstTimeoutId;
+  let cancelOnLoadCallback;
+  if (preloadableRequest.kind === 'PreloadableConcreteRequest') {
+    const preloadableConcreteRequest: PreloadableConcreteRequest<TQuery> = (preloadableRequest: $FlowFixMe);
+    ({params} = preloadableConcreteRequest);
+
+    const {id: moduleId} = params;
+    invariant(
+      moduleId !== null,
+      'Relay: `loadQuery` requires that the preloadable query `%s` has persisted query id',
+      params.name,
+    );
+
+    const module = PreloadableQueryRegistry.get(moduleId);
+
+    if (module != null) {
+      checkAvailabilityAndExecute(module);
+    } else {
+      // If the module isn't synchronously available, we launch the network request
+      // immediately and ignore the fetch policy.
+      const source = makeNetworkRequest(params);
+      ({dispose: cancelOnLoadCallback} = PreloadableQueryRegistry.onLoad(
+        moduleId,
+        preloadedModule => {
+          clearTimeout(loadQueryAstTimeoutId);
+          cancelOnLoadCallback();
+          const operation = createOperationDescriptor(
+            preloadedModule,
+            variables,
+          );
+          executeWithSource(operation, source);
+        },
+      ));
+      loadQueryAstTimeoutId = setTimeout(() => {
+        cancelOnLoadCallback();
+        const onError = options?.onQueryAstLoadTimeout;
+        const error = new Error(
+          `Relay: Query AST did not load in time for preloadable query ${params.name}`,
+        );
+        if (onError) {
+          onError(error);
+        } else {
+          throw error;
+        }
+      }, LOAD_QUERY_AST_MAX_TIMEOUT);
+    }
+  } else {
+    const graphQlTaggedNode: GraphQLTaggedNode = (preloadableRequest: $FlowFixMe);
+    const request = getRequest(graphQlTaggedNode);
+    params = request.params;
+    checkAvailabilityAndExecute(request);
+  }
+
+  return {
+    kind: 'PreloadedQuery',
+    environment,
+    dispose: () => {
+      unsubscribeFromExecute && unsubscribeFromExecute();
+      retainReference && retainReference.dispose();
+      cancelOnLoadCallback && cancelOnLoadCallback();
+      loadQueryAstTimeoutId != null && clearTimeout(loadQueryAstTimeoutId);
+    },
+    name: params.name,
+    fetchPolicy,
+    source: madeNetworkRequest ? returnedObservable : undefined,
+    variables,
+  };
+}
+
+module.exports = {loadQuery, useTrackLoadQueryInRender};

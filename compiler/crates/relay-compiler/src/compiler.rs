c552baef10693c44f817d63924cd895be0217761
Jan Kassens <jkassens@fb.com>
2020-07-29 19:49:41 -0700

    remove unused fn watch_with_callback
    Summary:
    This was added at some point while iterating on the LSP implementation, but is
    no longer used.
    
    Reviewed By: tyao1
    
    Differential Revision: D22824249
    
    fbshipit-source-id: 4863979a563e0c0e4b945b970ab478dc35d5f157

--- compiler/crates/relay-compiler/src/compiler.rs
+++ compiler/crates/relay-compiler/src/compiler.rs
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use crate::build_project::{build_project, build_schema, check_project, commit_project};
+use crate::build_project::{build_project, build_schema, commit_project};
 use crate::compiler_state::{ArtifactMapKind, CompilerState, ProjectName};
 use crate::config::Config;
 use crate::errors::{BuildProjectError, Error, Result};
@@ -67,63 +67,6 @@ impl<TPerfLogger: PerfLogger> Compiler<TPerfLogger> {
         schemas
     }
 
-    pub async fn watch_with_callback<F>(&self, mut callback: F) -> Result<()>
-    where
-        F: FnMut(Result<()>),
-    {
-        let setup_event = self.perf_logger.create_event("compiler_setup");
-
-        let file_source = FileSource::connect(&self.config, &setup_event).await?;
-        let (mut compiler_state, mut subscription) = file_source
-            .subscribe(&setup_event, self.perf_logger.as_ref())
-            .await?;
-        let schemas = self.build_schemas(&compiler_state, &setup_event);
-        callback(
-            self.check_projects(&mut compiler_state, &schemas, &setup_event)
-                .await,
-        );
-
-        self.perf_logger.complete_event(setup_event);
-
-        loop {
-            if let Some(file_source_changes) = subscription.next_change().await? {
-                let incremental_check_event =
-                    self.perf_logger.create_event("incremental_check_event");
-                let incremental_check_time =
-                    incremental_check_event.start("incremental_check_time");
-
-                // TODO Single change to file in VSCode sometimes produces
-                // 2 watchman change events for the same file
-                let had_new_changes = compiler_state.merge_file_source_changes(
-                    &self.config,
-                    &file_source_changes,
-                    &incremental_check_event,
-                    self.perf_logger.as_ref(),
-                )?;
-                if had_new_changes {
-                    // Clear out existing errors
-                    callback(Ok(()));
-                    // Report any new errors
-                    callback(
-                        self.check_projects(
-                            &mut compiler_state,
-                            &schemas,
-                            &incremental_check_event,
-                        )
-                        .await,
-                    );
-                } else {
-                    info!("[watch-mode] No re-compilation required");
-                }
-                incremental_check_event.stop(incremental_check_time);
-                self.perf_logger.complete_event(incremental_check_event);
-                // We probably don't want the messages queue to grow indefinitely
-                // and we need to flush then, as the check/build is completed
-                self.perf_logger.flush();
-            }
-        }
-    }
-
     pub async fn watch(&self) -> Result<()> {
         let setup_event = self.perf_logger.create_event("compiler_setup");
 
@@ -180,52 +123,6 @@ impl<TPerfLogger: PerfLogger> Compiler<TPerfLogger> {
                 // and we need to flush then, as the check/build is completed
                 self.perf_logger.flush();
             }
-        }
-    }
-
-    async fn check_projects(
-        &self,
-        compiler_state: &mut CompilerState,
-        schemas: &HashMap<ProjectName, Arc<Schema>>,
-        setup_event: &impl PerfLogEvent,
-    ) -> Result<()> {
-        let graphql_asts = setup_event.time("parse_sources_time", || {
-            compiler_state
-                .graphql_sources
-                .iter()
-                .map(|(&source_set_name, sources)| {
-                    let asts = GraphQLAsts::from_graphql_sources(sources)?;
-                    Ok((source_set_name, asts))
-                })
-                .collect::<Result<_>>()
-        })?;
-
-        let mut build_project_errors = vec![];
-
-        for project_config in self.config.enabled_projects() {
-            if compiler_state.project_has_pending_changes(project_config.name) {
-                let schema = Arc::clone(schemas.get(&project_config.name).unwrap());
-                // TODO: consider running all projects in parallel
-                check_project(
-                    project_config,
-                    compiler_state,
-                    &graphql_asts,
-                    schema,
-                    Arc::clone(&self.perf_logger),
-                )
-                .map_err(|err| {
-                    build_project_errors.push(err);
-                })
-                .ok();
-            }
-        }
-
-        if build_project_errors.is_empty() {
-            Ok(())
-        } else {
-            Err(Error::BuildProjectsErrors {
-                errors: build_project_errors,
-            })
         }
     }
 

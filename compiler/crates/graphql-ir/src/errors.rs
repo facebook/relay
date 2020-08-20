88cdceca8a40e1b0405a3589bd354d5b32b8f392
Jordan Eldredge <jeldredge@fb.com>
2020-08-20 10:56:51 -0700

    Add validation rules for @required
    Reviewed By: josephsavona
    
    Differential Revision: D23190643
    
    fbshipit-source-id: c2c6c3a22ba87cfa31bb5cbe02a40f3275c8764b

--- compiler/crates/graphql-ir/src/errors.rs
+++ compiler/crates/graphql-ir/src/errors.rs
@@ -473,4 +473,31 @@ pub enum ValidationMessage {
         "Expected all fields on the same parent with the name or alias '{field_name}' to have the same name and arguments."
     )]
     InvalidSameFieldWithDifferentArguments { field_name: StringKey },
+
+    #[error("Unexpected @required within inline fragment on an abstract type. At runtime we cannot know if this field is null, or if it's missing beacuse the inline fragment did not match")]
+    RequiredWithinAbstractInlineFragment,
+
+    #[error("Unexpected second @required directive. @requried may only be used once per field")]
+    RequiredOncePerField,
+
+    #[error("Missing `action` argument. @required expects an `action` argument")]
+    RequiredActionArgumentRequired,
+
+    #[error("Expected `action` argument to be a literal")]
+    RequiredActionArgumentConstant,
+
+    #[error("Expected `action` argument to be one of `NONE`, `LOG` or `THROW`")]
+    RequiredActionArgumentEnum,
+
+    #[error("All references to a @required field must have matching `action` arguments. The `action` used for '{field_name}'")]
+    RequiredActionMismatch { field_name: StringKey },
+
+    #[error("All references to a field must have matching @required declarations. The field '{field_name}` is @required here")]
+    RequiredFieldMismatch { field_name: StringKey },
+
+    #[error("@required fields must be included in all instances of their parent. The field '{field_name}` is marked as @required here")]
+    RequiredFieldMissing { field_name: StringKey },
+
+    #[error("`A @required field may not have an `action` less severe than that of its @required parent. This @required directive should probably have `action: {suggested_action}`")]
+    RequiredFieldInvalidNesting { suggested_action: StringKey },
 }

3e7a02a3f4c60e5c98ef327dfb60a606b573d418
Jan Kassens <jkassens@fb.com>
2020-08-27 15:08:34 -0700

    fix lint error with rust 1.46.0
    Reviewed By: rbalicki2
    
    Differential Revision: D23378994
    
    fbshipit-source-id: 72bee46003546f35ff2ee448c9efac5b320efbd6

--- compiler/crates/relay-compiler/src/watchman/mod.rs
+++ compiler/crates/relay-compiler/src/watchman/mod.rs
@@ -5,10 +5,6 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-#![deny(warnings)]
-#![deny(rust_2018_idioms)]
-#![deny(clippy::all)]
-
 mod extract_graphql;
 mod file_categorizer;
 mod file_group;

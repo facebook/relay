6e00d190c85d5f7e7137f1895c9645700d9d5865
Tianyu Yao <skyyao@fb.com>
2020-10-08 19:13:19 -0700

    Generate file dependency map
    Reviewed By: kassens
    
    Differential Revision: D24184797
    
    fbshipit-source-id: aef490c450b7fd5b1867a0d6012d24f68cf90bce

--- compiler/crates/relay-compiler/src/lib.rs
+++ compiler/crates/relay-compiler/src/lib.rs
@@ -22,7 +22,7 @@ mod watchman;
 
 pub use build_project::{
     add_to_mercurial, apply_transforms,
-    artifact_writer::{ArtifactDifferenceWriter, ArtifactFileWriter},
+    artifact_writer::{ArtifactDifferenceWriter, ArtifactFileWriter, ArtifactWriter},
     build_schema, check_project, create_path_for_artifact, generate_artifacts,
     generate_extra_artifacts::{GenerateExtraArtifactArgs, GenerateExtraArtifactsFn},
     validate, Artifact, ArtifactContent, Programs, SourceHashes,

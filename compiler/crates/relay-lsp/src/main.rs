042dff997bc029f326803feaa109056d19174e36
Jordan Eldredge <jeldredge@fb.com>
2020-10-20 15:01:26 -0700

    Expose LSP in fb_relay_compiler binary
    Reviewed By: rbalicki2
    
    Differential Revision: D24404052
    
    fbshipit-source-id: 169f6e75127d58a6fff5986b2fa857948d03c65b

--- compiler/crates/relay-lsp/src/main.rs
+++ compiler/crates/relay-lsp/src/main.rs
@@ -7,60 +7,12 @@
 
 #![warn(clippy::all)]
 
-mod client;
-mod completion;
-mod error;
-mod error_reporting;
-mod lsp;
-mod lsp_compiler;
-mod server;
-mod state;
-mod text_documents;
-use lsp_server::Connection;
-
 use env_logger::Env;
-use error::Result;
-use log::info;
+use relay_language_server::error::Result;
+use relay_language_server::start_language_server;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     env_logger::from_env(Env::default().default_filter_or("info, warn, error, debug")).init();
-    let (connection, io_handles) = Connection::stdio();
-    info!("Initialized stdio transport layer");
-    let params = server::initialize(&connection)?;
-    info!("JSON-RPC handshake completed");
-    server::run(connection, params).await?;
-    io_handles.join()?;
-    Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::client;
-    use super::error::Result;
-    use super::server;
-    use lsp_server::Connection;
-    use lsp_types::{ClientCapabilities, InitializeParams};
-    #[test]
-    fn initialize() -> Result<()> {
-        // Test with an in-memory connection pair
-        let (connection, client) = Connection::memory();
-        // Mock set of client parameters. The `root_path` field is deprecated, but
-        // still required to construct the params, so we allow deprecated fields here.
-        #[allow(deprecated)]
-        let init_params = InitializeParams {
-            process_id: Some(1),
-            root_path: None,
-            root_uri: None,
-            initialization_options: None,
-            capabilities: ClientCapabilities::default(),
-            trace: None,
-            workspace_folders: None,
-            client_info: None,
-        };
-        client::initialize(&client, &init_params, 0);
-        let params = server::initialize(&connection)?;
-        assert_eq!(params, init_params);
-        Ok(())
-    }
+    start_language_server().await
 }

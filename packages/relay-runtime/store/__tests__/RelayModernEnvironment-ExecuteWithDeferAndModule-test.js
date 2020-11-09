c9a137df8090046a4c926d66161f1e179c299e64
Luna Ruan <luna@fb.com>
2020-11-09 11:35:58 -0800

    Allow using @defer and @module
    Reviewed By: jstejada
    
    Differential Revision: D24583080
    
    fbshipit-source-id: 7465fc23970a48b45a7c706be2bf23926c52e5df

---
+++ packages/relay-runtime/store/__tests__/RelayModernEnvironment-ExecuteWithDeferAndModule-test.js
@@ -0,0 +1,255 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @format
+ * @flow
+ * @emails oncall+relay
+ */
+
+// flowlint ambiguous-object-type:error
+
+'use strict';
+
+const QueryUserNormalizationFragment = require('./__generated__/RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user$normalization.graphql');
+const RelayModernEnvironment = require('../RelayModernEnvironment');
+const RelayModernStore = require('../RelayModernStore');
+const RelayNetwork = require('../../network/RelayNetwork');
+const RelayObservable = require('../../network/RelayObservable');
+const RelayRecordSource = require('../RelayRecordSource');
+
+const {
+  createOperationDescriptor,
+} = require('../RelayModernOperationDescriptor');
+const {createReaderSelector} = require('../RelayModernSelector');
+const {graphql} = require('relay-runtime');
+
+import type {NormalizationRootNode} from '../../util/NormalizationNode';
+import type {ReaderFragment} from '../../util/ReaderNode';
+import type {ConcreteRequest} from '../../util/RelayConcreteNode';
+
+describe('execute() a query with @defer and @module', () => {
+  let callbacks;
+  let complete;
+  let dataSource;
+  let environment;
+  let error;
+  let fetch;
+  let fragment;
+  let next;
+  let operation;
+  let operationLoader: {|
+    +get: (reference: mixed) => ?NormalizationRootNode,
+    load: JestMockFn<$ReadOnlyArray<mixed>, Promise<?NormalizationRootNode>>,
+  |};
+  let operationCallback;
+  let query;
+  let resolveFragment;
+  let selector;
+  let source;
+  let store;
+  let variables;
+
+  beforeEach(() => {
+    jest.resetModules();
+    jest.mock('warning');
+    jest.spyOn(console, 'warn').mockImplementation(() => undefined);
+
+    query = graphql`
+      query RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery($id: ID!) {
+        node(id: $id) {
+          ...RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user
+            @defer(
+              label: "RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user"
+            )
+            @module(name: "User.react")
+        }
+      }
+    `;
+    fragment = graphql`
+      fragment RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user on User {
+        id
+        name
+      }
+    `;
+    variables = {id: '1'};
+    operation = createOperationDescriptor(
+      ((query: any): ConcreteRequest),
+      variables,
+    );
+    selector = createReaderSelector(
+      ((fragment: any): ReaderFragment),
+      '1',
+      {},
+      operation.request,
+    );
+
+    complete = jest.fn();
+    error = jest.fn();
+    next = jest.fn();
+    callbacks = {complete, error, next};
+    fetch = (_query, _variables, _cacheConfig) => {
+      return RelayObservable.create(sink => {
+        dataSource = sink;
+      });
+    };
+    operationLoader = {
+      load: jest.fn(moduleName => {
+        return new Promise(resolve => {
+          resolveFragment = resolve;
+        });
+      }),
+      get: jest.fn(),
+    };
+    source = RelayRecordSource.create();
+    store = new RelayModernStore(source);
+
+    environment = new RelayModernEnvironment({
+      network: RelayNetwork.create(fetch),
+      store,
+      operationLoader,
+    });
+
+    const operationSnapshot = environment.lookup(operation.fragment);
+    operationCallback = jest.fn();
+    environment.subscribe(operationSnapshot, operationCallback);
+  });
+
+  it('calls next() and publishes the initial payload to the store', () => {
+    const initialSnapshot = environment.lookup(selector);
+
+    const callback = jest.fn();
+    environment.subscribe(initialSnapshot, callback);
+    environment.execute({operation}).subscribe(callbacks);
+    const payload = {
+      data: {
+        node: {
+          id: '1',
+          __typename: 'User',
+        },
+      },
+    };
+
+    dataSource.next(payload);
+    jest.runAllTimers();
+
+    expect(next.mock.calls.length).toBe(1);
+    expect(complete).not.toBeCalled();
+    expect(error).not.toBeCalled();
+    expect(callback.mock.calls.length).toBe(1);
+    const snapshot = callback.mock.calls[0][0];
+    expect(snapshot.isMissingData).toBe(true);
+    expect(snapshot.data).toEqual({
+      id: '1',
+      name: undefined,
+    });
+  });
+
+  it('processes deferred payloads', () => {
+    const initialSnapshot = environment.lookup(selector);
+    const callback = jest.fn();
+    environment.subscribe(initialSnapshot, callback);
+
+    environment.execute({operation}).subscribe(callbacks);
+    dataSource.next({
+      data: {
+        node: {
+          id: '1',
+          __typename: 'User',
+        },
+      },
+    });
+    jest.runAllTimers();
+    expect(operationCallback).toBeCalledTimes(1);
+    next.mockClear();
+    callback.mockClear();
+    operationCallback.mockClear();
+
+    dataSource.next({
+      data: {
+        id: '1',
+        name: 'joe',
+        __typename: 'User',
+        __module_component_RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery:
+          'User.react',
+        __module_operation_RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery:
+          'RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user$normalization.graphql',
+      },
+      label:
+        'RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery$defer$RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user',
+      path: ['node'],
+    });
+
+    expect(operationLoader.load).toBeCalledTimes(1);
+    expect(operationLoader.load.mock.calls[0][0]).toEqual(
+      'RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user$normalization.graphql',
+    );
+
+    resolveFragment(QueryUserNormalizationFragment);
+    jest.runAllTimers();
+    operationCallback.mockClear();
+
+    expect(complete).toBeCalledTimes(0);
+    expect(error).toBeCalledTimes(0);
+    expect(next).toBeCalledTimes(1);
+    expect(callback).toBeCalledTimes(1);
+    const snapshot = callback.mock.calls[0][0];
+    expect(snapshot.isMissingData).toBe(false);
+    expect(snapshot.data).toEqual({
+      id: '1',
+      name: 'joe',
+    });
+  });
+
+  it('synchronously normalizes deferred payload if normalization fragment avaiable synchronously', () => {
+    const initialSnapshot = environment.lookup(selector);
+    const callback = jest.fn();
+    environment.subscribe(initialSnapshot, callback);
+
+    environment.execute({operation}).subscribe(callbacks);
+    jest
+      .spyOn(operationLoader, 'get')
+      .mockImplementationOnce(() => QueryUserNormalizationFragment);
+
+    dataSource.next({
+      data: {
+        node: {
+          id: '1',
+          __typename: 'User',
+        },
+      },
+    });
+
+    jest.runAllTimers();
+    next.mockClear();
+    callback.mockClear();
+    expect(operationCallback).toBeCalledTimes(1);
+
+    dataSource.next({
+      data: {
+        id: '1',
+        name: 'joe',
+        __typename: 'User',
+        __module_component_RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery:
+          'User.react',
+        __module_operation_RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery:
+          'RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user$normalization.graphql',
+      },
+      label:
+        'RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery$defer$RelayModernEnvironmentExecuteWithDeferAndModuleTestQuery_user',
+      path: ['node'],
+    });
+
+    expect(operationLoader.get).toBeCalledTimes(1);
+    operationCallback.mockClear();
+
+    const snapshot = callback.mock.calls[0][0];
+    expect(snapshot.isMissingData).toBe(false);
+    expect(snapshot.data).toEqual({
+      id: '1',
+      name: 'joe',
+    });
+  });
+});

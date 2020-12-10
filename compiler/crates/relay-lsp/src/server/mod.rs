25be5dbef8e6e71bb3eeff1ea1f9bcfff0067eb9
Tianyu Yao <skyyao@fb.com>
2020-12-10 07:55:30 -0800

    Trigger autocomplete on new line and ","
    Reviewed By: rbalicki2
    
    Differential Revision: D25441290
    
    fbshipit-source-id: f13bc6ccc13bbb853a896e6200824e42a27fc5d1

--- compiler/crates/relay-lsp/src/server/mod.rs
+++ compiler/crates/relay-lsp/src/server/mod.rs
@@ -52,7 +52,7 @@ pub fn initialize(connection: &Connection) -> LSPProcessResult<InitializeParams>
 
     server_capabilities.completion_provider = Some(CompletionOptions {
         resolve_provider: Some(true),
-        trigger_characters: Some(vec!["(".into(), "{".into()]),
+        trigger_characters: Some(vec!["(".into(), "{".into(), "\n".into(), ",".into()]),
         work_done_progress_options: WorkDoneProgressOptions {
             work_done_progress: None,
         },

20051f61f8d6d7d9e4e99cd228c75a92a23c75be
Andrey Lunyov <alunyov@fb.com>
2021-01-13 06:14:18 -0800

    Convert ReactRelayRefetchContainer-WithFragmentOwnership-test to use the new compiler
    Summary:
    We're working on deprecating JS compiler.
    One of the steps, is to remove it from our unit-tests.
    
    This is the test from https://github.com/facebook/relay/issues/3199
    
    Steps:
    - Remove generateAndCompile
    - Wrap graphql documents with graphql tag + add unique prefix
    - Run new compiler: scripts/compile-tests.sh
    
    Reviewed By: tyao1
    
    Differential Revision: D25875336
    
    fbshipit-source-id: 0be387d9881a615cca47bd6a05a0249b10334d49

---
+++ packages/react-relay/__tests__/__generated__/ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment.graphql.js
@@ -0,0 +1,121 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<da7434e737ded60e040490bedc2b9e93>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ReaderFragment } from 'relay-runtime';
+type ReactRelayRefetchContainerWithFragmentOwnershipTestUserFriendFragment$ref = any;
+import type { FragmentReference } from "relay-runtime";
+declare export opaque type ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$ref: FragmentReference;
+declare export opaque type ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$fragmentType: ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$ref;
+export type ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment = {|
+  +id: string,
+  +name?: ?string,
+  +profile_picture: ?{|
+    +uri: ?string
+  |},
+  +$fragmentRefs: ReactRelayRefetchContainerWithFragmentOwnershipTestUserFriendFragment$ref,
+  +$refType: ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$ref,
+|};
+export type ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$data = ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment;
+export type ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$key = {
+  +$data?: ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$data,
+  +$fragmentRefs: ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment$ref,
+  ...
+};
+*/
+
+var node/*: ReaderFragment*/ = {
+  "argumentDefinitions": [
+    {
+      "defaultValue": true,
+      "kind": "LocalArgument",
+      "name": "cond"
+    },
+    {
+      "kind": "RootArgument",
+      "name": "scale"
+    }
+  ],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "ReactRelayRefetchContainerWithFragmentOwnershipTestUserFragment",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "id",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": [
+        {
+          "kind": "Variable",
+          "name": "scale",
+          "variableName": "scale"
+        }
+      ],
+      "concreteType": "Image",
+      "kind": "LinkedField",
+      "name": "profile_picture",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "uri",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "condition": "cond",
+      "kind": "Condition",
+      "passingValue": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "name",
+          "storageKey": null
+        }
+      ]
+    },
+    {
+      "args": [
+        {
+          "kind": "Variable",
+          "name": "cond",
+          "variableName": "cond"
+        }
+      ],
+      "kind": "FragmentSpread",
+      "name": "ReactRelayRefetchContainerWithFragmentOwnershipTestUserFriendFragment"
+    }
+  ],
+  "type": "User",
+  "abstractKey": null
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "00fbadab26a32ac69d9c6d12533b85f2";
+}
+
+module.exports = node;

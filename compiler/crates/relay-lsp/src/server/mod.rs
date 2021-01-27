68c2f221a66afea6be7f2b25b8c6362f0a7dea75
Tianyu Yao <skyyao@fb.com>
2021-01-27 07:59:12 -0800

    Remove "{" from auto complete trigger characters
    Reviewed By: alunyov
    
    Differential Revision: D26088839
    
    fbshipit-source-id: dc145641d7ff8d41cad5e6026dacc6f29a5bb545

--- compiler/crates/relay-lsp/src/server/mod.rs
+++ compiler/crates/relay-lsp/src/server/mod.rs
@@ -55,13 +55,7 @@ pub fn initialize(connection: &Connection) -> LSPProcessResult<InitializeParams>
 
     server_capabilities.completion_provider = Some(CompletionOptions {
         resolve_provider: Some(true),
-        trigger_characters: Some(vec![
-            "(".into(),
-            "{".into(),
-            "\n".into(),
-            ",".into(),
-            "@".into(),
-        ]),
+        trigger_characters: Some(vec!["(".into(), "\n".into(), ",".into(), "@".into()]),
         work_done_progress_options: WorkDoneProgressOptions {
             work_done_progress: None,
         },

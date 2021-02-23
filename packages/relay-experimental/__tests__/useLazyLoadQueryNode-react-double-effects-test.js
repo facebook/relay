a502c49afde3f9fc204a976b25722c953b6e8684
Juan Tejada <jstejada@fb.com>
2021-02-22 16:47:44 -0800

    Verify useLazyLoadQuery supports React double invoke effects when using incremental delivery
    Reviewed By: rbalicki2
    
    Differential Revision: D26584244
    
    fbshipit-source-id: b0196423cb49cca7ea1b41efdf37e794b4d77be9

--- packages/relay-experimental/__tests__/useLazyLoadQueryNode-react-double-effects-test.js
+++ packages/relay-experimental/__tests__/useLazyLoadQueryNode-react-double-effects-test.js
@@ -17,15 +17,12 @@ const React = require('react');
 const ReactTestRenderer = require('react-test-renderer');
 const RelayEnvironmentProvider = require('../RelayEnvironmentProvider');
 
-const useLazyLoadQueryNode = require('../useLazyLoadQueryNode');
+const useLazyLoadQuery = require('../useLazyLoadQuery');
 
 const {useEffect} = require('react');
-const {
-  createOperationDescriptor,
-  __internal: {fetchQuery},
-} = require('relay-runtime');
+const {Observable, createOperationDescriptor} = require('relay-runtime');
 
-function expectToHaveFetched(environment, query, cacheConfig) {
+function expectToHaveFetched(environment, query) {
   expect(environment.execute).toBeCalledTimes(1);
   expect(environment.execute.mock.calls[0][0].operation).toMatchObject({
     fragment: expect.anything(),
@@ -36,11 +33,9 @@ function expectToHaveFetched(environment, query, cacheConfig) {
     root: expect.anything(),
   });
   expect(
-    environment.mock.isLoading(
-      query.request.node,
-      query.request.variables,
-      cacheConfig,
-    ),
+    environment.mock.isLoading(query.request.node, query.request.variables, {
+      force: true,
+    }),
   ).toEqual(true);
 }
 
@@ -48,15 +43,23 @@ function expectToHaveFetched(environment, query, cacheConfig) {
 describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
   let environment;
   let gqlQuery;
+  let gqlQueryWithDefer;
   let createMockEnvironment;
   let generateAndCompile;
   let query;
+  let queryWithDefer;
   let variables;
   let release;
+  let cancelNetworkRequest;
 
   beforeEach(() => {
-    jest.mock('scheduler', () => require('scheduler/unstable_mock'));
     jest.spyOn(console, 'warn').mockImplementationOnce(() => {});
+    jest.mock('ReactFeatureFlags', () => {
+      return {
+        ...jest.requireActual('ReactFeatureFlags'),
+        enableDoubleInvokingEffects: true,
+      };
+    });
 
     ({
       createMockEnvironment,
@@ -77,9 +80,23 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
       };
     });
 
+    cancelNetworkRequest = jest.fn();
+    const originalExecute = environment.execute;
+    (environment: $FlowFixMe).execute = jest.fn((...args) => {
+      const originalObservable = originalExecute(...args);
+
+      return Observable.create(sink => {
+        const sub = originalObservable.subscribe(sink);
+        return () => {
+          cancelNetworkRequest();
+          sub.unsubscribe();
+        };
+      });
+    });
+
     const generated = generateAndCompile(`
       fragment UserFragment on User {
-        name
+        firstName
       }
 
       query UserQuery($id: ID) {
@@ -89,10 +106,22 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
           ...UserFragment
         }
       }
+
+      query UserQueryWithDefer($id: ID) {
+        node(id: $id) {
+          id
+          name
+          ...UserFragment @defer
+        }
+      }
     `);
     gqlQuery = generated.UserQuery;
+    gqlQueryWithDefer = generated.UserQueryWithDefer;
     variables = {id: '1'};
-    query = createOperationDescriptor(gqlQuery, variables);
+    query = createOperationDescriptor(gqlQuery, variables, {force: true});
+    queryWithDefer = createOperationDescriptor(gqlQueryWithDefer, variables, {
+      force: true,
+    });
   });
 
   afterEach(() => {
@@ -103,11 +132,8 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
   it('forces a re-render when effects are double invoked and refetches when policy is network-only', () => {
     let renderLogs = [];
     const QueryComponent = function() {
-      const result = useLazyLoadQueryNode<_>({
-        componentDisplayName: 'TestDisplayName',
-        fetchObservable: fetchQuery(environment, query),
+      const result = useLazyLoadQuery<_>(gqlQuery, variables, {
         fetchPolicy: 'network-only',
-        query,
       });
 
       const name = result?.node?.name ?? 'Empty';
@@ -142,7 +168,7 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
     }
 
     // Assert that query is suspended
-    expectToHaveFetched(environment, query, {});
+    expectToHaveFetched(environment, query);
     expect(environment.retain).toHaveBeenCalledTimes(1);
     expect(renderLogs).toEqual([]);
     expect(instance.toJSON()).toEqual('Fallback');
@@ -154,6 +180,7 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
         data: {
           node: {
             __typename: 'User',
+            firstName: 'Alice',
             id: variables.id,
             name: 'Alice 1',
           },
@@ -169,11 +196,13 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
     // The effect cleanup will execute, so we assert that
     // the query is disposed:
     expect(release).toHaveBeenCalledTimes(1);
+    // Unsubscribing after completion is a no-op
+    expect(cancelNetworkRequest).toHaveBeenCalledTimes(1);
 
     // The effect setup will re-execute, so we assert that
     // a re-render is triggered to refetch, re-retain, and
     // re-suspend:
-    expectToHaveFetched(environment, query, {});
+    expectToHaveFetched(environment, query);
     expect(environment.retain).toHaveBeenCalledTimes(2);
     expect(instance.toJSON()).toEqual('Fallback');
 
@@ -198,6 +227,7 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
         data: {
           node: {
             __typename: 'User',
+            firstName: 'Alice',
             id: variables.id,
             name: 'Alice 2',
           },
@@ -230,11 +260,8 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
   it('forces a re-render when effects are double invoked and does not refetch when policy is store-or-network', () => {
     const renderLogs = [];
     const QueryComponent = function() {
-      const result = useLazyLoadQueryNode<_>({
-        componentDisplayName: 'TestDisplayName',
-        fetchObservable: fetchQuery(environment, query),
+      const result = useLazyLoadQuery<_>(gqlQuery, variables, {
         fetchPolicy: 'store-or-network',
-        query,
       });
 
       const name = result?.node?.name ?? 'Empty';
@@ -269,7 +296,7 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
     }
 
     // Assert that query is suspended
-    expectToHaveFetched(environment, query, {});
+    expectToHaveFetched(environment, query);
     expect(environment.retain).toHaveBeenCalledTimes(1);
     expect(renderLogs).toEqual([]);
     expect(instance.toJSON()).toEqual('Fallback');
@@ -281,6 +308,7 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
         data: {
           node: {
             __typename: 'User',
+            firstName: 'Alice',
             id: variables.id,
             name: 'Alice 1',
           },
@@ -296,6 +324,8 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
     // The effect cleanup will execute, so we assert that
     // the query is disposed:
     expect(release).toHaveBeenCalledTimes(1);
+    // Unsubscribing after completion is a no-op
+    expect(cancelNetworkRequest).toHaveBeenCalledTimes(1);
 
     // The effect setup will re-execute, so we assert that
     // a re-render is triggered to re-retain, but it does not
@@ -328,5 +358,309 @@ describe.skip('useLazyLoadQueryNode-react-double-effects', () => {
     });
     expect(release).toHaveBeenCalledTimes(1);
     expect(environment.retain).toHaveBeenCalledTimes(2);
+  });
+
+  describe('with incremental delivery', () => {
+    it('forces a re-render when effects are double invoked and refetches when policy is network-only', () => {
+      let renderLogs = [];
+      const QueryComponent = function() {
+        const result = useLazyLoadQuery<_>(gqlQueryWithDefer, variables, {
+          fetchPolicy: 'network-only',
+        });
+
+        const name = result?.node?.name ?? 'Empty';
+        useEffect(() => {
+          renderLogs.push(`commit: ${name}`);
+          return () => {
+            renderLogs.push(`cleanup: ${name}`);
+          };
+        }, [name]);
+
+        renderLogs.push(`render: ${name}`);
+        return name;
+      };
+
+      let instance;
+      ReactTestRenderer.act(() => {
+        instance = ReactTestRenderer.create(
+          // Using StrictMode will trigger double invoke effect behavior
+          <React.StrictMode>
+            <RelayEnvironmentProvider environment={environment}>
+              <React.Suspense fallback="Fallback">
+                <QueryComponent variables={variables} />
+              </React.Suspense>
+            </RelayEnvironmentProvider>
+          </React.StrictMode>,
+          // $FlowFixMe
+          {unstable_isConcurrent: true},
+        );
+      });
+      if (!instance) {
+        throw new Error('Failed to render during test.');
+      }
+
+      // Assert that query is suspended
+      expectToHaveFetched(environment, queryWithDefer);
+      expect(environment.retain).toHaveBeenCalledTimes(1);
+      expect(renderLogs).toEqual([]);
+      expect(instance.toJSON()).toEqual('Fallback');
+
+      // Resolve network response
+      environment.execute.mockClear();
+      ReactTestRenderer.act(() => {
+        environment.mock.nextValue(gqlQueryWithDefer, {
+          data: {
+            node: {
+              __typename: 'User',
+              id: variables.id,
+              name: 'Alice 1',
+            },
+          },
+        });
+        jest.runAllImmediates();
+      });
+
+      // After the first payload of the query resolves, the component
+      // will mount, and React double invoke effects will be triggered,
+      // simulating what would happen if the component was hidden and re-shown:
+
+      // The effect cleanup will execute, so we assert that
+      // the query is disposed and the current network request is cancelled:
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(cancelNetworkRequest).toHaveBeenCalledTimes(1);
+
+      // The effect setup will re-execute, so we assert that
+      // a re-render is triggered to refetch, re-retain, and
+      // re-suspend:
+      expectToHaveFetched(environment, queryWithDefer);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+      expect(instance.toJSON()).toEqual('Fallback');
+
+      // Assert render state of component using the query up until
+      // the point of re-suspending:
+      expect(renderLogs).toEqual([
+        // Assert component rendered and committed when network resolved:
+        'render: Alice 1',
+        'commit: Alice 1',
+
+        // Assert double invoked effects simulates an unmount and remount.
+        // Note that render doesn't happen in between:
+        'cleanup: Alice 1',
+        'commit: Alice 1',
+      ]);
+
+      // Resolve second network response
+      renderLogs = [];
+      environment.execute.mockClear();
+      ReactTestRenderer.act(() => {
+        environment.mock.nextValue(gqlQueryWithDefer, {
+          data: {
+            node: {
+              __typename: 'User',
+              id: variables.id,
+              name: 'Alice 2',
+            },
+          },
+        });
+        jest.runAllImmediates();
+      });
+
+      // Assert that after refetch: double invoke effects don't trigger
+      // again, and that we don't trigger a second refetch
+      expect(environment.execute).toHaveBeenCalledTimes(0);
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+      expect(renderLogs).toEqual([
+        'render: Alice 2',
+        'cleanup: Alice 1',
+        'commit: Alice 2',
+      ]);
+      expect(instance.toJSON()).toEqual('Alice 2');
+
+      // Resolve incremental payload for second network request
+      ReactTestRenderer.act(() => {
+        environment.mock.resolve(gqlQueryWithDefer, {
+          data: {
+            __typename: 'User',
+            firstName: 'Alice',
+            id: variables.id,
+          },
+          label: 'UserQueryWithDefer$defer$UserFragment',
+          path: ['node'],
+        });
+        jest.runAllImmediates();
+      });
+
+      // Assert that after refetch: double invoke effects don't trigger
+      // again, and that we don't trigger a second refetch
+      expect(environment.execute).toHaveBeenCalledTimes(0);
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+      expect(renderLogs).toEqual([
+        'render: Alice 2',
+        'cleanup: Alice 1',
+        'commit: Alice 2',
+      ]);
+      expect(instance.toJSON()).toEqual('Alice 2');
+
+      // Assert that query was correctly permanently retained,
+      // and not released after a timeout
+      ReactTestRenderer.act(() => {
+        jest.runAllTimers();
+      });
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+    });
+
+    it('forces a re-render when effects are double invoked and refetches when policy is store-or-network', () => {
+      let renderLogs = [];
+      const QueryComponent = function() {
+        const result = useLazyLoadQuery<_>(gqlQueryWithDefer, variables, {
+          fetchPolicy: 'store-or-network',
+        });
+
+        const name = result?.node?.name ?? 'Empty';
+        useEffect(() => {
+          renderLogs.push(`commit: ${name}`);
+          return () => {
+            renderLogs.push(`cleanup: ${name}`);
+          };
+        }, [name]);
+
+        renderLogs.push(`render: ${name}`);
+        return name;
+      };
+
+      let instance;
+      ReactTestRenderer.act(() => {
+        instance = ReactTestRenderer.create(
+          // Using StrictMode will trigger double invoke effect behavior
+          <React.StrictMode>
+            <RelayEnvironmentProvider environment={environment}>
+              <React.Suspense fallback="Fallback">
+                <QueryComponent variables={variables} />
+              </React.Suspense>
+            </RelayEnvironmentProvider>
+          </React.StrictMode>,
+          // $FlowFixMe
+          {unstable_isConcurrent: true},
+        );
+      });
+      if (!instance) {
+        throw new Error('Failed to render during test.');
+      }
+
+      // Assert that query is suspended
+      expectToHaveFetched(environment, queryWithDefer);
+      expect(environment.retain).toHaveBeenCalledTimes(1);
+      expect(renderLogs).toEqual([]);
+      expect(instance.toJSON()).toEqual('Fallback');
+
+      // Resolve network response
+      environment.execute.mockClear();
+      ReactTestRenderer.act(() => {
+        environment.mock.nextValue(gqlQueryWithDefer, {
+          data: {
+            node: {
+              __typename: 'User',
+              id: variables.id,
+              name: 'Alice 1',
+            },
+          },
+        });
+        jest.runAllImmediates();
+      });
+
+      // After the first payload of the query resolves, the component
+      // will mount, and React double invoke effects will be triggered,
+      // simulating what would happen if the component was hidden and re-shown:
+
+      // The effect cleanup will execute, so we assert that
+      // the query is disposed and the current network request is cancelled:
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(cancelNetworkRequest).toHaveBeenCalledTimes(1);
+
+      // The effect setup will re-execute, so we assert that
+      // a re-render is triggered to refetch, re-retain, and
+      // re-suspend:
+      expectToHaveFetched(environment, queryWithDefer);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+      expect(instance.toJSON()).toEqual('Fallback');
+
+      // Assert render state of component using the query up until
+      // the point of re-suspending:
+      expect(renderLogs).toEqual([
+        // Assert component rendered and committed when network resolved:
+        'render: Alice 1',
+        'commit: Alice 1',
+
+        // Assert double invoked effects simulates an unmount and remount.
+        // Note that render doesn't happen in between:
+        'cleanup: Alice 1',
+        'commit: Alice 1',
+      ]);
+
+      // Resolve second network response
+      renderLogs = [];
+      environment.execute.mockClear();
+      ReactTestRenderer.act(() => {
+        environment.mock.nextValue(gqlQueryWithDefer, {
+          data: {
+            node: {
+              __typename: 'User',
+              id: variables.id,
+              name: 'Alice 2',
+            },
+          },
+        });
+        jest.runAllImmediates();
+      });
+
+      // Assert that after refetch: double invoke effects don't trigger
+      // again, and that we don't trigger a second refetch
+      expect(environment.execute).toHaveBeenCalledTimes(0);
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+      expect(renderLogs).toEqual([
+        'render: Alice 2',
+        'cleanup: Alice 1',
+        'commit: Alice 2',
+      ]);
+      expect(instance.toJSON()).toEqual('Alice 2');
+
+      // Resolve incremental payload for second network request
+      ReactTestRenderer.act(() => {
+        environment.mock.resolve(gqlQueryWithDefer, {
+          data: {
+            __typename: 'User',
+            firstName: 'Alice',
+            id: variables.id,
+          },
+          label: 'UserQueryWithDefer$defer$UserFragment',
+          path: ['node'],
+        });
+        jest.runAllImmediates();
+      });
+
+      // Assert that after refetch: double invoke effects don't trigger
+      // again, and that we don't trigger a second refetch
+      expect(environment.execute).toHaveBeenCalledTimes(0);
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+      expect(renderLogs).toEqual([
+        'render: Alice 2',
+        'cleanup: Alice 1',
+        'commit: Alice 2',
+      ]);
+      expect(instance.toJSON()).toEqual('Alice 2');
+
+      // Assert that query was correctly permanently retained,
+      // and not released after a timeout
+      ReactTestRenderer.act(() => {
+        jest.runAllTimers();
+      });
+      expect(release).toHaveBeenCalledTimes(1);
+      expect(environment.retain).toHaveBeenCalledTimes(2);
+    });
   });
 });

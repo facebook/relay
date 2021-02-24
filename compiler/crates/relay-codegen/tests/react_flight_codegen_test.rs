c75f90b76c6fb9067edf420baa1a9435170e03d7
Jan Kassens <jkassens@fb.com>
2021-02-24 11:15:54 -0800

    fail Flight transform when errors are found
    Reviewed By: ginfung
    
    Differential Revision: D26639756
    
    fbshipit-source-id: 597f80cdb56101a858dcc0739ab95b7f9024ebc1

--- compiler/crates/relay-codegen/tests/react_flight_codegen_test.rs
+++ compiler/crates/relay-codegen/tests/react_flight_codegen_test.rs
@@ -4,13 +4,20 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<663a08ce2f55bbc992ce9c3920d8ab88>>
+ * @generated SignedSource<<b79ece844b76ca22207725fcb597f8ce>>
  */
 
 mod react_flight_codegen;
 
 use react_flight_codegen::transform_fixture;
 use fixture_tests::test_fixture;
+
+#[test]
+fn flight_invalid() {
+    let input = include_str!("react_flight_codegen/fixtures/flight.invalid.graphql");
+    let expected = include_str!("react_flight_codegen/fixtures/flight.invalid.expected");
+    test_fixture(transform_fixture, "flight.invalid.graphql", "react_flight_codegen/fixtures/flight.invalid.expected", input, expected);
+}
 
 #[test]
 fn flight_props() {

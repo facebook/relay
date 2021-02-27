8e9814ee7542d94a89b95f5cb8d9181947437134
Andrey Lunyov <alunyov@fb.com>
2021-02-26 18:48:58 -0800

    Add a sandcastle validation specifically for tests
    Reviewed By: josephsavona
    
    Differential Revision: D26692858
    
    fbshipit-source-id: 63c61d65b074f298d63297f334621e9feecb13b0

--- packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentNoInlineTest_noInline$normalization.graphql.js
+++ packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentNoInlineTest_noInline$normalization.graphql.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<12b54d789a26bef4abcff6ff679f9269>>
+ * @generated SignedSource<<d9cd5d18e8d5aadd5cba147a5af78628>>
  * @flow
  * @lightSyntaxTransform
  * @nogrep
@@ -16,6 +16,15 @@
 
 /*::
 import type { NormalizationSplitOperation } from 'relay-runtime';
+
+export type RelayModernEnvironmentNoInlineTest_noInline$normalization = {|
+  +profile_picture: ?{|
+    +uri: ?string
+  |},
+  +profile_picture_inner: ?{|
+    +uri: ?string
+  |},
+|};
 
 */
 

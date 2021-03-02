1eb96681f0177c3fa03d6a6e6ccaba6cfacbff1e
Rene Murillo <renem@fb.com>
2021-03-02 00:29:33 -0800

    Convert RelayModernEnvironment-ExecuteWithOverlappingModule-test.js to graphql tag
    Reviewed By: alunyov
    
    Differential Revision: D26715247
    
    fbshipit-source-id: 62884d254331393e0a9033db8af9fb1b994b81ca

---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name$normalization.graphql.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<6e382cfc5328cd36e893f86532470c39>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { NormalizationSplitOperation } from 'relay-runtime';
+
+*/
+
+var node/*: NormalizationSplitOperation*/ = {
+  "kind": "SplitOperation",
+  "metadata": {},
+  "name": "RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name$normalization",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "plaintext",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "PlainUserNameData",
+      "kind": "LinkedField",
+      "name": "data",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "text",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "id",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ]
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "771a707877f5334e80dff3cf8d305a68";
+}
+
+module.exports = node;
---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery.graphql.js
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<de1c0a19b7462f602825c4f2fd1f3ada>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+// @dataDrivenDependency RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery.node.nameRenderer {"branches":{"MarkdownUserNameRenderer":{"component":"MarkdownUserNameRenderer.react","fragment":"RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name$normalization.graphql"},"PlainUserNameRenderer":{"component":"PlainUserNameRenderer.react","fragment":"RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name$normalization.graphql"}},"plural":false}
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+type RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name$ref = any;
+type RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name$ref = any;
+export type RelayModernEnvironmentExecuteWithOverlappingModuleTestQueryVariables = {|
+  id: string,
+|};
+export type RelayModernEnvironmentExecuteWithOverlappingModuleTestQueryResponse = {|
+  +node: ?{|
+    +nameRenderer?: ?{|
+      +__fragmentPropName?: ?string,
+      +__module_component?: ?string,
+      +$fragmentRefs: RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name$ref & RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name$ref,
+    |},
+  |},
+|};
+export type RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery = {|
+  variables: RelayModernEnvironmentExecuteWithOverlappingModuleTestQueryVariables,
+  response: RelayModernEnvironmentExecuteWithOverlappingModuleTestQueryResponse,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  }
+],
+v2 = {
+  "kind": "InlineFragment",
+  "selections": [
+    {
+      "documentName": "RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery",
+      "fragmentName": "RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name",
+      "fragmentPropName": "name",
+      "kind": "ModuleImport"
+    }
+  ],
+  "type": "MarkdownUserNameRenderer",
+  "abstractKey": null
+},
+v3 = {
+  "kind": "InlineFragment",
+  "selections": [
+    {
+      "documentName": "RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery",
+      "fragmentName": "RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name",
+      "fragmentPropName": "name",
+      "kind": "ModuleImport"
+    }
+  ],
+  "type": "PlainUserNameRenderer",
+  "abstractKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": null,
+                "kind": "LinkedField",
+                "name": "nameRenderer",
+                "plural": false,
+                "selections": [
+                  (v2/*: any*/),
+                  (v3/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "type": "User",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v4/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": null,
+                "kind": "LinkedField",
+                "name": "nameRenderer",
+                "plural": false,
+                "selections": [
+                  (v4/*: any*/),
+                  (v2/*: any*/),
+                  (v3/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "type": "User",
+            "abstractKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "f2718e4ee5d2fafa8940ba7fba368456",
+    "id": null,
+    "metadata": {},
+    "name": "RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery",
+    "operationKind": "query",
+    "text": "query RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery(\n  $id: ID!\n) {\n  node(id: $id) {\n    __typename\n    ... on User {\n      nameRenderer {\n        __typename\n        ... on MarkdownUserNameRenderer {\n          ...RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name\n          __module_operation_RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery: js(module: \"RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name$normalization.graphql\", id: \"RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery.node.nameRenderer\")\n          __module_component_RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery: js(module: \"MarkdownUserNameRenderer.react\", id: \"RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery.node.nameRenderer\")\n        }\n        ... on PlainUserNameRenderer {\n          ...RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name\n          __module_operation_RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery: js(module: \"RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name$normalization.graphql\", id: \"RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery.node.nameRenderer\")\n          __module_component_RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery: js(module: \"PlainUserNameRenderer.react\", id: \"RelayModernEnvironmentExecuteWithOverlappingModuleTestQuery.node.nameRenderer\")\n        }\n      }\n    }\n    id\n  }\n}\n\nfragment RelayModernEnvironmentExecuteWithOverlappingModuleTestMarkdownUserNameRenderer_name on MarkdownUserNameRenderer {\n  __typename\n  markdown\n  data {\n    markup\n    id\n  }\n}\n\nfragment RelayModernEnvironmentExecuteWithOverlappingModuleTestPlainUserNameRenderer_name on PlainUserNameRenderer {\n  plaintext\n  data {\n    text\n    id\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "67220472e4a3160320bb23054988237e";
+}
+
+module.exports = node;

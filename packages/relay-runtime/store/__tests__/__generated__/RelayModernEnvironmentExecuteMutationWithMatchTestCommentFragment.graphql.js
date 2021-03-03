774388ea9bd21a71a6d1f35391f0753727b6006d
Jan Kassens <jkassens@fb.com>
2021-03-03 13:07:02 -0800

    convert RelayModernEnvironment-ExecuteMutationWithMatch-test
    Reviewed By: alunyov
    
    Differential Revision: D26786883
    
    fbshipit-source-id: 3f84132b34039f28d20837fcac500e452d2d505f

---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment.graphql.js
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<af4c9f011f881fb1a4f81dc1b7b78436>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+// @dataDrivenDependency RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment.actor.nameRenderer {"branches":{"MarkdownUserNameRenderer":{"component":"MarkdownUserNameRenderer.react","fragment":"RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name$normalization.graphql"},"PlainUserNameRenderer":{"component":"PlainUserNameRenderer.react","fragment":"RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name$normalization.graphql"}},"plural":false}
+
+/*::
+import type { ReaderFragment } from 'relay-runtime';
+type RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name$ref = any;
+type RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name$ref = any;
+import type { FragmentReference } from "relay-runtime";
+declare export opaque type RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$ref: FragmentReference;
+declare export opaque type RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$fragmentType: RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$ref;
+export type RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment = {|
+  +id: string,
+  +actor: ?{|
+    +name: ?string,
+    +nameRenderer: ?{|
+      +__fragmentPropName?: ?string,
+      +__module_component?: ?string,
+      +$fragmentRefs: RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name$ref & RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name$ref,
+    |},
+  |},
+  +$refType: RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$ref,
+|};
+export type RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$data = RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment;
+export type RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$key = {
+  +$data?: RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$data,
+  +$fragmentRefs: RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$ref,
+  ...
+};
+*/
+
+var node/*: ReaderFragment*/ = {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "id",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": null,
+      "kind": "LinkedField",
+      "name": "actor",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "name",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": [
+            {
+              "kind": "Literal",
+              "name": "supported",
+              "value": [
+                "PlainUserNameRenderer",
+                "MarkdownUserNameRenderer"
+              ]
+            }
+          ],
+          "concreteType": null,
+          "kind": "LinkedField",
+          "name": "nameRenderer",
+          "plural": false,
+          "selections": [
+            {
+              "kind": "InlineFragment",
+              "selections": [
+                {
+                  "documentName": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment",
+                  "fragmentName": "RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name",
+                  "fragmentPropName": "name",
+                  "kind": "ModuleImport"
+                }
+              ],
+              "type": "PlainUserNameRenderer",
+              "abstractKey": null
+            },
+            {
+              "kind": "InlineFragment",
+              "selections": [
+                {
+                  "documentName": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment",
+                  "fragmentName": "RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name",
+                  "fragmentPropName": "name",
+                  "kind": "ModuleImport"
+                }
+              ],
+              "type": "MarkdownUserNameRenderer",
+              "abstractKey": null
+            }
+          ],
+          "storageKey": "nameRenderer(supported:[\"PlainUserNameRenderer\",\"MarkdownUserNameRenderer\"])"
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "Comment",
+  "abstractKey": null
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "f1ff7fec552839f61ccff1a73226c40e";
+}
+
+module.exports = node;
---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentExecuteMutationWithMatchTestCommentQuery.graphql.js
@@ -0,0 +1,202 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<c7e4a64a506b5492bedf2717247ca658>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+type RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$ref = any;
+export type RelayModernEnvironmentExecuteMutationWithMatchTestCommentQueryVariables = {|
+  id: string,
+|};
+export type RelayModernEnvironmentExecuteMutationWithMatchTestCommentQueryResponse = {|
+  +node: ?{|
+    +id: string,
+    +$fragmentRefs: RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment$ref,
+  |},
+|};
+export type RelayModernEnvironmentExecuteMutationWithMatchTestCommentQuery = {|
+  variables: RelayModernEnvironmentExecuteMutationWithMatchTestCommentQueryVariables,
+  response: RelayModernEnvironmentExecuteMutationWithMatchTestCommentQueryResponse,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v2/*: any*/),
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v3/*: any*/),
+          (v2/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": null,
+                "kind": "LinkedField",
+                "name": "actor",
+                "plural": false,
+                "selections": [
+                  (v3/*: any*/),
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "name",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": [
+                      {
+                        "kind": "Literal",
+                        "name": "supported",
+                        "value": [
+                          "PlainUserNameRenderer",
+                          "MarkdownUserNameRenderer"
+                        ]
+                      }
+                    ],
+                    "concreteType": null,
+                    "kind": "LinkedField",
+                    "name": "nameRenderer",
+                    "plural": false,
+                    "selections": [
+                      (v3/*: any*/),
+                      {
+                        "kind": "InlineFragment",
+                        "selections": [
+                          {
+                            "documentName": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment",
+                            "fragmentName": "RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name",
+                            "fragmentPropName": "name",
+                            "kind": "ModuleImport"
+                          }
+                        ],
+                        "type": "PlainUserNameRenderer",
+                        "abstractKey": null
+                      },
+                      {
+                        "kind": "InlineFragment",
+                        "selections": [
+                          {
+                            "documentName": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment",
+                            "fragmentName": "RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name",
+                            "fragmentPropName": "name",
+                            "kind": "ModuleImport"
+                          }
+                        ],
+                        "type": "MarkdownUserNameRenderer",
+                        "abstractKey": null
+                      }
+                    ],
+                    "storageKey": "nameRenderer(supported:[\"PlainUserNameRenderer\",\"MarkdownUserNameRenderer\"])"
+                  },
+                  (v2/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "type": "Comment",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "03951fd4d968bac68fca4c91e1e0be58",
+    "id": null,
+    "metadata": {},
+    "name": "RelayModernEnvironmentExecuteMutationWithMatchTestCommentQuery",
+    "operationKind": "query",
+    "text": "query RelayModernEnvironmentExecuteMutationWithMatchTestCommentQuery(\n  $id: ID!\n) {\n  node(id: $id) {\n    __typename\n    id\n    ...RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment\n  }\n}\n\nfragment RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment on Comment {\n  id\n  actor {\n    __typename\n    name\n    nameRenderer(supported: [\"PlainUserNameRenderer\", \"MarkdownUserNameRenderer\"]) {\n      __typename\n      ... on PlainUserNameRenderer {\n        ...RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name\n        __module_operation_RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment: js(module: \"RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name$normalization.graphql\", id: \"RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment.actor.nameRenderer\")\n        __module_component_RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment: js(module: \"PlainUserNameRenderer.react\", id: \"RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment.actor.nameRenderer\")\n      }\n      ... on MarkdownUserNameRenderer {\n        ...RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name\n        __module_operation_RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment: js(module: \"RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name$normalization.graphql\", id: \"RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment.actor.nameRenderer\")\n        __module_component_RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment: js(module: \"MarkdownUserNameRenderer.react\", id: \"RelayModernEnvironmentExecuteMutationWithMatchTestCommentFragment.actor.nameRenderer\")\n      }\n    }\n    id\n  }\n}\n\nfragment RelayModernEnvironmentExecuteMutationWithMatchTestMarkdownUserNameRenderer_name on MarkdownUserNameRenderer {\n  __typename\n  markdown\n  data {\n    markup\n    id\n  }\n}\n\nfragment RelayModernEnvironmentExecuteMutationWithMatchTestPlainUserNameRenderer_name on PlainUserNameRenderer {\n  plaintext\n  data {\n    text\n    id\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "85aee16ac1e83847bd25d209521afc27";
+}
+
+module.exports = node;

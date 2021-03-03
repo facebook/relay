b954891233334a0bff488ec50b674dbd6b9ad621
Jordan Eldredge <jeldredge@fb.com>
2021-03-03 11:48:57 -0800

    Test that we can don't error on schema descriptions
    Reviewed By: josephsavona
    
    Differential Revision: D26783659
    
    fbshipit-source-id: c0e7904bf8c4a280b4459f5444b5d509e82b0b01

--- compiler/crates/graphql-syntax/tests/parse_schema_document_test.rs
+++ compiler/crates/graphql-syntax/tests/parse_schema_document_test.rs
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<d56b9f5d378a674702fb32eb7105fddd>>
+ * @generated SignedSource<<22ce9736a830906bad18b3dc95711ef4>>
  */
 
 mod parse_schema_document;
@@ -17,6 +17,13 @@ fn schema_kitchen_sink() {
     let input = include_str!("parse_schema_document/fixtures/schema_kitchen_sink.graphql");
     let expected = include_str!("parse_schema_document/fixtures/schema_kitchen_sink.expected");
     test_fixture(transform_fixture, "schema_kitchen_sink.graphql", "parse_schema_document/fixtures/schema_kitchen_sink.expected", input, expected);
+}
+
+#[test]
+fn schema_with_leading_comment() {
+    let input = include_str!("parse_schema_document/fixtures/schema_with_leading_comment.graphql");
+    let expected = include_str!("parse_schema_document/fixtures/schema_with_leading_comment.expected");
+    test_fixture(transform_fixture, "schema_with_leading_comment.graphql", "parse_schema_document/fixtures/schema_with_leading_comment.expected", input, expected);
 }
 
 #[test]

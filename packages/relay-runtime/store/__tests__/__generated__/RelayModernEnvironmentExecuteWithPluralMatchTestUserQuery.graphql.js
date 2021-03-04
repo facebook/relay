67793c4fe2b74114b0fbb55f0f71a17c0110dabf
Jan Kassens <jkassens@fb.com>
2021-03-03 17:34:37 -0800

    convert RelayModernEnvironment-ExecuteWithPluralMatch-test
    Reviewed By: alunyov
    
    Differential Revision: D26795716
    
    fbshipit-source-id: fd5d4bb8af09a6e8fc80fda051472c9b8e03998e

---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery.graphql.js
@@ -0,0 +1,204 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<df94d7ec6ffaaed1e6ca8716ddc72088>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+// @dataDrivenDependency RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery.node.nameRenderers {"branches":{"MarkdownUserNameRenderer":{"component":"MarkdownUserNameRenderer.react","fragment":"RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name$normalization.graphql"},"PlainUserNameRenderer":{"component":"PlainUserNameRenderer.react","fragment":"RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name$normalization.graphql"}},"plural":true}
+
+/*::
+import type { ConcreteRequest } from 'relay-runtime';
+type RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name$ref = any;
+type RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name$ref = any;
+export type RelayModernEnvironmentExecuteWithPluralMatchTestUserQueryVariables = {|
+  id: string,
+|};
+export type RelayModernEnvironmentExecuteWithPluralMatchTestUserQueryResponse = {|
+  +node: ?{|
+    +nameRenderers?: ?$ReadOnlyArray<?{|
+      +__fragmentPropName?: ?string,
+      +__module_component?: ?string,
+      +$fragmentRefs: RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name$ref & RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name$ref,
+    |}>,
+  |},
+|};
+export type RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery = {|
+  variables: RelayModernEnvironmentExecuteWithPluralMatchTestUserQueryVariables,
+  response: RelayModernEnvironmentExecuteWithPluralMatchTestUserQueryResponse,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  }
+],
+v2 = [
+  {
+    "kind": "Literal",
+    "name": "supported",
+    "value": [
+      "PlainUserNameRenderer",
+      "MarkdownUserNameRenderer"
+    ]
+  }
+],
+v3 = {
+  "kind": "InlineFragment",
+  "selections": [
+    {
+      "documentName": "RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery",
+      "fragmentName": "RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name",
+      "fragmentPropName": "name",
+      "kind": "ModuleImport"
+    }
+  ],
+  "type": "PlainUserNameRenderer",
+  "abstractKey": null
+},
+v4 = {
+  "kind": "InlineFragment",
+  "selections": [
+    {
+      "documentName": "RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery",
+      "fragmentName": "RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name",
+      "fragmentPropName": "name",
+      "kind": "ModuleImport"
+    }
+  ],
+  "type": "MarkdownUserNameRenderer",
+  "abstractKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              {
+                "alias": null,
+                "args": (v2/*: any*/),
+                "concreteType": null,
+                "kind": "LinkedField",
+                "name": "nameRenderers",
+                "plural": true,
+                "selections": [
+                  (v3/*: any*/),
+                  (v4/*: any*/)
+                ],
+                "storageKey": "nameRenderers(supported:[\"PlainUserNameRenderer\",\"MarkdownUserNameRenderer\"])"
+              }
+            ],
+            "type": "User",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          (v5/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              {
+                "alias": null,
+                "args": (v2/*: any*/),
+                "concreteType": null,
+                "kind": "LinkedField",
+                "name": "nameRenderers",
+                "plural": true,
+                "selections": [
+                  (v5/*: any*/),
+                  (v3/*: any*/),
+                  (v4/*: any*/)
+                ],
+                "storageKey": "nameRenderers(supported:[\"PlainUserNameRenderer\",\"MarkdownUserNameRenderer\"])"
+              }
+            ],
+            "type": "User",
+            "abstractKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "2f2f487c79940398dd302db6b426a423",
+    "id": null,
+    "metadata": {},
+    "name": "RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery",
+    "operationKind": "query",
+    "text": "query RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery(\n  $id: ID!\n) {\n  node(id: $id) {\n    __typename\n    ... on User {\n      nameRenderers(supported: [\"PlainUserNameRenderer\", \"MarkdownUserNameRenderer\"]) {\n        __typename\n        ... on PlainUserNameRenderer {\n          ...RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name\n          __module_operation_RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery: js(module: \"RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name$normalization.graphql\", id: \"RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery.node.nameRenderers\")\n          __module_component_RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery: js(module: \"PlainUserNameRenderer.react\", id: \"RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery.node.nameRenderers\")\n        }\n        ... on MarkdownUserNameRenderer {\n          ...RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name\n          __module_operation_RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery: js(module: \"RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name$normalization.graphql\", id: \"RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery.node.nameRenderers\")\n          __module_component_RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery: js(module: \"MarkdownUserNameRenderer.react\", id: \"RelayModernEnvironmentExecuteWithPluralMatchTestUserQuery.node.nameRenderers\")\n        }\n      }\n    }\n    id\n  }\n}\n\nfragment RelayModernEnvironmentExecuteWithPluralMatchTestMarkdownUserNameRenderer_name on MarkdownUserNameRenderer {\n  __typename\n  markdown\n  data {\n    markup\n    id\n  }\n}\n\nfragment RelayModernEnvironmentExecuteWithPluralMatchTestPlainUserNameRenderer_name on PlainUserNameRenderer {\n  plaintext\n  data {\n    text\n    id\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "5ba0549be6b9d3358f8bc52844e64484";
+}
+
+module.exports = node;

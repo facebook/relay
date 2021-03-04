5f6afe6d80a69119b30e15b7205d81a63336d8fd
Jan Kassens <jkassens@fb.com>
2021-03-03 16:03:58 -0800

    convert readInlineData-test
    Reviewed By: alunyov
    
    Differential Revision: D26793751
    
    fbshipit-source-id: 16610d2189e8e38f1ca82595b0c03572d85d6081

---
+++ packages/relay-runtime/store/__tests__/__generated__/readInlineDataTestUserFragment.graphql.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<4993d88f3c4eca607fdb03e19458a34b>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ReaderInlineDataFragment } from 'relay-runtime';
+import type { FragmentReference } from "relay-runtime";
+declare export opaque type readInlineDataTestUserFragment$ref: FragmentReference;
+declare export opaque type readInlineDataTestUserFragment$fragmentType: readInlineDataTestUserFragment$ref;
+export type readInlineDataTestUserFragment = {|
+  +id: string,
+  +name: ?string,
+  +$refType: readInlineDataTestUserFragment$ref,
+|};
+export type readInlineDataTestUserFragment$data = readInlineDataTestUserFragment;
+export type readInlineDataTestUserFragment$key = {
+  +$data?: readInlineDataTestUserFragment$data,
+  +$fragmentRefs: readInlineDataTestUserFragment$ref,
+  ...
+};
+*/
+
+var node/*: ReaderInlineDataFragment*/ = {
+  "kind": "InlineDataFragment",
+  "name": "readInlineDataTestUserFragment"
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "ba453fc5b1f337b3ed0db8680df627f5";
+}
+
+module.exports = node;

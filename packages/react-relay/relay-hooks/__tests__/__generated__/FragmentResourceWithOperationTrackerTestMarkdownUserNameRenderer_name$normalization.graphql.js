5e3e235a97a142e1ee37f1e582686a1ec2991482
Andrey Lunyov <alunyov@fb.com>
2021-03-04 13:31:33 -0800

    Convert FragmentResource-WithOperationTracker-test.js
    Reviewed By: tyao1
    
    Differential Revision: D26825424
    
    fbshipit-source-id: 5e9cb60639da816ea0c62c639f96468929f8b993

---
+++ packages/react-relay/relay-hooks/__tests__/__generated__/FragmentResourceWithOperationTrackerTestMarkdownUserNameRenderer_name$normalization.graphql.js
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<11c828f94f91e18999a2acdfb3037a6d>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { NormalizationSplitOperation } from 'relay-runtime';
+
+*/
+
+var node/*: NormalizationSplitOperation*/ = {
+  "kind": "SplitOperation",
+  "metadata": {},
+  "name": "FragmentResourceWithOperationTrackerTestMarkdownUserNameRenderer_name$normalization",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "markdown",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "MarkdownUserNameData",
+      "kind": "LinkedField",
+      "name": "data",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "markup",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "id",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ]
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "b24e267fc49e1c18ab519d695f719894";
+}
+
+module.exports = node;
---
+++ packages/react-relay/relay-hooks/__tests__/__generated__/FragmentResourceWithOperationTrackerTestUserFragment.graphql.js
@@ -0,0 +1,157 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<9afab5fa1b2726aafb993e5e1d779497>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+// @dataDrivenDependency FragmentResourceWithOperationTrackerTestUserFragment.nameRenderer {"branches":{"MarkdownUserNameRenderer":{"component":"MarkdownUserNameRenderer.react","fragment":"FragmentResourceWithOperationTrackerTestMarkdownUserNameRenderer_name$normalization.graphql"},"PlainUserNameRenderer":{"component":"PlainUserNameRenderer.react","fragment":"FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name$normalization.graphql"}},"plural":false}
+// @dataDrivenDependency FragmentResourceWithOperationTrackerTestUserFragment.plainNameRenderer {"branches":{"PlainUserNameRenderer":{"component":"PlainUserNameRenderer.react","fragment":"FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name$normalization.graphql"}},"plural":false}
+
+/*::
+import type { ReaderFragment } from 'relay-runtime';
+type FragmentResourceWithOperationTrackerTestMarkdownUserNameRenderer_name$ref = any;
+type FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name$ref = any;
+import type { FragmentReference } from "relay-runtime";
+declare export opaque type FragmentResourceWithOperationTrackerTestUserFragment$ref: FragmentReference;
+declare export opaque type FragmentResourceWithOperationTrackerTestUserFragment$fragmentType: FragmentResourceWithOperationTrackerTestUserFragment$ref;
+export type FragmentResourceWithOperationTrackerTestUserFragment = {|
+  +id: string,
+  +name: ?string,
+  +nameRenderer: ?{|
+    +__fragmentPropName?: ?string,
+    +__module_component?: ?string,
+    +$fragmentRefs: FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name$ref & FragmentResourceWithOperationTrackerTestMarkdownUserNameRenderer_name$ref,
+  |},
+  +plainNameRenderer: ?{|
+    +__fragmentPropName?: ?string,
+    +__module_component?: ?string,
+    +$fragmentRefs: FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name$ref,
+  |},
+  +$refType: FragmentResourceWithOperationTrackerTestUserFragment$ref,
+|};
+export type FragmentResourceWithOperationTrackerTestUserFragment$data = FragmentResourceWithOperationTrackerTestUserFragment;
+export type FragmentResourceWithOperationTrackerTestUserFragment$key = {
+  +$data?: FragmentResourceWithOperationTrackerTestUserFragment$data,
+  +$fragmentRefs: FragmentResourceWithOperationTrackerTestUserFragment$ref,
+  ...
+};
+*/
+
+var node/*: ReaderFragment*/ = {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "FragmentResourceWithOperationTrackerTestUserFragment",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "id",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "name",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": [
+        {
+          "kind": "Literal",
+          "name": "supported",
+          "value": [
+            "PlainUserNameRenderer",
+            "MarkdownUserNameRenderer"
+          ]
+        }
+      ],
+      "concreteType": null,
+      "kind": "LinkedField",
+      "name": "nameRenderer",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "InlineFragment",
+          "selections": [
+            {
+              "documentName": "FragmentResourceWithOperationTrackerTestUserFragment",
+              "fragmentName": "FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name",
+              "fragmentPropName": "name",
+              "kind": "ModuleImport"
+            }
+          ],
+          "type": "PlainUserNameRenderer",
+          "abstractKey": null
+        },
+        {
+          "kind": "InlineFragment",
+          "selections": [
+            {
+              "documentName": "FragmentResourceWithOperationTrackerTestUserFragment",
+              "fragmentName": "FragmentResourceWithOperationTrackerTestMarkdownUserNameRenderer_name",
+              "fragmentPropName": "name",
+              "kind": "ModuleImport"
+            }
+          ],
+          "type": "MarkdownUserNameRenderer",
+          "abstractKey": null
+        }
+      ],
+      "storageKey": "nameRenderer(supported:[\"PlainUserNameRenderer\",\"MarkdownUserNameRenderer\"])"
+    },
+    {
+      "alias": "plainNameRenderer",
+      "args": [
+        {
+          "kind": "Literal",
+          "name": "supported",
+          "value": [
+            "PlainUserNameRenderer"
+          ]
+        }
+      ],
+      "concreteType": null,
+      "kind": "LinkedField",
+      "name": "nameRenderer",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "InlineFragment",
+          "selections": [
+            {
+              "documentName": "FragmentResourceWithOperationTrackerTestUserFragment_plainNameRenderer",
+              "fragmentName": "FragmentResourceWithOperationTrackerTestPlainUserNameRenderer_name",
+              "fragmentPropName": "name",
+              "kind": "ModuleImport"
+            }
+          ],
+          "type": "PlainUserNameRenderer",
+          "abstractKey": null
+        }
+      ],
+      "storageKey": "nameRenderer(supported:[\"PlainUserNameRenderer\"])"
+    }
+  ],
+  "type": "User",
+  "abstractKey": null
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "b3d16c15af5579c578a209cce953b3e2";
+}
+
+module.exports = node;

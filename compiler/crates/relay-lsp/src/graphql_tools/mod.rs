2d1c423cae99b3da61d51945ad7096826f6d1a38
Andrey Lunyov <alunyov@fb.com>
2021-04-06 11:33:13 -0700

    Create a program for printing with queries, and fragment from the initial text
    Reviewed By: tyao1
    
    Differential Revision: D27537187
    
    fbshipit-source-id: 83813f610457891df2d95d22ebb4e49244bb3f6f

--- compiler/crates/relay-lsp/src/graphql_tools/mod.rs
+++ compiler/crates/relay-lsp/src/graphql_tools/mod.rs
@@ -5,28 +5,29 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use std::sync::Arc;
+use std::{collections::HashSet, sync::Arc};
 
 use common::{PerfLogger, SourceLocationKey};
 use graphql_ir::{
-    build_ir_with_extra_features, BuilderOptions, FragmentVariablesSemantic, OperationDefinition,
-    Program, Selection,
+    build_ir_with_extra_features, BuilderOptions, ExecutableDefinition, FragmentDefinition,
+    FragmentVariablesSemantic, OperationDefinition, Program, Selection,
 };
-use graphql_syntax::{parse_executable_with_error_recovery, ExecutableDefinition};
+use graphql_syntax::parse_executable_with_error_recovery;
 use graphql_text_printer::print_full_operation;
 use interner::{Intern, StringKey};
 use lsp_types::request::Request;
 use relay_compiler::{
     apply_transforms,
     config::{Config, ProjectConfig},
+    Programs,
 };
+use schema::SDLSchema;
 use serde_json::Value;
 
 use crate::{
     lsp_extra_data_provider::Query,
     lsp_runtime_error::LSPRuntimeResult,
     server::{LSPState, SourcePrograms},
-    Schemas,
 };
 use serde::{Deserialize, Serialize};
 
@@ -57,21 +58,40 @@ impl Request for GraphQlExecuteQuery {
 /// We can use it to print the full query text
 fn get_operation_only_program(
     operation: Arc<OperationDefinition>,
+    fragments: Vec<Arc<FragmentDefinition>>,
     project_name: StringKey,
     programs: &SourcePrograms,
 ) -> Option<Program> {
-    let program = programs.get(&project_name)?;
+    let full_program = programs.get(&project_name)?;
 
     let mut selections_to_visit: Vec<_> = vec![&operation.selections];
-    let mut next_program = Program::new(program.schema.clone());
+    let mut next_program = Program::new(full_program.schema.clone());
     next_program.insert_operation(Arc::clone(&operation));
+    for fragment in fragments.iter() {
+        selections_to_visit.push(&fragment.selections);
+        next_program.insert_fragment(Arc::clone(fragment));
+    }
+
+    let mut visited_fragments: HashSet<StringKey> = HashSet::default();
 
     while !selections_to_visit.is_empty() {
         let current_selections = selections_to_visit.pop()?;
         for selection in current_selections {
             match selection {
                 Selection::FragmentSpread(spread) => {
-                    let fragment = program
+                    // Skip, if we already visited this fragment
+                    if visited_fragments.contains(&spread.fragment.item) {
+                        continue;
+                    }
+                    visited_fragments.insert(spread.fragment.item);
+                    // Second, if this fragment is already in the `next_program`,
+                    // it selection already added to the visiting stack
+                    if next_program.fragment(spread.fragment.item).is_some() {
+                        continue;
+                    }
+
+                    // Finally, add all missing fragment spreads from the full program
+                    let fragment = full_program
                         .fragment(spread.fragment.item)
                         .expect("Expect fragment to exist");
                     selections_to_visit.push(&fragment.selections);
@@ -95,19 +115,17 @@ fn get_operation_only_program(
 }
 
 /// Given the `Program` that contain operation+all its fragments this
-/// function will `apply_transforms` and return a query text,
-/// that can be sent to the server
-fn get_full_operation_text<TPerfLogger: PerfLogger + 'static>(
-    project_name: StringKey,
+/// function will `apply_transforms` and create a full set of programs, including the one
+/// that may generate full operation text
+fn transform_program<TPerfLogger: PerfLogger + 'static>(
     project_config: &ProjectConfig,
     config: Arc<Config>,
-    operation_name: StringKey,
-    source_program: Arc<Program>,
+    program: Arc<Program>,
     perf_logger: Arc<TPerfLogger>,
-) -> String {
-    let programs = apply_transforms(
-        project_name,
-        source_program,
+) -> Result<Programs, String> {
+    apply_transforms(
+        project_config.name,
+        program,
         Default::default(),
         &config.connection_interface,
         Arc::new(
@@ -119,8 +137,10 @@ fn get_full_operation_text<TPerfLogger: PerfLogger + 'static>(
         ),
         perf_logger,
     )
-    .unwrap();
+    .map_err(|err| format!("{:?}", err))
+}
 
+fn print_full_operation_text(programs: Programs, operation_name: StringKey) -> String {
     let print_operation_node = programs
         .operation_text
         .operation(operation_name)
@@ -129,19 +149,16 @@ fn get_full_operation_text<TPerfLogger: PerfLogger + 'static>(
     print_full_operation(&programs.operation_text, print_operation_node)
 }
 
-/// Given only one operation AST it will construct the typed IR
-/// But it will ignore missing fragment spreads
-fn build_operation_ir(
-    project_config: &ProjectConfig,
-    schemas: &Schemas,
-    operation: graphql_syntax::OperationDefinition,
-    operation_name: StringKey,
-) -> Result<OperationDefinition, String> {
-    let schema = schemas.get(&project_config.name).unwrap().clone();
-    let definitions = vec![graphql_syntax::ExecutableDefinition::Operation(operation)];
+/// From the list of AST nodes we're trying to extract the operation and possible
+/// list of fragment, to construct the initial `Program` that we could later transform
+/// and print
+fn build_operation_ir_with_fragments(
+    definitions: &[graphql_syntax::ExecutableDefinition],
+    schema: Arc<SDLSchema>,
+) -> Result<(Arc<OperationDefinition>, Vec<Arc<FragmentDefinition>>), String> {
     let ir = build_ir_with_extra_features(
         &schema,
-        &definitions,
+        definitions,
         BuilderOptions {
             allow_undefined_fragment_spreads: true,
             fragment_variables_semantic: FragmentVariablesSemantic::PassedValue,
@@ -150,15 +167,28 @@ fn build_operation_ir(
     )
     .map_err(|errors| format!("{:?}", errors))?;
 
-    for item in ir {
-        if let graphql_ir::ExecutableDefinition::Operation(op) = item {
-            if op.name.item == operation_name {
-                return Ok(op);
-            }
+    if let Some(operation) = ir.iter().find_map(|item| {
+        if let ExecutableDefinition::Operation(operation) = item {
+            Some(Arc::new(operation.clone()))
+        } else {
+            None
         }
-    }
+    }) {
+        let fragments = ir
+            .iter()
+            .filter_map(|item| {
+                if let ExecutableDefinition::Fragment(fragment) = item {
+                    Some(Arc::new(fragment.clone()))
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>();
 
-    Err("Operation could not be constructed".to_string())
+        Ok((operation, fragments))
+    } else {
+        Err("Unable to find an operation.".to_string())
+    }
 }
 
 pub(crate) fn on_graphql_execute_query<TPerfLogger: PerfLogger + 'static>(
@@ -179,50 +209,36 @@ pub(crate) fn on_graphql_execute_query<TPerfLogger: PerfLogger + 'static>(
         )));
     }
 
-    let query_text: String = match &result.item.definitions[0] {
-        ExecutableDefinition::Fragment(_) => {
-            return Ok(GraphQlResponse::Error(Value::String(
-                "Cannot fetch data for fragments".to_string(),
-            )));
-        }
-        ExecutableDefinition::Operation(operation) => match &operation.name {
-            Some(operation_name) => {
-                let project_config = state.get_project_config_ref(project_name).unwrap();
+    let project_config = state.get_project_config_ref(project_name).unwrap();
+    let schema = state
+        .get_schemas()
+        .get(&project_config.name)
+        .unwrap()
+        .clone();
 
-                let operation_ir = build_operation_ir(
-                    project_config,
-                    &state.get_schemas(),
-                    operation.clone(),
-                    operation_name.value,
-                )
-                .unwrap();
+    let (operation, fragments) =
+        build_operation_ir_with_fragments(&result.item.definitions, schema).unwrap();
 
-                let operation_name = operation_ir.name.item;
+    let operation_name = operation.name.item;
+    let query_text = if let Some(operation_program) = get_operation_only_program(
+        operation,
+        fragments,
+        project_name,
+        state.get_source_programs_ref(),
+    ) {
+        let programs = transform_program(
+            project_config,
+            state.get_config(),
+            Arc::new(operation_program),
+            state.get_logger(),
+        )
+        .unwrap();
 
-                // Internally, we have a rule that only operations with names can be used
-                if let Some(operation_sources) = get_operation_only_program(
-                    Arc::new(operation_ir),
-                    project_name,
-                    state.get_source_programs_ref(),
-                ) {
-                    let operation_text = get_full_operation_text(
-                        project_name,
-                        state.get_project_config_ref(project_name).unwrap(),
-                        state.get_config(),
-                        operation_name,
-                        Arc::new(operation_sources),
-                        state.get_logger(),
-                    );
-
-                    operation_text
-                } else {
-                    // If, for some reason we could not get the operation text from the sources
-                    // we will send the original text to the server
-                    params.text
-                }
-            }
-            None => params.text,
-        },
+        print_full_operation_text(programs, operation_name)
+    } else {
+        // If, for some reason we could not get the operation text from the sources
+        // we will send the original text to the server
+        params.text
     };
 
     match state

0a263b1d6e4bdaee9115f15f8df5e24ccee38eb6
Aaron Buxbaum <me@aaronbuxbaum.com>
2021-07-16 17:58:50 -0400

    undo change

--- packages/relay-runtime/network/RelayNetworkTypes.js
+++ packages/relay-runtime/network/RelayNetworkTypes.js
@@ -119,7 +119,7 @@ export type SubscribeFunction = (
 ) => RelayObservable<GraphQLResponse>;
 
 export type Uploadable = File | Blob;
-export type UploadableMap = interface {[key: string]: Uploadable  | Array<Uploadable>};
+export type UploadableMap = interface {[key: string]: Uploadable};
 
 /**
  * React Flight tree created on the server.

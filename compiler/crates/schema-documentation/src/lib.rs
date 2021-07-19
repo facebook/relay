e25b94ab8a912bc4b467ddd7b6f5ab9fb40051a3
Robert Balicki <robertbalicki@fb.com>
2021-07-19 12:51:28 -0700

    Add CombinedSchemaDocumentation struct
    Reviewed By: alunyov
    
    Differential Revision: D29726539
    
    fbshipit-source-id: cb2b632b111999c5394fb63e1efe53f2ded8399d

--- compiler/crates/schema-documentation/src/lib.rs
+++ compiler/crates/schema-documentation/src/lib.rs
@@ -5,9 +5,12 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+mod combined_schema_documentation;
 mod sdl_schema_impl;
 
 use std::sync::Arc;
+
+pub use combined_schema_documentation::CombinedSchemaDocumentation;
 
 pub trait SchemaDocumentationLoader<T: SchemaDocumentation> {
     fn get_schema_documentation(&self, schema_name: &str) -> Arc<T>;
@@ -27,5 +30,50 @@ pub trait SchemaDocumentation {
         _argument_name: &str,
     ) -> Option<&str> {
         None
+    }
+}
+
+// This can probably be implemented more generically for AsRef<TSchemaDocumentation>
+impl<TSchemaDocumentation: SchemaDocumentation> SchemaDocumentation for Arc<TSchemaDocumentation> {
+    fn get_type_description(&self, type_name: &str) -> Option<&str> {
+        self.as_ref().get_type_description(type_name)
+    }
+
+    fn get_field_description(&self, type_name: &str, field_name: &str) -> Option<&str> {
+        self.as_ref().get_field_description(type_name, field_name)
+    }
+
+    fn get_field_argument_description(
+        &self,
+        type_name: &str,
+        field_name: &str,
+        argument_name: &str,
+    ) -> Option<&str> {
+        self.as_ref()
+            .get_field_argument_description(type_name, field_name, argument_name)
+    }
+}
+
+impl<TSchemaDocumentation: SchemaDocumentation> SchemaDocumentation
+    for Option<TSchemaDocumentation>
+{
+    fn get_type_description(&self, type_name: &str) -> Option<&str> {
+        self.as_ref()
+            .and_then(|s| s.get_type_description(type_name))
+    }
+
+    fn get_field_description(&self, type_name: &str, field_name: &str) -> Option<&str> {
+        self.as_ref()
+            .and_then(|s| s.get_field_description(type_name, field_name))
+    }
+
+    fn get_field_argument_description(
+        &self,
+        type_name: &str,
+        field_name: &str,
+        argument_name: &str,
+    ) -> Option<&str> {
+        self.as_ref()
+            .and_then(|s| s.get_field_argument_description(type_name, field_name, argument_name))
     }
 }

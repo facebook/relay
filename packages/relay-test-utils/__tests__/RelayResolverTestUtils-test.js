0325cee5b73d7b0503ca9d76a92b52b9b131b49a
Jordan Eldredge <jeldredge@fb.com>
2021-09-16 19:57:36 -0700

    Test utility
    Reviewed By: josephsavona
    
    Differential Revision: D30906282
    
    fbshipit-source-id: 1b486d4f60a7e21a00c16ed5fd190044693fd805

---
+++ packages/relay-test-utils/__tests__/RelayResolverTestUtils-test.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @emails oncall+relay
+ * @flow
+ * @format
+ */
+
+'use strict';
+
+const {testResolver} = require('../RelayResolverTestUtils');
+const {graphql} = require('relay-runtime');
+const {readFragment} = require('relay-runtime/store/ResolverFragments');
+
+import type {RelayResolverTestUtilsTest$key} from './__generated__/RelayResolverTestUtilsTest.graphql';
+
+function myTestResolver(rootKey: RelayResolverTestUtilsTest$key): string {
+  const user = readFragment(
+    graphql`
+      fragment RelayResolverTestUtilsTest on User {
+        name
+      }
+    `,
+    rootKey,
+  );
+
+  return `Hello ${user.name ?? 'stranger'}!`;
+}
+
+test('testResolver', () => {
+  const input = {name: 'Elizabeth', $refType: (null: any)};
+  const actual = testResolver(myTestResolver, input);
+  expect(actual).toBe('Hello Elizabeth!');
+});
+
+testResolver(
+  myTestResolver,
+  // $FlowExpectedError foo is an unexpected key
+  {
+    name: 'Elizabeth',
+    foo: 'bar',
+    $refType: (null: any),
+  },
+);
+testResolver(myTestResolver, {
+  // $FlowExpectedError Object is not a string
+  name: {},
+  $refType: (null: any),
+});

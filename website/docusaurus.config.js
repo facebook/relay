c0e214b8a7118cc5f4c8353037db1f53121bdb92
Anton Kastritskiy <antonk52@fb.com>
2021-11-24 11:33:19 +0000

    fix: remove unused redirect to remove a build warning

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -320,7 +320,6 @@ module.exports = {
             to: '/docs/guides/type-emission/',
             from: [
               '/docs/en/type-emission',
-              '/docs/v3.0.0/type-emission',
               '/docs/type-emission',
             ],
           },

dea3f325ba4dc1b842cb741a51adc8e9965db378
Vincent Riemer <vincentriemer@gmail.com>
2021-12-06 11:37:03 -0800

    Add MacOS ARM64 build target to relay-compiler for better M1 Mac support (#3667)
    Summary:
    idk what the performance difference is since I don't have a project large enough where I can notice a difference but I figured there's no harm in at least providing an M1 native executable for the new relay-compiler.
    
    Verified on my M1 MacBook Pro by manually downloading the new macOS-arm64 artifact built from the GH workflow and replacing a local executable which still runs successfully.
    
    Pull Request resolved: https://github.com/facebook/relay/pull/3667
    
    Test Plan:
    Imported from GitHub, without a `Test Plan:` line.
    
    **Static Docs Preview: relay**
    |[Full Site](https://our.intern.facebook.com/intern/staticdocs/eph/D32821010/V4/relay/)|
    
    |**Modified Pages**|
    
    **Static Docs Preview: relay**
    |[Full Site](https://our.intern.facebook.com/intern/staticdocs/eph/D32821010/V4/relay/)|
    
    |**Modified Pages**|
    
    Reviewed By: tyao1
    
    Differential Revision: D32821010
    
    Pulled By: vincentriemer
    
    fbshipit-source-id: 07d4311012a189327462925fd1c2d4bdb484830e

--- packages/relay-compiler/index.js
+++ packages/relay-compiler/index.js
@@ -13,8 +13,10 @@
 const path = require('path');
 
 let binary;
-if (process.platform === 'darwin') {
+if (process.platform === 'darwin' && process.arch === 'x64') {
   binary = path.join(__dirname, 'macos-x64', 'relay');
+} else if (process.platform === 'darwin' && process.arch === 'arm64') {
+  binary = path.join(__dirname, 'macos-arm64', 'relay');
 } else if (process.platform === 'linux' && process.arch === 'x64') {
   binary = path.join(__dirname, 'linux-x64', 'relay');
 } else if (process.platform === 'win32' && process.arch === 'x64') {

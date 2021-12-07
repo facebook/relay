03647ab63d9ae6fab8b80eb385531f943ac43b3a
Jordan Eldredge <jeldredge@fb.com>
2021-12-07 11:31:24 -0800

    Add utility to insert GraphMode into a MutableRecordSource
    Reviewed By: josephsavona
    
    Differential Revision: D32601674
    
    fbshipit-source-id: 001f9c8584c57948fd89cc08184f0a5a3228d6d7

---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery.graphql.js
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<ec32098ffbb49cad499744289b7e1313>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest, Query } from 'relay-runtime';
+export type RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$variables = {||};
+export type RelayExperimentalGraphResponseHandlerTestPluralLinkedQueryVariables = RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$variables;
+export type RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$data = {|
+  +me: ?{|
+    +allPhones: ?$ReadOnlyArray<?{|
+      +isVerified: ?boolean,
+    |}>,
+  |},
+|};
+export type RelayExperimentalGraphResponseHandlerTestPluralLinkedQueryResponse = RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$data;
+export type RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery = {|
+  variables: RelayExperimentalGraphResponseHandlerTestPluralLinkedQueryVariables,
+  response: RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$data,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "Phone",
+  "kind": "LinkedField",
+  "name": "allPhones",
+  "plural": true,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "isVerified",
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "be819dfeef09f1cb17dcae73fa1a4618",
+    "id": null,
+    "metadata": {},
+    "name": "RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery",
+    "operationKind": "query",
+    "text": "query RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery {\n  me {\n    allPhones {\n      isVerified\n    }\n    id\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "18e7873aba7d64d382b1880905180d86";
+}
+
+module.exports = ((node/*: any*/)/*: Query<
+  RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$variables,
+  RelayExperimentalGraphResponseHandlerTestPluralLinkedQuery$data,
+>*/);
---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayExperimentalGraphResponseHandlerTestPluralScalarQuery.graphql.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<4f3048b69547fb45e73f593e9feee344>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest, Query } from 'relay-runtime';
+export type RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$variables = {||};
+export type RelayExperimentalGraphResponseHandlerTestPluralScalarQueryVariables = RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$variables;
+export type RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$data = {|
+  +me: ?{|
+    +emailAddresses: ?$ReadOnlyArray<?string>,
+  |},
+|};
+export type RelayExperimentalGraphResponseHandlerTestPluralScalarQueryResponse = RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$data;
+export type RelayExperimentalGraphResponseHandlerTestPluralScalarQuery = {|
+  variables: RelayExperimentalGraphResponseHandlerTestPluralScalarQueryVariables,
+  response: RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$data,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "emailAddresses",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayExperimentalGraphResponseHandlerTestPluralScalarQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "RelayExperimentalGraphResponseHandlerTestPluralScalarQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "78aa076a70d061f008c10215caab9554",
+    "id": null,
+    "metadata": {},
+    "name": "RelayExperimentalGraphResponseHandlerTestPluralScalarQuery",
+    "operationKind": "query",
+    "text": "query RelayExperimentalGraphResponseHandlerTestPluralScalarQuery {\n  me {\n    emailAddresses\n    id\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "5a8e3902858c91e8036db2efd5dc6576";
+}
+
+module.exports = ((node/*: any*/)/*: Query<
+  RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$variables,
+  RelayExperimentalGraphResponseHandlerTestPluralScalarQuery$data,
+>*/);

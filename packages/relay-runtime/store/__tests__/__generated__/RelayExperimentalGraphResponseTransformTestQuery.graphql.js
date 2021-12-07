65bf22fa45d4cc18ddafc748f31dcf5d74d7ba28
Jordan Eldredge <jeldredge@fb.com>
2021-12-07 11:31:24 -0800

    Support more node types in transform
    Reviewed By: kassens
    
    Differential Revision: D32600337
    
    fbshipit-source-id: 11cfa6982528d9fc22f9a14ab85e83f003517e2a

---
+++ packages/relay-runtime/store/__tests__/__generated__/RelayExperimentalGraphResponseTransformTestQuery.graphql.js
@@ -0,0 +1,109 @@
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * 
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<936c57878d34425be01a644556900551>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest, Query } from 'relay-runtime';
+export type RelayExperimentalGraphResponseTransformTestQuery$variables = {||};
+export type RelayExperimentalGraphResponseTransformTestQueryVariables = RelayExperimentalGraphResponseTransformTestQuery$variables;
+export type RelayExperimentalGraphResponseTransformTestQuery$data = {|
+  +me: ?{|
+    +name: ?string,
+  |},
+|};
+export type RelayExperimentalGraphResponseTransformTestQueryResponse = RelayExperimentalGraphResponseTransformTestQuery$data;
+export type RelayExperimentalGraphResponseTransformTestQuery = {|
+  variables: RelayExperimentalGraphResponseTransformTestQueryVariables,
+  response: RelayExperimentalGraphResponseTransformTestQuery$data,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayExperimentalGraphResponseTransformTestQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "RelayExperimentalGraphResponseTransformTestQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "f86703987df93989b5dc96d851978043",
+    "id": null,
+    "metadata": {},
+    "name": "RelayExperimentalGraphResponseTransformTestQuery",
+    "operationKind": "query",
+    "text": "query RelayExperimentalGraphResponseTransformTestQuery {\n  me {\n    name\n    id\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "1d57607fb7a5036c7e61d762592bc61a";
+}
+
+module.exports = ((node/*: any*/)/*: Query<
+  RelayExperimentalGraphResponseTransformTestQuery$variables,
+  RelayExperimentalGraphResponseTransformTestQuery$data,
+>*/);

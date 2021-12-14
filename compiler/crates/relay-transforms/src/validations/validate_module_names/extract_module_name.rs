a0e8caf7b8bb3a1859276a41f05bab25d62d2151
Andrey Lunyov <alunyov@fb.com>
2021-12-14 18:34:25 -0500

    fix tests on windows

--- compiler/crates/relay-transforms/src/validations/validate_module_names/extract_module_name.rs
+++ compiler/crates/relay-transforms/src/validations/validate_module_names/extract_module_name.rs
@@ -66,6 +66,7 @@ fn to_camel_case(non_camelized_string: String) -> String {
 mod tests {
     use super::*;
 
+    #[cfg(not(windows))]
     #[test]
     fn extract_module_names_test() {
         assert_eq!(
@@ -159,6 +160,19 @@ mod tests {
         assert_eq!(
             extract_module_name("/path/non-numeric-end-.js"),
             Some("nonNumericEnd".to_string())
+        );
+    }
+
+    #[cfg(windows)]
+    #[test]
+    fn extract_module_names_test() {
+        assert_eq!(
+            extract_module_name("C:\\path\\Button.ios.js"),
+            Some("ButtonIos".to_string())
+        );
+        assert_eq!(
+            extract_module_name("\\path\\Button.android.js"),
+            Some("ButtonAndroid".to_string())
         );
     }
 }

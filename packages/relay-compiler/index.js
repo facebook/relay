7c64d56780858116d60eb6a3f1ca97823a1df509
Vadim Evseev <chiffa117@gmail.com>
2022-01-15 23:54:07 +0300

    add build for linux-musl target

--- packages/relay-compiler/index.js
+++ packages/relay-compiler/index.js
@@ -11,12 +11,15 @@
 'use strict';
 
 const path = require('path');
+const { isNonGlibcLinux } = require('detect-libc');
 
 let binary;
 if (process.platform === 'darwin' && process.arch === 'x64') {
   binary = path.join(__dirname, 'macos-x64', 'relay');
 } else if (process.platform === 'darwin' && process.arch === 'arm64') {
   binary = path.join(__dirname, 'macos-arm64', 'relay');
+} else if (process.platform === 'linux' && isNonGlibcLinux) {
+  binary = path.join(__dirname, 'linux-musl', 'relay');
 } else if (process.platform === 'linux' && process.arch === 'x64') {
   binary = path.join(__dirname, 'linux-x64', 'relay');
 } else if (process.platform === 'win32' && process.arch === 'x64') {

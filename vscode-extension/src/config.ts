e7b0f806506a8228f95814a146cdbe1db04bf950
Terence Bezman <terence.bezman@coinbase.com>
2022-04-05 14:45:23 -0700

    Strict types on config

--- vscode-extension/src/config.ts
+++ vscode-extension/src/config.ts
@@ -1,15 +1,15 @@
-import { workspace } from "vscode";
+import {ConfigurationScope, workspace} from 'vscode';
 
 export type Config = {
   pathToRelay: string | null;
   outputLevel: string;
-}
+};
 
-export function getConfig(): Config {
-  const configuration = workspace.getConfiguration('relay');
+export function getConfig(scope?: ConfigurationScope): Config {
+  const configuration = workspace.getConfiguration('relay', scope);
 
   return {
-    pathToRelay: configuration.get('pathToRelay'),
-    outputLevel: configuration.get('outputLevel'),
+    pathToRelay: configuration.get('pathToRelay') ?? null,
+    outputLevel: configuration.get('outputLevel') ?? 'quiet-with-errros',
   };
 }

3b127c21c44f4e7b23bf1a13289a244da1e2f9bc
Terence Bezman <terence.bezman@coinbase.com>
2022-04-07 11:16:48 -0700

    Remove timeout code

--- vscode-extension/src/commands/restartLanguageServer.ts
+++ vscode-extension/src/commands/restartLanguageServer.ts
@@ -1,14 +1,6 @@
-import {window, commands} from 'vscode';
+import {window} from 'vscode';
 import {RelayExtensionContext} from '../context';
 import {createAndStartClient} from '../languageClient';
-
-async function timeout(ms: number): Promise<'timeout'> {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('timeout');
-    }, ms);
-  });
-}
 
 export function handleRestartLanguageServerCommand(
   context: RelayExtensionContext,
@@ -17,24 +9,9 @@ export function handleRestartLanguageServerCommand(
     return;
   }
 
-  Promise.race([timeout(5000), context.client.stop()])
-    .then((result) => {
-      if (result === 'timeout') {
-        window
-          .showErrorMessage(
-            'Relay LSP Client did not stop after 5 seconds. Try restarting VSCode',
-            'Restart',
-            'Close',
-          )
-          .then((selection) => {
-            if (selection === 'Restart') {
-              commands.executeCommand('workbench.action.reloadWindow');
-            }
-          });
-
-        return;
-      }
-
+  context.client
+    .stop()
+    .then(() => {
       context.outputChannel.appendLine(
         'Successfully stopped existing relay lsp client',
       );

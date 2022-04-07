4b1ef43a9ef565dfe9c7a911b9d6fceccdaea2fd
Terence Bezman <terence.bezman@coinbase.com>
2022-04-07 11:03:26 -0700

    Timeout restart language server after five seconds

--- vscode-extension/src/commands/restartLanguageServer.ts
+++ vscode-extension/src/commands/restartLanguageServer.ts
@@ -1,6 +1,14 @@
-import {window} from 'vscode';
+import {window, commands} from 'vscode';
 import {RelayExtensionContext} from '../context';
 import {createAndStartClient} from '../languageClient';
+
+async function timeout(ms: number): Promise<'timeout'> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('timeout');
+    }, ms);
+  });
+}
 
 export function handleRestartLanguageServerCommand(
   context: RelayExtensionContext,
@@ -9,9 +17,28 @@ export function handleRestartLanguageServerCommand(
     return;
   }
 
-  context.client
-    .stop()
-    .then(() => {
+  Promise.race([timeout(5000), context.client.stop()])
+    .then((result) => {
+      if (result === 'timeout') {
+        window
+          .showErrorMessage(
+            'Relay LSP Client did not stop after 5 seconds. Try restarting VSCode',
+            'Restart',
+            'Close',
+          )
+          .then((selection) => {
+            if (selection === 'Restart') {
+              commands.executeCommand('workbench.action.reloadWindow');
+            }
+          });
+
+        return;
+      }
+
+      context.outputChannel.appendLine(
+        'Successfully stopped existing relay lsp client',
+      );
+
       context.client = null;
 
       createAndStartClient(context);

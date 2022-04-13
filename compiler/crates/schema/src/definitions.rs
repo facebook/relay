59ae56a132f5909aad3635543096ef9122f8f563
Ordencia Wu <ordencia@fb.com>
2022-04-13 10:52:39 -0700

    1/n render html content based on a given url
    Reviewed By: ginfung
    
    Differential Revision: D35251804
    
    fbshipit-source-id: 171b124d40fb70e2ef6fa59d397ff2df681fc9d3

--- compiler/crates/schema/src/definitions.rs
+++ compiler/crates/schema/src/definitions.rs
@@ -36,6 +36,12 @@ macro_rules! type_id {
                 write!(f, "{}({})", stringify!($name), self.0)
             }
         }
+
+        impl fmt::Display for $name {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
     };
 }
 

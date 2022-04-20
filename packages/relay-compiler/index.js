5e3c7b6d4f41461a1a6a59d3373d519ad9a0856d
Konstantin Nesterov <wasd171@gmail.com>
2022-04-20 10:15:08 -0700

    Build linux arm64 (#3882)
    Summary:
    Following discussions in https://github.com/facebook/relay/issues/3799 this PR adds support for a Linux ARM64 target for `relay-compiler`
    
    Pull Request resolved: https://github.com/facebook/relay/pull/3882
    
    Test Plan:
    Imported from GitHub, without a `Test Plan:` line.
    
    **Static Docs Preview: relay**
    |[Full Site](https://our.intern.facebook.com/intern/staticdocs/eph/D35760366/V2/relay/)|
    
    |**Modified Pages**|
    
    Reviewed By: josephsavona
    
    Differential Revision: D35760366
    
    Pulled By: alunyov
    
    fbshipit-source-id: 4e79ce2ccbe91a70f2d91d80667fd6960210c7f1

--- packages/relay-compiler/index.js
+++ packages/relay-compiler/index.js
@@ -22,6 +22,8 @@ if (process.platform === 'darwin' && process.arch === 'x64') {
   binary = path.join(__dirname, 'macos-arm64', 'relay');
 } else if (process.platform === 'linux' && process.arch === 'x64') {
   binary = path.join(__dirname, 'linux-x64', 'relay');
+} else if (process.platform === 'linux' && process.arch === 'arm64') {
+  binary = path.join(__dirname, 'linux-arm64', 'relay');
 } else if (process.platform === 'win32' && process.arch === 'x64') {
   binary = path.join(__dirname, 'win-x64', 'relay.exe');
 } else {

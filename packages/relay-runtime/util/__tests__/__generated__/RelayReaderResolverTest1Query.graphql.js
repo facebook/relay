ee2dcd3b1bf5842216d298240675f1134e10a961
Terence Bezman <terence.bezman@coinbase.com>
2022-05-10 16:14:36 -0700

    Basic resolver test

---
+++ packages/relay-runtime/util/__tests__/__generated__/RelayReaderResolverTest1Query.graphql.js
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @generated SignedSource<<3a7b8b8f7413a87a9e38f05d173cda28>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest, Query } from 'relay-runtime';
+import userGreetingResolver from "UserGreetingResolver";
+export type RelayReaderResolverTest1Query$variables = {||};
+export type RelayReaderResolverTest1Query$data = {|
+  +me: ?{|
+    +greeting: ?$Call<<R>((...empty[]) => R) => R, typeof userGreetingResolver>,
+  |},
+|};
+export type RelayReaderResolverTest1Query = {|
+  response: RelayReaderResolverTest1Query$data,
+  variables: RelayReaderResolverTest1Query$variables,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "RelayReaderResolverTest1Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "fragment": {
+              "args": null,
+              "kind": "FragmentSpread",
+              "name": "UserGreetingResolver"
+            },
+            "kind": "RelayResolver",
+            "name": "greeting",
+            "resolverModule": require('./../UserGreetingResolver'),
+            "path": "me.greeting"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "RelayReaderResolverTest1Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "User",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "name",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "f76f67dfcd484af2b1a08973cc613547",
+    "id": null,
+    "metadata": {},
+    "name": "RelayReaderResolverTest1Query",
+    "operationKind": "query",
+    "text": "query RelayReaderResolverTest1Query {\n  me {\n    ...UserGreetingResolver\n    id\n  }\n}\n\nfragment UserGreetingResolver on User {\n  name\n}\n"
+  }
+};
+
+if (__DEV__) {
+  (node/*: any*/).hash = "bdcddda7a9b9b6926a9fa0787d74a8f2";
+}
+
+module.exports = ((node/*: any*/)/*: Query<
+  RelayReaderResolverTest1Query$variables,
+  RelayReaderResolverTest1Query$data,
+>*/);

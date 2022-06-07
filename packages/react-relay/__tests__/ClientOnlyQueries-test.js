42ae9550a78eb5de24b419b38230fcced0b8b627
Andrey Lunyov <alunyov@fb.com>
2022-06-07 10:33:08 -0700

    Fix flow error in ClientOnlyQueries-test in OSS
    Reviewed By: captbaritone
    
    Differential Revision: D36972668
    
    fbshipit-source-id: 842fc7bc2b5c03eaf9bba7716850919139546111

--- packages/react-relay/__tests__/ClientOnlyQueries-test.js
+++ packages/react-relay/__tests__/ClientOnlyQueries-test.js
@@ -11,7 +11,6 @@
 
 'use strict';
 
-import type {ReactTestRenderer} from 'react-test-renderer';
 import type {
   FetchFunction,
   FetchPolicy,
@@ -50,7 +49,7 @@ function createEnvironment(
 }
 
 describe('Client Only Queries', () => {
-  let renderer: ReactTestRenderer;
+  let renderer;
   let environment: IEnvironment;
 
   const query = graphql`

9ba65d52679ccaea21f669643ccdecf46a381ef6
Terence Bezman <terence.bezman@coinbase.com>
2022-07-07 22:53:56 -0700

    use pathbuf instead

--- compiler/crates/relay-codegen/src/printer.rs
+++ compiler/crates/relay-codegen/src/printer.rs
@@ -39,6 +39,7 @@ use indexmap::IndexMap;
 use intern::string_key::StringKey;
 use std::fmt::Result as FmtResult;
 use std::fmt::Write;
+use std::path::Path;
 
 pub fn print_operation(
     schema: &SDLSchema,
@@ -470,15 +471,25 @@ impl<'b> JSONPrinter<'b> {
             },
             Primitive::JSModuleDependency(key) => match self.js_module_format {
                 JsModuleFormat::CommonJS => {
-                    let mut path = format!("./{}", key);
+                    let path = Path::new(key.lookup());
+                    let extension = path.extension();
 
-                    let extension = &path[path.len() - 3..path.len()];
+                    if let Some(extension) = extension {
+                        if extension == "ts" || extension == "js" {
+                            let path_without_extension = path.with_extension("");
 
-                    if extension == ".js" || extension == ".ts" {
-                        path = path[0..path.len() - 3].to_string();
+                            let path_without_extension =
+                                path_without_extension.to_str().expect("yikes");
+
+                            return self.write_js_dependency(
+                                f,
+                                key.to_string(),
+                                format!("./{}", path_without_extension),
+                            );
+                        }
                     }
 
-                    self.write_js_dependency(f, key.to_string(), path)
+                    self.write_js_dependency(f, key.to_string(), format!("./{}", key))
                 }
                 JsModuleFormat::Haste => {
                     self.write_js_dependency(f, key.to_string(), key.to_string())

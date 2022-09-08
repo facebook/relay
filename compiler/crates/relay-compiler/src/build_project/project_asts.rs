3b40fe55d22292c6ece390ebf44297622472ee83
Jordan Eldredge <jeldredge@fb.com>
2022-09-07 21:32:09 -0700

    Improve scoping of diagnostic location for duplicate queris/fragments
    Reviewed By: alunyov
    
    Differential Revision: D39329748
    
    fbshipit-source-id: c75124ebf9a35c46ee8399240312e8f62b44dcde

--- compiler/crates/relay-compiler/src/build_project/project_asts.rs
+++ compiler/crates/relay-compiler/src/build_project/project_asts.rs
@@ -116,14 +116,19 @@ pub fn find_duplicates(
 
     let mut errors = Vec::new();
     for def in asts.iter().chain(base_asts) {
-        if let Some(name) = def.name() {
-            if let Some(prev_def) = definitions.insert(name, def) {
+        if let Some(name) = def.name_identifier() {
+            if let Some(prev_def) = definitions.insert(name.value, def) {
                 errors.push(
                     Diagnostic::error(
-                        graphql_ir::ValidationMessage::DuplicateDefinition(name),
-                        def.location(),
+                        graphql_ir::ValidationMessage::DuplicateDefinition(name.value),
+                        def.location().with_span(name.span),
                     )
-                    .annotate("previously defined here", prev_def.location()),
+                    .annotate(
+                        "previously defined here",
+                        prev_def
+                            .name_location()
+                            .unwrap_or_else(|| prev_def.location()),
+                    ),
                 );
             }
         }

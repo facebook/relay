80db95193207c8f9dd458d3df3ef2fe2e9022f9a
Rob Hogan <robhogan@fb.com>
2022-09-13 11:39:17 -0700

    Explicitly use a legacy timer in `QueryResource-test.js`
    Summary:
    (This was missed in D39258784 (https://github.com/facebook/relay/commit/6b16da161f887c5dd59ecdff64de4a1a6ca9039f).)
    
    **Note for Relay folks**: This is part of an effort to update Jest in fbsource, including React Native and Metro OSS - first to Jest 27.5, then 28, then 29.
    
    I'm not currently planning to touch Relay OSS (except for these universally compatible test code updates), but I'm happy to attempt to bring that along too if green CI is enough to have confidence in the changes.
    
    Make this test code forward-compatible with Jest 27+ by explicitly specifying "legacy" timers, which are the default in Jest 26 - see https://jestjs.io/blog/2021/05/25/jest-27#flipping-defaults.
    
    Reviewed By: alunyov
    
    Differential Revision: D39467257
    
    fbshipit-source-id: 65bf3e14087a6ced0e38410991b2d7f0bc9df43f

--- packages/react-relay/relay-hooks/__tests__/QueryResource-test.js
+++ packages/react-relay/relay-hooks/__tests__/QueryResource-test.js
@@ -2940,7 +2940,7 @@ describe('QueryResource, with an environment meant for SSR', () => {
         fetchTime: null,
       });
 
-      jest.useFakeTimers();
+      jest.useFakeTimers('legacy');
       const result = QueryResource.prepare(
         query,
         fetchObservable,

cb052fc21fb2ac71d94d45a0ab973d61b3261850
Jordan Eldredge <jeldredge@meta.com>
2022-10-14 11:06:22 -0700

    Clean up go to definition for resolvers
    Reviewed By: alunyov
    
    Differential Revision: D40355780
    
    fbshipit-source-id: cff72a96d3d1943737e74a5a23fe8d66b0d5272a

--- compiler/crates/relay-lsp/src/goto_definition/goto_docblock_definition.rs
+++ compiler/crates/relay-lsp/src/goto_definition/goto_docblock_definition.rs
@@ -20,13 +20,7 @@ pub fn get_docblock_definition_description(
 ) -> LSPRuntimeResult<DefinitionDescription> {
     let resolution = create_docblock_resolution_info(docblock_ir, position_span)?;
     match resolution {
-        DocblockResolutionInfo::OnType(type_name) => Ok(DefinitionDescription::Type { type_name }),
-        DocblockResolutionInfo::OnInterface(interface_name) => Ok(DefinitionDescription::Type {
-            type_name: interface_name,
-        }),
-        DocblockResolutionInfo::OutputType(edge_type) => Ok(DefinitionDescription::Type {
-            type_name: edge_type,
-        }),
+        DocblockResolutionInfo::Type(type_name) => Ok(DefinitionDescription::Type { type_name }),
         DocblockResolutionInfo::RootFragment(fragment_name) => {
             Ok(DefinitionDescription::Fragment { fragment_name })
         }

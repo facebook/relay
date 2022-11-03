f91ba91a086c65eb4b439a5bf021cb9e0a2b522c
Miguel Lumapat <migu@meta.com>
2022-11-03 10:56:58 -0700

    Add @unselectable directive to prevent selection of internal fields
    Summary:
    ## Overview
    
    Add a new `unselectable` directive that behaves similarly to `deprecated` (i.e. discourage selection/usage of fields marked with that directive), except:
    
    * `unselectable` only operates on fields (for now)
    * `unselectable` returns an error on each validation failure
    
    ## Motivation
    
    This is largely motivated by a need to restrict selection of certain fields that don't really fit the `deprecated` category.
    
    For example, the `__relay_model_instance` field is a field generated on weak and strong Relay Resolver models. Without a mechanism to restrict selection, users could potential rely on that internal state/implementation and end up breaking later on due to incompatible changes (i.e. leaky abstractions).
    
    Outside of internal Relay restrictions, users can potentially use this as a stronger disabling mechanism.
    
    ## Tiny Note on Alternatives
    
    It's possible we can retrofit existing validations (e.g. reserved alises, deprecated fields) to achieve this same behavior?
    
    Reviewed By: alunyov
    
    Differential Revision: D40732814
    
    fbshipit-source-id: 7ed3cf72def60088c72be23202f8e64c568c49a7

--- compiler/crates/relay-config/src/project_config.rs
+++ compiler/crates/relay-config/src/project_config.rs
@@ -12,6 +12,7 @@ use std::str::FromStr;
 use std::sync::Arc;
 use std::usize;
 
+use common::DirectiveName;
 use common::FeatureFlags;
 use common::Rollout;
 use common::SourceLocationKey;
@@ -157,10 +158,18 @@ pub struct SchemaConfig {
 
     #[serde(default)]
     pub non_node_id_fields: Option<NonNodeIdFieldsConfig>,
+
+    /// The name of the directive indicating fields that cannot be selected
+    #[serde(default = "default_unselectable_directive_name")]
+    pub unselectable_directive_name: DirectiveName,
 }
 
 fn default_node_interface_id_field() -> StringKey {
     "id".intern()
+}
+
+fn default_unselectable_directive_name() -> DirectiveName {
+    DirectiveName("unselectable".intern())
 }
 
 impl Default for SchemaConfig {
@@ -169,6 +178,7 @@ impl Default for SchemaConfig {
             connection_interface: ConnectionInterface::default(),
             node_interface_id_field: default_node_interface_id_field(),
             non_node_id_fields: None,
+            unselectable_directive_name: default_unselectable_directive_name(),
         }
     }
 }

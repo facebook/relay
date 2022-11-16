c06a57be483f43feec2c44f1ecb20e23cf1b7d91
Jordan Eldredge <jeldredge@meta.com>
2022-11-16 10:09:01 -0800

    Prevent defining the id field with a resolver
    Reviewed By: davidmccabe
    
    Differential Revision: D41322803
    
    fbshipit-source-id: 0c085dbf19115c2f7d106fd356bda449924fada9

---
+++ compiler/crates/relay-docblock/tests/to_schema/fixtures/terse-relay-resolver-id.invalid.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+// expected-to-throw
+
+/**
+ * @RelayResolver PlainCommentBody.id: ID
+ *
+ * Relay expects id fields to be readable directly. They can't be implemented via resolvers.
+ */

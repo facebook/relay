1e13b201f2249ee48812b0d9e8f6691e21ba06a0
Terence Bezman <terencebezman@gmail.com>
2023-02-21 16:26:26 -0500

    add binary to generate schema

--- compiler/crates/relay-config-schema/src/main.rs
+++ compiler/crates/relay-config-schema/src/main.rs
@@ -1,3 +1,7 @@
+use relay_compiler::config::ConfigFile;
+use schemars::schema_for;
+
 fn main() {
-    // schemars::schema_for!()
+    let schema = schema_for!(ConfigFile);
+    println!("{}", serde_json::to_string_pretty(&schema).unwrap());
 }

b8083d99959cde5ad60532f1a0c4509b3a7cec50
Jordan Eldredge <jeldredge@fb.com>
2023-05-11 16:30:19 -0700

    Use Scheduler.unstable_clearLog

--- packages/react-relay/__tests__/ReactRelayQueryRenderer-test.js
+++ packages/react-relay/__tests__/ReactRelayQueryRenderer-test.js
@@ -173,7 +173,7 @@ describe('ReactRelayQueryRenderer', () => {
 
           // Flush some of the changes, but don't commit
           Scheduler.unstable_flushNumberOfYields(2);
-          expect(Scheduler.unstable_clearYields()).toEqual(['A', 'B']);
+          expect(Scheduler.unstable_clearLog()).toEqual(['A', 'B']);
           expect(renderer.toJSON()).toEqual(null);
           expect({
             error: null,

b306f9c9f2a7b1893801665a9f0cf6c26bcf8204
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2023-09-23 19:56:49 +0200

    Add RenameRequest

--- compiler/crates/relay-lsp/src/rename/mod.rs
+++ compiler/crates/relay-lsp/src/rename/mod.rs
@@ -8,12 +8,10 @@
 //! Utilities for providing the rename feature
 
 use std::collections::HashMap;
-use std::path::Path;
 use std::path::PathBuf;
 
 use common::Location as IRLocation;
 use common::SourceLocationKey;
-use common::Span;
 use extract_graphql::JavaScriptSourceFeature;
 use graphql_ir::FragmentDefinition;
 use graphql_ir::FragmentSpread;
@@ -29,7 +27,6 @@ use lsp_types::request::Request;
 use lsp_types::request::WillRenameFiles;
 use lsp_types::Location as LspLocation;
 use lsp_types::PrepareRenameResponse;
-use lsp_types::Range;
 use lsp_types::TextEdit;
 use lsp_types::Url;
 use lsp_types::WorkspaceEdit;
@@ -60,70 +57,16 @@ pub fn on_rename(
     let uri = &params.text_document_position.text_document.uri;
     let (feature, location) = state.extract_feature_from_text(&params.text_document_position, 1)?;
 
-    let program = &state.get_program(&state.extract_project_name_from_url(uri)?)?;
+    let program = &state.get_program(&state.extract_project_name_from_url(&uri)?)?;
     let root_dir = &state.root_dir();
 
-    let changes = match feature {
-        Feature::GraphQLDocument(document) => {
-            let node_path = document.resolve((), location.span());
+    let rename_request = create_rename_request(feature, location)?;
+    let changes = process_rename_request(rename_request, params.new_name, program, root_dir)?;
 
-            match node_path {
-                ResolutionPath::Ident(IdentPath {
-                    inner: fragment_spread_name,
-                    parent:
-                        IdentParent::FragmentSpreadName(_) | IdentParent::FragmentDefinitionName(_),
-                }) => rename_fragment(
-                    fragment_spread_name.value,
-                    params.new_name,
-                    program,
-                    root_dir,
-                ),
-                ResolutionPath::Ident(IdentPath {
-                    inner: operation_name,
-                    parent: IdentParent::OperationDefinitionName(_),
-                }) => {
-                    let location = IRLocation::new(location.source_location(), operation_name.span);
-                    let lsp_location =
-                        transform_relay_location_to_lsp_location(root_dir, location)?;
-
-                    Ok(rename_operation(params.new_name, lsp_location))
-                }
-                _ => Err(LSPRuntimeError::ExpectedError),
-            }
-        }
-        Feature::DocblockIr(docblock) => {
-            let resolution_info = create_docblock_resolution_info(&docblock, location.span());
-
-            match resolution_info {
-                Some(DocblockResolutionInfo::FieldName(docblock_field)) => {
-                    let parent_type = extract_parent_type(docblock);
-
-                    let mut changes = rename_relay_resolver_field(
-                        docblock_field.value,
-                        parent_type,
-                        &params.new_name,
-                        program,
-                        root_dir,
-                    )?;
-
-                    let location =
-                        common::Location::new(location.source_location(), docblock_field.span);
-                    let lsp_location =
-                        transform_relay_location_to_lsp_location(root_dir, location)?;
-
-                    merge_text_edit(&mut changes, lsp_location, &params.new_name);
-
-                    Ok(changes)
-                }
-                _ => Err(LSPRuntimeError::ExpectedError),
-            }
-        }
-    }?;
-
-    return Ok(Some(WorkspaceEdit {
+    Ok(Some(WorkspaceEdit {
         changes: Some(changes),
         ..Default::default()
-    }));
+    }))
 }
 
 /// Resolve a [`PrepareRenameRequest`] to a [`PrepareRenameResponse`]
@@ -131,43 +74,23 @@ pub fn on_prepare_rename(
     state: &impl GlobalState,
     params: <PrepareRenameRequest as Request>::Params,
 ) -> LSPRuntimeResult<<PrepareRenameRequest as Request>::Result> {
-    let (feature, location) = state.extract_feature_from_text(&params, 1)?;
     let root_dir = &state.root_dir();
+    let (feature, location) = state.extract_feature_from_text(&params, 1)?;
 
-    let range = match feature {
-        Feature::GraphQLDocument(document) => {
-            let node_path = document.resolve((), location.span());
+    let rename_request = create_rename_request(feature, location)?;
 
-            match node_path {
-                ResolutionPath::Ident(IdentPath {
-                    inner: fragment_spread_name,
-                    parent:
-                        IdentParent::FragmentSpreadName(_)
-                        | IdentParent::FragmentDefinitionName(_)
-                        | IdentParent::OperationDefinitionName(_),
-                }) => span_to_range(
-                    &root_dir,
-                    location.source_location(),
-                    fragment_spread_name.span,
-                ),
-                _ => Err(LSPRuntimeError::ExpectedError),
-            }
+    // TODO: Remove the condition, once https://github.com/facebook/relay/issues/4447 is resolved
+    match rename_request.kind {
+        RenameKind::ResolverName { .. } => Err(LSPRuntimeError::UnexpectedError(String::from(
+            "Relay Resolvers can not yet be reliably renamed",
+        ))),
+        _ => {
+            let lsp_location =
+                transform_relay_location_to_lsp_location(root_dir, rename_request.location)?;
+
+            Ok(Some(PrepareRenameResponse::Range(lsp_location.range)))
         }
-        Feature::DocblockIr(docblock) => {
-            let resolution_info = create_docblock_resolution_info(&docblock, location.span());
-
-            match resolution_info {
-                // TODO: Re-add once https://github.com/facebook/relay/issues/4447 has been resolved
-
-                // Some(DocblockResolutionInfo::FieldName(docblock_field)) => {
-                //     span_to_range(root_dir, location.source_location(), docblock_field.span)
-                // }
-                _ => Err(LSPRuntimeError::ExpectedError),
-            }
-        }
-    }?;
-
-    Ok(Some(PrepareRenameResponse::Range(range)))
+    }
 }
 
 /// Resolve a [`WillRenameFiles`] request to workspace edits
@@ -193,16 +116,11 @@ pub fn on_will_rename_files(
         let new_path = new_file_uri
             .to_file_path()
             .map_err(|_| LSPRuntimeError::ExpectedError)?;
-        let old_file_name = old_path
-            .file_stem()
-            .and_then(|s| s.to_str())
-            .ok_or(LSPRuntimeError::ExpectedError)?;
-        let new_file_name = new_path
-            .file_stem()
-            .and_then(|s| s.to_str())
-            .ok_or(LSPRuntimeError::ExpectedError)?;
 
-        if old_file_name == new_file_name {
+        let old_prefix = get_module_name(&old_path)?;
+        let new_prefix = get_module_name(&new_path)?;
+
+        if old_prefix == new_prefix {
             continue;
         }
 
@@ -213,9 +131,6 @@ pub fn on_will_rename_files(
             continue;
         }
 
-        let old_name_prefix = extract_module_name(old_file_name).unwrap();
-        let new_name_prefix = extract_module_name(new_file_name).unwrap();
-
         let program = &state.get_program(&state.extract_project_name_from_url(&old_file_uri)?)?;
         let root_dir = &state.root_dir();
 
@@ -223,45 +138,55 @@ pub fn on_will_rename_files(
             match embedded_source {
                 JavaScriptSourceFeature::GraphQL(graphql_source) => {
                     let text_source = graphql_source.text_source();
+                    let old_source_location =
+                        SourceLocationKey::embedded(old_path.to_str().unwrap(), index);
+                    let new_source_location =
+                        SourceLocationKey::embedded(new_path.to_str().unwrap(), index);
                     let document = parse_executable_with_error_recovery(
                         &text_source.text,
-                        SourceLocationKey::embedded(new_file_uri.as_ref(), index),
+                        new_source_location,
                     )
                     .item;
 
                     for definition in &document.definitions {
-                        let changes = match definition {
-                            ExecutableDefinition::Fragment(frag_def) => {
-                                let old_frag_name = frag_def.name.value;
-                                let new_frag_name = replace_prefix(
-                                    &old_frag_name.to_string(),
-                                    &old_name_prefix,
-                                    &new_name_prefix,
-                                );
+                        let (old_name, rename_request) = match definition {
+                            ExecutableDefinition::Fragment(
+                                graphql_syntax::FragmentDefinition {
+                                    name: fragment_name,
+                                    ..
+                                },
+                            ) => {
+                                let old_frag_name = fragment_name.value;
 
-                                rename_fragment(old_frag_name, new_frag_name, program, root_dir)?
+                                Some((
+                                    old_frag_name.to_string(),
+                                    RenameRequest::new(
+                                        RenameKind::FragmentName {
+                                            fragment_name: old_frag_name,
+                                        },
+                                        IRLocation::new(old_source_location, fragment_name.span),
+                                    ),
+                                ))
                             }
                             ExecutableDefinition::Operation(OperationDefinition {
-                                name: Some(operation_name_identifier),
+                                name: Some(operation_name),
                                 ..
-                            }) => {
-                                let new_operation_name = replace_prefix(
-                                    &operation_name_identifier.value.to_string(),
-                                    &old_name_prefix,
-                                    &new_name_prefix,
-                                );
-
-                                let name_range =
-                                    text_source.to_span_range(operation_name_identifier.span);
-
-                                let location = LspLocation::new(old_file_uri.clone(), name_range);
-
-                                rename_operation(new_operation_name, location)
-                            }
+                            }) => Some((
+                                operation_name.value.to_string(),
+                                RenameRequest::new(
+                                    RenameKind::OperationName,
+                                    IRLocation::new(old_source_location, operation_name.span),
+                                ),
+                            )),
                             ExecutableDefinition::Operation(
                                 graphql_syntax::OperationDefinition { name: None, .. },
-                            ) => HashMap::new(),
-                        };
+                            ) => None,
+                        }
+                        .ok_or(LSPRuntimeError::ExpectedError)?;
+
+                        let new_name = replace_prefix(&old_name, &old_prefix, &new_prefix);
+                        let changes =
+                            process_rename_request(rename_request, new_name, program, root_dir)?;
 
                         merge_text_changes(&mut rename_changes, changes);
                     }
@@ -277,14 +202,124 @@ pub fn on_will_rename_files(
     }))
 }
 
-fn replace_prefix(s: &str, old_prefix: &str, new_prefix: &str) -> String {
-    if let Some(rest) = s.strip_prefix(old_prefix) {
-        let mut result = String::from(new_prefix);
-        result.push_str(rest);
-        return result;
-    }
+#[derive(Debug, Clone)]
+pub enum RenameKind {
+    OperationName,
+    FragmentName {
+        fragment_name: StringKey,
+    },
+    ResolverName {
+        resolver_name: StringKey,
+        parent_type: StringKey,
+    },
+}
 
-    s.to_string()
+#[derive(Debug)]
+pub struct RenameRequest {
+    /// The type of the rename request we're responding to
+    kind: RenameKind,
+    location: IRLocation,
+}
+
+impl RenameRequest {
+    fn new(kind: RenameKind, location: IRLocation) -> Self {
+        Self { kind, location }
+    }
+}
+
+fn create_rename_request(
+    feature: Feature,
+    location: IRLocation,
+) -> LSPRuntimeResult<RenameRequest> {
+    match feature {
+        Feature::GraphQLDocument(document) => {
+            let node_path = document.resolve((), location.span());
+
+            match node_path {
+                ResolutionPath::Ident(IdentPath {
+                    inner: fragment_name,
+                    parent:
+                        IdentParent::FragmentSpreadName(_) | IdentParent::FragmentDefinitionName(_),
+                }) => {
+                    let location = IRLocation::new(location.source_location(), fragment_name.span);
+                    let kind = RenameKind::FragmentName {
+                        fragment_name: fragment_name.value,
+                    };
+
+                    Ok(RenameRequest::new(kind, location))
+                }
+                ResolutionPath::Ident(IdentPath {
+                    inner: operation_name,
+                    parent: IdentParent::OperationDefinitionName(_),
+                }) => {
+                    let location = IRLocation::new(location.source_location(), operation_name.span);
+
+                    Ok(RenameRequest::new(RenameKind::OperationName, location))
+                }
+                _ => Err(LSPRuntimeError::ExpectedError),
+            }
+        }
+        Feature::DocblockIr(docblock) => {
+            let resolution_info = create_docblock_resolution_info(&docblock, location.span());
+
+            match resolution_info {
+                Some(DocblockResolutionInfo::FieldName(docblock_field)) => {
+                    let location = IRLocation::new(location.source_location(), docblock_field.span);
+                    let kind = RenameKind::ResolverName {
+                        resolver_name: docblock_field.value,
+                        parent_type: extract_parent_type(docblock),
+                    };
+
+                    Ok(RenameRequest::new(kind, location))
+                }
+                _ => Err(LSPRuntimeError::ExpectedError),
+            }
+        }
+    }
+}
+
+fn process_rename_request(
+    rename_request: RenameRequest,
+    new_name: String,
+    program: &Program,
+    root_dir: &PathBuf,
+) -> LSPRuntimeResult<HashMap<Url, Vec<TextEdit>>> {
+    match rename_request.kind {
+        RenameKind::OperationName => {
+            let lsp_location =
+                transform_relay_location_to_lsp_location(root_dir, rename_request.location)?;
+
+            Ok(HashMap::from([(
+                lsp_location.uri,
+                vec![TextEdit {
+                    new_text: new_name,
+                    range: lsp_location.range,
+                }],
+            )]))
+        }
+        RenameKind::FragmentName { fragment_name } => {
+            rename_fragment(fragment_name, new_name, program, root_dir)
+        }
+        RenameKind::ResolverName {
+            parent_type,
+            resolver_name,
+        } => {
+            let mut changes = rename_relay_resolver_field(
+                resolver_name,
+                parent_type,
+                &new_name,
+                program,
+                root_dir,
+            )?;
+
+            let lsp_location =
+                transform_relay_location_to_lsp_location(root_dir, rename_request.location)?;
+
+            merge_text_edit(&mut changes, lsp_location, &new_name);
+
+            Ok(changes)
+        }
+    }
 }
 
 fn rename_relay_resolver_field(
@@ -304,19 +339,6 @@ fn rename_relay_resolver_field(
     // TODO: Rename JS resolver function as well
 
     Ok(changes)
-}
-
-fn rename_operation(
-    new_operation_name: String,
-    location: LspLocation,
-) -> HashMap<Url, Vec<TextEdit>> {
-    HashMap::from([(
-        location.uri,
-        vec![TextEdit {
-            new_text: new_operation_name,
-            range: location.range,
-        }],
-    )])
 }
 
 fn rename_fragment(
@@ -400,14 +422,21 @@ fn merge_text_edit(source: &mut HashMap<Url, Vec<TextEdit>>, location: LspLocati
     });
 }
 
-fn span_to_range(
-    root_dir: &Path,
-    source_location_key: SourceLocationKey,
-    span: Span,
-) -> LSPRuntimeResult<Range> {
-    let location = common::Location::new(source_location_key, span);
+fn replace_prefix(s: &str, old_prefix: &str, new_prefix: &str) -> String {
+    if let Some(rest) = s.strip_prefix(old_prefix) {
+        let mut result = String::from(new_prefix);
+        result.push_str(rest);
+        return result;
+    }
 
-    let lsp_location = transform_relay_location_to_lsp_location(root_dir, location)?;
+    s.to_string()
+}
 
-    Ok(lsp_location.range)
+fn get_module_name(path: &PathBuf) -> LSPRuntimeResult<String> {
+    let file_name = path
+        .file_stem()
+        .and_then(|s| s.to_str())
+        .ok_or(LSPRuntimeError::ExpectedError)?;
+
+    extract_module_name(file_name).ok_or(LSPRuntimeError::ExpectedError)
 }

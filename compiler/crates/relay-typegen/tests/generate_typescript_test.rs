783b6e3e111be2704c923c899da512160fb9c009
Ernie <ernie.turner@coinbase.com>
2023-09-25 15:42:42 -0600

    Update SignedSource

--- compiler/crates/relay-typegen/tests/generate_typescript_test.rs
+++ compiler/crates/relay-typegen/tests/generate_typescript_test.rs
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<c12cbcb2699b05580be1a62dde8a3bcf>>
+ * @generated SignedSource<<9a0b1dc4bd249d31e8cab05d665b685f>>
  */
 
 mod generate_typescript;

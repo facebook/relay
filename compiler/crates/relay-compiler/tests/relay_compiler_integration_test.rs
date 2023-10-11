780987adbb94aa616f1597bb9cd09abbfa8ee97c
Jordan Eldredge <jeldredge@meta.com>
2023-10-11 13:24:20 -0700

    Regression test for D45661548
    Reviewed By: voideanvalue
    
    Differential Revision: D50129691
    
    fbshipit-source-id: c9061bfc2bc6ee507b07a2828e62cfd7234ebe65

--- compiler/crates/relay-compiler/tests/relay_compiler_integration_test.rs
+++ compiler/crates/relay-compiler/tests/relay_compiler_integration_test.rs
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<6abb22a0a499e1ac42e8a792ac9b15ed>>
+ * @generated SignedSource<<04c298d7b18b4ef4e4be2c9c820b2e46>>
  */
 
 mod relay_compiler_integration;
@@ -17,4 +17,11 @@ async fn simple_fragment() {
     let input = include_str!("relay_compiler_integration/fixtures/simple_fragment.input");
     let expected = include_str!("relay_compiler_integration/fixtures/simple_fragment.expected");
     test_fixture(transform_fixture, "simple_fragment.input", "relay_compiler_integration/fixtures/simple_fragment.expected", input, expected).await;
+}
+
+#[tokio::test]
+async fn typescript_resolver_type_import() {
+    let input = include_str!("relay_compiler_integration/fixtures/typescript_resolver_type_import.input");
+    let expected = include_str!("relay_compiler_integration/fixtures/typescript_resolver_type_import.expected");
+    test_fixture(transform_fixture, "typescript_resolver_type_import.input", "relay_compiler_integration/fixtures/typescript_resolver_type_import.expected", input, expected).await;
 }

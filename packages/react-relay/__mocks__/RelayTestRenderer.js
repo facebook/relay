95e4f9df5d8feb16a9eefc4252e9385b204c273b
Andrey Lunyov <alunyov@meta.com>
2023-10-12 10:13:42 -0700

    Add $FlowFixMe to unblock code sync to WWW
    Reviewed By: voideanvalue
    
    Differential Revision: D50225729
    
    fbshipit-source-id: b5be42956e6bb64017cc7c119acfca47dba7c4a0

--- packages/react-relay/__mocks__/RelayTestRenderer.js
+++ packages/react-relay/__mocks__/RelayTestRenderer.js
@@ -58,6 +58,7 @@ class RelayTestRenderer extends React.Component<Props, $FlowFixMe> {
   };
 
   render(): React.Element<typeof ReactRelayContext.Provider> {
+    // $FlowFixMe(site=www)[prop-missing]
     const childProps = this.props.children.props;
     const newProps = {...childProps, ...this.state.data};
     return (

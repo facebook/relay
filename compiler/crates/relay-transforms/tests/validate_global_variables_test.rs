f90edc4cb52a77fd67a2ac601a437430559c48a7
Monica Tang <monicatang@meta.com>
2023-10-24 10:18:05 -0700

    add regression test for fragment spreads with undefined global variables as arguments
    Summary:
    Don't think we have a similar test for this where we are passing an undefined global variable as an argument to a fragment spread, so added one while working on T160746170
    
    Add regression test for fragment spreads with undefined global variables as arguments
    
    Reviewed By: alunyov
    
    Differential Revision: D50573429
    
    fbshipit-source-id: b16bb3e2fecb32f1c642f912808e1b81a991bfe5

--- compiler/crates/relay-transforms/tests/validate_global_variables_test.rs
+++ compiler/crates/relay-transforms/tests/validate_global_variables_test.rs
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<2727cdaebb7d446fd26bcade2da67b8f>>
+ * @generated SignedSource<<7435d162790438e443e6e90825864a09>>
  */
 
 mod validate_global_variables;
@@ -17,6 +17,13 @@ async fn fragment_include_with_provided_argument() {
     let input = include_str!("validate_global_variables/fixtures/fragment-include-with-provided-argument.graphql");
     let expected = include_str!("validate_global_variables/fixtures/fragment-include-with-provided-argument.expected");
     test_fixture(transform_fixture, "fragment-include-with-provided-argument.graphql", "validate_global_variables/fixtures/fragment-include-with-provided-argument.expected", input, expected).await;
+}
+
+#[tokio::test]
+async fn fragment_spread_with_undefined_argument_invalid() {
+    let input = include_str!("validate_global_variables/fixtures/fragment-spread-with-undefined-argument.invalid.graphql");
+    let expected = include_str!("validate_global_variables/fixtures/fragment-spread-with-undefined-argument.invalid.expected");
+    test_fixture(transform_fixture, "fragment-spread-with-undefined-argument.invalid.graphql", "validate_global_variables/fixtures/fragment-spread-with-undefined-argument.invalid.expected", input, expected).await;
 }
 
 #[tokio::test]

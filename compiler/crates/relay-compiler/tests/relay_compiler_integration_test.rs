22a3c89f9518d5c9bd84c0bde9fd5ec94c4ed13f
Alex Danoff <adanoff@meta.com>
2023-10-30 17:27:08 -0700

    import ClientRequest for client only mutations
    Reviewed By: alunyov
    
    Differential Revision: D50805852
    
    fbshipit-source-id: 222e4e81becfa62b108afdb1fab17189f7163d3b

--- compiler/crates/relay-compiler/tests/relay_compiler_integration_test.rs
+++ compiler/crates/relay-compiler/tests/relay_compiler_integration_test.rs
@@ -4,13 +4,27 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<04c298d7b18b4ef4e4be2c9c820b2e46>>
+ * @generated SignedSource<<6a4360c4cb2d5b7641f3a41ae33f6a27>>
  */
 
 mod relay_compiler_integration;
 
 use relay_compiler_integration::transform_fixture;
 use fixture_tests::test_fixture;
+
+#[tokio::test]
+async fn client_mutation_extension() {
+    let input = include_str!("relay_compiler_integration/fixtures/client_mutation_extension.input");
+    let expected = include_str!("relay_compiler_integration/fixtures/client_mutation_extension.expected");
+    test_fixture(transform_fixture, "client_mutation_extension.input", "relay_compiler_integration/fixtures/client_mutation_extension.expected", input, expected).await;
+}
+
+#[tokio::test]
+async fn client_mutation_resolver() {
+    let input = include_str!("relay_compiler_integration/fixtures/client_mutation_resolver.input");
+    let expected = include_str!("relay_compiler_integration/fixtures/client_mutation_resolver.expected");
+    test_fixture(transform_fixture, "client_mutation_resolver.input", "relay_compiler_integration/fixtures/client_mutation_resolver.expected", input, expected).await;
+}
 
 #[tokio::test]
 async fn simple_fragment() {

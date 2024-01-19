5718ca9fd9b2ac3474dd5b87d4d3030b66a1740a
Monica Tang <monicatang@meta.com>
2024-01-19 11:54:30 -0800

    transform to convert fragments and linked fields on abstract types to inline fragment
    Summary:
    ## context
    
    https://fburl.com/gdoc/3cv4msxz
    
    ## this diff
    
    Converts selections on fragments and linked fields on abstract types to selections on inline fragments on the concrete type. This is just a starting point, i.e. fragment spreads are tbd.
    
    Reviewed By: captbaritone
    
    Differential Revision: D52861037
    
    fbshipit-source-id: ff2ec2a3a44f2a6ec25246a52ce130993cb57e77

--- compiler/crates/relay-transforms/src/relay_resolvers_abstract_types/mod.rs
+++ compiler/crates/relay-transforms/src/relay_resolvers_abstract_types/mod.rs
@@ -5,11 +5,22 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+use std::sync::Arc;
+
 use common::Diagnostic;
 use common::DiagnosticsResult;
 use common::FeatureFlags;
+use common::Location;
+use graphql_ir::FragmentDefinition;
+use graphql_ir::InlineFragment;
+use graphql_ir::LinkedField;
 use graphql_ir::Program;
+use graphql_ir::Selection;
+use graphql_ir::Transformed;
 use graphql_ir::Transformer;
+use schema::SDLSchema;
+use schema::Schema;
+use schema::Type;
 
 /// Transform selections on abstract types.
 ///
@@ -39,23 +50,86 @@ pub fn relay_resolvers_abstract_types(
 }
 
 struct RelayResolverAbstractTypesTransform<'program> {
-    _program: &'program Program,
+    program: &'program Program,
     errors: Vec<Diagnostic>,
 }
 
 impl<'program> RelayResolverAbstractTypesTransform<'program> {
     fn new(program: &'program Program) -> Self {
         Self {
-            _program: program,
+            program,
             errors: Default::default(),
         }
     }
-
-    // TODO T174693027 Implement transform
 }
 
 impl Transformer for RelayResolverAbstractTypesTransform<'_> {
     const NAME: &'static str = "RelayResolverAbstractTypesTransform";
     const VISIT_ARGUMENTS: bool = false;
     const VISIT_DIRECTIVES: bool = false;
+
+    fn transform_fragment(
+        &mut self,
+        fragment: &FragmentDefinition,
+    ) -> Transformed<FragmentDefinition> {
+        if !matches!(fragment.type_condition, Type::Interface(_)) {
+            return Transformed::Keep;
+        }
+        let inline_fragment_selections = create_inline_fragment_selections_for_abstract_type(
+            &self.program.schema,
+            fragment.type_condition,
+            &fragment.selections,
+        );
+        Transformed::Replace(FragmentDefinition {
+            selections: inline_fragment_selections,
+            ..fragment.clone()
+        })
+    }
+
+    fn transform_linked_field(&mut self, field: &LinkedField) -> Transformed<Selection> {
+        let schema = &self.program.schema;
+        let field_type = schema.field(field.definition.item);
+        let edge_to_type = field_type.type_.inner();
+        if !matches!(edge_to_type, Type::Interface(_)) {
+            return Transformed::Keep;
+        }
+        let inline_fragment_selections = create_inline_fragment_selections_for_abstract_type(
+            &schema,
+            edge_to_type,
+            &field.selections,
+        );
+        Transformed::Replace(Selection::LinkedField(Arc::new(LinkedField {
+            selections: inline_fragment_selections,
+            ..field.clone()
+        })))
+    }
+}
+
+fn create_inline_fragment_selections_for_abstract_type(
+    schema: &Arc<SDLSchema>,
+    abstract_type: Type,
+    selections: &[Selection],
+) -> Vec<Selection> {
+    match abstract_type {
+        Type::Interface(interface_id) => {
+            let interface = schema.interface(interface_id);
+            let implementing_objects =
+                interface.recursively_implementing_objects(Arc::as_ref(&schema));
+            let mut sorted_implementing_objects =
+                implementing_objects.into_iter().collect::<Vec<_>>();
+            sorted_implementing_objects.sort();
+            sorted_implementing_objects
+                .iter()
+                .map(|object_id| {
+                    Selection::InlineFragment(Arc::new(InlineFragment {
+                        type_condition: Some(Type::Object(*object_id)),
+                        directives: vec![], // TODO T174693027 do we need directives here?
+                        selections: selections.to_vec(),
+                        spread_location: Location::generated(),
+                    }))
+                })
+                .collect()
+        }
+        _ => panic!("Expected abstract type to be an interface"),
+    }
 }

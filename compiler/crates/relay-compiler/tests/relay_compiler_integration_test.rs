095b3916253d0909a05260a86a57eaf0a576c7fc
Jordan Eldredge <jeldredge@meta.com>
2024-01-23 17:03:24 -0800

    Add integration test documenting current support for M2
    Reviewed By: monicatang
    
    Differential Revision: D53012970
    
    fbshipit-source-id: 9cf9b4faeee74734fad228f8b826087040f69f10

--- compiler/crates/relay-compiler/tests/relay_compiler_integration_test.rs
+++ compiler/crates/relay-compiler/tests/relay_compiler_integration_test.rs
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<a06755e39058e30909990af90e891e89>>
+ * @generated SignedSource<<f4ccc21e23d8888078c2d4cd5515ae6e>>
  */
 
 mod relay_compiler_integration;
@@ -87,6 +87,13 @@ async fn resolver_on_interface() {
     let input = include_str!("relay_compiler_integration/fixtures/resolver_on_interface.input");
     let expected = include_str!("relay_compiler_integration/fixtures/resolver_on_interface.expected");
     test_fixture(transform_fixture, file!(), "resolver_on_interface.input", "relay_compiler_integration/fixtures/resolver_on_interface.expected", input, expected).await;
+}
+
+#[tokio::test]
+async fn resolver_returns_interface_of_all_strong_model_type() {
+    let input = include_str!("relay_compiler_integration/fixtures/resolver_returns_interface_of_all_strong_model_type.input");
+    let expected = include_str!("relay_compiler_integration/fixtures/resolver_returns_interface_of_all_strong_model_type.expected");
+    test_fixture(transform_fixture, file!(), "resolver_returns_interface_of_all_strong_model_type.input", "relay_compiler_integration/fixtures/resolver_returns_interface_of_all_strong_model_type.expected", input, expected).await;
 }
 
 #[tokio::test]

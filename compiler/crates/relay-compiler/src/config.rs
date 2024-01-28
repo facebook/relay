71b6b63576fd9e55c4cb6f8d106ae5fecb0a79ed
Xinkai Chen <xinkai.chen@qq.com>
2024-01-28 22:59:15 +0800

    Allow diagnosticReportConfig in SingleProjectConfig.

--- compiler/crates/relay-compiler/src/config.rs
+++ compiler/crates/relay-compiler/src/config.rs
@@ -726,6 +726,9 @@ pub struct SingleProjectConfigFile {
 
     #[serde(default)]
     pub resolvers_schema_module: Option<ResolversSchemaModuleConfig>,
+
+    #[serde(default)]
+    pub diagnostic_report_config: DiagnosticReportConfig,
 }
 
 impl Default for SingleProjectConfigFile {
@@ -749,6 +752,7 @@ impl Default for SingleProjectConfigFile {
             feature_flags: None,
             module_import_config: Default::default(),
             resolvers_schema_module: Default::default(),
+            diagnostic_report_config: Default::default(),
         }
     }
 }
@@ -854,6 +858,7 @@ impl SingleProjectConfigFile {
             feature_flags: self.feature_flags,
             module_import_config: self.module_import_config,
             resolvers_schema_module: self.resolvers_schema_module,
+            diagnostic_report_config: self.diagnostic_report_config,
             ..Default::default()
         };
 

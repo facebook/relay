1876cb211c13e8a9d5f9a9a324ef769c744d6936
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-02-26 19:27:56 +0100

    Add test case for variable being used

--- compiler/crates/relay-compiler/tests/compile_relay_artifacts_test.rs
+++ compiler/crates/relay-compiler/tests/compile_relay_artifacts_test.rs
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<8a09ce6acc7e554856ffda29bab1161c>>
+ * @generated SignedSource<<a462cf5b6b402f99514438fe2a6c56fe>>
  */
 
 mod compile_relay_artifacts;
@@ -157,6 +157,13 @@ async fn append_node_literal_edge_type_name_not_object_type() {
     let input = include_str!("compile_relay_artifacts/fixtures/append-node-literal-edge-type-name-not-object-type.graphql");
     let expected = include_str!("compile_relay_artifacts/fixtures/append-node-literal-edge-type-name-not-object-type.expected");
     test_fixture(transform_fixture, file!(), "append-node-literal-edge-type-name-not-object-type.graphql", "compile_relay_artifacts/fixtures/append-node-literal-edge-type-name-not-object-type.expected", input, expected).await;
+}
+
+#[tokio::test]
+async fn append_node_literal_edge_type_name_variable() {
+    let input = include_str!("compile_relay_artifacts/fixtures/append-node-literal-edge-type-name-variable.graphql");
+    let expected = include_str!("compile_relay_artifacts/fixtures/append-node-literal-edge-type-name-variable.expected");
+    test_fixture(transform_fixture, file!(), "append-node-literal-edge-type-name-variable.graphql", "compile_relay_artifacts/fixtures/append-node-literal-edge-type-name-variable.expected", input, expected).await;
 }
 
 #[tokio::test]

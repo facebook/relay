0ed5e93d5e90a0930feda1c0713fda14abe6a373
tobias-tengler <45513122+tobias-tengler@users.noreply.github.com>
2024-02-26 22:13:31 +0100

    Add go to definition support for type defined in variable definition

--- compiler/crates/relay-lsp/src/goto_definition/goto_graphql_definition.rs
+++ compiler/crates/relay-lsp/src/goto_definition/goto_graphql_definition.rs
@@ -64,6 +64,12 @@ pub fn get_graphql_definition_description(
         }) => Ok(DefinitionDescription::Type {
             type_name: type_condition.type_.value,
         }),
+        ResolutionPath::Ident(IdentPath {
+            inner: type_name,
+            parent: IdentParent::NamedTypeAnnotation(_),
+        }) => Ok(DefinitionDescription::Type {
+            type_name: type_name.value,
+        }),
         _ => Err(LSPRuntimeError::ExpectedError),
     }
 }

c715513f9736b3b1b5884af811c4c1c9a2c81257
Jordan Eldredge <jordan@jordaneldredge.com>
2024-06-07 10:24:23 -0700

    Normalize paths in diagnostics CLI output

--- compiler/crates/graphql-cli/src/diagnostic_printer.rs
+++ compiler/crates/graphql-cli/src/diagnostic_printer.rs
@@ -85,7 +85,7 @@ impl<TSources: Sources> DiagnosticPrinter<TSources> {
             writeln!(
                 writer,
                 "  {}{}",
-                location.source_location().path().underline(),
+                normalize_path(location.source_location().path()).underline(),
                 format!(":{}:{}", range.start.line + 1, range.start.character + 1).dimmed()
             )?;
             source_printer.write_span_with_highlight_style(
@@ -99,7 +99,7 @@ impl<TSources: Sources> DiagnosticPrinter<TSources> {
             writeln!(
                 writer,
                 "{}: <missing source>",
-                location.source_location().path()
+                normalize_path(location.source_location().path())
             )?;
         }
         Ok(())
@@ -117,4 +117,10 @@ where
     fn get(&self, source_location: SourceLocationKey) -> Option<TextSource> {
         self(source_location)
     }
+}
+
+/// Normalize Windows paths to Unix style. This is important for stable test
+/// output across Mac/Windows/Linux.
+fn normalize_path(path: &str) -> String {
+    path.replace("\\", "/")
 }

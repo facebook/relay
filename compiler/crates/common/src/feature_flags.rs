1ad498a1a5337bfa8ab18f56ccf2d104f83042e9
Drew Atkinson <drewatkinson5@gmail.com>
2024-09-18 10:54:54 -0700

    fix missing FFs

--- compiler/crates/common/src/feature_flags.rs
+++ compiler/crates/common/src/feature_flags.rs
@@ -147,14 +147,12 @@ impl Default for FeatureFlags {
     fn default() -> Self {
         Self {
             enable_relay_resolver_transform: false,
-            enable_catch_directive_transform: Default::default(),
             relay_resolver_enable_interface_output_type: Default::default(),
             no_inline: Default::default(),
             enable_3d_branch_arg_generation: false,
             actor_change_support: Default::default(),
             text_artifacts: Default::default(),
             skip_printing_nulls: Default::default(),
-            enable_fragment_aliases: Default::default(),
             enforce_fragment_alias_where_ambiguous: Default::default(),
             compact_query_text: Default::default(),
             enable_resolver_normalization_ast: false,
@@ -166,10 +164,12 @@ impl Default for FeatureFlags {
             enable_fragment_argument_transform: false,
             allow_resolver_non_nullable_return_type: Default::default(),
             disable_schema_validation: false,
-            disallow_required_on_non_null_fields: false,
             prefer_fetchable_in_refetch_queries: false,
             disable_edge_type_name_validation_on_declerative_connection_directives: Default::default(),
             typescript_disable_checking_generated_files: default_true(),
+            enable_exec_time_resolvers_directive: false,
+            disable_full_argument_type_validation: Default::default(),
+            enable_custom_artifacts_path: Default::default(),
         }
     }
 }

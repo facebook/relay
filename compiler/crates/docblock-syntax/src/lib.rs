524f5c4d0fb1e85132eedf39ebb4b1cf31ac8d35
Gordy French <gordyf@meta.com>
2024-10-04 10:47:28 -0700

    Unicode computed location fix
    Reviewed By: captbaritone
    
    Differential Revision: D63879176
    
    fbshipit-source-id: 9093a8916524dad92cab7a4d91bf6917218ee95c

--- compiler/crates/docblock-syntax/src/lib.rs
+++ compiler/crates/docblock-syntax/src/lib.rs
@@ -85,7 +85,7 @@ pub fn parse_docblock(
  * strings with quotation marks.
  *
  * To account for this, we parse in a single pass, essentially treating each
- * character as a token. This allows us to easily intemperate characters
+ * character as a token. This allows us to easily interpret characters
  * differently in different contexts.
  */
 struct DocblockParser<'a> {
@@ -283,7 +283,7 @@ impl<'a> DocblockParser<'a> {
 
     fn next(&mut self) {
         self.chars.next();
-        self.offset += 1;
+        self.offset += 1; // Is this correct for unicode characters?
     }
 
     /// Advance over a string of characters matching predicate.
@@ -307,7 +307,7 @@ impl<'a> DocblockParser<'a> {
                 break;
             }
         }
-        self.offset += result.len() as u32;
+        self.offset += result.len() as u32; // result.len() returns byte length
         result
     }
 

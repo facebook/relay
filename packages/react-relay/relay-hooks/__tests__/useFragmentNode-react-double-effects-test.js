2372a3fbf77515f4e3442e4829d5e0164724ee59
Panos Vekris <pvekris@meta.com>
2024-10-08 10:28:56 -0700

    replace '$TEMPORARY$object' with '$ReadOnly' [1/n]
    Reviewed By: SamChou19815
    
    Differential Revision: D64019269
    
    fbshipit-source-id: 811deeff3be2f9ebed750719da67a4e87a074a67

--- packages/react-relay/relay-hooks/__tests__/useFragmentNode-react-double-effects-test.js
+++ packages/react-relay/relay-hooks/__tests__/useFragmentNode-react-double-effects-test.js
@@ -74,7 +74,7 @@ describe.skip('useFragmentNode-react-double-effects-test', () => {
     warning.mockClear();
 
     let renderLogs = [];
-    const FragmentComponent = ({user}: $TEMPORARY$object<{user: mixed}>) => {
+    const FragmentComponent = ({user}: $ReadOnly<{user: mixed}>) => {
       const {data} = useFragmentNode<any>(gqlFragment, user, 'TestComponent');
       useEffect(() => {
         renderLogs.push(`commit: ${data.name}`);

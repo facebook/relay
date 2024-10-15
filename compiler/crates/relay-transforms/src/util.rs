af35418528761f3fde37a48d47eb5fa4bee2d9f9
Jordan Eldredge <jeldredge@meta.com>
2024-10-15 12:25:13 -0700

    Ignore @dangerously_unaliased_fixme when comparing structs
    Reviewed By: gordyf
    
    Differential Revision: D64410096
    
    fbshipit-source-id: 08a5c9934477e145742086af00694f201b4b6191

--- compiler/crates/relay-transforms/src/util.rs
+++ compiler/crates/relay-transforms/src/util.rs
@@ -26,6 +26,7 @@ use schema::Type;
 use crate::catch_directive::CATCH_DIRECTIVE_NAME;
 use crate::client_extensions::CLIENT_EXTENSION_DIRECTIVE_NAME;
 use crate::connections::ConnectionMetadataDirective;
+use crate::fragment_alias_directive::FRAGMENT_DANGEROUSLY_UNALIAS_DIRECTIVE_NAME;
 use crate::handle_fields::HANDLE_FIELD_DIRECTIVE_NAME;
 use crate::inline_data_fragment::InlineDirectiveMetadata;
 use crate::refetchable_fragment::RefetchableMetadata;
@@ -107,7 +108,7 @@ lazy_static! {
         ProvidedVariableMetadata::directive_name(),
         FragmentAliasMetadata::directive_name(),
     ];
-    static ref DIRECTIVES_SKIPPED_IN_NODE_IDENTIFIER: [DirectiveName; 9] = [
+    static ref DIRECTIVES_SKIPPED_IN_NODE_IDENTIFIER: [DirectiveName; 10] = [
         *CATCH_DIRECTIVE_NAME,
         *CLIENT_EXTENSION_DIRECTIVE_NAME,
         ConnectionMetadataDirective::directive_name(),
@@ -117,6 +118,7 @@ lazy_static! {
         *INTERNAL_METADATA_DIRECTIVE,
         *ARGUMENT_DEFINITION,
         *REQUIRED_DIRECTIVE_NAME,
+        *FRAGMENT_DANGEROUSLY_UNALIAS_DIRECTIVE_NAME
     ];
     static ref RELAY_CUSTOM_INLINE_FRAGMENT_DIRECTIVES: [DirectiveName; 8] = [
         *CLIENT_EXTENSION_DIRECTIVE_NAME,

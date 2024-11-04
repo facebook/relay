15a19e91174db8b751194fcab1a4670ee8777f8f
Xiangxin Sun <xiangxin@meta.com>
2024-11-04 15:18:59 -0800

    Update metadata generation for Live Query in Relay compiler
    Reviewed By: tcr
    
    Differential Revision: D65232885
    
    fbshipit-source-id: 1293cadedc41785c928d90062345156355005979

--- compiler/crates/relay-transforms/tests/generate_live_query_metadata_test.rs
+++ compiler/crates/relay-transforms/tests/generate_live_query_metadata_test.rs
@@ -4,13 +4,27 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @generated SignedSource<<bf0ee933ed7d251f881746f03c402397>>
+ * @generated SignedSource<<c5b485781bb5c70ef55568195c695df1>>
  */
 
 mod generate_live_query_metadata;
 
 use generate_live_query_metadata::transform_fixture;
 use fixture_tests::test_fixture;
+
+#[tokio::test]
+async fn live_by_at_live() {
+    let input = include_str!("generate_live_query_metadata/fixtures/live_by_at_live.graphql");
+    let expected = include_str!("generate_live_query_metadata/fixtures/live_by_at_live.expected");
+    test_fixture(transform_fixture, file!(), "live_by_at_live.graphql", "generate_live_query_metadata/fixtures/live_by_at_live.expected", input, expected).await;
+}
+
+#[tokio::test]
+async fn live_by_at_live_config_id() {
+    let input = include_str!("generate_live_query_metadata/fixtures/live_by_at_live_config_id.graphql");
+    let expected = include_str!("generate_live_query_metadata/fixtures/live_by_at_live_config_id.expected");
+    test_fixture(transform_fixture, file!(), "live_by_at_live_config_id.graphql", "generate_live_query_metadata/fixtures/live_by_at_live_config_id.expected", input, expected).await;
+}
 
 #[tokio::test]
 async fn live_by_config_id() {

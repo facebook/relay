79c12a09a11da8f6bf8ae13464aec7ecbc27c4ad
Lauren Tan <laurentan@meta.com>
2024-11-21 13:34:04 -0800

    Add new Relay flag
    Reviewed By: captbaritone, jackpope, lynnshaoyu
    
    Differential Revision: D66303103
    
    fbshipit-source-id: cee2c8aaebb742cf84188fe1673ed53bb84be104

--- packages/relay-runtime/util/RelayFeatureFlags.js
+++ packages/relay-runtime/util/RelayFeatureFlags.js
@@ -58,6 +58,11 @@ export type FeatureFlags = {
   // Adds a prefix to the storage key of read time resolvers. This is used to
   // disambiguate the same resolver being used at both read time and exec time.
   ENABLE_READ_TIME_RESOLVER_STORAGE_KEY_PREFIX: boolean,
+
+  // Enables the use of an experimental new useResourceEffect React API. This
+  // hook allows you to manage a resource that is not tied to the component
+  // and replaces the need for multiple useEffects to manage the same resource.
+  ENABLE_RESOURCE_EFFECTS: boolean,
 };
 
 const RelayFeatureFlags: FeatureFlags = {
@@ -81,6 +86,7 @@ const RelayFeatureFlags: FeatureFlags = {
   ENABLE_CYLE_DETECTION_IN_VARIABLES: false,
   ENABLE_ACTIVITY_COMPATIBILITY: false,
   ENABLE_READ_TIME_RESOLVER_STORAGE_KEY_PREFIX: true,
+  ENABLE_RESOURCE_EFFECTS: false,
 };
 
 module.exports = RelayFeatureFlags;

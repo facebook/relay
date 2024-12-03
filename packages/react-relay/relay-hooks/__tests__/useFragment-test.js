a4b1a4fe65b8bb8f0ff0eab2fee13e56b393dd9c
Jack Pope <jackpope@meta.com>
2024-12-03 08:29:52 -0800

    Experimentally replace Relay userspace crud effects with useResourceEffect
    Reviewed By: josephsavona
    
    Differential Revision: D66303102
    
    fbshipit-source-id: 15bcda870096292c4296d4496b5e5f9702b0a937

--- packages/react-relay/relay-hooks/__tests__/useFragment-test.js
+++ packages/react-relay/relay-hooks/__tests__/useFragment-test.js
@@ -98,310 +98,320 @@ function createFragmentRef(id: string, owner: OperationDescriptor) {
 }
 
 describe.each([
-  ['Experimental', true],
-  ['Current', false],
-])('useFragment (%s)', (name, ENABLE_ACTIVITY_COMPATIBILITY) => {
-  beforeEach(() => {
-    RelayFeatureFlags.ENABLE_ACTIVITY_COMPATIBILITY =
-      ENABLE_ACTIVITY_COMPATIBILITY;
+  ['Experimental', true, false],
+  ['Experimental + ResourceEffects', true, true],
+  ['Current', false, false],
+])(
+  'useFragment (%s, ResourceEffects=%s)',
+  (name, ENABLE_ACTIVITY_COMPATIBILITY, ENABLE_RESOURCE_EFFECTS) => {
+    beforeEach(() => {
+      RelayFeatureFlags.ENABLE_ACTIVITY_COMPATIBILITY =
+        ENABLE_ACTIVITY_COMPATIBILITY;
+      // experimental_useResourceEffect is not yet available on experimental build for OSS testing
+      RelayFeatureFlags.ENABLE_RESOURCE_EFFECTS =
+        ENABLE_RESOURCE_EFFECTS &&
+        // $FlowFixMe[prop-missing]
+        typeof React.experimental_useResourceEffect === 'function';
 
-    renderSpy = jest.fn<
-      [useFragmentTestUserFragment$data | useFragmentTestUsersFragment$data],
-      mixed,
-    >();
+      renderSpy = jest.fn<
+        [useFragmentTestUserFragment$data | useFragmentTestUsersFragment$data],
+        mixed,
+      >();
 
-    // Set up environment and base data
-    environment = createMockEnvironment();
-    graphql`
-      fragment useFragmentTestNestedUserFragment on User {
-        username
-      }
-    `;
-    singularVariables = {id: '1'};
-    pluralVariables = {ids: ['1', '2']};
-    gqlSingularQuery = graphql`
-      query useFragmentTestUserQuery($id: ID!) {
-        node(id: $id) {
-          ...useFragmentTestUserFragment
+      // Set up environment and base data
+      environment = createMockEnvironment();
+      graphql`
+        fragment useFragmentTestNestedUserFragment on User {
+          username
         }
-      }
-    `;
-    gqlSingularFragment = graphql`
-      fragment useFragmentTestUserFragment on User {
-        id
-        name
-        ...useFragmentTestNestedUserFragment
-      }
-    `;
-    gqlPluralQuery = graphql`
-      query useFragmentTestUsersQuery($ids: [ID!]!) {
-        nodes(ids: $ids) {
-          ...useFragmentTestUsersFragment
+      `;
+      singularVariables = {id: '1'};
+      pluralVariables = {ids: ['1', '2']};
+      gqlSingularQuery = graphql`
+        query useFragmentTestUserQuery($id: ID!) {
+          node(id: $id) {
+            ...useFragmentTestUserFragment
+          }
         }
-      }
-    `;
-    gqlPluralFragment = graphql`
-      fragment useFragmentTestUsersFragment on User @relay(plural: true) {
-        id
-        name
-        ...useFragmentTestNestedUserFragment
-      }
-    `;
-    singularQuery = createOperationDescriptor(
-      gqlSingularQuery,
-      singularVariables,
-    );
-    pluralQuery = createOperationDescriptor(gqlPluralQuery, pluralVariables);
-    environment.commitPayload(singularQuery, {
-      node: {
-        __typename: 'User',
-        id: '1',
-        name: 'Alice',
-        username: 'useralice',
-      },
-    });
-    environment.commitPayload(pluralQuery, {
-      nodes: [
-        {
+      `;
+      gqlSingularFragment = graphql`
+        fragment useFragmentTestUserFragment on User {
+          id
+          name
+          ...useFragmentTestNestedUserFragment
+        }
+      `;
+      gqlPluralQuery = graphql`
+        query useFragmentTestUsersQuery($ids: [ID!]!) {
+          nodes(ids: $ids) {
+            ...useFragmentTestUsersFragment
+          }
+        }
+      `;
+      gqlPluralFragment = graphql`
+        fragment useFragmentTestUsersFragment on User @relay(plural: true) {
+          id
+          name
+          ...useFragmentTestNestedUserFragment
+        }
+      `;
+      singularQuery = createOperationDescriptor(
+        gqlSingularQuery,
+        singularVariables,
+      );
+      pluralQuery = createOperationDescriptor(gqlPluralQuery, pluralVariables);
+      environment.commitPayload(singularQuery, {
+        node: {
           __typename: 'User',
           id: '1',
           name: 'Alice',
           username: 'useralice',
-          profile_picture: null,
         },
-        {
-          __typename: 'User',
-          id: '2',
-          name: 'Bob',
-          username: 'userbob',
-          profile_picture: null,
+      });
+      environment.commitPayload(pluralQuery, {
+        nodes: [
+          {
+            __typename: 'User',
+            id: '1',
+            name: 'Alice',
+            username: 'useralice',
+            profile_picture: null,
+          },
+          {
+            __typename: 'User',
+            id: '2',
+            name: 'Bob',
+            username: 'userbob',
+            profile_picture: null,
+          },
+        ],
+      });
+
+      // Set up renderers
+      SingularRenderer = (props: {
+        user: ?(
+          | useFragmentTestUserFragment$data
+          | useFragmentTestUsersFragment$data
+        ),
+      }) => null;
+      PluralRenderer = (props: {
+        users: ?(
+          | useFragmentTestUserFragment$data
+          | useFragmentTestUsersFragment$data
+        ),
+      }) => null;
+      const SingularContainer = (props: {
+        userRef?: {$data?: {...}, ...},
+        owner: $FlowFixMe,
+        ...
+      }) => {
+        // We need a render a component to run a Hook
+        const owner = props.owner;
+        const userRef = props.hasOwnProperty('userRef')
+          ? props.userRef
+          : {
+              [ID_KEY]: owner.request.variables.id,
+              [FRAGMENTS_KEY]: {
+                useFragmentTestUserFragment: {},
+              },
+              [FRAGMENT_OWNER_KEY]: owner.request,
+            };
+        const userData = useFragment(gqlSingularFragment, userRef);
+        return <SingularRenderer user={userData} />;
+      };
+
+      const PluralContainer = (props: {
+        usersRef?: $ReadOnlyArray<{$data?: {...}, ...}>,
+        owner: $FlowFixMe,
+        ...
+      }) => {
+        const owner = props.owner;
+        const usersRef = props.hasOwnProperty('usersRef')
+          ? props.usersRef
+          : owner.request.variables.ids.map(id => ({
+              [ID_KEY]: id,
+              [FRAGMENTS_KEY]: {
+                useFragmentTestUsersFragment: {},
+              },
+              [FRAGMENT_OWNER_KEY]: owner.request,
+            }));
+
+        const usersData = useFragment(gqlPluralFragment, usersRef);
+        return <PluralRenderer users={usersData} />;
+      };
+
+      ContextProvider = ({children}: {children: React.Node}) => {
+        // $FlowFixMe[react-rule-hook]
+        const [env, _setEnv] = React.useState(environment);
+        // $FlowFixMe[react-rule-hook]
+        const relayContext = React.useMemo(() => ({environment: env}), [env]);
+
+        setEnvironment = _setEnv;
+        return (
+          <ReactRelayContext.Provider value={relayContext}>
+            {children}
+          </ReactRelayContext.Provider>
+        );
+      };
+
+      renderSingularFragment = (
+        props?: {
+          owner?: $FlowFixMe,
+          userRef?: $FlowFixMe,
+          ...
         },
-      ],
+        rerender: $FlowFixMe,
+      ) => {
+        const elements = (
+          <React.Suspense fallback="Singular Fallback">
+            <ContextProvider>
+              <SingularContainer owner={singularQuery} {...props} />
+            </ContextProvider>
+          </React.Suspense>
+        );
+        if (rerender) {
+          return rerender(elements);
+        } else {
+          return ReactTestingLibrary.render(elements);
+        }
+      };
+
+      renderPluralFragment = (
+        props?: {
+          owner?: $FlowFixMe,
+          userRef?: $FlowFixMe,
+          ...
+        },
+        rerender: $FlowFixMe,
+      ) => {
+        const elements = (
+          <React.Suspense fallback="Plural Fallback">
+            <ContextProvider>
+              <PluralContainer owner={pluralQuery} {...props} />
+            </ContextProvider>
+          </React.Suspense>
+        );
+        if (rerender) {
+          return rerender(elements);
+        } else {
+          return ReactTestingLibrary.render(elements);
+        }
+      };
     });
 
-    // Set up renderers
-    SingularRenderer = (props: {
-      user: ?(
-        | useFragmentTestUserFragment$data
-        | useFragmentTestUsersFragment$data
-      ),
-    }) => null;
-    PluralRenderer = (props: {
-      users: ?(
-        | useFragmentTestUserFragment$data
-        | useFragmentTestUsersFragment$data
-      ),
-    }) => null;
-    const SingularContainer = (props: {
-      userRef?: {$data?: {...}, ...},
-      owner: $FlowFixMe,
-      ...
-    }) => {
-      // We need a render a component to run a Hook
-      const owner = props.owner;
-      const userRef = props.hasOwnProperty('userRef')
-        ? props.userRef
-        : {
-            [ID_KEY]: owner.request.variables.id,
-            [FRAGMENTS_KEY]: {
-              useFragmentTestUserFragment: {},
-            },
-            [FRAGMENT_OWNER_KEY]: owner.request,
-          };
-      const userData = useFragment(gqlSingularFragment, userRef);
-      return <SingularRenderer user={userData} />;
-    };
-
-    const PluralContainer = (props: {
-      usersRef?: $ReadOnlyArray<{$data?: {...}, ...}>,
-      owner: $FlowFixMe,
-      ...
-    }) => {
-      const owner = props.owner;
-      const usersRef = props.hasOwnProperty('usersRef')
-        ? props.usersRef
-        : owner.request.variables.ids.map(id => ({
-            [ID_KEY]: id,
-            [FRAGMENTS_KEY]: {
-              useFragmentTestUsersFragment: {},
-            },
-            [FRAGMENT_OWNER_KEY]: owner.request,
-          }));
-
-      const usersData = useFragment(gqlPluralFragment, usersRef);
-      return <PluralRenderer users={usersData} />;
-    };
-
-    ContextProvider = ({children}: {children: React.Node}) => {
-      // $FlowFixMe[react-rule-hook]
-      const [env, _setEnv] = React.useState(environment);
-      // $FlowFixMe[react-rule-hook]
-      const relayContext = React.useMemo(() => ({environment: env}), [env]);
-
-      setEnvironment = _setEnv;
-      return (
-        <ReactRelayContext.Provider value={relayContext}>
-          {children}
-        </ReactRelayContext.Provider>
-      );
-    };
-
-    renderSingularFragment = (
-      props?: {
-        owner?: $FlowFixMe,
-        userRef?: $FlowFixMe,
-        ...
-      },
-      rerender: $FlowFixMe,
-    ) => {
-      const elements = (
-        <React.Suspense fallback="Singular Fallback">
-          <ContextProvider>
-            <SingularContainer owner={singularQuery} {...props} />
-          </ContextProvider>
-        </React.Suspense>
-      );
-      if (rerender) {
-        return rerender(elements);
-      } else {
-        return ReactTestingLibrary.render(elements);
-      }
-    };
-
-    renderPluralFragment = (
-      props?: {
-        owner?: $FlowFixMe,
-        userRef?: $FlowFixMe,
-        ...
-      },
-      rerender: $FlowFixMe,
-    ) => {
-      const elements = (
-        <React.Suspense fallback="Plural Fallback">
-          <ContextProvider>
-            <PluralContainer owner={pluralQuery} {...props} />
-          </ContextProvider>
-        </React.Suspense>
-      );
-      if (rerender) {
-        return rerender(elements);
-      } else {
-        return ReactTestingLibrary.render(elements);
-      }
-    };
-  });
-
-  afterEach(() => {
-    environment.mockClear();
-    renderSpy.mockClear();
-    RelayFeatureFlags.ENABLE_ACTIVITY_COMPATIBILITY = false;
-  });
-
-  it('handles environnment changes', () => {
-    renderSingularFragment();
-    assertFragmentResults({
-      id: '1',
-      name: 'Alice',
-      ...createFragmentRef('1', singularQuery),
+    afterEach(() => {
+      environment.mockClear();
+      renderSpy.mockClear();
+      RelayFeatureFlags.ENABLE_ACTIVITY_COMPATIBILITY = false;
+      RelayFeatureFlags.ENABLE_RESOURCE_EFFECTS = false;
     });
 
-    const newEnvironment = createMockEnvironment();
-    newEnvironment.commitPayload(singularQuery, {
-      node: {
-        __typename: 'User',
+    it('handles environnment changes', () => {
+      renderSingularFragment();
+      assertFragmentResults({
         id: '1',
-        name: 'Alice in a different env',
-        username: null,
-      },
-    });
+        name: 'Alice',
+        ...createFragmentRef('1', singularQuery),
+      });
 
-    ReactTestingLibrary.act(() => {
-      setEnvironment(newEnvironment);
-    });
-
-    assertFragmentResults({
-      id: '1',
-      name: 'Alice in a different env',
-      ...createFragmentRef('1', singularQuery),
-    });
-
-    ReactTestingLibrary.act(() => {
+      const newEnvironment = createMockEnvironment();
       newEnvironment.commitPayload(singularQuery, {
         node: {
           __typename: 'User',
           id: '1',
-          // Update name
-          name: 'Alice in Wonderland',
+          name: 'Alice in a different env',
           username: null,
         },
       });
-    });
-    assertFragmentResults({
-      id: '1',
-      // Assert that name is updated
-      name: 'Alice in Wonderland',
-      ...createFragmentRef('1', singularQuery),
-    });
-  });
 
-  it('should render singular fragment without error when data is available', () => {
-    renderSingularFragment();
-    assertFragmentResults({
-      id: '1',
-      name: 'Alice',
-      ...createFragmentRef('1', singularQuery),
+      ReactTestingLibrary.act(() => {
+        setEnvironment(newEnvironment);
+      });
+
+      assertFragmentResults({
+        id: '1',
+        name: 'Alice in a different env',
+        ...createFragmentRef('1', singularQuery),
+      });
+
+      ReactTestingLibrary.act(() => {
+        newEnvironment.commitPayload(singularQuery, {
+          node: {
+            __typename: 'User',
+            id: '1',
+            // Update name
+            name: 'Alice in Wonderland',
+            username: null,
+          },
+        });
+      });
+      assertFragmentResults({
+        id: '1',
+        // Assert that name is updated
+        name: 'Alice in Wonderland',
+        ...createFragmentRef('1', singularQuery),
+      });
     });
-  });
 
-  it('should return the same data object if rendered multiple times: singular fragment', () => {
-    const result = renderSingularFragment();
-    expect(renderSpy).toBeCalledTimes(1);
-    const actualData = renderSpy.mock.calls[0][0];
-    renderSingularFragment({}, result.rerender);
-    expect(renderSpy).toBeCalledTimes(2);
-    const actualData2 = renderSpy.mock.calls[1][0];
-    expect(actualData).toEqual(actualData2);
-  });
-
-  it('should render plural fragment without error when data is available', () => {
-    renderPluralFragment();
-    assertFragmentResults([
-      {
+    it('should render singular fragment without error when data is available', () => {
+      renderSingularFragment();
+      assertFragmentResults({
         id: '1',
         name: 'Alice',
-        ...createFragmentRef('1', pluralQuery),
-      },
-      {
-        id: '2',
-        name: 'Bob',
-        ...createFragmentRef('2', pluralQuery),
-      },
-    ]);
-  });
-
-  it('should return the same data object if rendered multiple times: plural fragment', () => {
-    const result = renderPluralFragment();
-    expect(renderSpy).toBeCalledTimes(1);
-    const actualData = renderSpy.mock.calls[0][0];
-    renderPluralFragment({}, result?.rerender);
-    expect(renderSpy).toBeCalledTimes(2);
-    const actualData2 = renderSpy.mock.calls[1][0];
-    expect(actualData).toEqual(actualData2);
-  });
-
-  it('Returns [] when the fragment ref is [] (for plural fragments)', () => {
-    const container = renderPluralFragment({usersRef: []});
-    assertFragmentResults([]);
-    ReactTestingLibrary.act(() => {
-      container?.unmount();
+        ...createFragmentRef('1', singularQuery),
+      });
     });
-  });
 
-  it('Returns null when the fragment ref is null (for plural fragments)', () => {
-    const container = renderPluralFragment({usersRef: null});
-    assertFragmentResults(null);
-    ReactTestingLibrary.act(() => {
-      container?.unmount();
+    it('should return the same data object if rendered multiple times: singular fragment', () => {
+      const result = renderSingularFragment();
+      expect(renderSpy).toBeCalledTimes(1);
+      const actualData = renderSpy.mock.calls[0][0];
+      renderSingularFragment({}, result.rerender);
+      expect(renderSpy).toBeCalledTimes(2);
+      const actualData2 = renderSpy.mock.calls[1][0];
+      expect(actualData).toEqual(actualData2);
     });
-  });
-});
+
+    it('should render plural fragment without error when data is available', () => {
+      renderPluralFragment();
+      assertFragmentResults([
+        {
+          id: '1',
+          name: 'Alice',
+          ...createFragmentRef('1', pluralQuery),
+        },
+        {
+          id: '2',
+          name: 'Bob',
+          ...createFragmentRef('2', pluralQuery),
+        },
+      ]);
+    });
+
+    it('should return the same data object if rendered multiple times: plural fragment', () => {
+      const result = renderPluralFragment();
+      expect(renderSpy).toBeCalledTimes(1);
+      const actualData = renderSpy.mock.calls[0][0];
+      renderPluralFragment({}, result?.rerender);
+      expect(renderSpy).toBeCalledTimes(2);
+      const actualData2 = renderSpy.mock.calls[1][0];
+      expect(actualData).toEqual(actualData2);
+    });
+
+    it('Returns [] when the fragment ref is [] (for plural fragments)', () => {
+      const container = renderPluralFragment({usersRef: []});
+      assertFragmentResults([]);
+      ReactTestingLibrary.act(() => {
+        container?.unmount();
+      });
+    });
+
+    it('Returns null when the fragment ref is null (for plural fragments)', () => {
+      const container = renderPluralFragment({usersRef: null});
+      assertFragmentResults(null);
+      ReactTestingLibrary.act(() => {
+        container?.unmount();
+      });
+    });
+  },
+);

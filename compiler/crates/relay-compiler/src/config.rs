be8f5f77e113bd641d3514b913a55182341ff984
Sverre Johansen <sverre.johansen@gmail.com>
2025-01-14 11:39:02 -0800

    Enhance `schemaExtensions` to Support Both Files and Directories (#4859)
    Summary:
    **Summary**
    
    This update enhances the behavior of the `"schemaExtensions"` configuration key, allowing it to specify either:
    - A folder containing GraphQL schema extension files (as originally documented).
    - A specific .graphql schema extension file (new functionality).
    
    This change unifies behavior between the Watchman query and the walkdir implementation, ensuring consistent handling of schema extensions regardless of configuration.
    
    **Details**
    
    Previously, the Relay documentation instructed users to configure "schemaExtensions" with a folder path containing the GraphQL schema extensions. However:
    	•	Relay did not enforce that the target was a folder.
    	•	Users could already specify a single .graphql schema extension file directly and it would work when not using Watchman.
    
    This update ensures both approaches work seamlessly with Watchman and enhancing flexibility. The documentation has also been updated to reflect this new functionality.
    
    **Why This Change?**
    
    This improvement resolves an inconsistency between Watchman and non-Watchman setups, offering better support for varying project configurations without breaking existing workflows.
    
    Pull Request resolved: https://github.com/facebook/relay/pull/4859
    
    Reviewed By: evanyeung
    
    Differential Revision: D68106213
    
    Pulled By: captbaritone
    
    fbshipit-source-id: ca2f2f401429face4b60c88b033099450e6a3e05

--- compiler/crates/relay-compiler/src/config.rs
+++ compiler/crates/relay-compiler/src/config.rs
@@ -994,12 +994,12 @@ impl SingleProjectConfigFile {
                 }
             })?,
         );
-        for extension_dir in self.schema_extensions.iter() {
+        for extension_path in self.schema_extensions.iter() {
             paths.push(
-                canonicalize(root_dir.join(extension_dir.clone())).map_err(|_| {
-                    ConfigValidationError::ExtensionDirNotExistent {
+                canonicalize(root_dir.join(extension_path.clone())).map_err(|_| {
+                    ConfigValidationError::ExtensionPathNotExistent {
                         project_name: self.project_name,
-                        extension_dir: extension_dir.clone(),
+                        extension_path: extension_path.clone(),
                     }
                 })?,
             );

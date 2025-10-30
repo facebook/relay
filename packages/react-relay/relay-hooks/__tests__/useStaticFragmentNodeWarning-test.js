ef3e6cf895bbcc345cc31726d2f43926d7dc8ec9
Jack Pope <jackpope@meta.com>
2025-10-30 11:49:54 -0700

    Migrate useStaticFragmentNodeWarning-test to testing-library/react
    Reviewed By: EugeneChoi4
    
    Differential Revision: D84861512
    
    fbshipit-source-id: 0cfbb46e93691c8ee30a22fdd73be67067530cad

--- packages/react-relay/relay-hooks/__tests__/useStaticFragmentNodeWarning-test.js
+++ packages/react-relay/relay-hooks/__tests__/useStaticFragmentNodeWarning-test.js
@@ -4,9 +4,10 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  *
- * @flow strict-local
+ * @flow
  * @format
  * @oncall relay
+ * @jest-environment jsdom
  */
 
 'use strict';
@@ -15,8 +16,9 @@ const mockWarning = jest.fn<$FlowFixMe & $ReadOnlyArray<mixed>, mixed>();
 jest.mock('warning', () => mockWarning);
 
 const useStaticFragmentNodeWarning = require('../useStaticFragmentNodeWarning');
+const ReactTestingLibrary = require('@testing-library/react');
 const React = require('react');
-const TestRenderer = require('react-test-renderer');
+const {act} = require('react');
 
 const warningMessage =
   'Relay: The %s has to remain the same over the lifetime of a component. Changing ' +
@@ -30,13 +32,13 @@ function Example(props: {+foo: {+name: string}, +bar: string}) {
   return null;
 }
 
-test('warn when a static prop changes', () => {
+test('warn when a static prop changes', async () => {
   const fragmentNode = {name: 'Fragment_foo'};
 
   // initial render doesn't warn
   let testRenderer;
-  TestRenderer.act(() => {
-    testRenderer = TestRenderer.create(
+  await act(() => {
+    testRenderer = ReactTestingLibrary.render(
       <Example foo={fragmentNode} bar="bar1" />,
     );
   });
@@ -44,29 +46,29 @@ test('warn when a static prop changes', () => {
   expect(mockWarning.mock.calls[0]).toEqual(notWarned);
 
   // not updating props doesnt warn
-  TestRenderer.act(() => {
-    testRenderer?.update(<Example foo={fragmentNode} bar="bar1" />);
+  await act(() => {
+    testRenderer?.rerender(<Example foo={fragmentNode} bar="bar1" />);
   });
   expect(mockWarning.mock.calls.length).toBe(2);
   expect(mockWarning.mock.calls[1]).toEqual(notWarned);
 
   // updating a non-checked prop doesn't warn
-  TestRenderer.act(() => {
-    testRenderer?.update(<Example foo={fragmentNode} bar="bar2" />);
+  await act(() => {
+    testRenderer?.rerender(<Example foo={fragmentNode} bar="bar2" />);
   });
   expect(mockWarning.mock.calls.length).toBe(3);
   expect(mockWarning.mock.calls[2]).toEqual(notWarned);
 
   // different fragment node with same name doesn't warn
-  TestRenderer.act(() => {
-    testRenderer?.update(<Example foo={{...fragmentNode}} bar="bar1" />);
+  await act(() => {
+    testRenderer?.rerender(<Example foo={{...fragmentNode}} bar="bar1" />);
   });
   expect(mockWarning.mock.calls.length).toBe(4);
   expect(mockWarning.mock.calls[3]).toEqual(notWarned);
 
   // updating a expected static prop warns
-  TestRenderer.act(() => {
-    testRenderer?.update(
+  await act(() => {
+    testRenderer?.rerender(
       <Example foo={{name: 'OtherFragment_foo'}} bar="bar1" />,
     );
   });

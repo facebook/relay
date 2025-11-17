bac932fb74ee66cd044d30e0256d58aaa6a496be
Jordan Eldredge <jeldredge@meta.com>
2025-11-17 12:25:28 -0800

    Add experimental command to OSS the Relay compiler to update schema to be the used subset of a larger schema
    Reviewed By: mjmahone, nijiaju
    
    Differential Revision: D86989563
    
    fbshipit-source-id: 1ccedbb63f772c3c9a39fec326a5424c13b00f14

--- compiler/crates/relay-bin/src/main.rs
+++ compiler/crates/relay-bin/src/main.rs
@@ -7,6 +7,8 @@
 
 use std::env;
 use std::env::current_dir;
+use std::fs;
+use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 use std::sync::Arc;
@@ -17,6 +19,7 @@ use common::ConsoleLogger;
 use intern::string_key::Intern;
 use log::error;
 use log::info;
+use program_with_dependencies::ProgramWithDependencies;
 use relay_codemod::AvailableCodemod;
 use relay_codemod::run_codemod;
 use relay_compiler::FileSourceKind;
@@ -25,6 +28,7 @@ use relay_compiler::OperationPersister;
 use relay_compiler::PersistConfig;
 use relay_compiler::ProjectName;
 use relay_compiler::RemotePersister;
+use relay_compiler::SchemaLocation;
 use relay_compiler::build_project::artifact_writer::ArtifactValidationWriter;
 use relay_compiler::build_project::generate_extra_artifacts::default_generate_extra_artifacts_fn;
 use relay_compiler::compiler::Compiler;
@@ -39,6 +43,8 @@ use relay_lsp::LSPExtraDataProvider;
 use relay_lsp::start_language_server;
 use schema::SDLSchema;
 use schema_documentation::SchemaDocumentationLoader;
+use schema_set::SchemaSet;
+use schema_set::UsedSchemaCollectionOptions;
 use simplelog::ColorChoice;
 use simplelog::ConfigBuilder as SimpleLogConfigBuilder;
 use simplelog::LevelFilter;
@@ -125,6 +131,27 @@ struct CompileCommand {
 
 #[derive(Parser)]
 #[clap(
+    rename_all = "camel_case",
+    about = "EXPERIMENTAL! Replaces the currently configured schema file with the portion of the provided --fullSchema that is actually used by this Relay project."
+)]
+struct UpdateSchemaCommand {
+    /// Compile using this config file. If not provided, searches for a config in
+    /// package.json under the `relay` key or `relay.config.json` files among other up
+    /// from the current working directory.
+    #[clap(long)]
+    config: Option<PathBuf>,
+
+    /// Path to the full schema file or directory to use as the source for extracting the used subset
+    #[clap(long)]
+    full_schema: PathBuf,
+
+    /// Verbosity level
+    #[clap(long, value_enum, default_value = "verbose")]
+    output: OutputKind,
+}
+
+#[derive(Parser)]
+#[clap(
     about = "Run the language server. Used by IDEs.",
     rename_all = "camel_case"
 )]
@@ -154,6 +181,7 @@ enum Commands {
     Lsp(LspCommand),
     ConfigJsonSchema(ConfigJsonSchemaCommand),
     Codemod(CodemodCommand),
+    ExperimentalRegenerateSubSchema(UpdateSchemaCommand),
 }
 
 #[derive(ValueEnum, Clone, Copy)]
@@ -215,6 +243,9 @@ async fn main() {
             Ok(())
         }
         Commands::Codemod(command) => handle_codemod_command(command).await,
+        Commands::ExperimentalRegenerateSubSchema(command) => {
+            handle_regenerate_subschema_command(command).await
+        }
     };
 
     if let Err(err) = result {
@@ -229,6 +260,11 @@ fn get_config(config_path: Option<PathBuf>) -> Result<Config, Error> {
         None => Config::search(&current_dir().expect("Unable to get current working directory."))
             .map_err(Error::ConfigError),
     }
+}
+
+fn normalize_relative_path(root_dir: &Path, path: &PathBuf) -> PathBuf {
+    let absolute = root_dir.join(path);
+    absolute.strip_prefix(root_dir).unwrap().to_path_buf()
 }
 
 fn configure_logger(output: OutputKind, terminal_mode: TerminalMode) {
@@ -295,6 +331,128 @@ async fn handle_codemod_command(command: CodemodCommand) -> Result<(), Error> {
         Err(e) => Err(Error::CodemodError {
             details: format!("{:?}", e),
         }),
+    }
+}
+
+async fn handle_regenerate_subschema_command(command: UpdateSchemaCommand) -> Result<(), Error> {
+    configure_logger(command.output, TerminalMode::Mixed);
+    let mut config = get_config(command.config)?;
+
+    if config.projects.len() != 1 {
+        return Err(Error::ConfigError(CompilerError::ConfigError {
+            details: format!(
+                "Expected exactly one project, but found {}",
+                config.projects.len()
+            ),
+        }));
+    }
+
+    let project_name = config.projects.keys().next().unwrap().clone();
+
+    let original_schema_location = match &config.projects[&project_name].schema_location {
+        SchemaLocation::File(file) => file.clone(),
+        SchemaLocation::Directory(_) => {
+            return Err(Error::ConfigError(CompilerError::ConfigError {
+                details: "Expected a single file schema, but found a directory schema location"
+                    .to_string(),
+            }));
+        }
+    };
+
+    let relative_full_schema = normalize_relative_path(&config.root_dir, &command.full_schema);
+
+    let schema_location = match fs::metadata(&command.full_schema) {
+        Ok(metadata) => {
+            if metadata.is_dir() {
+                Ok(SchemaLocation::Directory(relative_full_schema))
+            } else if metadata.is_file() {
+                Ok(SchemaLocation::File(relative_full_schema))
+            } else {
+                Err(Error::ConfigError(CompilerError::ConfigError {
+                    details: format!(
+                        "{} exists on disk but is neither a file nor directory.",
+                        command.full_schema.to_string_lossy(),
+                    ),
+                }))
+            }
+        }
+        Err(_) => Err(Error::ConfigError(CompilerError::ConfigError {
+            details: format!(
+                "{} not found on disk.",
+                command.full_schema.to_string_lossy(),
+            ),
+        })),
+    }?;
+
+    // Modify the schema location to point to the full schema
+    config
+        .projects
+        .get_mut(&project_name)
+        .unwrap()
+        .schema_location = schema_location;
+
+    // Produce IR based on the full schema. This confirms the project typechecks against this schema.
+    let programs_result = get_programs(config, Arc::new(ConsoleLogger))
+        .await
+        .map(|(programs, _, _)| programs.values().cloned().collect::<Vec<_>>());
+
+    let programs_vec = programs_result.map_err(|e| Error::CompilerError {
+        details: format!("{}", e),
+    })?;
+
+    // Expect exactly one program based on exactly one project asserted earlier.
+    let programs = programs_vec
+        .into_iter()
+        .next()
+        .expect("Expected exactly one program based on above assertion about the config.");
+
+    // Convert Programs to ProgramWithDependencies by using the source program
+    let program_with_deps = ProgramWithDependencies::from_full_program(
+        &programs.source.schema,
+        // Pass the operation text program since it has had all the Relay-specific features stripped out
+        // and should pass validaiton
+        &programs.operation_text,
+    );
+
+    let mut used_schema = SchemaSet::from_ir(
+        &program_with_deps,
+        UsedSchemaCollectionOptions {
+            include_implementations_when_typename_requested: None,
+            include_all_overlapping_concrete_types: false,
+            include_directives_on_schema_definitions: false,
+            include_directive_definitions: false,
+            include_implicit_output_enum_values: true,
+            include_implicit_input_fields_and_enum_values: true,
+        },
+    );
+
+    used_schema.fix_all_types();
+
+    let (printed_base_schema, printed_client_schema) =
+        used_schema.print_base_and_client_definitions();
+
+    if !printed_client_schema.is_empty() {
+        // Since subschema was computed from operation text IR and the provided
+        // full server schema, it should not contain any client schema.
+        panic!("Expected client schema to be empty")
+    }
+    let mut output_contents = printed_base_schema
+        .into_iter()
+        .collect::<Vec<_>>()
+        .join("\n\n");
+
+    output_contents.push('\n');
+
+    // Write the used schema back to the original schema location
+    match std::fs::write(&original_schema_location, output_contents) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(Error::ConfigError(CompilerError::ConfigError {
+            details: format!(
+                "Failed to write used schema file to {}: {}",
+                original_schema_location.to_string_lossy(),
+                e
+            ),
+        })),
     }
 }
 

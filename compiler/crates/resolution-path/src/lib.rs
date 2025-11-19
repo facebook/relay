e253e1a787b588b46aa5cc772104d5e71a866fb3
Jordan Eldredge <jordan@jordaneldredge.com>
2025-11-19 10:45:41 -0800

    Fix Rust build in CI

--- compiler/crates/resolution-path/src/lib.rs
+++ compiler/crates/resolution-path/src/lib.rs
@@ -69,8 +69,7 @@ mod selection_parent_type;
 mod variable_definition_path;
 
 #[cfg(test)]
-#[macro_use]
-extern crate assert_matches;
+use std::assert_matches;
 
 /// This module resolves a position (`Span`) to a `ResolvePosition` which
 /// enumerates the types of AST nodes which have a "surface area", meaning a

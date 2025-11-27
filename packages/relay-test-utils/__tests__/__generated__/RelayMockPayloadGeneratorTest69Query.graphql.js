12a1dcf2c145c8828fa8d01d103ea801ef2d705a
Jordan Eldredge <jordan@jordaneldredge.com>
2025-11-26 18:14:48 -0800

    Update MockPayloadGenerator tests to acknowledge invalid payload warnings

---
+++ packages/relay-test-utils/__tests__/__generated__/RelayMockPayloadGeneratorTest69Query.graphql.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @oncall relay
+ *
+ * @generated SignedSource<<4422994e49c1cede9d1256fca239655a>>
+ * @flow
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ConcreteRequest, Query } from 'relay-runtime';
+export type RelayMockPayloadGeneratorTest69Query$variables = {||};
+export type RelayMockPayloadGeneratorTest69Query$data = {|
+  +a: ?{|
+    +id: string,
+    +name: ?string,
+  |},
+  +b: ?{|
+    +emailAddresses: ?ReadonlyArray<?string>,
+    +id: string,
+  |},
+|};
+export type RelayMockPayloadGeneratorTest69Query = {|
+  response: RelayMockPayloadGeneratorTest69Query$data,
+  variables: RelayMockPayloadGeneratorTest69Query$variables,
+|};
+*/
+
+var node/*: ConcreteRequest*/ = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v1 = [
+  {
+    "alias": "a",
+    "args": null,
+    "concreteType": "User",
+    "kind": "LinkedField",
+    "name": "me",
+    "plural": false,
+    "selections": [
+      (v0/*: any*/),
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "name",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  },
+  {
+    "alias": "b",
+    "args": null,
+    "concreteType": "User",
+    "kind": "LinkedField",
+    "name": "me",
+    "plural": false,
+    "selections": [
+      (v0/*: any*/),
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "emailAddresses",
+        "storageKey": null
+      }
+    ],
+    "storageKey": null
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": {
+      "throwOnFieldError": true
+    },
+    "name": "RelayMockPayloadGeneratorTest69Query",
+    "selections": (v1/*: any*/),
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "RelayMockPayloadGeneratorTest69Query",
+    "selections": (v1/*: any*/)
+  },
+  "params": {
+    "cacheID": "aa793dad7066cf4e3f72d5686a723d95",
+    "id": null,
+    "metadata": {},
+    "name": "RelayMockPayloadGeneratorTest69Query",
+    "operationKind": "query",
+    "text": "query RelayMockPayloadGeneratorTest69Query {\n  a: me {\n    id\n    name\n  }\n  b: me {\n    id\n    emailAddresses\n  }\n}\n"
+  }
+};
+})();
+
+if (__DEV__) {
+  (node/*: any*/).hash = "23a95993ade379d66f839404d6bfcff9";
+}
+
+module.exports = ((node/*: any*/)/*: Query<
+  RelayMockPayloadGeneratorTest69Query$variables,
+  RelayMockPayloadGeneratorTest69Query$data,
+>*/);

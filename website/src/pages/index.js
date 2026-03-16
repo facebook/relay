39170b0fd2593339d159671a8a73fbb73b77fc02
Dmitry Vinnik <dvinnik@meta.com>
2026-03-16 06:36:16 -0700

    Remove Support Ukraine banner from Relay website
    Reviewed By: mattcarrollcode
    
    Differential Revision: D96559634
    
    fbshipit-source-id: 22bc833af7193fcd2036e473e979fde37d0ce0d2

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -45,25 +45,10 @@ function UseFragmentLink() {
   );
 }
 
-function HomeBanner() {
-  return (
-    <div className="homeBanner">
-      Support Ukraine 🇺🇦{' '}
-      <Link to="https://opensource.facebook.com/support-ukraine">
-        Help Provide Humanitarian Aid to Ukraine
-      </Link>
-      .
-    </div>
-  );
-}
-
 const HomeSplash = () => {
   const {siteConfig} = useDocusaurusContext();
   return (
     <div>
-      <div>
-        <HomeBanner />
-      </div>
       <div className="homeContainer">
         <div className="homeSplashFade">
           <div className="logo">

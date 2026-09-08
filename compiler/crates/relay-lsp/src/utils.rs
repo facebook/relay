facebook/relay

commit 6098dee48d5998323107f6e22d67a8aa3fb94fd0

Update from lsp-types 0.94 to 0.97
Reviewed By: diliop

Differential Revision: D105444102

fbshipit-source-id: 3e9cef3a030dfa58bca86592cb0d1bbb3874b0e3

compiler/crates/relay-lsp/src/utils.rs
@@ -5,7 +5,7 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-use std::path::PathBuf;
+use std::path::Path;
 
 use common::Location;
 use common::SourceLocationKey;
@@ -22,7 +22,7 @@ use intern::string_key::StringKey;
 use log::debug;
 use lsp_types::Position;
 use lsp_types::TextDocumentPositionParams;
-use lsp_types::Url;
+use lsp_types::Uri;
 use relay_compiler::FileCategorizer;
 use relay_compiler::FileGroup;
 use relay_compiler::ProjectConfig;
@@ -35,18 +35,16 @@ use crate::Feature;
 use crate::lsp_runtime_error::LSPRuntimeError;
 use crate::lsp_runtime_error::LSPRuntimeResult;
 
-pub fn is_file_uri_in_dir(root_dir: PathBuf, file_uri: &Url) -> bool {
-    let file_path_result = file_uri.to_file_path();
-
-    match file_path_result {
-        Ok(file_path) => file_path.starts_with(root_dir),
-        Err(()) => false,
-    }
+pub fn is_file_uri_in_dir(root_dir: &Path, file_uri: &Uri) -> bool {
+    file_uri
+        .scheme()
+        .is_some_and(|scheme| scheme.eq_lowercase("file"))
+        && Path::new(file_uri.path().as_str()).starts_with(root_dir)
 }
 
 pub fn extract_executable_definitions_from_text_document(
-    text_document_uri: &Url,
-    source_feature_cache: &DashMap<Url, Vec<JavaScriptSourceFeature>>,
+    text_document_uri: &Uri,
+    source_feature_cache: &DashMap<Uri, Vec<JavaScriptSourceFeature>>,
     parser_features: ParserFeatures,
 ) -> LSPRuntimeResult<Vec<ExecutableDefinition>> {
     let source_features = source_feature_cache
@@ -67,7 +65,7 @@ pub fn extract_executable_definitions_from_text_document(
         .flat_map(|(i, graphql_source)| {
             let document = parse_executable_with_error_recovery_and_parser_features(
                 &graphql_source.text_source().text,
-                SourceLocationKey::embedded(path, i),
+                SourceLocationKey::embedded(path.as_str(), i),
                 parser_features,
             )
             .item;
@@ -81,13 +79,16 @@ pub fn extract_executable_definitions_from_text_document(
 
 pub fn get_file_group_from_uri(
     file_categorizer: &FileCategorizer,
-    url: &Url,
-    root_dir: &PathBuf,
+    uri: &Uri,
+    root_dir: &Path,
     config: &Config,
 ) -> LSPRuntimeResult<FileGroup> {
-    let absolute_file_path = url.to_file_path().map_err(|_| {
-        LSPRuntimeError::UnexpectedError(format!("Unable to convert URL to file path: {url:?}"))
-    })?;
+    let absolute_file_path = Path::new(uri.path().as_str());
+    if !absolute_file_path.is_absolute() {
+        return Err(LSPRuntimeError::UnexpectedError(format!(
+            "Unable to convert URI to file path: {uri:?}",
+        )));
+    }
 
     let file_path = absolute_file_path.strip_prefix(root_dir).map_err(|_e| {
         LSPRuntimeError::UnexpectedError(format!(
@@ -122,8 +123,8 @@ pub fn get_project_name_from_file_group(file_group: &FileGroup) -> Result<String
 /// request, only if the request occurs within a GraphQL document or Docblock.
 pub fn extract_feature_from_text(
     project_config: &ProjectConfig,
-    js_source_feature_cache: &DashMap<Url, Vec<JavaScriptSourceFeature>>,
-    schema_source_cache: &DashMap<Url, GraphQLSource>,
+    js_source_feature_cache: &DashMap<Uri, Vec<JavaScriptSourceFeature>>,
+    schema_source_cache: &DashMap<Uri, GraphQLSource>,
     text_document_position: &TextDocumentPositionParams,
     index_offset: usize,
 ) -> LSPRuntimeResult<(Feature, Location)> {
@@ -131,7 +132,7 @@ pub fn extract_feature_from_text(
     let position = text_document_position.position;
 
     if let Some(schema_source) = schema_source_cache.get(uri) {
-        let source_location_key = SourceLocationKey::standalone(uri.as_ref());
+        let source_location_key = SourceLocationKey::standalone(uri.as_str());
         let schema_document = graphql_syntax::parse_schema_document(
             &schema_source.text_source().text,
             source_location_key,
@@ -162,7 +163,7 @@ pub fn extract_feature_from_text(
         })
         .ok_or(LSPRuntimeError::ExpectedError)?;
 
-    let source_location_key = SourceLocationKey::embedded(uri.path(), index);
+    let source_location_key = SourceLocationKey::embedded(uri.path().as_str(), index);
 
     let parser_features = get_parser_features(project_config);
 

compiler/crates/common/Cargo.toml
@@ -17,7 +17,7 @@ colored = "2.1.0"
 indexmap = { version = "2.14.0", features = ["arbitrary", "rayon", "serde"] }
 intern = { path = "../intern" }
 log = { version = "0.4.29", features = ["kv_unstable", "kv_unstable_std"] }
-lsp-types = "0.94.1"
+lsp-types = "0.97"
 md-5 = "0.10"
 rayon = "1.11.0"
 schemars = { version = "1.2.1", features = ["indexmap2"] }

compiler/crates/relay-codemod/Cargo.toml
@@ -12,7 +12,7 @@ license = "MIT"
 clap = { version = "4.6.0", features = ["derive", "env", "string", "unicode", "wrap_help"] }
 common = { path = "../common" }
 log = { version = "0.4.29", features = ["kv_unstable", "kv_unstable_std"] }
-lsp-types = "0.94.1"
+lsp-types = "0.97"
 relay-compiler = { path = "../relay-compiler" }
 relay-lsp = { path = "../relay-lsp" }
 relay-transforms = { path = "../relay-transforms" }

compiler/crates/relay-codemod/src/codemod.rs
@@ -21,7 +21,7 @@ use common::RolloutRange;
 use log::info;
 use lsp_types::CodeActionOrCommand;
 use lsp_types::TextEdit;
-use lsp_types::Url;
+use lsp_types::Uri;
 use relay_compiler::errors::BuildProjectError;
 use relay_compiler::errors::Error as CompilerError;
 use relay_compiler::errors::Result as CompilerResult;
@@ -162,7 +162,7 @@ fn apply_actions(actions: Vec<CodeActionOrCommand>) -> Result<(), std::io::Error
         sort_changes(&file, &mut changes)?;
 
         // Read file into memory and apply changes
-        let file_contents: String = fs::read_to_string(file.path())?;
+        let file_contents: String = fs::read_to_string(file.path().as_str())?;
         let mut lines: Vec<String> = file_contents.lines().map(|s| s.to_string()).collect();
         for change in &changes {
             let line = change.range.start.line as usize;
@@ -175,17 +175,17 @@ fn apply_actions(actions: Vec<CodeActionOrCommand>) -> Result<(), std::io::Error
 
         // Write file back out
         let new_file_contents = lines.join("\n");
-        fs::write(file.path(), new_file_contents)?;
+        fs::write(file.path().as_str(), new_file_contents)?;
 
         info!("Applied {} changes to {}", changes.len(), file.path());
     }
     Ok(())
 }
 
-fn sort_changes(url: &Url, changes: &mut Vec<TextEdit>) -> Result<(), std::io::Error> {
+fn sort_changes(uri: &Uri, changes: &mut Vec<TextEdit>) -> Result<(), std::io::Error> {
     // Now we have all the changes for this file. Sort them by position within the file, end of file first
     // This way the changes are applied in reverse order, so we don't have to worry about altering the positions of the remaining changes
-    changes.sort_by(|a, b| b.range.start.cmp(&a.range.start));
+    changes.sort_by_key(|change| change.range.start);
 
     // Verify none of the changes overlap
     let mut prev_change: Option<&TextEdit> = None;
@@ -197,7 +197,7 @@ fn sort_changes(url: &Url, changes: &mut Vec<TextEdit>) -> Result<(), std::io::E
         {
             return Err(std::io::Error::other(format!(
                 "Codemod produced changes that overlap: File {}, changes: {:?} vs {:?}",
-                url.path(),
+                uri.path(),
                 change,
                 prev_change
             )));

compiler/crates/relay-codemod/src/lib.rs
@@ -8,6 +8,7 @@
 #![deny(warnings)]
 #![deny(rust_2018_idioms)]
 #![deny(clippy::all)]
+#![allow(clippy::mutable_key_type)] // lsp_types::Uri
 
 mod codemod;
 

compiler/crates/relay-lsp/Cargo.toml
@@ -34,7 +34,7 @@ itertools = "0.14.0"
 lazy_static = "1.5"
 log = { version = "0.4.29", features = ["kv_unstable", "kv_unstable_std"] }
 lsp-server = "0.7.9"
-lsp-types = "0.94.1"
+lsp-types = "0.97"
 percent-encoding = "2.1"
 rand = "0.10.1"
 rayon = "1.11.0"

compiler/crates/relay-lsp/src/code_action.rs
@@ -25,7 +25,7 @@ use lsp_types::Position;
 use lsp_types::Range;
 use lsp_types::TextDocumentPositionParams;
 use lsp_types::TextEdit;
-use lsp_types::Url;
+use lsp_types::Uri;
 use lsp_types::WorkspaceEdit;
 use lsp_types::request::CodeActionRequest;
 use lsp_types::request::Request;
@@ -50,7 +50,7 @@ pub fn on_code_action(
 ) -> LSPRuntimeResult<<CodeActionRequest as Request>::Result> {
     let uri = params.text_document.uri.clone();
 
-    if !is_file_uri_in_dir(state.root_dir(), &uri) {
+    if !is_file_uri_in_dir(&state.root_dir(), &uri) {
         return Err(LSPRuntimeError::ExpectedError);
     }
 
@@ -79,7 +79,7 @@ pub fn on_code_action(
 }
 
 pub fn get_code_actions_from_diagnostic(
-    url: &Url,
+    uri: &Uri,
     diagnostic: Diagnostic,
 ) -> Option<Vec<CodeActionOrCommand>> {
     let code_actions = if let Some(Value::Array(data)) = &diagnostic.data {
@@ -88,7 +88,7 @@ pub fn get_code_actions_from_diagnostic(
                 Value::String(suggestion) => Some(create_code_action(
                     "Fix Error",
                     suggestion.to_string(),
-                    url,
+                    uri,
                     diagnostic.range,
                 )),
                 _ => None,
@@ -135,7 +135,7 @@ fn get_definition_names(definitions: &[ExecutableDefinition]) -> FragmentAndOper
 fn get_code_actions(
     path: ResolutionPath<'_>,
     used_definition_names: FragmentAndOperationNames,
-    url: Url,
+    uri: Uri,
     range: Range,
 ) -> Option<Vec<CodeActionOrCommand>> {
     match path {
@@ -164,7 +164,7 @@ fn get_code_actions(
                 operation_name.value.lookup(),
                 used_definition_names.operation_names,
                 suffix,
-                &url,
+                &uri,
                 code_action_range,
             ))
         }
@@ -180,7 +180,7 @@ fn get_code_actions(
             Some(create_rename_fragment_code_actions(
                 fragment_definition.name.value.lookup(),
                 used_definition_names.fragment_names,
-                &url,
+                &uri,
                 code_action_range,
             ))
         }
@@ -191,13 +191,13 @@ fn get_code_actions(
 fn create_rename_fragment_code_actions(
     _original_name: &str,
     used_names: HashSet<String>,
-    url: &Url,
+    uri: &Uri,
     range: Range,
 ) -> Vec<CodeActionOrCommand> {
     let mut suggested_names = Vec::with_capacity(2);
-    suggested_names.push(create_default_fragment_name(url.path()));
+    suggested_names.push(create_default_fragment_name(uri.path().as_str()));
     suggested_names.push(create_default_fragment_name_with_index(
-        url.path(),
+        uri.path().as_str(),
         &used_names,
     ));
 
@@ -212,7 +212,7 @@ fn create_rename_fragment_code_actions(
                 Some(create_code_action(
                     "Rename Fragment",
                     name.clone(),
-                    url,
+                    uri,
                     range,
                 ))
             } else {
@@ -226,18 +226,22 @@ fn create_rename_operation_code_actions(
     original_name: &str,
     used_names: HashSet<String>,
     suffix: DefinitionNameSuffix,
-    url: &Url,
+    uri: &Uri,
     range: Range,
 ) -> Vec<CodeActionOrCommand> {
     let mut suggested_names = Vec::with_capacity(4);
-    suggested_names.push(create_default_name(url.path(), suffix));
+    suggested_names.push(create_default_name(uri.path().as_str(), suffix));
     suggested_names.push(create_default_name_with_index(
-        url.path(),
+        uri.path().as_str(),
         suffix,
         &used_names,
     ));
-    suggested_names.push(create_name_wrapper(original_name, url.path(), suffix));
-    suggested_names.push(create_impactful_name(url.path(), suffix));
+    suggested_names.push(create_name_wrapper(
+        original_name,
+        uri.path().as_str(),
+        suffix,
+    ));
+    suggested_names.push(create_impactful_name(uri.path().as_str(), suffix));
     suggested_names
         .iter()
         .filter_map(|suggested_name| {
@@ -249,7 +253,7 @@ fn create_rename_operation_code_actions(
                 Some(create_code_action(
                     "Rename Operation",
                     name.clone(),
-                    url,
+                    uri,
                     range,
                 ))
             } else {
@@ -275,7 +279,7 @@ fn get_code_action_range(range: Range, span: Span) -> Range {
 fn create_code_action(
     title: &str,
     new_name: String,
-    url: &Url,
+    uri: &Uri,
     range: Range,
 ) -> CodeActionOrCommand {
     let mut changes = HashMap::new();
@@ -284,7 +288,7 @@ fn create_code_action(
         range,
         new_text: new_name,
     };
-    changes.insert(url.clone(), vec![text_edit]);
+    changes.insert(uri.clone(), vec![text_edit]);
 
     CodeActionOrCommand::CodeAction(CodeAction {
         title,
@@ -303,11 +307,13 @@ fn create_code_action(
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr as _;
+
     use lsp_types::CodeActionOrCommand;
     use lsp_types::Diagnostic;
     use lsp_types::Position;
     use lsp_types::Range;
-    use lsp_types::Url;
+    use lsp_types::Uri;
     use serde_json::json;
 
     use crate::code_action::get_code_actions_from_diagnostic;
@@ -329,8 +335,8 @@ mod tests {
             data: Some(json!(vec!["item1", "item2"])),
             ..Default::default()
         };
-        let url = Url::parse("file://relay.js").unwrap();
-        let code_actions = get_code_actions_from_diagnostic(&url, diagnostic);
+        let uri = Uri::from_str("file://relay.js").unwrap();
+        let code_actions = get_code_actions_from_diagnostic(&uri, diagnostic);
 
         assert_eq!(
             code_actions

compiler/crates/relay-lsp/src/completion.rs
@@ -1314,7 +1314,7 @@ pub fn on_completion(
     match state.extract_executable_document_from_text(&params.text_document_position, 0) {
         Ok((document, position_span)) => {
             let project_name = state
-                .extract_project_name_from_url(&params.text_document_position.text_document.uri)?;
+                .extract_project_name_from_uri(&params.text_document_position.text_document.uri)?;
             let schema = &state.get_schema(&project_name)?;
             let items = resolve_completion_items(
                 document,

commit d1228b1dca144182c058a976a409a5c30bf187e0

Fix relay compiler path issues for Windows (#5365) (#5365)
Summary:
Fixes https://github.com/facebook/relay/issues/5347

## The bug

On Windows, the Relay LSP breaks for every graphql tagged literal starting with `relay-compiler` 21.0.0 (20.0.0 works). The schema can't be resolved and IDE features (hover, autocomplete, diagnostics) silently stop working. The LSP log shows:

```
Unable to convert URI to file path: Uri { scheme: Some("file"), … path: "/c%3A/Development/repo-relay-compiler-bug/src/something.graphql" }
```

The give-away is `%3A` (a URL-encoded `:`) and the leading slash before the drive letter: the compiler is treating the raw URI path as a filesystem path instead of decoding it into `c:\Development\…`.

macOS/Linux are unaffected because their URI paths (`/Users/…`) need no decoding and have no drive letter.

## Root cause

This is a regression from `6098dee` ("Update from lsp-types 0.94 to 0.97"), which migrated relay-lsp from `lsp_types::Url` (backed by the `url` crate) to `lsp_types::Uri` (backed by `fluent-uri`).

`Url::to_file_path()` used to do two Windows-critical things that the new API does not:

1. **Percent-decode** the path (`%3A` → `:`).
2. **Normalize the Windows drive letter** by stripping the leading `/` (`/C:/…` → `C:\…`).

The migration replaced those calls with a naïve `Path::new(uri.path().as_str())`, and `Uri::path()` returns the raw, still-percent-encoded path. So on Windows the path stays `/c%3A/…`, which is:

- not absolute → `get_file_group_from_uri` returns the *"Unable to convert URI to file path"* error above (schema resolution fails), and
- not `starts_with(root_dir)` → `is_file_uri_in_dir` returns `false`, so the LSP treats the file as outside the project and ignores its `didOpen`/`didChange` entirely (hover/autocomplete break).

compiler/crates/relay-lsp/src/utils.rs
@@ -6,6 +6,7 @@
  */
 
 use std::path::Path;
+use std::path::PathBuf;
 
 use common::Location;
 use common::SourceLocationKey;
@@ -23,6 +24,7 @@ use log::debug;
 use lsp_types::Position;
 use lsp_types::TextDocumentPositionParams;
 use lsp_types::Uri;
+use percent_encoding::percent_decode_str;
 use relay_compiler::FileCategorizer;
 use relay_compiler::FileGroup;
 use relay_compiler::ProjectConfig;
@@ -36,10 +38,7 @@ use crate::lsp_runtime_error::LSPRuntimeError;
 use crate::lsp_runtime_error::LSPRuntimeResult;
 
 pub fn is_file_uri_in_dir(root_dir: &Path, file_uri: &Uri) -> bool {
-    file_uri
-        .scheme()
-        .is_some_and(|scheme| scheme.eq_lowercase("file"))
-        && Path::new(file_uri.path().as_str()).starts_with(root_dir)
+    uri_to_file_path(file_uri).is_some_and(|file_path| file_path.starts_with(root_dir))
 }
 
 pub fn extract_executable_definitions_from_text_document(
@@ -53,7 +52,12 @@ pub fn extract_executable_definitions_from_text_document(
         // the source has no graphql documents.
         .ok_or(LSPRuntimeError::ExpectedError)?;
 
-    let path = text_document_uri.path();
+    let file_path = uri_to_file_path(text_document_uri).ok_or_else(|| {
+        LSPRuntimeError::UnexpectedError(format!(
+            "Unable to convert URI to file path: {text_document_uri:?}",
+        ))
+    })?;
+    let path = file_path.to_string_lossy();
 
     let definitions = source_features
         .iter()
@@ -65,7 +69,7 @@ pub fn extract_executable_definitions_from_text_document(
         .flat_map(|(i, graphql_source)| {
             let document = parse_executable_with_error_recovery_and_parser_features(
                 &graphql_source.text_source().text,
-                SourceLocationKey::embedded(path.as_str(), i),
+                SourceLocationKey::embedded(&path, i),
                 parser_features,
             )
             .item;
@@ -83,12 +87,11 @@ pub fn get_file_group_from_uri(
     root_dir: &Path,
     config: &Config,
 ) -> LSPRuntimeResult<FileGroup> {
-    let absolute_file_path = Path::new(uri.path().as_str());
-    if !absolute_file_path.is_absolute() {
-        return Err(LSPRuntimeError::UnexpectedError(format!(
-            "Unable to convert URI to file path: {uri:?}",
-        )));
-    }
+    let absolute_file_path = uri_to_file_path(uri)
+        .filter(|path| path.is_absolute())
+        .ok_or_else(|| {
+            LSPRuntimeError::UnexpectedError(format!("Unable to convert URI to file path: {uri:?}"))
+        })?;
 
     let file_path = absolute_file_path.strip_prefix(root_dir).map_err(|_e| {
         LSPRuntimeError::UnexpectedError(format!(
@@ -163,7 +166,10 @@ pub fn extract_feature_from_text(
         })
         .ok_or(LSPRuntimeError::ExpectedError)?;
 
-    let source_location_key = SourceLocationKey::embedded(uri.path().as_str(), index);
+    let file_path = uri_to_file_path(uri).ok_or_else(|| {
+        LSPRuntimeError::UnexpectedError(format!("Unable to convert URI to file path: {uri:?}"))
+    })?;
+    let source_location_key = SourceLocationKey::embedded(&file_path.to_string_lossy(), index);
 
     let parser_features = get_parser_features(project_config);
 
@@ -296,6 +302,57 @@ pub fn position_to_offset(
     None
 }
 
+/// Converts a `file://` URI into a filesystem path, handling both Unix and
+/// Windows paths.
+///
+/// This restores the behavior that was lost when relay-lsp migrated from
+/// `lsp_types::Url` (backed by the `url` crate) to `lsp_types::Uri` (backed by
+/// `fluent-uri`). `Url::to_file_path()` used to percent-decode the path and
+/// normalize Windows drive letters, whereas `Uri::path()` exposes only the
+/// raw, still-percent-encoded path. Without this, Windows editors that send
+/// URIs such as `file:///c%3A/dir/file.graphql` produce the path
+/// `/c%3A/dir/file.graphql`, which is neither absolute nor a real file (see
+/// https://github.com/facebook/relay/issues/5347).
+///
+/// Returns `None` for non-`file` URIs.
+pub fn uri_to_file_path(uri: &Uri) -> Option<PathBuf> {
+    if !uri
+        .scheme()
+        .is_some_and(|scheme| scheme.eq_lowercase("file"))
+    {
+        return None;
+    }
+
+    // Percent-decode the path, e.g. `%3A` -> `:` for Windows drive letters and
+    // `%20` -> ` ` for paths containing spaces.
+    let decoded = percent_decode_str(uri.path().as_str()).decode_utf8_lossy();
+    Some(decoded_uri_path_to_file_path(&decoded))
+}
+
+/// Maps the (already percent-decoded) path component of a `file://` URI to a
+/// filesystem path.
+fn decoded_uri_path_to_file_path(decoded_path: &str) -> PathBuf {
+    // A `file://` URI always begins its path with `/`. On Windows the path is
+    // of the form `/C:/dir/file`, and the leading slash must be removed so it
+    // maps to the absolute path `C:/dir/file`. Unix paths (`/home/user`) are
+    // used verbatim.
+    let path = decoded_path
+        .strip_prefix('/')
+        .filter(|rest| is_windows_drive_prefix(rest))
+        .unwrap_or(decoded_path);
+    PathBuf::from(path)
+}
+
+/// Returns `true` if `path` begins with a Windows drive-letter component such
+/// as `C:`, `C:/`, or `C:\`.
+fn is_windows_drive_prefix(path: &str) -> bool {
+    let bytes = path.as_bytes();
+    bytes.len() >= 2
+        && bytes[0].is_ascii_alphabetic()
+        && bytes[1] == b':'
+        && (bytes.len() == 2 || bytes[2] == b'/' || bytes[2] == b'\\')
+}
+
 /// Converts a filesystem path to a `file://` URI, handling both Unix and Windows paths.
 pub fn path_to_file_uri(path: &Path) -> Option<Uri> {
     let path_str = path.to_str()?;
@@ -317,4 +374,87 @@ fn str_path_to_file_uri(path: &str) -> Option<Uri> {
         format!("file:///{normalized}")
     };
     uri_str.parse::<Uri>().ok()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    use lsp_types::Uri;
+
+    use super::is_windows_drive_prefix;
+    use super::path_to_file_uri;
+    use super::uri_to_file_path;
+
+    fn uri(s: &str) -> Uri {
+        s.parse::<Uri>().unwrap()
+    }
+
+    #[test]
+    fn uri_to_file_path_unix() {
+        assert_eq!(
+            uri_to_file_path(&uri("file:///Users/alice/project/src/foo.graphql")),
+            Some(PathBuf::from("/Users/alice/project/src/foo.graphql"))
+        );
+    }
+
+    #[test]
+    fn uri_to_file_path_windows_percent_encoded_drive_letter() {
+        // Editors such as VSCode encode the drive-letter colon as `%3A`. This
+        // is the exact shape reported in facebook/relay#5347.
+        assert_eq!(
+            uri_to_file_path(&uri(
+                "file:///c%3A/Development/repo-relay-compiler-bug/src/something.graphql"
+            )),
+            Some(PathBuf::from(
+                "c:/Development/repo-relay-compiler-bug/src/something.graphql"
+            ))
+        );
+    }
+
+    #[test]
+    fn uri_to_file_path_windows_unencoded_drive_letter() {
+        assert_eq!(
+            uri_to_file_path(&uri("file:///C:/Development/foo.graphql")),
+            Some(PathBuf::from("C:/Development/foo.graphql"))
+        );
+    }
+
+    #[test]
+    fn uri_to_file_path_decodes_spaces() {
+        assert_eq!(
+            uri_to_file_path(&uri("file:///Users/alice/my%20project/foo.graphql")),
+            Some(PathBuf::from("/Users/alice/my project/foo.graphql"))
+        );
+    }
+
+    #[test]
+    fn uri_to_file_path_rejects_non_file_scheme() {
+        assert_eq!(
+            uri_to_file_path(&uri("https://example.com/foo.graphql")),
+            None
+        );
+    }
+
+    #[test]
+    fn uri_to_file_path_roundtrips_windows_path() {
+        // `path_to_file_uri` (the reverse direction) followed by
+        // `uri_to_file_path` should recover a usable absolute Windows path.
+        let file_uri = path_to_file_uri(&PathBuf::from("C:\\Development\\foo.graphql")).unwrap();
+        assert_eq!(
+            uri_to_file_path(&file_uri),
+            Some(PathBuf::from("C:/Development/foo.graphql"))
+        );
+    }
+
+    #[test]
+    fn windows_drive_prefix_detection() {
+        assert!(is_windows_drive_prefix("C:"));
+        assert!(is_windows_drive_prefix("c:/dir"));
+        assert!(is_windows_drive_prefix("Z:\\dir"));
+        assert!(!is_windows_drive_prefix("Users/alice"));
+        assert!(!is_windows_drive_prefix("/c:/dir"));
+        assert!(!is_windows_drive_prefix("cc:/dir"));
+        assert!(!is_windows_drive_prefix("1:/dir"));
+    }
 }